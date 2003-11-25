d9190a331d5cf01fd3b60d356257e8124608ccc4
doemming <doemming@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2003-11-25 13:28:15 +0000

    update classpath

--- _import/trunk/others/KalypsoJboss/src/kalypso-client/bin/KalypsoRRM.sh
+++ _import/trunk/others/KalypsoJboss/src/kalypso-client/bin/KalypsoRRM.sh
@@ -1,10 +1,12 @@
 #/bin/bash
 CLASSPATH=.
 CLASSPATH=$CLASSPATH:edbc.jar:openroad.jar
+CLASSPATH=$CLASSPATH:braju.jar
 CLASSPATH=$CLASSPATH:javaGis1-ejb.jar:javaGis2-ejb.jar:javaGis3-ejb.jar
 CLASSPATH=$CLASSPATH:jbcl.jar
-CLASSPATH=$CLASSPATH:jbossall-client.jar:log4j.jar
+CLASSPATH=$CLASSPATH:jbossall-client.jar
+CLASSPATH=$CLASSPATH:log4j.jar
 echo $CLASSPATH
-
+export CLASSPATH
 java  -Xms100m -Xmx200m -Xss100m de/tuhh/wb/javagis/Main
 #java de/tuhh/wb/javagis/simpleclient/Main
