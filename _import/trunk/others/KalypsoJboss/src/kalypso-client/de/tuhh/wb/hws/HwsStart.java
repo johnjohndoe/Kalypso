35798bc0ff87e79cdf59ef8c23f399df86d3dd89
doemming <doemming@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2003-12-17 10:57:57 +0000

    *** empty log message ***

---
+++ _import/trunk/others/KalypsoJboss/src/kalypso-client/de/tuhh/wb/hws/HwsStart.java
@@ -0,0 +1,18 @@
+package de.tuhh.wb.hws;
+
+import java.io.File;
+
+public class HwsStart
+{
+    public static void hwsCalculation(File hwsSystem,File hwsSzenario,File hwsLogistics,File hwsResults)
+    {
+
+	System.out.println("hwsCalculation ...");
+	System.out.println("Files: "+hwsSystem.toString());
+	System.out.println("Files: "+hwsSzenario.toString());
+	System.out.println("Files: "+hwsLogistics.toString());
+	System.out.println("Files: "+hwsResults.toString());
+	// ...
+    }
+    
+}
