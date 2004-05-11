645bfd7021c2021b876e80792493210350559aed
doemming <doemming@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-05-11 16:53:35 +0000

    update switch scripts

--- trunk/KalypsoDeegree/switchToTroubadix.sh
+++ trunk/KalypsoDeegree/switchToTroubadix.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# CVS -> deegreecvs
+# CVS -> troubadix
 for pfad in `find -name CVS`
 do
  new=`echo $pfad | sed -e s/CVS/deegreecvs/g`
+ mv $pfad $new
+done;
+for pfad in `find -name troubadixcvs`
+do
+ new=`echo $pfad | sed -e s/troubadixcvs/CVS/g`
  mv $pfad $new
 done;
