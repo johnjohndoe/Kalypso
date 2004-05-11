18c5d4ef9e357b0fbacb218a6ae9a4e37a6d7635
doemming <doemming@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-05-11 16:53:35 +0000

    update switch scripts

--- KalypsoDeegree/switchToDeegree.sh
+++ KalypsoDeegree/switchToDeegree.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# CVS -> troubadixcvs
+# CVS -> Deegree
 for pfad in `find -name CVS`
 do
  new=`echo $pfad | sed -e s/CVS/troubadixcvs/g`
+ mv $pfad $new
+done;
+
+for pfad in `find -name deegreecvs`
+do
+ new=`echo $pfad | sed -e s/deegreecvs/CVS/g`
  mv $pfad $new
 done;
