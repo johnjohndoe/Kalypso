9b881217523b036b00db01a7701898114e52ca2f
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-07-20 16:44:13 +0000

    *** empty log message ***

--- KalypsoUI/src/org/kalypso/util/repository/action/AddRepositoryAction.java
+++ KalypsoUI/src/org/kalypso/util/repository/action/AddRepositoryAction.java
@@ -5,47 +5,50 @@ import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.dialogs.ListSelectionDialog;
+import org.eclipse.ui.dialogs.ListDialog;
 import org.kalypso.eclipse.jface.action.FullAction;
 import org.kalypso.plugin.RepositoryProvider;
+import org.kalypso.util.repository.IRepositoryContainer;
 import org.kalypso.util.repository.file.FileRepository;
-import org.kalypso.util.repository.view.RepositoriesContentProvider;
 
 /**
  * Ein Repository hinzufügen.
  * 
  * @author schlienger
- *
+ *  
  */
 public class AddRepositoryAction extends FullAction
 {
-  private final RepositoriesContentProvider m_cp;
   private final Shell m_shell;
+  private final IRepositoryContainer m_cp;
 
-  public AddRepositoryAction( final Shell shell, final RepositoriesContentProvider cp )
+  public AddRepositoryAction( final Shell shell, final IRepositoryContainer cp )
   {
     super( "Repository hinzufügen", null, "Fügt ein Repository hinzu..." );
-    
+
     m_cp = cp;
     m_shell = shell;
   }
-  
+
   /**
    * @see org.eclipse.jface.action.IAction#run()
    */
   public void run()
   {
-      ListSelectionDialog dlg = new ListSelectionDialog( m_shell, new Object(), RepositoryProvider.getInstance(), new LabelProvider(), "Repository Typ auswählen" );
-      dlg.setTitle("Repositories");
-      if( dlg.open() != Window.OK )
-         return;
+    ListDialog dlg = new ListDialog( m_shell );
+    dlg.setLabelProvider( new LabelProvider() );
+    dlg.setContentProvider( RepositoryProvider.getInstance() );
+    dlg.setTitle( "Repository Typ auswählen" );
+    dlg.setInput( new Object() );
+    if( dlg.open() != Window.OK )
+      return;
 
-      String selType = (String)dlg.getResult()[0];
-      
-      DirectoryDialog fileDlg = new DirectoryDialog( m_shell, SWT.OPEN );
-      if( fileDlg.open() == null )
-          return;
-      
-      m_cp.addRepository( new FileRepository( selType, fileDlg.getFilterPath(), null ) );
+    String selType = (String)dlg.getResult()[0];
+
+    DirectoryDialog fileDlg = new DirectoryDialog( m_shell, SWT.OPEN );
+    if( fileDlg.open() == null )
+      return;
+
+    m_cp.addRepository( new FileRepository( selType, fileDlg.getFilterPath(), null ) );
   }
 }
--- KalypsoUI/src/org/kalypso/util/repository/view/RepositoryExplorerPart.java
+++ KalypsoUI/src/org/kalypso/util/repository/view/RepositoryExplorerPart.java
@@ -1,26 +1,65 @@
 package org.kalypso.util.repository.view;
 
+import org.eclipse.compare.Splitter;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.part.ViewPart;
+import org.kalypso.plugin.KalypsoGisPlugin;
+import org.kalypso.util.repository.DefaultRepositoryContainer;
+import org.kalypso.util.repository.IRepositoryContainer;
+import org.kalypso.util.repository.IRepositoryContainerListener;
 import org.kalypso.util.repository.action.AddRepositoryAction;
+import org.kalypso.util.repository.action.RemoveRepositoryAction;
 
 /**
  * 
  * 
  * @author schlienger
  */
-public class RepositoryExplorerPart extends ViewPart
+public class RepositoryExplorerPart extends ViewPart implements IRepositoryContainerListener,
+    ISelectionProvider, ISelectionChangedListener
 {
-  private TreeViewer m_viewer = null;
-  private RepositoriesContentProvider m_contentProvider = null;
-  
-  public RepositoryExplorerPart(  )
+  private TreeViewer m_repViewer = null;
+
+  private Label m_metaViewer = null;
+
+  private final DefaultRepositoryContainer m_repContainer;
+
+  private RemoveRepositoryAction m_removeAction;
+
+  public RepositoryExplorerPart()
   {
     super();
-    
-    m_contentProvider = new RepositoriesContentProvider();
+
+    m_repContainer = KalypsoGisPlugin.getDefault().getRepositoryContainer();
+
+    m_repContainer.addRepositoryContainerListener( this );
+  }
+
+  /**
+   * @see org.eclipse.ui.IWorkbenchPart#dispose()
+   */
+  public void dispose()
+  {
+    super.dispose();
+
+    m_repContainer.removeRepositoryContainerListener( this );
+
+    if( m_removeAction != null )
+      m_removeAction.dispose();
+
+    if( m_repViewer != null )
+      removeSelectionChangedListener( this );
   }
 
   /**
@@ -28,12 +67,26 @@ public class RepositoryExplorerPart extends ViewPart
    */
   public void createPartControl( Composite parent )
   {
-    m_viewer = new TreeViewer( parent, SWT.H_SCROLL | SWT.V_SCROLL );
-    
-    m_viewer.setContentProvider( m_contentProvider );
-    
-    getViewSite().getActionBars().getToolBarManager().add( new AddRepositoryAction( getSite().getShell(), m_contentProvider) );
+    Splitter split = new Splitter( parent, SWT.VERTICAL | SWT.H_SCROLL | SWT.V_SCROLL );
+
+    m_repViewer = new TreeViewer( split, SWT.H_SCROLL | SWT.V_SCROLL );
+    m_repViewer.setContentProvider( new RepositoryTreeContentProvider() );
+    m_repViewer.setLabelProvider( new LabelProvider() );
+    m_repViewer.setInput( m_repContainer );
+
+    m_metaViewer = new Label( split, SWT.CENTER );
+    m_metaViewer.setText( "Metadata..." );
+
+    final Shell shell = getSite().getShell();
+    m_removeAction = new RemoveRepositoryAction( shell, this );
+
+    final IToolBarManager toolBarManager = getViewSite().getActionBars().getToolBarManager();
+    toolBarManager.add( new AddRepositoryAction( shell, m_repContainer ) );
+    toolBarManager.add( m_removeAction );
+
     getViewSite().getActionBars().updateActionBars();
+    
+    addSelectionChangedListener( this );
   }
 
   /**
@@ -41,6 +94,72 @@ public class RepositoryExplorerPart extends ViewPart
    */
   public void setFocus()
   {
-    // TODO
+  // TODO
+  }
+
+  /**
+   * @see org.kalypso.util.repository.IRepositoryContainerListener#onRepositoryContainerChanged()
+   */
+  public void onRepositoryContainerChanged()
+  {
+    final Runnable r = new Runnable()
+    {
+      public void run()
+      {
+        m_repViewer.refresh();
+      }
+    };
+
+    getSite().getShell().getDisplay().asyncExec( r );
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.ISelectionProvider#addSelectionChangedListener(org.eclipse.jface.viewers.ISelectionChangedListener)
+   */
+  public void addSelectionChangedListener( ISelectionChangedListener listener )
+  {
+    m_repViewer.addSelectionChangedListener( listener );
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.ISelectionProvider#getSelection()
+   */
+  public ISelection getSelection()
+  {
+    return m_repViewer.getSelection();
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.ISelectionProvider#removeSelectionChangedListener(org.eclipse.jface.viewers.ISelectionChangedListener)
+   */
+  public void removeSelectionChangedListener( ISelectionChangedListener listener )
+  {
+    m_repViewer.removeSelectionChangedListener( listener );
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.ISelectionProvider#setSelection(org.eclipse.jface.viewers.ISelection)
+   */
+  public void setSelection( ISelection selection )
+  {
+    m_repViewer.setSelection( selection );
+  }
+
+  public IRepositoryContainer getRepositoryContainer()
+  {
+    return m_repContainer;
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.ISelectionChangedListener#selectionChanged(org.eclipse.jface.viewers.SelectionChangedEvent)
+   */
+  public void selectionChanged( final SelectionChangedEvent event )
+  {
+    final IStructuredSelection selection = (IStructuredSelection)event.getSelection();
+
+    String text = "<Keim Element selektiert>";
+    if( !selection.isEmpty() )
+      text = selection.getFirstElement().toString();
+    m_metaViewer.setText( text );
   }
 }
