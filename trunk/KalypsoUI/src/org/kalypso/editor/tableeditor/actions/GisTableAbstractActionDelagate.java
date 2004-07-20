7b8ba0478e66438fd8371d5fd5319e6dd42210e6
belger <belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-07-20 13:53:15 +0000

    *** empty log message ***

--- trunk/KalypsoUI/src/org/kalypso/editor/tableeditor/actions/GisTableAbstractActionDelagate.java
+++ trunk/KalypsoUI/src/org/kalypso/editor/tableeditor/actions/GisTableAbstractActionDelagate.java
@@ -4,6 +4,8 @@ import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.ui.IActionDelegate2;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorPart;
 import org.kalypso.editor.tableeditor.GisTableEditor;
@@ -11,9 +13,11 @@ import org.kalypso.editor.tableeditor.GisTableEditor;
 /**
  * @author belger
  */
-public abstract class GisTableAbstractActionDelagate implements IEditorActionDelegate, ISelectionChangedListener
+public abstract class GisTableAbstractActionDelagate implements IActionDelegate2,
+    IEditorActionDelegate, ISelectionChangedListener
 {
   private GisTableEditor m_editor;
+
   private IAction m_action;
 
   /**
@@ -24,15 +28,15 @@ public abstract class GisTableAbstractActionDelagate implements IEditorActionDel
   {
     if( m_editor != null )
       m_editor.removeSelectionChangedListener( this );
-    
+
     m_editor = (GisTableEditor)targetEditor;
     m_action = action;
-    
+
     if( m_editor != null )
       m_editor.addSelectionChangedListener( this );
 
     final boolean bEnabled = m_editor == null ? false : isEnabled( m_editor.getSelection() );
-    action.setEnabled(bEnabled);
+    action.setEnabled( bEnabled );
   }
 
   /**
@@ -41,18 +45,46 @@ public abstract class GisTableAbstractActionDelagate implements IEditorActionDel
    */
   public void selectionChanged( final IAction action, final ISelection selection )
   {
-    // nichts tun
+  // nichts tun
   }
 
   public GisTableEditor getEditor()
   {
     return m_editor;
   }
+
+  /**
+   * @see org.eclipse.ui.IActionDelegate2#dispose()
+   */
+  public void dispose()
+  {
+    if( m_editor != null )
+      m_editor.removeSelectionChangedListener( this );
+  }
   
+  /**
+   * @see org.eclipse.ui.IActionDelegate2#init(org.eclipse.jface.action.IAction)
+   */
+  public void init( final IAction action )
+  {
+    // nichts zu tun  
+  }
+
   public void selectionChanged( final SelectionChangedEvent event )
   {
     final boolean bEnabled = m_editor == null ? false : isEnabled( m_editor.getSelection() );
-    m_action.setEnabled(bEnabled);
+    m_action.setEnabled( bEnabled );
+  }
+
+  /**
+   * @see org.eclipse.ui.IActionDelegate2#runWithEvent(org.eclipse.jface.action.IAction,
+   *      org.eclipse.swt.widgets.Event)
+   */
+  public void runWithEvent( final IAction action, final Event event )
+  {
+    // event wird ignoriert
+
+    run( action );
   }
 
   protected abstract boolean isEnabled( final ISelection selection );
