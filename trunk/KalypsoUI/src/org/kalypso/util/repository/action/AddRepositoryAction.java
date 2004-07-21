c47b886a77710e510495c030f6ea01afec3ed1e7
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-07-21 17:28:48 +0000

    *** empty log message ***

--- trunk/KalypsoUI/src/org/kalypso/util/repository/action/AddRepositoryAction.java
+++ trunk/KalypsoUI/src/org/kalypso/util/repository/action/AddRepositoryAction.java
@@ -1,15 +1,16 @@
 package org.kalypso.util.repository.action;
 
+import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.window.Window;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.dialogs.ListDialog;
 import org.kalypso.eclipse.jface.action.FullAction;
-import org.kalypso.plugin.RepositoryProvider;
+import org.kalypso.plugin.KalypsoGisPlugin;
 import org.kalypso.util.repository.IRepositoryContainer;
-import org.kalypso.util.repository.file.FileRepository;
+import org.kalypso.util.repository.IRepositoryFactory;
+import org.kalypso.util.repository.RepositoryException;
+import org.kalypso.util.repository.RepositorySpecification;
 
 /**
  * Ein Repository hinzufügen.
@@ -37,18 +38,24 @@ public class AddRepositoryAction extends FullAction
   {
     ListDialog dlg = new ListDialog( m_shell );
     dlg.setLabelProvider( new LabelProvider() );
-    dlg.setContentProvider( RepositoryProvider.getInstance() );
+    dlg.setContentProvider( new ArrayContentProvider(  ) );
     dlg.setTitle( "Repository Typ auswählen" );
-    dlg.setInput( new Object() );
+    dlg.setInput( KalypsoGisPlugin.getDefault().getRepositoriesSpecifications() );
     if( dlg.open() != Window.OK )
       return;
 
-    String selType = (String)dlg.getResult()[0];
-
-    DirectoryDialog fileDlg = new DirectoryDialog( m_shell, SWT.OPEN );
-    if( fileDlg.open() == null )
-      return;
-
-    m_cp.addRepository( new FileRepository( selType, fileDlg.getFilterPath(), null ) );
+    RepositorySpecification spec = (RepositorySpecification)dlg.getResult()[0];
+    IRepositoryFactory f = spec.createFactory();
+    
+    if( f.configureRepository( m_shell, null ) )
+      try
+      {
+        m_cp.addRepository( f.createRepository() );
+      }
+      catch( RepositoryException e )
+      {
+        // TODO: logging
+        e.printStackTrace();
+      }
   }
 }
---
+++ trunk/KalypsoUI/src/org/kalypso/util/repository/file/FileRepositoryFactory.java
@@ -0,0 +1,48 @@
+package org.kalypso.util.repository.file;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.kalypso.util.repository.IRepository;
+import org.kalypso.util.repository.IRepositoryFactory;
+import org.kalypso.util.repository.file.FileRepository;
+
+/**
+ * @author schlienger
+ *
+ */
+public class FileRepositoryFactory implements IRepositoryFactory
+{
+  private String m_location = null;
+  
+  /**
+   * @see org.kalypso.util.repository.IRepositoryFactory#configureRepository(org.eclipse.swt.widgets.Shell, org.kalypso.util.repository.IRepository)
+   */
+  public boolean configureRepository( final Shell shell, IRepository rep )
+  {
+    DirectoryDialog fileDlg = new DirectoryDialog( shell, SWT.OPEN );
+
+    if( rep != null )
+       fileDlg.setFilterPath( rep.getLocation() );
+    
+    String path = fileDlg.open();
+
+    if( path != null )
+    {
+      m_location = path;
+      
+      return true;
+    }
+    
+    return false;
+  }
+
+  /**
+   * @see org.kalypso.util.repository.IRepositoryFactory#createRepository()
+   */
+  public IRepository createRepository()
+  {
+    // TODO: set the FileFilter here so that only ZML files are visible 
+    return new FileRepository( m_location, null );
+  }
+}
