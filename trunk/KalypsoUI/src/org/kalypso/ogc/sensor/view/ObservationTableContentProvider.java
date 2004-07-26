ceedafd8694f1195be90482d4a35efe5d1e71686
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-07-26 17:09:59 +0000

    *** empty log message ***

---
+++ trunk/KalypsoUI/src/org/kalypso/ogc/sensor/view/ObservationTableContentProvider.java
@@ -0,0 +1,30 @@
+package org.kalypso.ogc.sensor.view;
+
+import org.eclipse.jface.viewers.IContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+/**
+ * 
+ * 
+ * @author schlienger
+ */
+public class ObservationTableContentProvider implements IContentProvider
+{
+  public ObservationTableContentProvider()
+  {
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+   */
+  public void dispose()
+  {
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+   */
+  public void inputChanged( Viewer viewer, Object oldInput, Object newInput )
+  {
+  }
+}
