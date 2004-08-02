3a6b29f64c81daf834123b015a3d18bb735a8ac0
lindemann <lindemann@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-08-02 15:51:59 +0000

    d

--- KalypsoUI/src/org/kalypso/editor/mapeditor/actions/OpenStyleDialogAction.java
+++ KalypsoUI/src/org/kalypso/editor/mapeditor/actions/OpenStyleDialogAction.java
@@ -1,113 +1,102 @@
 package org.kalypso.editor.mapeditor.actions;
 
-import java.awt.Color;
 
-import org.deegree.graphics.sld.LineSymbolizer;
-import org.deegree.graphics.sld.Stroke;
-import org.deegree.graphics.sld.Symbolizer;
+import org.deegree.graphics.Layer;
 import org.deegree.model.feature.FeatureType;
+
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.internal.Workbench;
 import org.kalypso.eclipse.jface.action.FullAction;
+import org.kalypso.editor.mapeditor.GisMapOutlinePage;
 import org.kalypso.editor.mapeditor.ThemeStyleTreeObject;
-import org.kalypso.ogc.event.ModellEvent;
+import org.kalypso.editor.mapeditor.views.StyleEditorViewPart;
 import org.kalypso.ogc.gml.KalypsoFeatureLayer;
+import org.kalypso.ogc.gml.KalypsoTheme;
 import org.kalypso.ogc.gml.KalypsoUserStyle;
-import org.kalypso.util.list.IListManipulator;
 
 /**
  * @author belger
  */
-public class OpenStyleDialogAction extends FullAction implements ISelectionChangedListener
-{
-  private final StructuredViewer m_viewer;
+public class OpenStyleDialogAction extends FullAction implements ISelectionChangedListener {
+	
+	private final StructuredViewer m_viewer;
 
-  private IListManipulator m_listManipulator;
+	private final GisMapOutlinePage m_page;
 
-  public OpenStyleDialogAction( final String text, final ImageDescriptor image,
-      final String tooltipText, final StructuredViewer structuredViewer,
-      final IListManipulator listManip )
-  {
-    super( text, image, tooltipText );
+	public OpenStyleDialogAction(final String text,final ImageDescriptor image, final String tooltipText, final StructuredViewer structuredViewer, GisMapOutlinePage page) 
+	{
+		super(text, image, tooltipText);
 
-    m_viewer = structuredViewer;
-    m_listManipulator = listManip;
+		m_viewer = structuredViewer;
 
-    m_viewer.addSelectionChangedListener( this );
+		m_viewer.addSelectionChangedListener(this);		
 
-    refresh();
-  }
+		m_page = page;
 
-  public void dispose()
-  {
-    m_viewer.removeSelectionChangedListener( this );
-  }
+		refresh();
+	}
 
-  private static boolean test = true;
+	public void dispose() {
+		m_viewer.removeSelectionChangedListener(this);
+	}
 
-  /**
-   * @see org.eclipse.jface.action.Action#run()
-   */
-  public void run()
-  {
-    Object o = ( (IStructuredSelection)m_viewer.getSelection() ).getFirstElement();
-    if( o instanceof ThemeStyleTreeObject )
-    {
-      KalypsoFeatureLayer layer = ( (ThemeStyleTreeObject)o ).getTheme().getLayer();
-      FeatureType ft = layer.getFeatureType();
-      System.out.println( "FeatureType:" + ft );
+	private static boolean test = true;
 
-      KalypsoUserStyle kalypsoStyle = ( (ThemeStyleTreeObject)o ).getStyle();
-      Symbolizer symbolizer = kalypsoStyle.getFeatureTypeStyles()[0].getRules()[0].getSymbolizers()[0];
-      if( symbolizer instanceof LineSymbolizer )
-      {
-        Stroke stroke = ( (LineSymbolizer)symbolizer ).getStroke();
-        if( test )
-        {
-          test = !test;
-          stroke.setStroke( Color.RED );
-        }
-        else
-        {
-          test = !test;
-          stroke.setStroke( Color.GREEN );
-        }
-        kalypsoStyle.fireModellEvent( new ModellEvent( ModellEvent.STYLE_CHANGE ) );
-      }
-    }
+	/**
+	 * @see org.eclipse.jface.action.Action#run()
+	 */
+	public void run() {
 
-    // 	((IStructuredSelection)m_viewer.getSelection()).
-    //	m_viewer.g getContentProvider().
-    //  System.out.println(o.getClass().toString());
-    //  		// m_listManipulator.moveElementDown(
-    // ((IStructuredSelection)m_viewer.getSelection()).getFirstElement() );
-  }
+		StyleEditorViewPart part = null;
+		IWorkbenchWindow window = Workbench.getInstance().getActiveWorkbenchWindow();
+		Object o = ((IStructuredSelection) m_viewer.getSelection()).getFirstElement();		
+		
+		try {							
+			part = (StyleEditorViewPart) window.getActivePage().showView("org.kalypso.editor.mapeditor.views.styleeditor");
+		
+			if (part != null) {
+				part.setSelectionChangedProvider(m_page);
+			}
+			// if UserStyle selected path that on to styleeditor
+			if (o instanceof ThemeStyleTreeObject) {
+				KalypsoFeatureLayer layer = ((ThemeStyleTreeObject) o).getTheme().getLayer();
+				if (layer instanceof KalypsoFeatureLayer) {
+					FeatureType ft = ((KalypsoFeatureLayer) layer).getFeatureType();
+					KalypsoUserStyle kalypsoStyle = ((ThemeStyleTreeObject) o).getStyle();
+					if (part != null)
+						part.initStyleEditor(kalypsoStyle,ft);
+					else
+						part.initStyleEditor(null, null);
+				}
+			}	
+			else if(o instanceof KalypsoTheme)
+				part.initStyleEditor(null, null);
+		} catch (Exception e) {			
+			e.printStackTrace();
+		}		
+	}
 
-  /**
-   * @see org.eclipse.jface.viewers.ISelectionChangedListener#selectionChanged(org.eclipse.jface.viewers.SelectionChangedEvent)
-   */
-  public void selectionChanged( final SelectionChangedEvent event )
-  {
-    refresh();
-  }
+	/**
+	 * @see org.eclipse.jface.viewers.ISelectionChangedListener#selectionChanged(org.eclipse.jface.viewers.SelectionChangedEvent)
+	 */
+	public void selectionChanged(final SelectionChangedEvent event) {
+		refresh();
+	}
 
-  private void refresh()
-  {
-    boolean bEnable = false;
+	private void refresh() {
+		boolean bEnable = false;
 
-    final IStructuredSelection s = (IStructuredSelection)m_viewer.getSelection();
-    if( !s.isEmpty() )
-    {
-      final Object[] elements = ( (IStructuredContentProvider)m_viewer.getContentProvider() )
-          .getElements( m_viewer.getInput() );
+		final IStructuredSelection s = (IStructuredSelection) m_viewer
+				.getSelection();
 
-      bEnable = ( elements[elements.length - 1] != s.getFirstElement() );
-    }
+		if (s.getFirstElement() instanceof ThemeStyleTreeObject)
+			bEnable = true;
 
-    setEnabled( bEnable );
-  }
+		setEnabled(bEnable);
+	}
 }
