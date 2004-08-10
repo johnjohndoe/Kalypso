fcb3345576cd00cda885a5b1296eb0d3af1322e4
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-08-10 14:51:55 +0000

    *** empty log message ***

--- KalypsoUI/src/org/kalypso/ogc/sensor/zml/ZmlLoader.java
+++ KalypsoUI/src/org/kalypso/ogc/sensor/zml/ZmlLoader.java
@@ -1,15 +1,16 @@
 package org.kalypso.ogc.sensor.zml;
 
-import java.io.InputStream;
 import java.net.URL;
 import java.util.Properties;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.kalypso.loader.AbstractLoader;
 import org.kalypso.loader.LoaderException;
 import org.kalypso.ogc.sensor.DefaultObservationProvider;
 import org.kalypso.ogc.sensor.ObservationUtilities;
+import org.xml.sax.InputSource;
 
 /**
  * A specific loader for ZML-Files. Load liefert ein IObservationProvider.
@@ -19,7 +20,9 @@ import org.kalypso.ogc.sensor.ObservationUtilities;
 public class ZmlLoader extends AbstractLoader
 {
   /**
-   * @see org.kalypso.loader.AbstractLoader#loadIntern(java.util.Properties, org.eclipse.core.resources.IProject, org.eclipse.core.runtime.IProgressMonitor)
+   * @see org.kalypso.loader.AbstractLoader#loadIntern(java.util.Properties,
+   *      org.eclipse.core.resources.IProject,
+   *      org.eclipse.core.runtime.IProgressMonitor)
    */
   protected Object loadIntern( Properties source, IProject project, IProgressMonitor monitor )
       throws LoaderException
@@ -28,35 +31,37 @@ public class ZmlLoader extends AbstractLoader
     String location = source.getProperty( "LOCATION" );
     String sharedAxis = source.getProperty( "SHARED_AXIS" );
     String valueAxis = source.getProperty( "VALUE_AXIS" );
-    
+
     monitor.beginTask( "Laden von ZML-Datei von " + location, 2 );
-    
-    InputStream ins = null;
-    
+
+    InputSource ins = null;
+
     try
     {
-    if( type.equals( "relative" ) )
-    {
-      // TODO: Anstatt InputStream --> Reader mit Encoding erstellen und an ZmlObservation geben
-      ins = project.getFile( location ).getContents();
-    }
-    else if( type.equals( "absolute" ) )
-    {
-      ins = new URL( location ).openStream();
-    }
+      if( type.equals( "relative" ) )
+      {
+        IFile f = project.getFile( location );
+        ins = new InputSource( f.getContents() );
+        ins.setEncoding( f.getCharset() );
+      }
+      else if( type.equals( "absolute" ) )
+      {
+        ins = new InputSource( new URL( location ).openStream() );
+      }
     }
     catch( Exception e )
     {
       throw new LoaderException( e );
     }
-    
+
     monitor.worked( 1 );
-    
-    ZmlObservation obs = new ZmlObservation( location, ins );
-    
+
+    ZmlObservation obs = new ZmlObservation( project.getLocation().toOSString(), location, ins );
+
     monitor.worked( 1 );
-    
-    return new DefaultObservationProvider( obs, ObservationUtilities.findAxis( obs, sharedAxis ), ObservationUtilities.findAxis( obs, valueAxis ) );
+
+    return new DefaultObservationProvider( obs, ObservationUtilities.findAxis( obs, sharedAxis ),
+        ObservationUtilities.findAxis( obs, valueAxis ) );
   }
 
   /**
