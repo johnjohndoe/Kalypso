96576a7be7d3d8d92d258708a691b54ab0344c18
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-08-17 19:00:26 +0000

    *** empty log message ***

--- trunk/KalypsoUI/src/org/kalypso/ogc/sensor/template/TableViewTemplate.java
+++ trunk/KalypsoUI/src/org/kalypso/ogc/sensor/template/TableViewTemplate.java
@@ -5,18 +5,21 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
+import org.kalypso.ogc.sensor.tableview.ITableViewColumn;
+import org.kalypso.ogc.sensor.tableview.ITableViewTemplate;
 import org.kalypso.ogc.sensor.tableview.template.RenderingRule;
 import org.kalypso.ogc.sensor.tableview.template.Rules;
 import org.kalypso.template.obstableview.ObjectFactory;
 import org.kalypso.template.obstableview.ObstableviewType;
 import org.kalypso.template.obstableview.TypeRenderingRule;
+import org.kalypso.template.obstableview.ObstableviewType.RulesType;
 
 /**
  * A TableViewTemplate that uses a template file.
  * 
  * @author schlienger
  */
-public class TableViewTemplate
+public class TableViewTemplate implements ITableViewTemplate
 {
   private final static ObjectFactory m_baseFactory = new ObjectFactory();
 
@@ -52,7 +55,7 @@ public class TableViewTemplate
       catch( Exception e )
       {
         // TODO: handling
-        e.printStackTrace();
+        throw new RuntimeException( e );
       }
     }
 
@@ -62,7 +65,7 @@ public class TableViewTemplate
   /**
    * @see org.kalypso.ogc.sensor.tableview.ITableViewTemplate#getColumns()
    */
-  public ColumnPair[] getColumns()
+  public ITableViewColumn[] getColumns()
   {
     if( m_columns == null )
     {
@@ -95,7 +98,11 @@ public class TableViewTemplate
    */
   public Rules getRules()
   {
-    List xrules = getBaseTemplate().getRules().getRenderingrule();
+    final RulesType trules = getBaseTemplate().getRules();
+    if( trules == null )
+      return new Rules();
+    
+    List xrules = trules.getRenderingrule();
     
     RenderingRule[] rules = new RenderingRule[ xrules.size() ];
     
