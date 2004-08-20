1c9a972cce1b3b1cf01a36b40421d8128af52a33
belger <belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-08-20 16:28:20 +0000

    *** empty log message ***

--- trunk/KalypsoUI/src/org/kalypso/ui/calcwizard/MapAndTableWizardPage.java
+++ trunk/KalypsoUI/src/org/kalypso/ui/calcwizard/MapAndTableWizardPage.java
@@ -7,6 +7,8 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.action.GroupMarker;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
 import org.eclipse.swt.custom.SashForm;
@@ -14,6 +16,10 @@ import org.eclipse.swt.custom.ViewForm;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.ToolBar;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.time.TimeSeriesCollection;
 import org.kalypso.ogc.IMapModell;
 import org.kalypso.ogc.MapPanel;
 import org.kalypso.ogc.event.ModellEvent;
@@ -36,16 +42,33 @@ import org.opengis.cs.CS_CoordinateSystem;
 /**
  * @author Belger
  */
-public class MapAndTableWizardPage extends AbstractCalcWizardPage
+public class MapAndTableWizardPage extends AbstractCalcWizardPage implements ISelectionChangedListener
 {
+  /** Pfad auf Vorlage für die Karte (.gmt Datei) */
   public final static String PROP_MAPTEMPLATE = "mapTemplate";
 
+  /** Der Titel der Seite */
   public static final String PROP_MAPTITLE = "mapTitle";
 
+  /** Pfad auf Vorlage für die Gis-Tabell (.gtt Datei) */
   public final static String PROP_TABLETEMPLATE = "tableTemplate";
+
+  /** Position des Haupt-Sash: Integer von 0 bis 100 */
+  public final static String PROP_MAINSASH = "mainSash";
+
+  /** Position des rechten Sash: Integer von 0 bis 100 */
+  public final static String PROP_RIGHTSASH = "rightSash";
+
+  /**
+   * Property-Names zum Layer in der Tabelle: alle Zeitreihen dieser Spalten
+   * werden im diagram angezeigt
+   */
+  public final static String PROP_TIMEPROPNAME = "timeseriesPropertyNames";
 
   private static final int SELECTION_ID = 0x10;
 
+  private final TimeSeriesCollection m_tsCol = new TimeSeriesCollection();
+  
   private LayerTableViewer m_viewer;
 
   public MapAndTableWizardPage()
@@ -54,27 +77,59 @@ public class MapAndTableWizardPage extends AbstractCalcWizardPage
   }
   
   /**
+   * @see org.eclipse.jface.dialogs.IDialogPage#dispose()
+   */
+  public void dispose()
+  {
+    m_viewer.removeSelectionChangedListener(this);
+  }
+
+  /**
    * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
    */
   public void createControl( final Composite parent )
   {
-    final SashForm sashForm = new SashForm( parent, SWT.HORIZONTAL );
-
     try
     {
-      // links karte
+      final SashForm sashForm = new SashForm( parent, SWT.HORIZONTAL );
       createMapPanel( sashForm );
-      createTablePanel( sashForm );
+      final SashForm rightSash = new SashForm( sashForm, SWT.VERTICAL );
+      createTablePanel( rightSash );
+      createDiagramPanel( rightSash );
 
+      final int mainWeight = Integer.parseInt( getArguments().getProperty( PROP_MAINSASH, "50" ) );
+      final int rightWeight = Integer.parseInt( getArguments().getProperty( PROP_RIGHTSASH, "50" ) );
+
+      // TODO: konfigure
       sashForm.setWeights( new int[]
-      { 70, 30 } );
+      { mainWeight, 100 - mainWeight } );
+
+      rightSash.setWeights( new int[]
+      { rightWeight, 100 - rightWeight } );
+
+      setControl( sashForm );
     }
     catch( final Exception e )
     {
       e.printStackTrace();
     }
+  }
 
-    setControl( sashForm );
+  private void createDiagramPanel( final Composite parent )
+  {
+    final Composite composite = new Composite( parent, SWT.RIGHT | SWT.EMBEDDED );
+
+    final Frame vFrame = SWT_AWT.new_Frame( composite );
+
+    final JFreeChart chart = ChartFactory.createTimeSeriesChart( "", "Datum", "Wert", m_tsCol,
+        false, false, false );
+
+    final ChartPanel chartPanel = new ChartPanel( chart );
+    chartPanel.setMouseZoomable( true, false );
+
+    vFrame.setVisible( true );
+    chartPanel.setVisible( true );
+    vFrame.add( chartPanel );
   }
 
   private void createTablePanel( final Composite parent )
@@ -86,8 +141,10 @@ public class MapAndTableWizardPage extends AbstractCalcWizardPage
       final Gistableview template = GisTemplateHelper.loadGisTableview( templateFile );
 
       m_viewer = new LayerTableViewer( parent, getProject(), KalypsoGisPlugin.getDefault()
-                .createFeatureTypeCellEditorFactory(), SELECTION_ID );
+          .createFeatureTypeCellEditorFactory(), SELECTION_ID );
       m_viewer.applyTableTemplate( template, getProject() );
+      
+      m_viewer.addSelectionChangedListener( this );
     }
     catch( final Exception e )
     {
@@ -163,7 +220,18 @@ public class MapAndTableWizardPage extends AbstractCalcWizardPage
   {
     // TODO: error handling?
     m_viewer.saveData();
-    
+
     return true;
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.ISelectionChangedListener#selectionChanged(org.eclipse.jface.viewers.SelectionChangedEvent)
+   */
+  public void selectionChanged( final SelectionChangedEvent event )
+  {
+    final String propNames = getArguments().getProperty(  PROP_TIMEPROPNAME, "" );
+    final String[] timeNames = propNames.split( "#" );
+
+    System.out.println( event.getSelection() );
   }
 }
