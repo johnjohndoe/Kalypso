07e91c3fb5dccf2537938d82f7a055a146b85288
belger <belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-09-13 17:31:54 +0000

    *** empty log message ***

---
+++ _import/trunk/obsolete/KalypsoUtil/src/org/kalypso/repository/AbstractRepositoryFactory.java
@@ -0,0 +1,27 @@
+package org.kalypso.repository;
+
+/**
+ * Abstract implementation of the <code>IRepositoryFactory</code> to permit
+ * subclasses to inherit from this common functionality.
+ * <p>
+ * This class mainly provides a constructor with the configuration string
+ * that each repository factory will handle differently.
+ * 
+ * @author schlienger
+ */
+public abstract class AbstractRepositoryFactory
+{
+  /** configuration string, may be used by subclasses */
+  protected String m_configuration;
+
+  /**
+   * Constructor with configuration string.
+   * 
+   * @param conf the configuration string. Client implementation of this class will use
+   * this to configure themselves.
+   */
+  public AbstractRepositoryFactory( final String conf )
+  {
+    m_configuration = conf;
+  }
+}
