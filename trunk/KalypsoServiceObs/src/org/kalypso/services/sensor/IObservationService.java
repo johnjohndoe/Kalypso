13d476ee146506069f7726cdfc26379806dd0d41
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-10-01 15:08:13 +0000

    *** empty log message ***

--- trunk/KalypsoServiceObs/src/org/kalypso/services/sensor/IObservationService.java
+++ trunk/KalypsoServiceObs/src/org/kalypso/services/sensor/IObservationService.java
@@ -4,7 +4,6 @@ import java.rmi.RemoteException;
 
 import org.kalypso.ogc.sensor.beans.ObservationBean;
 import org.kalypso.ogc.sensor.beans.ObservationDataDescriptorBean;
-import org.kalypso.repository.beans.ItemBean;
 import org.kalypso.services.repository.IRepositoryService;
 
 /**
@@ -25,16 +24,17 @@ public interface IObservationService extends IRepositoryService
   public int getServiceVersion() throws RemoteException;
   
   /**
-   * Returns true when the given node contains Observations.
-   */
-  public boolean hasObservations( final ItemBean node ) throws RemoteException;
-  
-  /**
    * Reads the data out. Does not return the data itself but a descriptor which
    * describes where the data is to be found.
    */
   public ObservationDataDescriptorBean readData( final ObservationBean observation ) throws RemoteException;
 
+  /**
+   * Call this method once client is done with manipulation of the data underlying
+   * the given bean. The service will then free any resources hold by the bean.
+   */
+  public void clearTempData( final ObservationDataDescriptorBean bean ) throws RemoteException;
+  
   /**
    * Writes the data in. Does not take the data as argument but a descriptor
    * which describes where the data is to be found.
