f1a0a9884058f47fcaa1ba18b7976a610669ab07
belger <belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-10-07 18:58:18 +0000

    *** empty log message ***

--- trunk/KalypsoUI/src/org/kalypso/ui/repository/file/ZmlRepositoryConfigDialog.java
+++ trunk/KalypsoUI/src/org/kalypso/ui/repository/file/ZmlRepositoryConfigDialog.java
@@ -1,46 +1,62 @@
 package org.kalypso.ui.repository.file;
 
-import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.jface.preference.DirectoryFieldEditor;
+import org.eclipse.jface.preference.PreferenceStore;
+import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 
 /**
+ * Config Dialog for ZmlRepositoryFactory.
+ * 
  * @author schlienger
  */
-public class ZmlRepositoryConfigDialog extends InputDialog
+public class ZmlRepositoryConfigDialog extends TitleAreaDialog
 {
-  private final static String msg = "hier kommt der message...";
+  private final static String msg = "Bitte wählen Sie zuerst ein Basisverzeichnis aus.\n" +
+      "Geben Sie anschliessend eine oder mehrere Dateiendungen (Komma getrennt)";
   
   private String m_location;
   private String m_filters;
 
-  private DirectoryFieldEditor m_dir;
-  
+  private StringFieldEditor m_fFilters;
+  private DirectoryFieldEditor m_fLocation;
   
   
   public ZmlRepositoryConfigDialog( final Shell parentShell, final String location, final String filters )
   {
-    super( parentShell, "ZML-Repository Konfiguration", msg, filters, null );
+    super( parentShell );
     
     m_location = location;
     m_filters = filters;
   }
 
-
   /**
-   * @see org.eclipse.jface.dialogs.InputDialog#createDialogArea(org.eclipse.swt.widgets.Composite)
+   * @see org.eclipse.jface.dialogs.Dialog#createDialogArea(org.eclipse.swt.widgets.Composite)
    */
   protected Control createDialogArea( final Composite parent )
   {
+    setTitle( "ZML-Repository Konfiguration" );
+    setMessage( msg );
+
+    final PreferenceStore store = new PreferenceStore();
+    
     final Composite c = (Composite)super.createDialogArea( parent );
     
-    Composite sub = new Composite( c, SWT.BEGINNING );
+    final Composite sub1 = new Composite( c, SWT.TOP );
+    m_fLocation = new DirectoryFieldEditor( "basedir", "Basis-Verzeichnis:", sub1 );
+    m_fLocation.setPreferenceStore( store );
+    m_fLocation.setStringValue( m_location );
+    sub1.setSize( 200, 20 );
     
-    m_dir = new DirectoryFieldEditor( "", "Basis-Verzeichnis:", sub );
-    m_dir.setStringValue( m_location );
+    final Composite sub2 = new Composite( c, SWT.BOTTOM );
+    m_fFilters = new StringFieldEditor( "filter", "Dateiendung:", sub2 );
+    m_fFilters.setPreferenceStore( store );
+    m_fFilters.setStringValue( m_filters );
+    sub2.setSize( 200, 20 );
     
     return c;
   }
@@ -50,10 +66,13 @@ public class ZmlRepositoryConfigDialog extends InputDialog
    */
   protected void okPressed()
   {
-    super.okPressed();
+    m_fLocation.store();
+    m_fFilters.store();
     
-    m_filters = getValue();
-    m_location = m_dir.getStringValue();
+    m_location = m_fLocation.getStringValue().trim();
+    m_filters = m_fFilters.getStringValue().trim();
+    
+    super.okPressed();
   }
   
   public String getLocation()
