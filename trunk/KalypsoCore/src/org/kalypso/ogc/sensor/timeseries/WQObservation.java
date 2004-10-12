71b98912647113ff122472b4a1278393ca71fc93
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-10-12 19:40:05 +0000

    *** empty log message ***

--- trunk/KalypsoCore/src/org/kalypso/ogc/sensor/timeseries/WQObservation.java
+++ trunk/KalypsoCore/src/org/kalypso/ogc/sensor/timeseries/WQObservation.java
@@ -1,6 +1,12 @@
 package org.kalypso.ogc.sensor.timeseries;
 
+import org.kalypso.ogc.sensor.IAxis;
 import org.kalypso.ogc.sensor.IObservation;
+import org.kalypso.ogc.sensor.ITuppleModel;
+import org.kalypso.ogc.sensor.MetadataList;
+import org.kalypso.ogc.sensor.SensorException;
+import org.kalypso.util.runtime.IVariableArguments;
+import org.kalypso.util.xml.xlink.IXlink;
 
 /**
  * TODO
@@ -15,4 +21,66 @@ public class WQObservation implements IObservation
   {
     m_obs = obs;
   }
+
+  /**
+   * @see org.kalypso.ogc.sensor.IObservation#getIdentifier()
+   */
+  public String getIdentifier()
+  {
+    return null;
+  }
+
+  /**
+   * @see org.kalypso.ogc.sensor.IObservation#getName()
+   */
+  public String getName()
+  {
+    return null;
+  }
+
+  /**
+   * @see org.kalypso.ogc.sensor.IObservation#isEditable()
+   */
+  public boolean isEditable()
+  {
+    return false;
+  }
+
+  /**
+   * @see org.kalypso.ogc.sensor.IObservation#getTarget()
+   */
+  public IXlink getTarget()
+  {
+    return null;
+  }
+
+  /**
+   * @see org.kalypso.ogc.sensor.IObservation#getMetadataList()
+   */
+  public MetadataList getMetadataList()
+  {
+    return null;
+  }
+
+  /**
+   * @see org.kalypso.ogc.sensor.IObservation#getAxisList()
+   */
+  public IAxis[] getAxisList()
+  {
+    return null;
+  }
+
+  /**
+   * @see org.kalypso.ogc.sensor.IObservation#getValues(org.kalypso.util.runtime.IVariableArguments)
+   */
+  public ITuppleModel getValues( IVariableArguments args ) throws SensorException
+  {
+    return null;
+  }
+
+  /**
+   * @see org.kalypso.ogc.sensor.IObservation#setValues(org.kalypso.ogc.sensor.ITuppleModel)
+   */
+  public void setValues( ITuppleModel values ) throws SensorException
+  {}
 }
