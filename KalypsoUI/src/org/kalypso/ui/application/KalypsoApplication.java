9241d86891657b2ff70d2380d5f0d4490439ba90
belger <belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-10-14 19:41:26 +0000

    *** empty log message ***

--- KalypsoUI/src/org/kalypso/ui/application/KalypsoApplication.java
+++ KalypsoUI/src/org/kalypso/ui/application/KalypsoApplication.java
@@ -15,11 +15,26 @@ public class KalypsoApplication implements IPlatformRunnable
    */
   public Object run( final Object args ) throws Exception
   {
-    final WorkbenchAdvisor advisor = new KalypsoWorkbenchAdvisor();
+    boolean bPrognose = false;
+    if( args instanceof String[] )
+    {
+      final String[] strgargs = (String[])args;
+      for( int i = 0; i < strgargs.length; i++ )
+      {
+        if( "-prognose".equals( strgargs[i] ) )
+        {
+          bPrognose = true;
+          break;
+        }
+      }
+    }
+    
+    final WorkbenchAdvisor advisor = bPrognose ? (WorkbenchAdvisor)new PrognoseWorkbenchAdvisor() : new KalypsoWorkbenchAdvisor();
     final Display display = PlatformUI.createDisplay();
-    
+
     final int returnCode = PlatformUI.createAndRunWorkbench( display, advisor );
-    
-    return returnCode == PlatformUI.RETURN_RESTART ? IPlatformRunnable.EXIT_RESTART : IPlatformRunnable.EXIT_OK;
+
+    return returnCode == PlatformUI.RETURN_RESTART ? IPlatformRunnable.EXIT_RESTART
+        : IPlatformRunnable.EXIT_OK;
   }
 }
