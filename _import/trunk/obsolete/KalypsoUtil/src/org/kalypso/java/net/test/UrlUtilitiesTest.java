c7b4329b0004f6f4800d0ceed4eed8caded92d99
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-10-17 15:40:46 +0000

    *** empty log message ***

---
+++ _import/trunk/obsolete/KalypsoUtil/src/org/kalypso/java/net/test/UrlUtilitiesTest.java
@@ -0,0 +1,30 @@
+package org.kalypso.java.net.test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import junit.framework.TestCase;
+
+import org.kalypso.java.net.UrlUtilities;
+
+/**
+ * UrlUtilitiesTest
+ * 
+ * @author schlienger
+ */
+public class UrlUtilitiesTest extends TestCase
+{
+
+  public void testResolveURL( ) throws MalformedURLException
+  {
+    final URL base = new URL("file://c/temp/hello");
+    
+    final URL u1 = UrlUtilities.resolveURL( base, "" );
+    System.out.println( u1 );
+    assertEquals( base, u1 );
+
+    final URL u2 = UrlUtilities.resolveURL( base, "world/toYou.txt" );
+    System.out.println( u2 );
+  }
+
+}
