8220dc81ed61a9dac4a71374598d262c7942e88c
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-10-17 15:40:47 +0000

    *** empty log message ***

--- trunk/KalypsoUI/src/org/kalypso/eclipse/core/resources/ResourceUtilities.java
+++ trunk/KalypsoUI/src/org/kalypso/eclipse/core/resources/ResourceUtilities.java
@@ -5,8 +5,9 @@ import java.net.URL;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
 
+import org.eclipse.core.internal.resources.PlatformURLResourceConnection;
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdaptable;
@@ -18,38 +19,65 @@ import org.eclipse.jface.viewers.IStructuredSelection;
  */
 public class ResourceUtilities
 {
-  private ResourceUtilities()
+  private ResourceUtilities( )
   {
     // do not instantiate
   }
 
-  public static URL createURL( final IResource resource ) throws MalformedURLException
+  /**
+   * Creates an URL given a resource. Uses the eclipse scheme
+   * defined in PlatformURLResourceConnection.RESOURCE_URL_STRING.
+   * 
+   * @see PlatformURLResourceConnection#RESOURCE_URL_STRING
+   * 
+   * @param resource
+   * @return platform URL
+   * @throws MalformedURLException
+   */
+  public static URL createURL( final IResource resource )
+      throws MalformedURLException
   {
-    return new URL( "platform:/resource/" + resource.getFullPath().toString() );
+    final String strUrl;
+    
+    if( resource instanceof IContainer )
+      strUrl = PlatformURLResourceConnection.RESOURCE_URL_STRING + resource.getFullPath().toString()
+          + '/';
+    else
+      strUrl = PlatformURLResourceConnection.RESOURCE_URL_STRING + resource.getFullPath().toString();
+    
+    return new URL( strUrl );
   }
 
-  /** Findet alle Projekte einer Selektion von Resourcen */
-  public static IProject[] findeProjectsFromSelection( final ISelection selection )
+  /**
+   * Findet alle Projekte einer Selektion von Resourcen
+   * 
+   * @param selection
+   * @return array of IProject
+   */
+  public static IProject[] findeProjectsFromSelection(
+      final ISelection selection )
   {
     // gleiche Projekte sollen nur einen Eintrag gebens
     final Collection projects = new HashSet();
-    if( selection != null && !selection.isEmpty() && selection instanceof IStructuredSelection )
+    if( selection != null && !selection.isEmpty()
+        && selection instanceof IStructuredSelection )
     {
-      final IStructuredSelection ssel = (IStructuredSelection)selection;
+      final IStructuredSelection ssel = (IStructuredSelection) selection;
       for( final Iterator iter = ssel.iterator(); iter.hasNext(); )
       {
         final Object resource = iter.next();
         if( resource instanceof IResource )
-          projects.add( ((IResource)resource).getProject() );
+          projects.add( ((IResource) resource).getProject() );
         else if( resource instanceof IAdaptable )
         {
-          final IResource res = (IResource)((IAdaptable)resource).getAdapter( IResource.class );
+          final IResource res = (IResource) ((IAdaptable) resource)
+              .getAdapter( IResource.class );
           if( res != null )
             projects.add( res.getProject() );
         }
       }
     }
 
-    return (IProject[])projects.toArray( new IProject[projects.size()] );
+    return (IProject[]) projects.toArray( new IProject[projects.size()] );
   }
 }
