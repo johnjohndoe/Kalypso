000b407e0599302970a811f03f5bdf4106294aec
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-10-26 18:03:50 +0000

    *** empty log message ***

--- trunk/KalypsoCore/src/org/kalypso/repository/conf/RepositoryConfigItem.java
+++ trunk/KalypsoCore/src/org/kalypso/repository/conf/RepositoryConfigItem.java
@@ -2,10 +2,7 @@ package org.kalypso.repository.conf;
 
 import org.kalypso.java.lang.reflect.ClassUtilities;
 import org.kalypso.java.lang.reflect.ClassUtilities.ClassUtilityException;
-import org.kalypso.java.xml.DomHelper;
-import org.kalypso.java.xml.DomHelper.DomLoadException;
 import org.kalypso.repository.IRepositoryFactory;
-import org.w3c.dom.Document;
 
 /**
  * Using such an item you can create the <code>IRepositoryFactory</code> for
@@ -21,12 +18,8 @@ public class RepositoryConfigItem
 
   private final boolean m_readOnly;
 
-  // tags used for state persistence
-  private final static String TAG_ROOT = "RepositoryConfigItem";
-  private final static String TAG_CLASSNAME = "classname";
-  private final static String TAG_CONF = "conf";
-  private final static String TAG_READONLY = "readonly";
-
+  private final static String SEPARATOR = ";";
+  
   /**
    * Constructor with:
    * 
@@ -47,6 +40,7 @@ public class RepositoryConfigItem
 
   /**
    * Constructor with factory.
+   * @param factory
    */
   public RepositoryConfigItem( final IRepositoryFactory factory )
   {
@@ -55,6 +49,9 @@ public class RepositoryConfigItem
 
   /**
    * Creates the underlying factory.
+   * @param cl
+   * @return factory
+   * @throws ClassUtilityException
    */
   public IRepositoryFactory createFactory( final ClassLoader cl ) throws ClassUtilityException
   {
@@ -69,45 +66,31 @@ public class RepositoryConfigItem
   }
 
   /**
-   * Saves the state of this object in a XML-like string.
+   * Saves the state of this object in a simple string representation.
+   * @return
    */
   public String saveState()
   {
     final StringBuffer bf = new StringBuffer();
 
-    bf.append( "<" ).append( TAG_ROOT ).append( ">" );
-    
-    bf.append( "<" ).append( TAG_CLASSNAME ).append( ">" );
-    bf.append( m_className );
-    bf.append( "</" ).append( TAG_CLASSNAME ).append( ">" );
+    bf.append( m_className ).append( SEPARATOR ).append( m_conf ).append( SEPARATOR ).append( String.valueOf( m_readOnly ) );
 
-    bf.append( "<" ).append( TAG_CONF ).append( ">" );
-    bf.append( m_conf );
-    bf.append( "</" ).append( TAG_CONF ).append( ">" );
-
-    bf.append( "<" ).append( TAG_READONLY ).append( ">" );
-    bf.append( String.valueOf( m_readOnly ) );
-    bf.append( "</" ).append( TAG_READONLY ).append( ">" );
-
-    bf.append( "</" ).append( TAG_ROOT ).append( ">\n" );
-    
     return bf.toString();
   }
 
   /**
    * Restores a <code>RepositoryConfigItem</code> from the state provided as a
    * string. This is the pendant to the saveState() method.
-   * 
-   * @throws DomLoadException when error occured during parsing of state
+   * @param state
+   * @return a repository config item
    */
-  public static RepositoryConfigItem restore( final String state ) throws DomLoadException
+  public static RepositoryConfigItem restore( final String state )
   {
-    Document doc = DomHelper.loadDocument( state );
+    final String[] splits = state.split( SEPARATOR );
     
-    final String className = doc.getElementsByTagName( TAG_CLASSNAME ).item(0).getFirstChild().getNodeValue();
-    final String conf = doc.getElementsByTagName( TAG_CONF ).item(0).getFirstChild().getNodeValue();
-    final boolean ro = Boolean.valueOf( doc.getElementsByTagName( TAG_READONLY ).item(0).getFirstChild().getNodeValue() ).booleanValue();
+    if( splits.length != 3 )
+      throw new IllegalArgumentException( "state must be an string-array of length 3 with following separator: " + SEPARATOR );
     
-    return new RepositoryConfigItem( className, conf, ro );
+    return new RepositoryConfigItem( splits[0], splits[1], Boolean.valueOf( splits[2] ).booleanValue() );
   }
 }
