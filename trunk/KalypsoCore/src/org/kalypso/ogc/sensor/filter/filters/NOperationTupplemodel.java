974c64b3645bca727497a3d0a35576984f66b2ba
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-11-04 17:27:39 +0000

    *** empty log message ***

--- trunk/KalypsoCore/src/org/kalypso/ogc/sensor/filter/filters/NOperationTupplemodel.java
+++ trunk/KalypsoCore/src/org/kalypso/ogc/sensor/filter/filters/NOperationTupplemodel.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.kalypso.ogc.sensor.IAxis;
 import org.kalypso.ogc.sensor.ITuppleModel;
+import org.kalypso.ogc.sensor.ObservationUtilities;
 import org.kalypso.ogc.sensor.SensorException;
 
 /**
@@ -11,87 +12,87 @@ import org.kalypso.ogc.sensor.SensorException;
  */
 public class NOperationTupplemodel implements ITuppleModel
 {
-    private final int m_operation;
+  private final int m_operation;
 
-    private final ITuppleModel[] m_baseModels;
+  private final ITuppleModel[] m_baseModels;
 
-    public NOperationTupplemodel(ITuppleModel[] models, int operation)
+  public NOperationTupplemodel( ITuppleModel[] models, int operation )
+  {
+    m_baseModels = models;
+    m_operation = operation;
+  }
+
+  public IAxis[] getAxisList( )
+  {
+    return m_baseModels[0].getAxisList();
+  }
+
+  public int getCount( ) throws SensorException
+  {
+    return m_baseModels[0].getCount();
+  }
+
+  public int hashCode( )
+  {
+    return m_baseModels[0].hashCode();
+  }
+
+  public Object getElement( int index, IAxis axis ) throws SensorException
+  {
+    String axisName = axis.getName();
+    Class dataClass = axis.getDataClass();
+    if( dataClass.equals( Date.class ) )
     {
-        m_baseModels = models;
-        m_operation = operation;
+      IAxis a = ObservationUtilities.findAxisByName( m_baseModels[0].getAxisList(), axisName );
+      return m_baseModels[0].getElement( index, a );
     }
-
-    public IAxis[] getAxisList()
+    if( dataClass.equals( Double.class ) )
     {
-        return m_baseModels[0].getAxisList();
-    }
-
-    public int getCount() throws SensorException
-    {
-        return m_baseModels[0].getCount();
-    }
-
-    public int hashCode()
-    {
-        return m_baseModels[0].hashCode();
-    }
-   
-    public Object getElement(int index, IAxis axis) throws SensorException
-    {
-      String axisName = axis.getName();  
-      Class dataClass = axis.getDataClass();
-        if (dataClass.equals(Date.class))
+      IAxis a = ObservationUtilities.findAxisByName( m_baseModels[0].getAxisList(), axisName );
+      double value = ((Number) m_baseModels[0].getElement( index, a ))
+          .doubleValue();
+      for( int i = 1; i < m_baseModels.length; i++ )
+      {
+        ITuppleModel model = m_baseModels[i];
+        IAxis a2 = ObservationUtilities.findAxisByName( m_baseModels[i].getAxisList(), axisName );
+        double nextValue = ((Number) model.getElement( index, a2 ))
+            .doubleValue();
+        switch( m_operation )
         {
-            IAxis a=FilterHelper.getAxisByName(m_baseModels[0], axisName);
-            return m_baseModels[0].getElement(index,a);
+          case OperationFilter.OPERATION_PLUS:
+            value += nextValue;
+            break;
+          case OperationFilter.OPERATION_MINUS:
+            value -= nextValue;
+            break;
+          case OperationFilter.OPERATION_MAL:
+            value *= nextValue;
+            break;
+          case OperationFilter.OPERATION_DURCH: // macht das sinn, bei mehr als
+                                                // zwei ?
+            value /= nextValue;
+            break;
         }
-            if (dataClass.equals(Double.class))
-        {
-              IAxis a=FilterHelper.getAxisByName(m_baseModels[0], axisName);
-              double value = ((Double) m_baseModels[0].getElement(index, a))
-                    .doubleValue();
-            for (int i = 1; i < m_baseModels.length; i++)
-            {
-                ITuppleModel model = m_baseModels[i];
-                IAxis a2=FilterHelper.getAxisByName(m_baseModels[i], axisName);
-                double nextValue = ((Double) model.getElement(index, a2))
-                        .doubleValue();
-                switch (m_operation)
-                {
-                case OperationFilter.OPERATION_PLUS:
-                    value += nextValue;
-                break;
-                case OperationFilter.OPERATION_MINUS:
-                    value -= nextValue;
-                break;
-                case OperationFilter.OPERATION_MAL:
-                    value *= nextValue;
-                break;
-                case OperationFilter.OPERATION_DURCH: // macht das sinn, bei mehr als zwei ?
-                    value /= nextValue;
-                break;
-                }
-            }
-            return new Double(value);
-        }
-        throw new UnsupportedOperationException(getClass().getName()
-                + " unterstuetzt den datentyp " + axis.getDataClass().getName()
-                + " nicht.");
+      }
+      return new Double( value );
     }
+    throw new UnsupportedOperationException( getClass().getName()
+        + " unterstuetzt den datentyp " + axis.getDataClass().getName()
+        + " nicht." );
+  }
 
-    public void setElement(int index, Object element, IAxis axis)            
-    {
-        throw new UnsupportedOperationException(getClass().getName()
-                + " unterstuetzt setElement() nicht.");
-    }
+  public void setElement( int index, Object element, IAxis axis )
+  {
+    throw new UnsupportedOperationException( getClass().getName()
+        + " unterstuetzt setElement() nicht." );
+  }
 
-    public int indexOf(Object element, IAxis axis) throws SensorException
-    {
-        if (element instanceof Date)
-            return m_baseModels[0].indexOf(element, axis);
-        throw new UnsupportedOperationException(getClass().getName()
-                + " unterstuetzt indexOf fuer die Axe " + axis.getName()
-                + " nicht.");
-        // TODO support it
-    }
+  public int indexOf( Object element, IAxis axis ) throws SensorException
+  {
+    if( element instanceof Date )
+      return m_baseModels[0].indexOf( element, axis );
+    throw new UnsupportedOperationException( getClass().getName()
+        + " unterstuetzt indexOf fuer die Axe " + axis.getName() + " nicht." );
+    // TODO support it
+  }
 }
