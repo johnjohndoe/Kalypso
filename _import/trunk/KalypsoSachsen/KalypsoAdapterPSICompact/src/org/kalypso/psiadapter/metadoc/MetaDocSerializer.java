51ae00e8fbc263c6a5155f8c5dfe858e5cb9d57e
belger <belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-11-09 20:17:21 +0000

    *** empty log message ***

--- _import/trunk/KalypsoSachsen/KalypsoAdapterPSICompact/src/org/kalypso/psiadapter/metadoc/MetaDocSerializer.java
+++ _import/trunk/KalypsoSachsen/KalypsoAdapterPSICompact/src/org/kalypso/psiadapter/metadoc/MetaDocSerializer.java
@@ -7,7 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
 
-import org.kalypso.psiadapter.PSICompactFactory;
+import org.apache.commons.io.IOUtils;
 
 /**
  * MetaDocSerializer
@@ -17,72 +17,120 @@ import org.kalypso.psiadapter.PSICompactFactory;
 public class MetaDocSerializer
 {
   private final static String XML_HEADER = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n";
-  
+
   private final static String TAG_FOLDERDATA = "FolderData";
+
   private final static String TAG_HEADER = "Header";
+
   private final static String TAG_IMPORTMODE = "ImportMode";
+
   private final static String TAG_RECORD = "Record";
+
   private final static String TAG_DOKUMENTTYP = "Dokumenttyp";
+
   private final static String TAG_ERSTELLER = "Ersteller";
+
   private final static String TAG_AUTOR = "Autor";
+
   private final static String TAG_REGION = "Region";
+
   private final static String TAG_EINGANGSDATUM = "Eingangsdatum";
+
   private final static String TAG_ERSTELLUNGSDATUM = "Erstellungsdatum";
+
   private final static String TAG_ENDEGDATUM = "EndeGDatum";
+
   private final static String TAG_VERSENDEN = "Versenden";
+
   private final static String TAG_APPFILES = "AppFiles";
 
   /** date format for the date elements of the xml file */
-  private final static DateFormat DF = new SimpleDateFormat( PSICompactFactory.getProperties().getProperty( PSICompactFactory.PROP_METADOC_DF, "yyyy-MM-dd'T'HH:mm:ss" ) );
-  
+  private final static DateFormat DF = new SimpleDateFormat(
+      "yyyy-MM-dd'T'HH:mm:ss" );
+
   /**
    * Prepares the properties with some default value
    * 
-   * TRICKY: we currently given the XSD-type of the value we expect
+   * TRICKY: we currently given the XSD-type of the value we expect and the
+   * default value. The value stuff is a ';' separated string
    * 
+   * @param serviceProps
    * @param props
    */
-  public static void prepareProperties( final Properties props )
+  public static void prepareProperties( final Properties serviceProps,
+      final Properties props )
   {
-    props.setProperty( TAG_DOKUMENTTYP, "string" );
-    props.setProperty( TAG_ERSTELLER, "string" );
-    props.setProperty( TAG_AUTOR, "string" );
+    props.setProperty( TAG_DOKUMENTTYP, "string;"
+        + serviceProps.getProperty( TAG_DOKUMENTTYP, "Dokumenttyp" ) );
+    props.setProperty( TAG_REGION, "string;" + serviceProps.getProperty( TAG_REGION, "Region" ) );
+    props.setProperty( TAG_ERSTELLER, "string;"
+        + serviceProps.getProperty( TAG_ERSTELLER, "Ersteller" ) );
+    props.setProperty( TAG_AUTOR, "string;Autor" );
     props.setProperty( TAG_ERSTELLUNGSDATUM, "dateTime" );
     props.setProperty( TAG_ENDEGDATUM, "dateTime" );
-    props.setProperty( TAG_VERSENDEN, "integer" );
   }
-  
+
   /**
    * Builds the XML using the given writer.
    * 
-   * @param props
+   * @param serviceProps
+   *          properties of the service
+   * @param mdProps
+   *          properties of the metadata
    * @param writer
-   * @param fileName file name TODO: check if it is possible to have more that one file, and if yes, what is the separator char
+   * @param fileName
    * @throws IOException
    */
-  public static void buildXML( final Properties props, final Writer writer, final String fileName ) throws IOException
+  public static void buildXML( final Properties serviceProps,
+      final Properties mdProps, final Writer writer, final String fileName ) throws IOException
   {
-    writer.write( XML_HEADER );
-    writer.write( "<" + TAG_FOLDERDATA + ">" );
-    writer.write( "<" + TAG_HEADER + ">" );
-    writer.write( "<" + TAG_IMPORTMODE + ">" + 1 + "</" + TAG_IMPORTMODE + ">" );
-    writer.write( "</" + TAG_HEADER + ">" );
-    writer.write( "<" + TAG_RECORD + ">" );
-    writer.write( "<" + TAG_DOKUMENTTYP + ">" + props.getProperty( TAG_DOKUMENTTYP, "" ) + "</" + TAG_DOKUMENTTYP + ">" );
-    writer.write( "<" + TAG_ERSTELLER + ">" + props.getProperty( TAG_ERSTELLER, "" ) + "</" + TAG_ERSTELLER + ">" );
-    writer.write( "<" + TAG_AUTOR + ">" + props.getProperty( TAG_AUTOR, "" ) + "</" + TAG_AUTOR + ">" );
-    writer.write( "<" + TAG_REGION + ">" + props.getProperty( TAG_REGION, "ohne" ) + "</" + TAG_REGION + ">" );
-    
-    final String defaultDate = DF.format( new Date() );
-    writer.write( "<" + TAG_EINGANGSDATUM + ">" + props.getProperty( TAG_EINGANGSDATUM, defaultDate ) + "</" + TAG_EINGANGSDATUM + ">" );
-    writer.write( "<" + TAG_ERSTELLUNGSDATUM + ">" + props.getProperty( TAG_ERSTELLUNGSDATUM, defaultDate ) + "</" + TAG_ERSTELLUNGSDATUM + ">" );
+    try
+    {
+      writer.write( XML_HEADER );
+      writer.write( "<" + TAG_FOLDERDATA + ">" );
+      writer.write( "<" + TAG_HEADER + ">" );
+      writer.write( "<" + TAG_IMPORTMODE + ">"
+          + serviceProps.getProperty( TAG_IMPORTMODE, "1" ) + "</"
+          + TAG_IMPORTMODE + ">" );
+      writer.write( "</" + TAG_HEADER + ">" );
+      writer.write( "<" + TAG_RECORD + ">" );
+      writer.write( "<" + TAG_DOKUMENTTYP + ">"
+          + mdProps.getProperty( TAG_DOKUMENTTYP, "" ) + "</" + TAG_DOKUMENTTYP
+          + ">" );
+      writer.write( "<" + TAG_ERSTELLER + ">"
+          + mdProps.getProperty( TAG_ERSTELLER, "" ) + "</" + TAG_ERSTELLER
+          + ">" );
+      writer.write( "<" + TAG_AUTOR + ">" + mdProps.getProperty( TAG_AUTOR, "" )
+          + "</" + TAG_AUTOR + ">" );
+      writer
+          .write( "<" + TAG_REGION + ">"
+              + mdProps.getProperty( TAG_REGION, "ohne" ) + "</" + TAG_REGION
+              + ">" );
 
-    writer.write( "<" + TAG_ENDEGDATUM + ">" + props.getProperty( TAG_ENDEGDATUM, "" ) + "</" + TAG_ENDEGDATUM + ">" );
-    writer.write( "<" + TAG_VERSENDEN + ">" + props.getProperty( TAG_VERSENDEN, "0" ) + "</" + TAG_VERSENDEN + ">" );
+      final String defaultDate = DF.format( new Date() );
+      writer.write( "<" + TAG_EINGANGSDATUM + ">"
+          + mdProps.getProperty( TAG_EINGANGSDATUM, defaultDate ) + "</"
+          + TAG_EINGANGSDATUM + ">" );
+      writer.write( "<" + TAG_ERSTELLUNGSDATUM + ">"
+          + mdProps.getProperty( TAG_ERSTELLUNGSDATUM, defaultDate ) + "</"
+          + TAG_ERSTELLUNGSDATUM + ">" );
 
-    writer.write( "<" + TAG_APPFILES + ">" + fileName + "</" + TAG_APPFILES + ">" );
+      writer.write( "<" + TAG_ENDEGDATUM + ">"
+          + mdProps.getProperty( TAG_ENDEGDATUM, "" ) + "</" + TAG_ENDEGDATUM
+          + ">" );
+      writer.write( "<" + TAG_VERSENDEN + ">"
+          + serviceProps.getProperty( TAG_VERSENDEN, "0" ) + "</"
+          + TAG_VERSENDEN + ">" );
 
-    writer.write( "</" + TAG_RECORD + ">" );
-    writer.write( "</" + TAG_FOLDERDATA + ">" );
+      writer.write( "<" + TAG_APPFILES + ">" + fileName + "</" + TAG_APPFILES
+          + ">" );
+
+      writer.write( "</" + TAG_RECORD + ">" );
+      writer.write( "</" + TAG_FOLDERDATA + ">" );
+    }
+    finally
+    {
+      IOUtils.closeQuietly( writer );
+    }
   }
 }
---
+++ _import/trunk/KalypsoSachsen/KalypsoAdapterPSICompact/src/org/kalypso/psiadapter/metadoc/PSICompactCommiter.java
@@ -0,0 +1,72 @@
+package org.kalypso.psiadapter.metadoc;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Properties;
+
+import org.kalypso.java.io.FileUtilities;
+import org.kalypso.metadoc.IMetaDocCommiter;
+import org.kalypso.metadoc.MetaDocException;
+import org.kalypso.metadoc.beans.DocBean;
+import org.kalypso.psiadapter.PSICompactFactory;
+
+/**
+ * PSICompactCommiter
+ * 
+ * @author schlienger
+ */
+public class PSICompactCommiter implements IMetaDocCommiter
+{
+  /** distribution directory property */
+  public final static String PSICOMPACT_DIST = "PSICOMPACT_DIST";
+  
+  /**
+   * @see org.kalypso.metadoc.IMetaDocCommiter#prepareMetainf(java.util.Properties, org.kalypso.metadoc.beans.DocBean)
+   */
+  public void prepareMetainf( final Properties serviceProps, final DocBean docBean )
+  {
+    final Properties props = new Properties();
+    props.putAll( docBean.getMetadata() );
+
+    MetaDocSerializer.prepareProperties( serviceProps, props );
+
+    docBean.getMetadata().putAll( props );
+  }
+
+  /**
+   * @see org.kalypso.metadoc.IMetaDocCommiter#commitDocument(java.util.Properties, org.kalypso.metadoc.beans.DocBean)
+   */
+  public void commitDocument( final Properties serviceProps, final DocBean docBean ) throws MetaDocException
+  {
+    final File xmlFile = new File( FileUtilities.nameWithoutExtension( docBean
+        .getLocation() )
+        + ".xml" );
+
+    try
+    {
+      // just fetch the name of the file (without path, not necessary for PSICompact)
+      final File docFile = new File( docBean.getLocation() );
+      
+      final Properties mdProps = new Properties();
+      mdProps.putAll( docBean.getMetadata() );
+      
+      final FileWriter writer = new FileWriter( xmlFile );
+      // closes writer 
+      MetaDocSerializer.buildXML( serviceProps, mdProps, writer, docFile.getName() );
+      
+      // commit the both files (important: last one is the xml file)
+      final String dist = serviceProps.getProperty( PSICOMPACT_DIST ) + "/";
+      String distDocFile = dist + docFile.getName();
+      distDocFile = distDocFile.replace( '\\', '/' );
+      String distXmlFile = dist + xmlFile.getName();
+      distXmlFile = distXmlFile.replace( '\\', '/' );
+      
+      PSICompactFactory.getConnection().copyanddistributeFile( docFile, distDocFile );
+      PSICompactFactory.getConnection().copyanddistributeFile( xmlFile, distXmlFile );
+    }
+    catch( Exception e ) // generic for simplicity
+    {
+      throw new MetaDocException( e );
+    }
+  }
+}
