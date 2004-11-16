792adf8b5b0adbdf45a7299d107c85b775375469
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-11-16 20:02:46 +0000

    *** empty log message ***

--- trunk/KalypsoUI/src/org/kalypso/ogc/sensor/view/DiagramViewPart.java
+++ trunk/KalypsoUI/src/org/kalypso/ogc/sensor/view/DiagramViewPart.java
@@ -1,5 +1,6 @@
 package org.kalypso.ogc.sensor.view;
 
+import java.awt.Font;
 import java.awt.Frame;
 
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -13,12 +14,14 @@ import org.eclipse.ui.IPartListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.ViewPart;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.title.TextTitle;
 import org.kalypso.ogc.sensor.IObservation;
 import org.kalypso.ogc.sensor.SensorException;
 import org.kalypso.ogc.sensor.diagview.impl.ObservationDiagramTemplate;
 import org.kalypso.ogc.sensor.diagview.jfreechart.ObservationChart;
 import org.kalypso.repository.IRepositoryItem;
 import org.kalypso.ui.repository.view.RepositoryExplorerPart;
+import org.kalypso.util.runtime.args.DateRangeArgument;
 
 /**
  * Diagram QuickView.
@@ -31,6 +34,8 @@ public class DiagramViewPart extends ViewPart implements
   protected final ObservationDiagramTemplate m_template = new ObservationDiagramTemplate();
 
   private ObservationChart m_chart;
+
+  private TextTitle m_subTitle;
 
   /**
    * @see org.eclipse.ui.IWorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
@@ -49,6 +54,9 @@ public class DiagramViewPart extends ViewPart implements
 
     m_template.addTemplateEventListener( m_chart );
 
+    m_subTitle = new TextTitle("", new Font( "Default", Font.PLAIN, 12));
+    m_chart.addSubtitle( m_subTitle );
+    
     ChartPanel chartPanel = new ChartPanel( m_chart );
     chartPanel.setMouseZoomable( true, false );
 
@@ -103,9 +111,18 @@ public class DiagramViewPart extends ViewPart implements
 
     final IObservation obs = ObservationCache.getInstance().getObservationFor(
         item );
+
     if( obs != null )
-      m_template.setObservation( obs, ObservationViewHelper
-          .makeDateRange( item ) );
+    {
+      final DateRangeArgument dra = ObservationViewHelper.makeDateRange( item );
+
+      m_template.setObservation( obs, dra );
+
+      // sub title of diagram contains date-range info
+      m_subTitle.setText("");
+      if( dra != null )
+        m_subTitle.setText( dra.toString() );
+    }
   }
 
   /**
