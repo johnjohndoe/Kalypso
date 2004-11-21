7c90fc68b28bb45de465dc6bbd776f1d2cce3abe
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2004-11-21 21:57:33 +0000

    *** empty log message ***

--- KalypsoUI/src/org/kalypso/ogc/sensor/tableview/swing/renderer/MaskedNumberTableCellRenderer.java
+++ KalypsoUI/src/org/kalypso/ogc/sensor/tableview/swing/renderer/MaskedNumberTableCellRenderer.java
@@ -8,62 +8,56 @@ import java.text.NumberFormat;
 import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.TableModel;
 
 import org.kalypso.ogc.sensor.tableview.rules.RenderingRule;
 import org.kalypso.ogc.sensor.tableview.swing.ObservationTableModel;
 
-
 /**
  * @author schlienger
- *
+ *  
  */
 public class MaskedNumberTableCellRenderer extends DefaultTableCellRenderer
 {
   private final static NumberFormat nf = NumberFormat.getInstance();
-  
-  /**
-   * @see javax.swing.table.TableCellRenderer#getTableCellRendererComponent(javax.swing.JTable, java.lang.Object, boolean, boolean, int, int)
-   */
-  public Component getTableCellRendererComponent( JTable table, Object value, boolean isSelected,
-      boolean hasFocus, int row, int column )
-  {
-    final JLabel label = (JLabel)super.getTableCellRendererComponent( table, value, isSelected,
-        hasFocus, row, column );
 
-    final Number n = (Number)value;
-    
+  /**
+   * @see javax.swing.table.TableCellRenderer#getTableCellRendererComponent(javax.swing.JTable,
+   *      java.lang.Object, boolean, boolean, int, int)
+   */
+  public Component getTableCellRendererComponent( JTable table, Object value,
+      boolean isSelected, boolean hasFocus, int row, int column )
+  {
+    final JLabel label = (JLabel) super.getTableCellRendererComponent( table,
+        value, isSelected, hasFocus, row, column );
+
+    final Number n = (Number) value;
+
     if( n == null )
       return label;
-    
-    TableModel tm = table.getModel();
-    if( tm instanceof ObservationTableModel )
-    {
-      ObservationTableModel otm = (ObservationTableModel)tm;
-      
-      final RenderingRule[] r = otm.findRules( row, column );
 
-      String ttext = "";
-      
-      for( int i = 0; i < r.length; i++ )
-      {
-        final Font f = r[i].getFont();
-        if( f != null )
-          label.setFont( f );
-        
-        ttext += r[i].getTooltipText();
-        
-        final Color fgc = r[i].getForegroundColor();
-        if( fgc != null )
-          label.setForeground( fgc );
-        
-        final Color bgc = r[i].getBackgroundColor();
-        if( bgc != null )
-          label.setBackground( bgc );        
-      }
-      
-      label.setToolTipText( ttext );
+    final ObservationTableModel otm = (ObservationTableModel) table.getModel();
+    final RenderingRule[] r = otm.findRules( row, column );
+
+    String ttext = "";
+
+    for( int i = 0; i < r.length; i++ )
+    {
+      final Font f = r[i].getFont();
+      if( f != null )
+        label.setFont( f );
+
+      ttext += r[i].getTooltipText();
+
+      final Color fgc = r[i].getForegroundColor();
+      if( fgc != null )
+        label.setForeground( fgc );
+
+      final Color bgc = r[i].getBackgroundColor();
+      if( bgc != null )
+        label.setBackground( bgc );
     }
+
+    label.setToolTipText( ttext );
 
     label.setText( nf.format( n.doubleValue() ) );
 
