298731ad4c0ae28ed9384872bcad10794d908924
doemming <doemming@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-01-03 01:37:09 +0000

    *** empty log message ***

--- trunk/KalypsoUI/src/org/kalypso/util/transformation/CopyTransformation.java
+++ trunk/KalypsoUI/src/org/kalypso/util/transformation/CopyTransformation.java
@@ -23,49 +23,58 @@ public class CopyTransformation extends AbstractTransformation
   /** Ausgabedatei: Absoluter Pfad im Workspace */
   public final static String PROP_OUTPUT = "output";
 
+  /** ignore error */
+  public final static String PROP_IGNOREERROR = "ignoreError";
+
   /**
    * @see org.kalypso.util.transformation.AbstractTransformation#transformIntern(java.util.Properties,
    *      java.io.BufferedWriter, java.io.BufferedWriter,
    *      org.eclipse.core.runtime.IProgressMonitor)
    */
-  public void transformIntern( final Properties properties,
-      final BufferedWriter msgWriter, final BufferedWriter logWriter,
-      final IProgressMonitor monitor ) throws TransformationException
+  public void transformIntern( final Properties properties, final BufferedWriter msgWriter,
+      final BufferedWriter logWriter, final IProgressMonitor monitor )
+      throws TransformationException
   {
     monitor.beginTask( "Transform", 3000 );
 
     final String input = properties.getProperty( PROP_INPUT );
-    final String output = properties.getProperty( "output" );
-
-    if( input == null )
-      throw new TransformationException( "Parameter 'input' nicht gesetzt" );
-    if( output == null || output.length() == 0 )
-      throw new TransformationException( "Parameter 'output' nicht gesetzt" );
-
-    final IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-    final IFile inputFile = root.getFile( new Path( input ) );
-    if( inputFile == null || !inputFile.exists() )
-      throw new TransformationException(
-          "input file doesn't exist or is not a file: " + input );
-
-    final IFile outputFile = root.getFile( new Path( output ) );
+    final String output = properties.getProperty( PROP_OUTPUT );
+    final String ignoreError = properties.getProperty( PROP_IGNOREERROR );
 
     try
     {
-      FolderUtilities.mkdirs( outputFile.getParent() );
-      if( outputFile.exists() )
-        outputFile
-            .delete( false, true, new SubProgressMonitor( monitor, 1000 ) );
-      else
-        monitor.worked( 1000 );
-      inputFile.copy( outputFile.getFullPath(), false, new SubProgressMonitor(
-          monitor, 1000 ) );
-      outputFile.setCharset( inputFile.getCharset(), new SubProgressMonitor(
-          monitor, 1000 ) );
+      if( input == null )
+        throw new TransformationException( "Parameter 'input' nicht gesetzt" );
+      if( output == null || output.length() == 0 )
+        throw new TransformationException( "Parameter 'output' nicht gesetzt" );
+
+      final IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+      final IFile inputFile = root.getFile( new Path( input ) );
+      if( inputFile == null || !inputFile.exists() )
+        throw new TransformationException( "input file doesn't exist or is not a file: " + input );
+
+      final IFile outputFile = root.getFile( new Path( output ) );
+
+      try
+      {
+        FolderUtilities.mkdirs( outputFile.getParent() );
+        if( outputFile.exists() )
+          outputFile.delete( false, true, new SubProgressMonitor( monitor, 1000 ) );
+        else
+          monitor.worked( 1000 );
+        inputFile.copy( outputFile.getFullPath(), false, new SubProgressMonitor( monitor, 1000 ) );
+        outputFile.setCharset( inputFile.getCharset(), new SubProgressMonitor( monitor, 1000 ) );
+      }
+      catch( final CoreException e )
+      {
+        throw new TransformationException( e );
+      }
     }
-    catch( final CoreException e )
+    catch( TransformationException e )
     {
-      throw new TransformationException( e );
+      if( ignoreError != null && "true".equals( ignoreError ) )
+        return;
+      throw e;
     }
   }
 }
