46e69890a784775abc85c25a8471c6bd91d110df
doemming <doemming@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-01-10 11:01:45 +0000

    *** empty log message ***

---
+++ KalypsoUI/src/org/kalypso/util/transformation/MoveTransformation.java
@@ -0,0 +1,14 @@
+package org.kalypso.util.transformation;
+
+/**
+ * @author doemming
+ */
+public class MoveTransformation extends CopyOrMoveTransformation
+{
+  
+  public MoveTransformation()
+  {
+    super(CopyOrMoveTransformation.MODE_MOVE);
+  }
+    
+}
