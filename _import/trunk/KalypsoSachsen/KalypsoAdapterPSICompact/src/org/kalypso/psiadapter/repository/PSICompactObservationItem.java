9a768adc2241b820d910d5ae1ff2c3a4093dee89
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-01-14 14:26:07 +0000

    *** empty log message ***

--- _import/trunk/KalypsoSachsen/KalypsoAdapterPSICompact/src/org/kalypso/psiadapter/repository/PSICompactObservationItem.java
+++ _import/trunk/KalypsoSachsen/KalypsoAdapterPSICompact/src/org/kalypso/psiadapter/repository/PSICompactObservationItem.java
@@ -373,6 +373,14 @@ public class PSICompactObservationItem implements IObservation
   {
     m_evtPrv.removeListener( listener );
   }
+  
+  /**
+   * @see org.kalypso.ogc.sensor.IObservationEventProvider#clearListeners()
+   */
+  public void clearListeners( )
+  {
+    m_evtPrv.clearListeners();
+  }
 
   /**
    * @see org.kalypso.ogc.sensor.IObservation#getHref()
