1ff8df54e9212c94ac67f01081ef707c0e744cbc
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-02-02 18:08:43 +0000

    *** empty log message ***

--- trunk/KalypsoCore/src/org/kalypso/ogc/sensor/zml/test/ZmlTest.java
+++ trunk/KalypsoCore/src/org/kalypso/ogc/sensor/zml/test/ZmlTest.java
@@ -36,11 +36,12 @@
  belger@bjoernsen.de
  schlienger@bjoernsen.de
  v.doemming@tuhh.de
-  
----------------------------------------------------------------------------------------------------*/
+ 
+ ---------------------------------------------------------------------------------------------------*/
 package org.kalypso.ogc.sensor.zml.test;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -62,46 +63,66 @@ import org.kalypso.ogc.sensor.zml.ZmlFactory;
 public class ZmlTest extends TestCase
 {
   private IObservation m_obs;
+
   private String m_obsID;
+
   private SimpleDateFormat df;
+
   private DoubleComparator dc;
 
-  protected void setUp() throws Exception
+  // contains the list of ZMLs to test
+  private final static String[] ZMLs = { "./etc/schemas/zml/beispiel.zml",
+      "./etc/schemas/zml/inline_ex.zml" };
+
+  public void testZmls( ) throws MalformedURLException, SensorException,
+      ParseException
   {
-    super.setUp();
-
-    final File zmlFile = new File( "./etc/schemas/zml/beispiel.zml" );
-    assertTrue( zmlFile.exists() );
-
-    m_obsID = zmlFile.getAbsolutePath();
-    
-    m_obs = ZmlFactory.parseXML( zmlFile.toURL(), m_obsID );
-    
     df = new SimpleDateFormat( "dd.MM.yyyy" );
     dc = new DoubleComparator( 0 );
+
+    for( int i = 0; i < ZMLs.length; i++ )
+    {
+      System.out.println( "Testing: " + ZMLs[i] );
+      
+      final File zmlFile = new File( ZMLs[i] );
+      assertTrue( zmlFile.exists() );
+
+      m_obsID = zmlFile.getAbsolutePath();
+
+      m_obs = ZmlFactory.parseXML( zmlFile.toURL(), m_obsID );
+
+      _testGetName();
+      _testGetTarget();
+      _testGetAxisList();
+      _testGetIdentifier();
+      _testGetMetadataList();
+      _testGetValues();
+      _testIsEditable();
+      _testSetValues();
+    }
   }
 
-  public void testGetName()
+  public void _testGetName( )
   {
     assertTrue( m_obs.getName().equals( "Eine Test-Observation" ) );
   }
 
-  public void testIsEditable()
+  public void _testIsEditable( )
   {
     assertTrue( m_obs.isEditable() );
   }
 
-  public void testGetIdentifier()
+  public void _testGetIdentifier( )
   {
     assertTrue( m_obs.getIdentifier().equals( m_obsID ) );
   }
-  
-  public void testGetTarget()
+
+  public void _testGetTarget( )
   {
     assertNull( m_obs.getTarget() );
   }
 
-  public void testGetMetadataList()
+  public void _testGetMetadataList( )
   {
     final MetadataList mdl = m_obs.getMetadataList();
     assertNotNull( mdl );
@@ -111,7 +132,7 @@ public class ZmlTest extends TestCase
     assertTrue( mdl.getProperty( "Alarmstufe 1" ).equals( "4.3" ) );
   }
 
-  public void testGetAxisList()
+  public void _testGetAxisList( )
   {
     final IAxis[] axes = m_obs.getAxisList();
     assertNotNull( axes );
@@ -119,86 +140,95 @@ public class ZmlTest extends TestCase
     assertTrue( axes.length == 3 );
   }
 
-  public void testGetValues() throws SensorException, ParseException
+  public void _testGetValues( ) throws SensorException, ParseException
   {
     final ITuppleModel values = m_obs.getValues( null );
     assertNotNull( values );
-    
+
     assertEquals( values.getCount(), 21 );
-    
+
     final IAxis[] axes = values.getAxisList();
-    
+
     final IAxis dateAxis = ObservationUtilities.findAxisByName( axes, "Datum" );
     assertNotNull( dateAxis );
-    
+
     final IAxis vAxis1 = ObservationUtilities.findAxisByName( axes, "Pegel1" );
     assertNotNull( vAxis1 );
-    
+
     final IAxis vAxis2 = ObservationUtilities.findAxisByName( axes, "Pegel2" );
     assertNotNull( vAxis2 );
-    
+
     assertEquals( values.getElement( 0, dateAxis ), df.parse( "01.01.2004" ) );
-    assertTrue( dc.compare( values.getElement( 0, vAxis1 ), Double.valueOf( "1.0" ) ) == 0 );
-    assertTrue( dc.compare( values.getElement( 0, vAxis2 ), new Double(11) ) == 0 );
-    
+    assertTrue( dc.compare( values.getElement( 0, vAxis1 ), Double
+        .valueOf( "1.0" ) ) == 0 );
+    assertTrue( dc.compare( values.getElement( 0, vAxis2 ), new Double( 11 ) ) == 0 );
+
     assertEquals( values.getElement( 20, dateAxis ), df.parse( "21.01.2004" ) );
-    assertTrue( dc.compare( values.getElement( 20, vAxis1 ), Double.valueOf( "16.6" ) ) == 0 );
-    assertTrue( dc.compare( values.getElement( 20, vAxis2 ), Double.valueOf( "18.5" ) ) == 0 );
+    assertTrue( dc.compare( values.getElement( 20, vAxis1 ), Double
+        .valueOf( "16.6" ) ) == 0 );
+    assertTrue( dc.compare( values.getElement( 20, vAxis2 ), Double
+        .valueOf( "18.5" ) ) == 0 );
   }
 
-  public void testSetValues() throws SensorException, ParseException
+  public void _testSetValues( ) throws SensorException, ParseException
   {
     final IAxis[] axes = m_obs.getAxisList();
-    
+
     final IAxis dateAxis = ObservationUtilities.findAxisByName( axes, "Datum" );
     assertNotNull( dateAxis );
-    
+
     final IAxis vAxis1 = ObservationUtilities.findAxisByName( axes, "Pegel1" );
     assertNotNull( vAxis1 );
-    
+
     final IAxis vAxis2 = ObservationUtilities.findAxisByName( axes, "Pegel2" );
     assertNotNull( vAxis2 );
-    
+
     final SimpleTuppleModel m = new SimpleTuppleModel( m_obs.getAxisList() );
-    
+
     final Object[] t1 = new Object[3];
-    t1[ m.getPositionFor(dateAxis) ] = df.parse( "20.01.2004" );
-    t1[ m.getPositionFor(vAxis1) ] = new Double(44);
-    t1[ m.getPositionFor(vAxis2) ] = new Double(11);
+    t1[m.getPositionFor( dateAxis )] = df.parse( "20.01.2004" );
+    t1[m.getPositionFor( vAxis1 )] = new Double( 44 );
+    t1[m.getPositionFor( vAxis2 )] = new Double( 11 );
     m.addTupple( t1 );
-    
+
     final Object[] t2 = new Object[3];
-    t2[ m.getPositionFor(dateAxis) ] = df.parse( "21.01.2004" );
-    t2[ m.getPositionFor(vAxis1) ] = new Double(55);
-    t2[ m.getPositionFor(vAxis2) ] = new Double(22);
+    t2[m.getPositionFor( dateAxis )] = df.parse( "21.01.2004" );
+    t2[m.getPositionFor( vAxis1 )] = new Double( 55 );
+    t2[m.getPositionFor( vAxis2 )] = new Double( 22 );
     m.addTupple( t2 );
 
     final Object[] t3 = new Object[3];
-    t3[ m.getPositionFor(dateAxis) ] = df.parse( "22.01.2004" );
-    t3[ m.getPositionFor(vAxis1) ] = new Double(66);
-    t3[ m.getPositionFor(vAxis2) ] = new Double(33);
+    t3[m.getPositionFor( dateAxis )] = df.parse( "22.01.2004" );
+    t3[m.getPositionFor( vAxis1 )] = new Double( 66 );
+    t3[m.getPositionFor( vAxis2 )] = new Double( 33 );
     m.addTupple( t3 );
 
     m_obs.setValues( m );
-    
+
     final ITuppleModel values = m_obs.getValues( null );
     assertNotNull( values );
-    
+
     assertEquals( values.getCount(), 22 );
-    
+
     int i = 19;
     assertEquals( values.getElement( i, dateAxis ), df.parse( "20.01.2004" ) );
-    assertTrue( dc.compare( values.getElement( i, vAxis1 ), Double.valueOf( "44" ) ) == 0 );
-    assertTrue( dc.compare( values.getElement( i, vAxis2 ), Double.valueOf( "11" ) ) == 0 );
+    assertTrue( dc.compare( values.getElement( i, vAxis1 ), Double
+        .valueOf( "44" ) ) == 0 );
+    assertTrue( dc.compare( values.getElement( i, vAxis2 ), Double
+        .valueOf( "11" ) ) == 0 );
 
     i = 20;
     assertEquals( values.getElement( i, dateAxis ), df.parse( "21.01.2004" ) );
-    assertTrue( dc.compare( values.getElement( i, vAxis1 ), Double.valueOf( "55" ) ) == 0 );
-    assertTrue( dc.compare( values.getElement( i, vAxis2 ), Double.valueOf( "22" ) ) == 0 );
-    
+    assertTrue( dc.compare( values.getElement( i, vAxis1 ), Double
+        .valueOf( "55" ) ) == 0 );
+    assertTrue( dc.compare( values.getElement( i, vAxis2 ), Double
+        .valueOf( "22" ) ) == 0 );
+
     i = 21;
     assertEquals( values.getElement( i, dateAxis ), df.parse( "22.01.2004" ) );
-    assertTrue( dc.compare( values.getElement( i, vAxis1 ), Double.valueOf( "66" ) ) == 0 );
-    assertTrue( dc.compare( values.getElement( i, vAxis2 ), Double.valueOf( "33" ) ) == 0 );
+    assertTrue( dc.compare( values.getElement( i, vAxis1 ), Double
+        .valueOf( "66" ) ) == 0 );
+    assertTrue( dc.compare( values.getElement( i, vAxis2 ), Double
+        .valueOf( "33" ) ) == 0 );
   }
 }
