4f8fff828903eb5ce4764d12b4295443cda8214e
doemming <doemming@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-03-03 11:27:14 +0000

    *** empty log message ***

--- temp/trunk/KalypsoCreateProjectNA/src/org/kalypso/wizard/KalypsoNAFileImportPage.java
+++ temp/trunk/KalypsoCreateProjectNA/src/org/kalypso/wizard/KalypsoNAFileImportPage.java
@@ -1,8 +1,47 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+
 /*
  * Created on 31.01.2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ *  
  */
 package org.kalypso.wizard;
 
@@ -50,276 +89,313 @@ import org.opengis.cs.CS_CoordinateSystem;
 
 /**
  * @author kuepfer
- * 
- * TODO To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Style - Code Templates
+ *  
  */
-public class KalypsoNAFileImportPage extends WizardPage {
+public class KalypsoNAFileImportPage extends WizardPage
+{
 
-	//constants
-	private static final String NULL_KEY = "-NULL-";
+  //constants
+  private static final String NULL_KEY = "-NULL-";
 
-	private static final int SIZING_TEXT_FIELD_WIDTH = 250;
+  private static final int SIZING_TEXT_FIELD_WIDTH = 250;
 
-	private static final String SOURCE_KEY = "source";
+  private static final String SOURCE_KEY = "source";
 
-	private static final String TARGET_KEY = "target";
+  private static final String TARGET_KEY = "target";
 
-	//widgets
-	private Group fileGroup;
+  //widgets
+  private Group fileGroup;
 
-	private Label fileLabel;
+  private Label fileLabel;
 
-	private Group sourceGroup;
+  private Group sourceGroup;
 
-	private Group buttonGroup;
+  private Group buttonGroup;
 
-	private Group targetGroup;
+  private Group targetGroup;
 
-	private Text textField;
+  private Text textField;
 
-	private Composite topComposite;
+  private Composite topComposite;
 
-	private ScrolledComposite topSCLMappingComposite;
+  private ScrolledComposite topSCLMappingComposite;
 
-	private Composite topMappingComposite;
-	
-	private Button skipRadioButton;
+  private Composite topMappingComposite;
 
-	private Button browseButton;
+  private Button skipRadioButton;
 
-	private Button okButton;
+  private Button browseButton;
 
-	//Geodata
-	private CS_CoordinateSystem customCS = null;
+  private Button okButton;
 
-	private CS_CoordinateSystem defaultCS = ConvenienceCSFactory.getInstance()
-			.getOGCCSByName("EPSG:31467");
+  //Geodata
+  private CS_CoordinateSystem customCS = null;
 
-	private GMLWorkspace sourceWorkspace;
+  private CS_CoordinateSystem defaultCS = ConvenienceCSFactory.getInstance().getOGCCSByName(
+      "EPSG:31467" );
 
-	private URL fileURL;
+  private GMLWorkspace sourceWorkspace;
 
-	//mapping
-	private HashMap mapping;
+  private URL fileURL;
 
-	/**
-	 * @param pageName
-	 */
-	public KalypsoNAFileImportPage(String pageName) {
-		super(pageName);
-		setDescription("Dieser Dialog liest eine ESRI Shape-Datei in den Workspace ein.");
-		setPageComplete(false);
-	}
+  //mapping
+  private HashMap mapping;
 
-	/**
-	 * @param pageName
-	 * @param title
-	 * @param titleImage
-	 */
-	public KalypsoNAFileImportPage(String pageName, String title,
-			ImageDescriptor titleImage) {
-		super(pageName, title, titleImage);
-		setDescription("Dieser Dialog liest eine ESRI Shape-Datei in den Workspace ein.");
-		setPageComplete(false);
-	}
+  /**
+   * @param pageName
+   */
+  public KalypsoNAFileImportPage( String pageName )
+  {
+    super( pageName );
+    setDescription( "Dieser Dialog liest eine ESRI Shape-Datei in den Workspace ein." );
+    setPageComplete( false );
+  }
 
-	private boolean checkSuffix(Text path) {
-		boolean test = false;
-		int dotLoc = path.getText().lastIndexOf('.');
-		if (dotLoc != -1) {
-			String ext = path.getText().substring(dotLoc + 1);
-			if (ext.equalsIgnoreCase("shp") == false)
-				test = false;
-			else
-				test = true;
-		}
-		return test;
-	}
+  /**
+   * @param pageName
+   * @param title
+   * @param titleImage
+   */
+  public KalypsoNAFileImportPage( String pageName, String title, ImageDescriptor titleImage )
+  {
+    super( pageName, title, titleImage );
+    setDescription( "Dieser Dialog liest eine ESRI Shape-Datei in den Workspace ein." );
+    setPageComplete( false );
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see wizard.eclipse.jface.dialogs.IDialogPage#createControl(wizard.eclipse.swt.widgets.Composite)
-	 */
-	public void createControl(Composite parent) {
+  private boolean checkSuffix( Text path )
+  {
+    boolean test = false;
+    int dotLoc = path.getText().lastIndexOf( '.' );
+    if( dotLoc != -1 )
+    {
+      String ext = path.getText().substring( dotLoc + 1 );
+      if( ext.equalsIgnoreCase( "shp" ) == false )
+        test = false;
+      else
+        test = true;
+    }
+    return test;
+  }
 
-		topComposite = new Composite(parent, SWT.NULL);
-		topComposite.setFont(parent.getFont());
+  /*
+   * (non-Javadoc)
+   * 
+   * @see wizard.eclipse.jface.dialogs.IDialogPage#createControl(wizard.eclipse.swt.widgets.Composite)
+   */
+  public void createControl( Composite parent )
+  {
 
-		initializeDialogUnits(parent);
+    topComposite = new Composite( parent, SWT.NULL );
+    topComposite.setFont( parent.getFont() );
 
-//		WorkbenchHelp.setHelp(topComposite,
-//				IHelpContextIds.NEW_PROJECT_WIZARD_PAGE);
+    initializeDialogUnits( parent );
 
-		topComposite.setLayout(new GridLayout());
-		topComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
-		// build wizard page
-		createFileGroup(topComposite);
-		setControl(topComposite);
+    //		WorkbenchHelp.setHelp(topComposite,
+    //				IHelpContextIds.NEW_PROJECT_WIZARD_PAGE);
 
-	}
+    topComposite.setLayout( new GridLayout() );
+    topComposite.setLayoutData( new GridData( GridData.FILL_BOTH ) );
+    // build wizard page
+    createFileGroup( topComposite );
+    setControl( topComposite );
 
-	private void createFileGroup(Composite parent) {
+  }
 
-		fileGroup = new Group(parent, SWT.NULL);
-		GridLayout topGroupLayout = new GridLayout();
-		GridData topGroupData = new GridData();
-		topGroupLayout.numColumns = 3;
-		topGroupData.horizontalAlignment = GridData.FILL;
-		fileGroup.setLayout(topGroupLayout);
-		fileGroup.setLayoutData(topGroupData);
-		fileGroup.setText("Shape-Datei");
+  private void createFileGroup( Composite parent )
+  {
 
-		fileLabel = new Label(fileGroup, SWT.NONE);
-		fileLabel.setText("Dateiname :");
+    fileGroup = new Group( parent, SWT.NULL );
+    GridLayout topGroupLayout = new GridLayout();
+    GridData topGroupData = new GridData();
+    topGroupLayout.numColumns = 3;
+    topGroupData.horizontalAlignment = GridData.FILL;
+    fileGroup.setLayout( topGroupLayout );
+    fileGroup.setLayoutData( topGroupData );
+    fileGroup.setText( "Shape-Datei" );
 
-		// Set width of Text fields
-		GridData dataCatchment = new GridData(GridData.FILL_HORIZONTAL);
-		dataCatchment.widthHint = SIZING_TEXT_FIELD_WIDTH;
+    fileLabel = new Label( fileGroup, SWT.NONE );
+    fileLabel.setText( "Dateiname :" );
 
-		textField = new Text(fileGroup, SWT.BORDER);
-		textField.setLayoutData(dataCatchment);
-		textField.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				validateFileField();
-			}
-		});
+    // Set width of Text fields
+    GridData dataCatchment = new GridData( GridData.FILL_HORIZONTAL );
+    dataCatchment.widthHint = SIZING_TEXT_FIELD_WIDTH;
 
-		browseButton = new Button(fileGroup, SWT.PUSH);
-		browseButton.setText("Durchsuchen...");
-		browseButton.setLayoutData(new GridData(GridData.END));
-		browseButton.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				//if (validateFileField())
-				handleFileBrowse();
-				validateFileField();
-			}
-		});
-		skipRadioButton = new Button(fileGroup, SWT.CHECK);
-		skipRadioButton.setText("Diese Datei einlesen");
-		skipRadioButton.setSelection(true);
-		skipRadioButton.addSelectionListener(new SelectionAdapter(){
-			public void widgetSelected(SelectionEvent e) {
-				Button w = (Button) e.widget;
-				if(!w.getSelection()) {
-					setPageComplete(true);
-					topSCLMappingComposite.setVisible(false);
-					buttonGroup.setVisible(false);
-					//remove mapping
-					mapping = null;
-				}
-				else {
-					setPageComplete(false);
-					topSCLMappingComposite.setVisible(true);
-					buttonGroup.setVisible(true);
-				}
-			}
+    textField = new Text( fileGroup, SWT.BORDER );
+    textField.setLayoutData( dataCatchment );
+    textField.addModifyListener( new ModifyListener()
+    {
+      public void modifyText( ModifyEvent e )
+      {
+        validateFileField();
+      }
+    } );
 
-		});
-		fileGroup.pack();
-		
+    browseButton = new Button( fileGroup, SWT.PUSH );
+    browseButton.setText( "Durchsuchen..." );
+    browseButton.setLayoutData( new GridData( GridData.END ) );
+    browseButton.addSelectionListener( new SelectionAdapter()
+    {
+      public void widgetSelected( SelectionEvent e )
+      {
+        //if (validateFileField())
+        handleFileBrowse();
+        validateFileField();
+      }
+    } );
+    skipRadioButton = new Button( fileGroup, SWT.CHECK );
+    skipRadioButton.setText( "Diese Datei einlesen" );
+    skipRadioButton.setSelection( true );
+    skipRadioButton.addSelectionListener( new SelectionAdapter()
+    {
+      public void widgetSelected( SelectionEvent e )
+      {
+        Button w = (Button)e.widget;
+        if( !w.getSelection() )
+        {
+          setPageComplete( true );
+          topSCLMappingComposite.setVisible( false );
+          buttonGroup.setVisible( false );
+          //remove mapping
+          mapping = null;
+        }
+        else
+        {
+          setPageComplete( false );
+          topSCLMappingComposite.setVisible( true );
+          buttonGroup.setVisible( true );
+        }
+      }
 
-	}
+    } );
+    fileGroup.pack();
 
-	
+  }
 
-	private FeatureType getSourceFT() {
-		Feature rootFeature = sourceWorkspace.getRootFeature();
+  private FeatureType getSourceFT()
+  {
+    Feature rootFeature = sourceWorkspace.getRootFeature();
 
-		FeatureType rootFT = rootFeature.getFeatureType();
-		FeatureAssociationTypeProperty ftp = (FeatureAssociationTypeProperty) rootFT
-				.getProperty("featureMember");
+    FeatureType rootFT = rootFeature.getFeatureType();
+    FeatureAssociationTypeProperty ftp = (FeatureAssociationTypeProperty)rootFT
+        .getProperty( "featureMember" );
 
-		FeatureType[] associationFeatureTypes = ftp
-				.getAssociationFeatureTypes();
-		FeatureType shapeFT = associationFeatureTypes[0];
-		return shapeFT;
-	}
+    FeatureType[] associationFeatureTypes = ftp.getAssociationFeatureTypes();
+    FeatureType shapeFT = associationFeatureTypes[0];
+    return shapeFT;
+  }
 
-	private void handelResetSelection() {
-		Control[] cArray = sourceGroup.getChildren();
-		for (int i = 0; i < cArray.length; i++) {
-			Combo combo = (Combo) cArray[i];
-			combo.setData(SOURCE_KEY, NULL_KEY);
-			combo.setRedraw(true);
-		}
-		sourceGroup.redraw();
-		setPageComplete(false);
-	}//handelRestSelection
+  private void handelResetSelection()
+  {
+    Control[] cArray = sourceGroup.getChildren();
+    for( int i = 0; i < cArray.length; i++ )
+    {
+      Combo combo = (Combo)cArray[i];
+      combo.setData( SOURCE_KEY, NULL_KEY );
+      combo.setRedraw( true );
+    }
+    sourceGroup.redraw();
+    setPageComplete( false );
+  }//handelRestSelection
 
-	/**
-	 * Uses the standard container selection dialog to choose the new value for
-	 * the container field.
-	 */
+  /**
+   * Uses the standard container selection dialog to choose the new value for
+   * the container field.
+   */
 
-	private void handleFileBrowse() {
-		FileDialog fdialog = new FileDialog(getShell(), SWT.OPEN | SWT.SINGLE);
-		fdialog.setFilterExtensions(new String[] { "shp" });
-		fdialog.setText("W‰hlen Sie eine ESRI Arc-View Shapedatei aus:");
-		fdialog
-				.setFilterNames(new String[] { "Shape Files", "All Files (*.*)" });
-		fdialog.setFilterExtensions(new String[] { "*.shp", "*.*" });
-		fdialog.setFileName("*.shp");
-		if (fdialog.open() != null) {
-			String textStr;
-			try {
+  private void handleFileBrowse()
+  {
+    FileDialog fdialog = new FileDialog( getShell(), SWT.OPEN | SWT.SINGLE );
+    fdialog.setFilterExtensions( new String[]
+    { "shp" } );
+    fdialog.setText( "W‰hlen Sie eine ESRI Arc-View Shapedatei aus:" );
+    fdialog.setFilterNames( new String[]
+    {
+        "Shape Files",
+        "All Files (*.*)" } );
+    fdialog.setFilterExtensions( new String[]
+    {
+        "*.shp",
+        "*.*" } );
+    fdialog.setFileName( "*.shp" );
+    if( fdialog.open() != null )
+    {
+      String textStr;
+      try
+      {
 
-				fileURL = (new File(fdialog.getFilterPath() + File.separator
-						+ fdialog.getFileName())).toURL();
-				textStr = fileURL.toExternalForm();
-				textField.setText(textStr);
-				//copyShapeFile(fileURL);
+        fileURL = ( new File( fdialog.getFilterPath() + File.separator + fdialog.getFileName() ) )
+            .toURL();
+        textStr = fileURL.toExternalForm();
+        textField.setText( textStr );
+        //copyShapeFile(fileURL);
 
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			}
+      }
+      catch( MalformedURLException e )
+      {
+        e.printStackTrace();
+      }
 
-		}//if fdialog
-	}//handleFileBrowse
+    }//if fdialog
+  }//handleFileBrowse
 
-	public boolean validateFile(URL url) {
-		try {
-			InputStream ios = url.openStream();
-			ios.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
+  public boolean validateFile( URL url )
+  {
+    try
+    {
+      InputStream ios = url.openStream();
+      ios.close();
+    }
+    catch( IOException e )
+    {
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
 
-	private boolean validateFileField() {
-		//	 checks catchment field entry and file suffix
-		if (textField.getText().length() == 0) {
-			setErrorMessage("Bitte eine Datei ausw‰hlen!");
-			setPageComplete(false);
-			return false;
-		} else if (checkSuffix(textField) == false) {
-			setErrorMessage("Falscher Suffix Datei, muss \"shp\" sein!");
-			setPageComplete(false);
-			return false;
-		} else if (validateFile(fileURL) == false) {
-			setErrorMessage("Gew‰hlte Shape-Datei nicht g¸ltg");
-			setPageComplete(false);
-			return false;
-		}
-		setPageComplete(true);
-		setErrorMessage(null);
-		setMessage(null);
-		return true;
-	}
+  private boolean validateFileField()
+  {
+    //	 checks catchment field entry and file suffix
+    if( textField.getText().length() == 0 )
+    {
+      setErrorMessage( "Bitte eine Datei ausw‰hlen!" );
+      setPageComplete( false );
+      return false;
+    }
+    else if( checkSuffix( textField ) == false )
+    {
+      setErrorMessage( "Falscher Suffix Datei, muss \"shp\" sein!" );
+      setPageComplete( false );
+      return false;
+    }
+    else if( validateFile( fileURL ) == false )
+    {
+      setErrorMessage( "Gew‰hlte Shape-Datei nicht g¸ltg" );
+      setPageComplete( false );
+      return false;
+    }
+    setPageComplete( true );
+    setErrorMessage( null );
+    setMessage( null );
+    return true;
+  }
 
-	public GMLWorkspace getSourceWorkspace() {
-		return sourceWorkspace;
-	}
+  public GMLWorkspace getSourceWorkspace()
+  {
+    return sourceWorkspace;
+  }
 
-	public URL getFileURL() {
-		return fileURL;
-	}
-	public CS_CoordinateSystem getCoordinateSystem(){
-		if(customCS != null)
-			return customCS;
-		else return defaultCS;
-	}
+  public URL getFileURL()
+  {
+    return fileURL;
+  }
+
+  public CS_CoordinateSystem getCoordinateSystem()
+  {
+    if( customCS != null )
+      return customCS;
+    else
+      return defaultCS;
+  }
 }
