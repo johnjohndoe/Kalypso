b9f80db1e8eb9de25af2e94dfd531d17523c0c9e
belger <belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-03-09 10:57:28 +0000

    New converter testclass

---
+++ _import/trunk/KalypsoSachsenAnhalt/KalypsoModelSaale/src/org/kalypso/lhwsachsenanhalt/saale/HWVOR00ConvertTest.java
@@ -0,0 +1,71 @@
+package org.kalypso.lhwsachsenanhalt.saale;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+
+import org.kalypso.ogc.sensor.IObservation;
+import org.kalypso.ogc.sensor.SensorException;
+import org.kalypso.ogc.sensor.timeseries.TimeserieConstants;
+
+import junit.framework.TestCase;
+
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+
+public class HWVOR00ConvertTest extends TestCase
+{
+  public void testhwvor002zml() throws ParseException, IOException
+  {
+    final InputStream is = HWVOR00ConvertTest.class.getResourceAsStream( "resources/TLDAT.OLD" );
+    final InputStreamReader reader = new InputStreamReader( is );
+    
+    final IObservation[] observations = HWVOR00Converter.hwvor002zml( TimeserieConstants.TYPE_RAINFALL, reader );
+    
+    FileWriter out = new FileWriter( "C:\\test.txt" );
+    HWVOR00Converter conv = new HWVOR00Converter();
+    conv.addObservation(observations[0], "date", "N");
+    conv.addObservation(observations[1], "date", "N");
+    conv.toHWVOR00(out);
+  }
+}
