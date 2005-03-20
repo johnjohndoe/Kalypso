143805d5ba7624f324b61dd7212ff88385059200
belger <belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-03-20 14:56:22 +0000

    *** empty log message ***

--- KalypsoDeegree/deegree/org/kalypsodeegree_impl/graphics/sld/Rule_Impl.java
+++ KalypsoDeegree/deegree/org/kalypsodeegree_impl/graphics/sld/Rule_Impl.java
@@ -60,10 +60,6 @@
 ---------------------------------------------------------------------------------------------------*/
 package org.kalypsodeegree_impl.graphics.sld;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.ArrayList;
 
 import org.kalypsodeegree.filterencoding.Filter;
