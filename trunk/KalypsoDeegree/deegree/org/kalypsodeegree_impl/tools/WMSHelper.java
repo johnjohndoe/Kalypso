7b9f280224a9d5bc7e43fcf1ca3cf7aad0d0ac80
doemming <doemming@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-04-08 16:15:13 +0000

    *** empty log message ***

--- trunk/KalypsoDeegree/deegree/org/kalypsodeegree_impl/tools/WMSHelper.java
+++ trunk/KalypsoDeegree/deegree/org/kalypsodeegree_impl/tools/WMSHelper.java
@@ -1,14 +1,14 @@
 package org.kalypsodeegree_impl.tools;
 
-import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 
-import org.deegree.services.wms.capabilities.Capability;
 import org.deegree.services.wms.capabilities.Layer;
 import org.deegree.services.wms.capabilities.WMSCapabilities;
+import org.kalypsodeegree_impl.model.cs.ConvenienceCSFactory;
 import org.opengis.cs.CS_CoordinateSystem;
 
 /*----------------    FILE HEADER KALYPSO ------------------------------------------
@@ -52,55 +52,96 @@ import org.opengis.cs.CS_CoordinateSystem;
  *   
  *  ---------------------------------------------------------------------------*/
 
+/**
+ * This class contains helper methods to handle wms specific tasks.
+ * 
+ * @author c.kuepferle@tuhh.de
+ *  
+ */
 public class WMSHelper
 {
-  private static final WMSHelper m_helper = new WMSHelper();
-
-  public static WMSHelper getInstance()
+  /**
+   * This method tries to find a common spatial reference system (srs) for a
+   * given set of layers. If all layers coorespond to the local crs the local
+   * crs is returned, otherwise the srs of the top layer is returned and the
+   * client must choose one to transform it to the local coordiante system
+   * 
+   * @param localCRS
+   *          the local spatial reference system
+   * @param capabilities
+   *          the capabilites document of the web map service
+   * @param layerNames
+   *          the layers that have to be matched to the local srs
+   * @return result an array of possible coordiante systems
+   */
+  public static CS_CoordinateSystem[] negotiateCRS( CS_CoordinateSystem localCRS,
+      WMSCapabilities capabilities, String[] layerNames ) throws Exception
   {
-    return m_helper;
+    Layer topLayer = capabilities.getCapability().getLayer();
+    CS_CoordinateSystem crs = matchCrs( topLayer, layerNames, localCRS );
+    if( crs != null )
+      return new CS_CoordinateSystem[]
+      {
+        localCRS
+      };
+    //get crs from top layer
+    String[] topLayerSRS = topLayer.getSrs();
+    List result = new ArrayList();
+    for( int i = 0; i < topLayerSRS.length; i++ )
+    {
+      result.add( ConvenienceCSFactory.getInstance().getOGCCSByName( topLayerSRS[i] ) );
+    }
+    return (CS_CoordinateSystem[])result.toArray( new CS_CoordinateSystem[result.size()] );
   }
 
-  public CS_CoordinateSystem negotiateCRS( CS_CoordinateSystem localCRS,
-      WMSCapabilities capabiliets, String layerName ) throws Exception
+  /**
+   * This method tries to match the local coordiante system to a given layer
+   * selection.
+   * 
+   * @param topLayer
+   *          the top layer of the layer structur of a web map service
+   * @param layerSelection
+   *          layers to be matched
+   * @param localCRS
+   *          the local coordinate system
+   * @return returns null if one element of the layers to be matched is not
+   *         available in the local coordinate system, otherwise it returns the
+   *         local crs
+   *  
+   */
+
+  private static CS_CoordinateSystem matchCrs( Layer topLayer, String[] layerSelection,
+      CS_CoordinateSystem localCRS ) throws Exception
   {
-    Layer topLayer = capabiliets.getCapability().getLayer();
-    String[] topSRS = topLayer.getSrs();
-    for( int i = 0; i < topSRS.length; i++ )
+    HashSet collector = new HashSet();
+
+    collect( collector, topLayer, layerSelection );
+    for( Iterator iter = collector.iterator(); iter.hasNext(); )
     {
-      String crs = topSRS[i];
-      if( crs.equals( localCRS.getName() ) )
-        return localCRS;
+      Layer layer = (Layer)iter.next();
+      String[] layerSRS = layer.getSrs();
+      if( contains( layerSRS, localCRS.getName() ) )
+        continue;
+      return null;
 
     }
-    matchCrs( topLayer.getLayer(), localCRS );
-    return null;
+    return localCRS;
   }
 
-  private void matchCrs( Layer[] layer, CS_CoordinateSystem localCRS ) throws Exception
-  {
-    for( int i = 0; i < layer.length; i++ )
-    {
-      Layer newLayer = layer[i];
-      if( newLayer.getLayer().length > 0 )
-      {
-        //recursive function call
-        matchCrs( newLayer.getLayer(), localCRS );
-      }
-      else
-      {
-
-      }
-
-    }
-  }
-
-  public void getAllLayers( WMSCapabilities capabilites, Set set )
+  /**
+   * This method collects all layers from a capabilites document.
+   * 
+   * @param capabilites
+   *          wms capabilites document
+   * @param set
+   *          the Set where the layers are collected in
+   */
+  public static void getAllLayers( WMSCapabilities capabilites, Set set )
   {
     try
     {
       Layer topLayer = capabilites.getCapability().getLayer();
-      collect( set, topLayer, false );
+      collect( set, topLayer, null );
     }
     catch( Exception e )
     {
@@ -108,8 +149,23 @@ public class WMSHelper
     }
   }
 
-  private void collect( Set collector, Layer layer, boolean levelsOnly ) throws Exception
+  /**
+   * This method collects all layers (or the specified layers) from the top
+   * layer of a WMSCapabilites document. If the parameter layerSeletion is empty
+   * or null the method collects all layers, otherwise returns all layers with
+   * the same name as in the layerSelection.
+   * 
+   * @param collector
+   *          The set that collects the layers found.
+   * @param layer
+   *          the top layer of the wms capabilites document.
+   * @param layerSelection
+   *          an array of layer names to search for.
+   */
+  private static void collect( Set collector, Layer layer, String[] layerSelection )
+      throws Exception
   {
+
     Layer[] layerTree = layer.getLayer();
     for( int i = 0; i < layerTree.length; i++ )
     {
@@ -117,27 +173,43 @@ public class WMSHelper
       if( newLayer.getLayer().length > 0 )
       {
         //recursive function call
-        collect( collector, newLayer, levelsOnly );
+        collect( collector, newLayer, layerSelection );
       }
       else
       {
-        System.out.println( layerTree[i].getName() );
-        collector.add( layerTree[i] );
+        //System.out.println( layerTree[i].getName() );
+        if( layerSelection != null  )
+        {
+
+          if( contains( layerSelection, layerTree[i].getName() ) )
+          {
+            collector.add( layerTree[i] );
+          }
+        }
+        else
+          collector.add( layerTree[i] );
       }
       continue;
     }
   }
 
-  public void getLevels( Set list, WMSCapabilities capabilites ) throws Exception
-  {
-    Layer topLayer = capabilites.getCapability().getLayer();
-    list.add( topLayer );
-    collect( list, topLayer, true );
-  }
+  /**
+   * This method checks an array of Strings for a given String to match.
+   * 
+   * @param array
+   *          strings to check for a match.
+   * @param toMatch
+   *          the string to match
+   * @return boolean true if the String is the array, false otherwise
+   */
 
-  private Layer getParent( String layerName, WMSCapabilities capabilities )
+  public static boolean contains( String[] array, String toMatch )
   {
-
-    return null;
+    for( int i = 0; i < array.length; i++ )
+    {
+      if( array[i].equals( toMatch ) )
+        return true;
+    }
+    return false;
   }
 }
