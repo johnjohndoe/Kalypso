5bd24748ff8d62fccc9eee384810bfcbb4846501
doemming <doemming@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-05-01 22:53:56 +0000

    *** empty log message ***

--- _import/trunk/obsolete/KalypsoUtil/src/org/kalypso/util/parser/impl/test/DoubleParserTest.java
+++ _import/trunk/obsolete/KalypsoUtil/src/org/kalypso/util/parser/impl/test/DoubleParserTest.java
@@ -36,8 +36,8 @@
  belger@bjoernsen.de
  schlienger@bjoernsen.de
  v.doemming@tuhh.de
-  
----------------------------------------------------------------------------------------------------*/
+ 
+ ---------------------------------------------------------------------------------------------------*/
 package org.kalypso.util.parser.impl.test;
 
 import junit.framework.TestCase;
@@ -54,10 +54,10 @@ public class DoubleParserTest extends TestCase
   public void testParse()
   {
     try
-    {                     
-      test(".101",0.101);
-      test("10.10",10.1);
-      test("10",10);
+    {
+      test( ".101", 0.101 );
+      test( "10.10", 10.1 );
+      test( "10", 10 );
     }
     catch( Exception e )
     {
@@ -65,14 +65,14 @@ public class DoubleParserTest extends TestCase
     }
   }
 
-  private void test(String value,double result) throws ParserException
+  private void test( String value, double result ) throws ParserException
   {
     DoubleParser parser = new DoubleParser();
-    Double object = (Double)parser.parse(value);
-    
-    System.out.println(value+" -> "+object.getClass().getName()+": "+object.toString());
-    String string = parser.toStringInternal(object);
-    System.out.println("= "+string+"\n\n");
-    assertTrue(object.doubleValue()==result);   
+    Double object = (Double)parser.parse( value );
+
+    System.out.println( value + " -> " + object.getClass().getName() + ": " + object.toString() );
+    String string = parser.toStringInternal( object );
+    System.out.println( "= " + string + "\n\n" );
+    assertTrue( object.doubleValue() == result );
   }
 }
