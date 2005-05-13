b47ba6104d8a00db9ee182c0c7f1812423ac7dd1
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-05-13 11:01:47 +0000

    *** empty log message ***

---
+++ _import/trunk/KalypsoSachsenAnhalt/KalypsoConfSachsenAnhalt/deployable-tomcatstuff/bin/kalypso_server_start.bat
@@ -0,0 +1,15 @@
+set KALYPSO_SERVER_HOME=d:\kalypso
+
+set JAVA_HOME=%KALYPSO_SERVER_HOME%\j2sdk1.4.2_06
+set CATALINA_HOME=%KALYPSO_SERVER_HOME%\Tomcat5.0
+
+set KALYPSO_SERVER_TMP=%CATALINA_HOME%\temp
+set KALYPSO_SERVER_WEBDAV=%CATALINA_HOME%\webapps\webdav
+set KALYPSO_SERVER_CONF=%KALYPSO_SERVER_WEBDAV%\srvconf
+set KALYPSO_SERVER_DATA=%KALYPSO_SERVER_WEBDAV%\data
+
+set JAVA_OPTS=-Xmx512m -Dkalypso.server.tempdir=%KALYPSO_SERVER_TMP% -Dkalypso.server.confdir=%KALYPSO_SERVER_CONF% -Dkalypso.server.datadir=%KALYPSO_SERVER_TEMP%
+
+rem set CATALINA_OPTS=-Djava.security.debug=help
+
+catalina.bat run -security 1> %CATALINA_HOME%/logs/kalypso.log 2> %CATALINA_HOME%/logs/kalypso.err
