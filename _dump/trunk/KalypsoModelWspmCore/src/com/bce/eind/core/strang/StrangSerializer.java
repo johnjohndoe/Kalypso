69ba1bc59da6c3bfa31c1fb8c4c479eaccdb1c80
belger <belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-06-09 14:51:03 +0000

    Jetzt ist ne Anwendung! Neues Feature-Projekt und Deploy-Projekt.

--- _dump/trunk/KalypsoModelWspmCore/src/com/bce/eind/core/strang/StrangSerializer.java
+++ _dump/trunk/KalypsoModelWspmCore/src/com/bce/eind/core/strang/StrangSerializer.java
@@ -1,5 +1,7 @@
 package com.bce.eind.core.strang;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.Collection;
@@ -7,9 +9,10 @@ import java.util.LinkedList;
 import java.util.Locale;
 import java.util.Scanner;
 
+import org.apache.commons.io.IOUtils;
+
 /**
  * @author gernot
- *
  */
 public class StrangSerializer
 {
@@ -17,46 +20,75 @@ public class StrangSerializer
   {
   }
 
-  public static StrangInfo load( final LineNumberReader lnr, final String basepath ) throws IOException
+  /**
+   * Liest eine .str Datei in einen StrangInfo.
+   * 
+   * @param strpath
+   *          Pfad auf die .str Datei
+   * @param prfname
+   *          Falls ungleich null, wird der Anfangsindex auf diese Profildatei gesetzt, sonst auf
+   *          den ersten.
+   * @throws IOException
+   * @throws IOException
+   */
+  public static StrangInfo load( final String strpath, final String prfname ) throws IOException
   {
-    final Collection<ProfilInfo> infos = new LinkedList<ProfilInfo>();
-    
-    final Scanner s = new Scanner( lnr );
-    // use decimal points
-    s.useLocale( Locale.US );
-    
-    final int profilcount = s.nextInt();
-    /* final int strangcount = */ s.nextInt();
-    
-    /* final String gewname = */ s.next();
-    /* final String strangname = */ s.next();
-    
-    for( int i = 0; i < profilcount; i++ )
+    LineNumberReader lnr = null;
+    try
     {
-      final IOException exception = s.ioException();
-      if( exception != null )
-        throw exception;
-      
-      // Gewässername
-      s.next();
-      
-      final String station = s.next();
+      final File strFile = new File( strpath );
+      lnr = new LineNumberReader( new FileReader( strFile ) );
 
-      // VZK
-      s.next();
-      
-      // MFB
-      s.next();
-      
-      // Zustandsname
-      s.next();
-      
-      final String filename = s.next();
-      
-      infos.add( new ProfilInfo( station, basepath + filename ) );
+      final File profDir = strFile.getParentFile();
+      final File prfSollFile = new File( profDir, prfname );
+
+      final Collection<ProfilInfo> infos = new LinkedList<ProfilInfo>();
+
+      final Scanner s = new Scanner( lnr );
+      // use decimal points
+      s.useLocale( Locale.US );
+
+      final int profilcount = s.nextInt();
+      /* final int strangcount = */s.nextInt();
+
+      /* final String gewname = */s.next();
+      /* final String strangname = */s.next();
+      int index = 0; // index to set the StrangInfo to
+      for( int i = 0; i < profilcount; i++ )
+      {
+        final IOException exception = s.ioException();
+        if( exception != null )
+          throw exception;
+
+        // Gewässername
+        s.next();
+
+        final String station = s.next();
+
+        // VZK
+        s.next();
+
+        // MFB
+        s.next();
+
+        // Zustandsname
+        s.next();
+
+        final String filename = s.next();
+
+        final File prfFile = new File( profDir, filename );
+        if( prfFile.equals( prfSollFile ) )
+          index = i;
+
+        infos.add( new ProfilInfo( station, prfFile.getAbsolutePath() ) );
+      }
+
+      final ProfilInfo[] pinfos = infos.toArray( new ProfilInfo[infos.size()] );
+      return new StrangInfo( pinfos, index );
     }
-    
-    final ProfilInfo[] pinfos = infos.toArray( new ProfilInfo[infos.size()] );
-    return new StrangInfo( pinfos, 0 );
+    finally
+    {
+      IOUtils.closeQuietly( lnr );
+    }
   }
 }
