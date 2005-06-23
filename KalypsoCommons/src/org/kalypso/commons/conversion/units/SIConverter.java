b6f72941d62e6d008dc26dfc3bfba43bf1188073
belger <belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-06-23 14:25:17 +0000

    Projekt neu angelegt

---
+++ KalypsoCommons/src/org/kalypso/commons/conversion/units/SIConverter.java
@@ -0,0 +1,61 @@
+package org.kalypso.commons.conversion.units;
+
+/**
+ * SIConverter
+ * 
+ * @author schlienger
+ */
+public class SIConverter implements IValueConverter
+{
+  private final double m_factor;
+
+  public SIConverter( final String orgUnit, final String destUnit )
+  {
+    final double orgf = factor( orgUnit );
+    final double destf = factor( destUnit );
+
+    m_factor = orgf / destf;
+  }
+
+  /**
+   * @see org.kalypso.commons.conversion.units.IValueConverter#convert(double)
+   */
+  public double convert( double value )
+  {
+    return value * m_factor;
+  }
+
+  /**
+   * @see org.kalypso.commons.conversion.units.IValueConverter#reverse(double)
+   */
+  public double reverse( double value )
+  {
+    return value / m_factor;
+  }
+
+  /**
+   * TODO: complete the list!
+   * 
+   * Returns the factor associated to the given unit
+   * 
+   * @param unit
+   * @return factor
+   */
+  private final static double factor( final String unit )
+  {
+    // BEWARE: 10^-x does not give the right value
+    if( unit.equals( "m" ) )
+      return 1;
+    if( unit.equals( "dm" ) )
+      return 0.1;
+    if( unit.equals( "cm" ) )
+      return 0.01;
+    if( unit.equals( "mm" ) )
+      return 0.001;
+
+    if( unit.equals( "m³" ) )
+      return 1;
+
+    return 1;
+  }
+}
