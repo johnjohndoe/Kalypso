58ebbe7b8da7fca30045cdaa2eb657a4ec43c74d
belger <belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-06-30 07:52:15 +0000

    Yellow thingis down to 250

--- temp/trunk/KalypsoDWD/src/org/kalypso/dwd/test/ForecastGeneratorTest.java
+++ temp/trunk/KalypsoDWD/src/org/kalypso/dwd/test/ForecastGeneratorTest.java
@@ -42,9 +42,7 @@ package org.kalypso.dwd.test;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStream;
-import java.text.ParseException;
 
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
@@ -53,10 +51,7 @@ import junit.framework.TestCase;
 
 import org.apache.commons.io.CopyUtils;
 import org.apache.commons.io.FileUtils;
-import org.kalypso.dwd.DWDRaster;
-import org.kalypso.dwd.DWDRasterGeoLayer;
 import org.kalypso.dwd.ForecastGenerator;
-import org.kalypso.dwd.RasterStorage;
 import org.kalypso.dwd.dwdzml.DwdzmlConf;
 import org.kalypso.dwd.dwdzml.ObjectFactory;
 
