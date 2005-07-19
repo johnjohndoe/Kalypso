34aefe57ea51d62b48490391172723f1eda10826
kuepfer <kuepfer@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-07-19 11:17:21 +0000

    *** empty log message ***

--- KalypsoGridInterpolation/src/org/kalypso/interpolation/KalypsoGridTools.java
+++ KalypsoGridInterpolation/src/org/kalypso/interpolation/KalypsoGridTools.java
@@ -1,13 +1,15 @@
 /*
  * Created on 05.01.2005
  * 
- * TODO To change the template for this generated file go to Window -
- * Preferences - Java - Code Style - Code Templates
+ * TODO To change the template for this generated file go to Window - Preferences - Java - Code Style - Code Templates
  */
 package org.kalypso.interpolation;
 
+import java.io.EOFException;
 import java.io.File;
+import java.io.IOException;
 
+import org.kalypso.interpolation.grid.Grid;
 import org.kalypso.interpolation.grid.GridFactory;
 import org.kalypso.interpolation.grid.IGrid;
 import org.kalypso.interpolation.mesh.Mesh;
@@ -15,47 +17,196 @@ import org.kalypso.interpolation.mesh.MeshFactory;
 import org.kalypso.interpolation.mesh.MeshReader;
 import org.kalypsodeegree.model.feature.Feature;
 import org.kalypsodeegree.model.geometry.GM_Envelope;
+import org.kalypsodeegree.model.geometry.GM_Position;
 import org.kalypsodeegree.model.geometry.GM_Surface;
+import org.kalypsodeegree_impl.model.geometry.GeometryFactory;
 import org.opengis.cs.CS_CoordinateSystem;
 
 /**
  * @author kuepfer
  * 
- * TODO To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Style - Code Templates
+ * TODO To change the template for this generated type comment go to Window - Preferences - Java - Code Style - Code
+ * Templates
  */
 public class KalypsoGridTools implements IKalyposGridTools
 {
   private static final MeshReader reader = new MeshReader();
 
+  public KalypsoGridTools()
+  {};
+
   /*
    * (non-Javadoc)
    * 
    * @see org.kalypso.interpolation.IKalyposGridTools#interpolateGrid(org.deegree.model.feature.Feature[],
-   *      org.deegree.model.feature.Feature[], java.lang.String,
-   *      java.lang.String, java.lang.String, double,
+   *      org.deegree.model.feature.Feature[], java.lang.String, java.lang.String, java.lang.String, double,
    *      org.deegree.model.geometry.GM_Envelope, java.lang.String)
    */
-  public void interpolateGrid( Feature[] meshElements, Feature[] nodes,
-      String geometryPropertyElement, String geometryPropertyPoint,
-      String valueProperty, double cellsize, GM_Envelope gridsize,
-      GM_Surface wishbox, GM_Surface polyline, CS_CoordinateSystem crs, File out )
-      throws Exception
+  public void interpolateGrid( Feature[] meshElements, Feature[] nodes, String geometryPropertyElement,
+      String geometryPropertyPoint, String valueProperty, double cellsize, GM_Envelope gridsize, GM_Surface wishbox,
+      GM_Surface polyline, CS_CoordinateSystem crs, File out ) throws Exception
   {
-    CS_CoordinateSystem cs = meshElements[0].getDefaultGeometryProperty()
-        .getCoordinateSystem();
-
-    Mesh mesh = MeshFactory.getInstance().getMesh( cs );
+    Mesh mesh = MeshFactory.getInstance().getMesh( crs );
     //read mesh
-    reader.importMesh( mesh, nodes, geometryPropertyPoint, valueProperty,
-        meshElements, geometryPropertyElement, wishbox );
-    IGrid grid = GridFactory.getInstance().createGrid( wishbox.getEnvelope(),
-        crs, cellsize, mesh );
+    reader.importMesh( mesh, nodes, geometryPropertyPoint, valueProperty, meshElements, geometryPropertyElement,
+        wishbox );
+    IGrid grid = GridFactory.getInstance().createGrid( wishbox.getEnvelope(), crs, cellsize, mesh );
     long st = System.currentTimeMillis();
-    mesh.interpolateGrid( out, polyline, grid );
+    mesh.interpolateGrid( grid );
+    grid.export( out );
     long dur = System.currentTimeMillis() - st;
     System.out.print( " in " + dur + " ms." );
 
+  }
+
+  public IGrid interpolateGrid( File[] inputFiles, CS_CoordinateSystem cs, GM_Envelope bbox, String borderPath,
+      double size )
+  {
+
+    System.out.println( "\n" + "Importing input files..started" );
+    long startTime = System.currentTimeMillis();
+    //creates mesh out of input files and set name
+    IGrid grid = null;
+    try
+    {
+      MeshFactory mf = MeshFactory.getInstance();
+
+      GM_Surface surfaceBBox = null;
+      if( bbox != null )
+        surfaceBBox = GeometryFactory.createGM_Surface( bbox, cs );
+      Mesh mesh = mf.readMesh( inputFiles, cs, surfaceBBox, borderPath );
+
+      long currentTime1 = System.currentTimeMillis();
+      System.out.println( "\n" + "total importing duration (in seconds) : " + ( currentTime1 - startTime ) / 1000 );
+      //create grid
+      grid = GridFactory.getInstance().createGrid( bbox, cs, size, mesh );
+      System.out.println( "\n" + "Interpolate Mesh: " + mesh.getMeshName() + " into Grid: " + grid.getGridID() );
+      //interpolate grid
+      mesh.interpolateGrid( grid );
+      long currentTime2 = System.currentTimeMillis();
+      System.out.println( "\n" + "total interpolation duration (in seconds) : " + ( currentTime2 - currentTime1 )
+          / 1000 );
+      System.out.println( "\n" + "total duration (in seconds) : " + ( currentTime2 - startTime ) / 1000 );
+    }
+    catch( Exception e )
+    {
+      e.printStackTrace();
+    }
+
+    return grid;
+  }
+
+  /**
+   * @param grid1
+   * 
+   * @param grid2
+   * 
+   * @see org.kalypso.interpolation.IKalyposGridTools#subtract(org.kalypso.interpolation.grid.IGrid,
+   *      org.kalypso.interpolation.grid.IGrid)
+   */
+  public IGrid subtract( IGrid grid1, IGrid grid2 )
+  {
+    if( grid1 == null && grid2 != null )
+      return grid2;
+    if( grid1 != null && grid2 == null )
+      return grid1;
+    if( grid1 == null && grid2 == null )
+      return null;
+    GM_Position llc1 = grid1.getOrigin();
+    GM_Position llc2 = grid2.getOrigin();
+    double nodata = Double.parseDouble( IGrid.DEFAULT_NO_DATA );
+    double cell1 = grid1.getCellSize();
+    double cell2 = grid2.getCellSize();
+    int maxRows1 = grid1.getRows();
+    int maxRows2 = grid2.getRows();
+    int maxCols1 = grid1.getCols();
+    int maxCols2 = grid2.getCols();
+    GM_Envelope env1 = grid1.getExtend().getEnvelope();
+    GM_Envelope env2 = grid2.getExtend().getEnvelope();
+    GM_Position check = GeometryFactory.createGM_Position( llc2.getX() + cell2 / 2, llc2.getY() + cell2 / 2 );
+    CS_CoordinateSystem cs1 = grid1.getCoordinateSystem();
+    CS_CoordinateSystem cs2 = grid2.getCoordinateSystem();
+    boolean coincides = grid1.isPointOnGrid( check, false );
+    int row = 0;
+    int col = 0;
+    if( cs1.equals( cs2 ) && cell1 == cell2 && ( llc1.equals( llc2 ) || coincides ) )
+    {
+      GM_Envelope mergedEnv = env1.getMerged( env2 );
+      IGrid grid = null;
+      try
+      {
+        grid = GridFactory.getInstance().createGrid( mergedEnv, grid1.getCoordinateSystem(), cell1, null );
+        int maxRows = grid.getRows();
+        int maxCols = grid.getCols();
+        for( row = 0; row < maxRows; row++ )
+        {
+          for( col = 0; col < maxCols; col++ )
+          {
+            if( row == 19 && col == 20 )
+              System.out.print( "rowAndCols" );
+            //            {
+            //              ( (Grid)grid1 ).writeGridValue( row, col, 50000d );
+            //
+            //              System.out.println( ( (Grid)grid1 ).readGridValue( row, col ) );
+            //              System.out.println( ( (Grid)grid1 ).getPosition( row, col ) );
+            //
+            //            }
+            if( row > maxRows1 || row > maxRows2 || col > maxCols1 || col > maxCols2 )
+              ( (Grid)grid ).writeGridValue( row, col, nodata );
+            else
+            {
+              double value1 = ( (Grid)grid1 ).readGridValue( row, col );
+              double value2 = ( (Grid)grid2 ).readGridValue( row, col );
+              double value = value1 - value2;
+              if( value1 != nodata && value2 != nodata )
+                ( (Grid)grid ).writeGridValue( row, col, value );
+              else
+                ( (Grid)grid ).writeGridValue( row, col, nodata );
+            }
+          }
+        }
+        return grid;
+      }
+      catch( EOFException e )
+      {
+        System.out.println( "error in row: " + row + "\tcol: " + col );
+        return null;
+      }
+      catch( IOException e )
+      {
+        e.printStackTrace();
+        return null;
+      }
+      catch( Exception e )
+      {
+        e.printStackTrace();
+        return null;
+      }
+    }
+    else
+      return null;
+  }
+
+  /**
+   * @see org.kalypso.interpolation.IKalyposGridTools#add(org.kalypso.interpolation.grid.IGrid,
+   *      org.kalypso.interpolation.grid.IGrid)
+   */
+  public IGrid add( IGrid grid1, IGrid grid2 )
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /**
+   * @throws IOException
+   * @throws Exception
+   * @throws
+   * @throws IOException
+   * @see org.kalypso.interpolation.IKalyposGridTools#importGrid(java.io.File)
+   */
+  public IGrid importGrid( File file, CS_CoordinateSystem cs ) throws Exception
+  {
+    return GridFactory.getInstance().importESRIasc( file, cs );
   }
 
 }
--- KalypsoGridInterpolation/src/org/kalypso/interpolation/MainInterpolate.java
+++ KalypsoGridInterpolation/src/org/kalypso/interpolation/MainInterpolate.java
@@ -1,23 +1,15 @@
 /*
  * Created on 14.12.2004
  * 
- * TODO To change the template for this generated file go to Window -
- * Preferences - Java - Code Style - Code Templates
+ * TODO To change the template for this generated file go to Window - Preferences - Java - Code Style - Code Templates
  */
 package org.kalypso.interpolation;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.kalypso.interpolation.grid.GridFactory;
 import org.kalypso.interpolation.grid.IGrid;
-import org.kalypso.interpolation.mesh.Mesh;
-import org.kalypso.interpolation.mesh.MeshFactory;
 import org.kalypsodeegree.model.geometry.GM_Envelope;
-import org.kalypsodeegree.model.geometry.GM_Surface;
 import org.kalypsodeegree_impl.model.cs.ConvenienceCSFactory;
 import org.kalypsodeegree_impl.model.geometry.GeometryFactory;
 import org.opengis.cs.CS_CoordinateSystem;
@@ -25,8 +17,8 @@ import org.opengis.cs.CS_CoordinateSystem;
 /**
  * @author kuepfer
  * 
- * TODO To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Style - Code Templates
+ * TODO To change the template for this generated type comment go to Window - Preferences - Java - Code Style - Code
+ * Templates
  */
 public class MainInterpolate
 {
@@ -83,8 +75,7 @@ public class MainInterpolate
         cs = ConvenienceCSFactory.getInstance().getOGCCSByName( str );
         if( cs == null )
         {
-          System.out.println( "Das Koordinatensystem: " + args[i]
-              + " wird nicht unterstützt" );
+          System.out.println( "Das Koordinatensystem: " + args[i] + " wird nicht unterstützt" );
           System.exit( 0 );
         }
         i++;
@@ -93,14 +84,12 @@ public class MainInterpolate
       {
         try
         {
-          bbox = GeometryFactory.createGM_Envelope( Double
-              .parseDouble( args[++i] ), Double.parseDouble( args[++i] ),
+          bbox = GeometryFactory.createGM_Envelope( Double.parseDouble( args[++i] ), Double.parseDouble( args[++i] ),
               Double.parseDouble( args[++i] ), Double.parseDouble( args[++i] ) );
         }
         catch( NumberFormatException e )
         {
-          System.out
-              .println( "Format Fehler: bbox-werte müssen \"doubles\" sein" );
+          System.out.println( "Format Fehler: bbox-werte müssen \"doubles\" sein" );
           System.exit( 0 );
         }
         i++;
@@ -121,8 +110,7 @@ public class MainInterpolate
             { file };
           else
           {
-            System.out.println( "Die Datei: " + file.toString()
-                + " existiert nicht" );
+            System.out.println( "Die Datei: " + file.toString() + " existiert nicht" );
             System.exit( 0 );
           }
           i++;
@@ -137,8 +125,7 @@ public class MainInterpolate
               inputFiles[j] = file;
             else
             {
-              System.out.println( "Die Datei: " + file.toString()
-                  + " existiert nicht" );
+              System.out.println( "Die Datei: " + file.toString() + " existiert nicht" );
               System.exit( 0 );
             }
           }
@@ -156,8 +143,7 @@ public class MainInterpolate
               inputFiles[j] = file;
             else
             {
-              System.out.println( "Die Datei: " + file.toString()
-                  + " existiert nicht" );
+              System.out.println( "Die Datei: " + file.toString() + " existiert nicht" );
               System.exit( 0 );
             }
           }
@@ -165,8 +151,7 @@ public class MainInterpolate
         }
         else
         {
-          System.out.println( "Der Dateityp: " + type
-              + " wird nicht untestützt" );
+          System.out.println( "Der Dateityp: " + type + " wird nicht untestützt" );
           System.exit( 0 );
         }
       }
@@ -183,8 +168,7 @@ public class MainInterpolate
         }
         catch( NumberFormatException e )
         {
-          System.out
-              .println( "Format Fehler: Rasterweite muss ein \"double\" sein" );
+          System.out.println( "Format Fehler: Rasterweite muss ein \"double\" sein" );
           System.exit( 0 );
         }
         i++;
@@ -197,75 +181,26 @@ public class MainInterpolate
     }// while
     try
     {
-      BufferedWriter logWriter = null;
-      //Starting interpolation
-      System.out.print( "\n" + "Starting logger..." );
+      KalypsoGridTools tools = new KalypsoGridTools();
+      //      File file = new File( "D://TEMP//res_Kellinghusen//grid_khHQ5_4.asc" );
+      //      File newGridFile = new File( "d://temp//grid_subtract.asc" );
+      //      IGrid girdHQ5 = tools.importGrid( file, cs );
+      //      girdHQ5.export( newGridFile );
 
-      long startTime = System.currentTimeMillis();
+      IGrid gridHQ10 = tools.interpolateGrid( inputFiles, cs, bbox, borderPath, size );
+      gridHQ10.export( gridFile );
 
-      logWriter = new BufferedWriter( new FileWriter( new File( LOG_PATH
-          + LOG_NAME ) ) );
+      //      IGrid grid = tools.subtract( gridHQ10, girdHQ5 );
+      //      grid.export( newGridFile );
 
-      System.out.println( "finished" );
-
-      SimpleDateFormat logformatter = new SimpleDateFormat(
-          "dd.MM.yyyy G 'at' hh:mm:ss z" );
-      Date today = new Date();
-      String dateLogformat = logformatter.format( today );
-      logWriter.write( "Start time: " + dateLogformat );
-
-      System.out.println( "\n" + "StartTime : " + dateLogformat );
-
-      System.out.println( "\n" + "Importing input files..started" );
-
-      //creates mesh out of input files and set name
-
-      MeshFactory mf = MeshFactory.getInstance();
-
-      GM_Surface surfaceBBox = null;
-      if( bbox != null )
-        surfaceBBox = GeometryFactory.createGM_Surface( bbox, cs );
-      Mesh mesh = mf.readMesh( inputFiles, cs, surfaceBBox, borderPath );
-
-      System.out.println( "\n"
-          + "total importing duration (in milliseconds) : "
-          + ( System.currentTimeMillis() - startTime ) );
-
-      System.out.println( "\n" + "Interpolate Mesh into Grid..started" );
-
-      //gets bounding box of created mesh and interpolate it with
-      // bounding box
-      System.out.println( "\nMesh bbox: " + mesh.getEnvelope() );
-
-      IGrid grid = GridFactory.getInstance().createGrid( bbox, cs, size, mesh );
-
-      mesh.interpolateGrid( gridFile, mesh.getBorderLine(), grid );
-
-      System.out.print( "\n..finished" );
-
-      System.out.print( "\n" + "Closing Logger..." );
-
-      today = new Date();
-      dateLogformat = logformatter.format( today );
-      logWriter.newLine();
-      logWriter.write( "End time: " + dateLogformat );
-      logWriter.close();
-      long exDuration = System.currentTimeMillis() - startTime;
-
-      System.out.print( "finished" );
-      System.out.println( "\n" + "EndTime : " + dateLogformat );
-      System.out.println( "\n" + "total running duration ( seconds ) : "
-          + exDuration / 1000 );
-      //JOptionPane.showMessageDialog(null,"Interpolation finished
-      // successfully in " + exDuration/1000 + " Seconds.");
-
-    }//try
+    }
     catch( Exception e )
     {
       e.printStackTrace( System.out );
     }
     finally
     {
+      GridFactory.getInstance().clearFactory();
       System.exit( 0 );
     }
   }//main
