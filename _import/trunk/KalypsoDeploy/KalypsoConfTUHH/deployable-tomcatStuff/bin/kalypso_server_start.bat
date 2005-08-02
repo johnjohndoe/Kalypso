4692f71fd9edc7668754a3dd965cfd417d99912c
huebsch <huebsch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-08-02 15:13:19 +0000

    *** empty log message ***

--- _import/trunk/KalypsoDeploy/KalypsoConfTUHH/deployable-tomcatStuff/bin/kalypso_server_start.bat
+++ _import/trunk/KalypsoDeploy/KalypsoConfTUHH/deployable-tomcatStuff/bin/kalypso_server_start.bat
@@ -7,15 +7,10 @@ set KALYPSO_SERVER_TMP=%CATALINA_HOME%\temp
 set KALYPSO_SERVER_WEBDAV=%CATALINA_HOME%\webapps\webdav
 
 REM The trailing '/' is important!
-#set KALYPSO_SERVER_CONF=%KALYPSO_SERVER_WEBDAV%\srvconf
 set KALYPSO_SERVER_CONF=http://134.28.87.75:8080/webdav/srvconf/
 set KALYPSO_SERVER_DATA=%KALYPSO_SERVER_WEBDAV%\data
 
 set JAVA_OPTS=-Xmx400m -Dkalypso.server.tempdir=%KALYPSO_SERVER_TMP% -Dkalypso.server.confdir=%KALYPSO_SERVER_CONF% -Dkalypso.server.datadir=%KALYPSO_SERVER_TEMP%
 
 rem set CATALINA_OPTS=-Djava.security.debug=help
-
 catalina.bat run -security 1> %CATALINA_HOME%/logs/kalypso.log 2> %CATALINA_HOME%/logs/kalypso.err
-
-
-rem set CATALINA_OPTS=-Djava.security.debug=help
