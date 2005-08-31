3ccc3e1a507824d9fd0729b361b9f7a77b39c1c6
doemming <doemming@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-08-31 14:21:33 +0000

    *** empty log message ***

--- temp/trunk/KalypsoModelNA/src/org/kalypso/convert/namodel/schema/UrlCatalogNA.java
+++ temp/trunk/KalypsoModelNA/src/org/kalypso/convert/namodel/schema/UrlCatalogNA.java
@@ -62,11 +62,11 @@ public class UrlCatalogNA extends AbstractUrlCatalog
    */
   protected void fillCatalog( final Class myClass, final Map catalog )
   {
-    catalog.put( NaModelConstants.NS_NAMETA, myClass.getResource( "v0.3/control.xsd" ) );
+    catalog.put( NaModelConstants.NS_NAMETA, myClass.getResource( "v0.4/control.xsd" ) );
     catalog.put( NaModelConstants.NS_NAMODELL, myClass.getResource( "v0.4/namodell.xsd" ) );
     catalog.put( NaModelConstants.NS_NACONTROL, myClass.getResource( "v0.4/nacontrol.xsd" ) );
     catalog.put( NaModelConstants.NS_NAHYDROTOP, myClass.getResource( "v0.2/hydrotop.xsd" ) );
     catalog.put( NaModelConstants.NS_NAPARAMETER, myClass.getResource( "v0.2/parameter.xsd" ) );
-    catalog.put( NaModelConstants.NS_OMBROMETER, myClass.getResource( "v0.1/ombrometer.xsd" ) );
+    catalog.put( NaModelConstants.NS_OMBROMETER, myClass.getResource( "v0.2/ombrometer.xsd" ) );
   }
 }
