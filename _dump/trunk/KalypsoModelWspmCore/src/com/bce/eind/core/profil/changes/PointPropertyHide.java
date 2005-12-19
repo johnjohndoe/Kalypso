bdf02ab0adaa185889a01827f456209259078f6c
kimwerner <kimwerner@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2005-12-19 10:59:34 +0000

    *** empty log message ***

---
+++ _dump/trunk/KalypsoModelWspmCore/src/com/bce/eind/core/profil/changes/PointPropertyHide.java
@@ -0,0 +1,31 @@
+package com.bce.eind.core.profil.changes;
+
+import com.bce.eind.core.profil.IProfilChange;
+import com.bce.eind.core.profil.ProfilDataException;
+import com.bce.eind.core.profil.IProfilPoint.PARAMETER;
+import com.bce.eind.core.profil.IProfilPoint.POINT_PROPERTY;
+
+public final class PointPropertyHide implements IProfilChange
+{
+  private final boolean m_visible;
+
+  private final POINT_PROPERTY m_property;
+
+  public PointPropertyHide(final POINT_PROPERTY property, final boolean visible )
+  {
+    m_visible = visible;
+    m_property = property;
+
+  }
+
+  public IProfilChange doChange( final ProfilChangeHint hint ) throws ProfilDataException
+  {
+    hint.setPointPropertiesChanged();
+
+    final boolean oldValue = (Boolean)m_property.getParameter(PARAMETER.VISIBLE );
+    m_property.setVisible(  m_visible);
+    return new PointPropertyHide( m_property, oldValue );
+  }
+
+ 
+}
