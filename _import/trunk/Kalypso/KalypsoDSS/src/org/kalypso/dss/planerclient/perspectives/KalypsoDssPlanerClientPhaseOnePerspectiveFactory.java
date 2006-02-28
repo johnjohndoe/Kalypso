d659b1296b94de50d28a921dbbdcf5049ad96aba
kuepfer <kuepfer@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-02-28 08:51:18 +0000

    *** empty log message ***

--- _import/trunk/Kalypso/KalypsoDSS/src/org/kalypso/dss/planerclient/perspectives/KalypsoDssPlanerClientPhaseOnePerspectiveFactory.java
+++ _import/trunk/Kalypso/KalypsoDSS/src/org/kalypso/dss/planerclient/perspectives/KalypsoDssPlanerClientPhaseOnePerspectiveFactory.java
@@ -5,47 +5,46 @@ import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 import org.kalypso.dss.planerclient.browser.KalypsoWebBrowserView;
 
-public class KalypsoDssPlanerClientPhaseOnePerspectiveFactory implements
-		IPerspectiveFactory {
+public class KalypsoDssPlanerClientPhaseOnePerspectiveFactory implements IPerspectiveFactory
+{
 
-	public KalypsoDssPlanerClientPhaseOnePerspectiveFactory() {
-	}
+  public KalypsoDssPlanerClientPhaseOnePerspectiveFactory( )
+  {
+  }
 
-	public void createInitialLayout(IPageLayout layout) {
-		defineLayout(layout);
+  public void createInitialLayout( IPageLayout layout )
+  {
+    defineLayout( layout );
 
-	}
+  }
 
-	/**
-	 * Defines the initial layout for a page.
-	 */
-	protected void defineLayout(IPageLayout layout) {
+  /**
+   * Defines the initial layout for a page.
+   */
+  protected void defineLayout( IPageLayout layout )
+  {
 
-		// Editors are placed for free.
-		final String editorArea = layout.getEditorArea();
+    // Editors are placed for free.
+    final String editorArea = layout.getEditorArea();
 
-		// Top left.
-		IFolderLayout topLeft = layout.createFolder(
-				"topLeft", IPageLayout.LEFT, (float) 0.26, editorArea);//$NON-NLS-1$
-		topLeft.addView(IPageLayout.ID_RES_NAV);
-		topLeft.addPlaceholder(IPageLayout.ID_BOOKMARKS);
+    // Top Right.
+    IFolderLayout topRight = layout.createFolder( "topRight", IPageLayout.RIGHT, (float) 0.75, editorArea );//$NON-NLS-1$
+    topRight.addView( IPageLayout.ID_RES_NAV );
 
-		// Bottom left.
-		IFolderLayout bottomLeft = layout.createFolder(
-				"bottomLeft", IPageLayout.BOTTOM, (float) 0.50,//$NON-NLS-1$
-				"topLeft");//$NON-NLS-1$
-		bottomLeft.addView(IPageLayout.ID_OUTLINE);
+    // Bottom Right.
+    IFolderLayout bottomRight = layout.createFolder( "bottomRight", IPageLayout.BOTTOM, (float) 0.50,//$NON-NLS-1$
+        "topRight" );//$NON-NLS-1$
+    bottomRight.addView( IPageLayout.ID_OUTLINE );
 
-		// Bottom right.
-		IFolderLayout bottomRight = layout.createFolder(
-				"bottomRight", IPageLayout.BOTTOM, (float) 0.66,//$NON-NLS-1$
-				editorArea);
+    // Bottom right.
+    IFolderLayout bottomLeft = layout.createFolder( "bottomLeft", IPageLayout.BOTTOM, (float) 0.66,//$NON-NLS-1$
+        editorArea );
 
-//		bottomRight.addPlaceholder(KalypsoWebBrowserView.WEB_BROWSER_VIEW_ID);
-		 bottomRight.addView(KalypsoWebBrowserView.WEB_BROWSER_VIEW_ID);
+    // bottomRight.addPlaceholder(KalypsoWebBrowserView.WEB_BROWSER_VIEW_ID);
+    bottomLeft.addView( KalypsoWebBrowserView.WEB_BROWSER_VIEW_ID );
 
-		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");//$NON-NLS-1$
-		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");//$NON-NLS-1$
-	}
+    layout.addNewWizardShortcut( "org.eclipse.ui.wizards.new.folder" );//$NON-NLS-1$
+    layout.addNewWizardShortcut( "org.eclipse.ui.wizards.new.file" );//$NON-NLS-1$
+  }
 
 }
