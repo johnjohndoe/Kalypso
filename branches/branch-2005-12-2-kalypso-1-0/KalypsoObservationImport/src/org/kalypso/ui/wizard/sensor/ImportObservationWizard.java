449f7f82c6b394936f82d5fa1b45f6847ad9f7a5
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-03-06 12:29:14 +0000

    *** empty log message ***

--- branches/branch-2005-12-2-kalypso-1-0/KalypsoObservationImport/src/org/kalypso/ui/wizard/sensor/ImportObservationWizard.java
+++ branches/branch-2005-12-2-kalypso-1-0/KalypsoObservationImport/src/org/kalypso/ui/wizard/sensor/ImportObservationWizard.java
@@ -172,8 +172,9 @@ public class ImportObservationWizard extends Wizard implements IImportWizard
           final WQTuppleModel wq = (WQTuppleModel)( tuppelModelTarget );
           final Object[][] newValues = new Object[countSrc + countTarget][axesNew.length - 1];
           final ITuppleModel model = new SimpleTuppleModel( axesNew, newValues );
-          newTuppelModel = new WQTuppleModel( model, axesNew, wq.getDateAxis(), wq.getSrcAxis(), wq.getSrcStatusAxis(), wq.getDestAxis(), wq
-              .getDestStatusAxis(), wq.getConverter() );
+          newTuppelModel = new WQTuppleModel( model, axesNew, wq.getDateAxis(), wq.getSrcAxis(), wq.getSrcStatusAxis(),
+              wq.getDestAxis(), wq.getDestStatusAxis(), wq.getConverter(), wq.getDestAxisPos(), wq
+                  .getDestStatusAxisPos() );
         }
         else
         {
