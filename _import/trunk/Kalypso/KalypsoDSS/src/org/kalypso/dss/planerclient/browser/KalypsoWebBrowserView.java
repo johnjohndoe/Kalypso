9a1622b028cc5bb6f5b2f926e876dd173c3ebaec
devgernot <devgernot@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-03-12 18:16:45 +0000

    Browser View für Inform.DSS

--- _import/trunk/Kalypso/KalypsoDSS/src/org/kalypso/dss/planerclient/browser/KalypsoWebBrowserView.java
+++ _import/trunk/Kalypso/KalypsoDSS/src/org/kalypso/dss/planerclient/browser/KalypsoWebBrowserView.java
@@ -20,195 +20,228 @@ import org.eclipse.ui.internal.browser.BrowserViewer;
 import org.eclipse.ui.internal.browser.IBrowserViewerContainer;
 import org.eclipse.ui.part.ViewPart;
 
-public class KalypsoWebBrowserView extends ViewPart implements
-		IBrowserViewerContainer, IKalypsoLinkHandler {
+public class KalypsoWebBrowserView extends ViewPart implements IBrowserViewerContainer, IKalypsoLinkHandler
+{
+  public static final String WEB_BROWSER_VIEW_ID = "org.kalypso.dss.planerclient.view.browser"; //$NON-NLS-1$
 
-	public static final String WEB_BROWSER_VIEW_ID = "org.kalypso.dss.planerclient.view.browser"; //$NON-NLS-1$
+  private IMemento m_memento;
 
-	private IMemento m_memento;
+  // Persistance tags.
 
-	// Persistance tags.
+  private static final String TAG_URL = "url"; //$NON-NLS-1$
 
-	private static final String TAG_URL = "url"; //$NON-NLS-1$
+  private static final String TAG_SCROLLBARS = "scrolbars";
 
-	private static final String TAG_SCROLLBARS = "scrolbars";
+  private static final String TAG_HORIZONTAL_BAR = "horizontal";
 
-	private static final String TAG_HORIZONTAL_BAR = "horizontal";
+  private static final String TAG_VERTICAL_BAR = "vertical";
 
-	private static final String TAG_VERTICAL_BAR = "vertical";
+  private static final String TAG_SELECTION = "selection";
 
-	private static final String TAG_SELECTION = "selection";
+  protected BrowserViewer viewer;
 
-	protected BrowserViewer viewer;
+  private LocationListener locationListener = new LocationAdapter()
+  {
+    @Override
+    public void changed( LocationEvent event )
+    {
+      event.doit = false;
+      String link = event.location;
+      if( link.startsWith( IKalypsoLinkHandler.PERSPECTIV_PROTOCOL ) )
+      {
+        String linkStr = link.replaceFirst( IKalypsoLinkHandler.PERSPECTIV_PROTOCOL, "" );
+        handlePerspectiveProtocol( linkStr );
 
-	private LocationListener locationListener = new LocationAdapter() {
-		@Override
-    public void changed(LocationEvent event) {
-			event.doit = false;
-			String link = event.location;
-			if (link.startsWith(IKalypsoLinkHandler.PERSPECTIV_PROTOCOL)) {
-				String linkStr = link.replaceFirst(
-						IKalypsoLinkHandler.PERSPECTIV_PROTOCOL, "");
-				handlePerspectiveProtocol(linkStr);
+      }
+      else if( link.startsWith( IKalypsoLinkHandler.LAUNCH_PROTOCOL ) )
+      {
+        MessageDialog.openConfirm( getSite().getShell(), "Not implemented yet", "Das launch-protokol ist noch nicht implementiert." );
+      }
+    }
+  };
 
-			} else if (link.startsWith(IKalypsoLinkHandler.LAUNCH_PROTOCOL)) {
-				MessageDialog.openConfirm(getSite().getShell(),
-						"Not implemented yet",
-						"Das launch-protokol ist noch nicht implementiert.");
-			}
-		}
-	};
+  public KalypsoWebBrowserView( )
+  {
+  }
 
-	public KalypsoWebBrowserView() {
-	}
+  @Override
+  public void init( IViewSite site, IMemento memento ) throws PartInitException
+  {
+    super.init( site, memento );
+    m_memento = memento;
+  }
 
-	@Override
-  public void init(IViewSite site, IMemento memento) throws PartInitException {
-		super.init(site, memento);
-		m_memento = memento;
-	}
+  private void addBrowserListener( )
+  {
+    viewer.getBrowser().addLocationListener( locationListener );
+  }
 
-	private void addBrowserListener() {
-		viewer.getBrowser().addLocationListener(locationListener);
-	}
+  private void removeLocationListener( )
+  {
+    if( viewer.getBrowser().isDisposed() )
+      return;
+    viewer.getBrowser().removeLocationListener( locationListener );
+  }
 
-	private void removeLocationListener() {
-		if (viewer.getBrowser().isDisposed())
-			return;
-		viewer.getBrowser().removeLocationListener(locationListener);
-	}
+  @Override
+  public void createPartControl( Composite parent )
+  {
+    viewer = new BrowserViewer( parent, SWT.NONE );// BrowserViewer.LOCATION_BAR
+    viewer.setContainer( this );
+    addBrowserListener();
+    if( m_memento != null )
+      restoreState( m_memento );
+    m_memento = null;
+  }
 
-	@Override
-  public void createPartControl(Composite parent) {
-		viewer = new BrowserViewer(parent, SWT.NONE);// BrowserViewer.LOCATION_BAR
-		viewer.setContainer(this);
-		addBrowserListener();
-		if (m_memento != null)
-			restoreState(m_memento);
-		m_memento = null;
-	}
+  protected void restoreState( IMemento memento )
+  {
+    String url = memento.getString( TAG_URL );
+    // set the url of the browser
+    viewer.setURL( url );
+    if( viewer.combo != null )
+      viewer.combo.setText( url );
 
-	protected void restoreState(IMemento memento) {
-		String url = memento.getString(TAG_URL);
-		// set the url of the browser
-		viewer.setURL(url);
-		if (viewer.combo != null)
-			viewer.combo.setText(url);
+    IMemento scrollbars = memento.getChild( TAG_SCROLLBARS );
+    if( scrollbars == null )
+      return;
+    IMemento horizontal = scrollbars.getChild( TAG_HORIZONTAL_BAR );
+    if( horizontal != null )
+    {
+      int hSelection = horizontal.getInteger( TAG_SELECTION ).intValue();
+      ScrollBar horizontalBar = viewer.getHorizontalBar();
+      if( horizontalBar != null )
+        horizontalBar.setSelection( hSelection );
+    }
+    IMemento vertical = scrollbars.getChild( TAG_VERTICAL_BAR );
+    if( vertical != null )
+    {
+      int vSelection = vertical.getInteger( TAG_SELECTION ).intValue();
+      ScrollBar verticalBar = viewer.getVerticalBar();
+      if( verticalBar != null )
+        verticalBar.setSelection( vSelection );
+    }
+  }
 
-		IMemento scrollbars = memento.getChild(TAG_SCROLLBARS);
-		if (scrollbars == null)
-			return;
-		IMemento horizontal = scrollbars.getChild(TAG_HORIZONTAL_BAR);
-		if (horizontal != null) {
-			int hSelection = horizontal.getInteger(TAG_SELECTION).intValue();
-			ScrollBar horizontalBar = viewer.getHorizontalBar();
-			if (horizontalBar != null)
-				horizontalBar.setSelection(hSelection);
-		}
-		IMemento vertical = scrollbars.getChild(TAG_VERTICAL_BAR);
-		if (vertical != null) {
-			int vSelection = vertical.getInteger(TAG_SELECTION).intValue();
-			ScrollBar verticalBar = viewer.getVerticalBar();
-			if (verticalBar != null)
-				verticalBar.setSelection(vSelection);
-		}
-	}
+  @Override
+  public void dispose( )
+  {
+    removeLocationListener();
+    if( viewer != null )
+      viewer.dispose();
+    super.dispose();
+  }
 
-	@Override
-  public void dispose() {
-		removeLocationListener();
-		if (viewer != null)
-			viewer.dispose();
-		super.dispose();
-	}
+  public void setHtml( final String html )
+  {
+    if( viewer != null )
+      viewer.getBrowser().setText( html );
+  }
 
-	public void setURL(String url) {
-		if (viewer != null)
-			viewer.setURL(url);
-	}
+  public void setURL( String url )
+  {
+    if( viewer != null )
+      viewer.setURL( url );
+  }
 
-	@Override
-  public void setFocus() {
-		viewer.setFocus();
-	}
+  @Override
+  public void setFocus( )
+  {
+    viewer.setFocus();
+  }
 
-	public boolean close() {
-		try {
-			getSite().getPage().hideView(this);
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
-	}
+  public boolean close( )
+  {
+    try
+    {
+      getSite().getPage().hideView( this );
+      return true;
+    }
+    catch( Exception e )
+    {
+      return false;
+    }
+  }
 
-	public IActionBars getActionBars() {
-		return getViewSite().getActionBars();
-	}
+  public IActionBars getActionBars( )
+  {
+    return getViewSite().getActionBars();
+  }
 
-	public void openInExternalBrowser(String url) {
-		try {
-			URL theURL = new URL(url);
-			IWorkbenchBrowserSupport support = PlatformUI.getWorkbench()
-					.getBrowserSupport();
-			support.getExternalBrowser().openURL(theURL);
-		} catch (MalformedURLException e) {
-			// TODO handle this
-		} catch (PartInitException e) {
-			// TODO handle this
-		}
-	}
+  public void openInExternalBrowser( String url )
+  {
+    try
+    {
+      URL theURL = new URL( url );
+      IWorkbenchBrowserSupport support = PlatformUI.getWorkbench().getBrowserSupport();
+      support.getExternalBrowser().openURL( theURL );
+    }
+    catch( MalformedURLException e )
+    {
+      // TODO handle this
+    }
+    catch( PartInitException e )
+    {
+      // TODO handle this
+    }
+  }
 
-	/**
-	 * Return true if the filename has a "web" extension.
-	 * 
-	 * @param name
-	 * @return
-	 */
-	protected boolean isWebFile(String name) {
-		return name.endsWith("html") || name.endsWith("htm") || name.endsWith("gif") || //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-				name.endsWith("jpg"); //$NON-NLS-1$
-	}
+  /**
+   * Return true if the filename has a "web" extension.
+   * 
+   * @param name
+   * @return
+   */
+  protected boolean isWebFile( String name )
+  {
+    return name.endsWith( "html" ) || name.endsWith( "htm" ) || name.endsWith( "gif" ) || //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        name.endsWith( "jpg" ); //$NON-NLS-1$
+  }
 
-	public boolean handleLaunchProtocol(Object link) {
-		return false;
-	}
+  public boolean handleLaunchProtocol( Object link )
+  {
+    return false;
+  }
 
-	@Override
-  public void saveState(IMemento memento) {
-		if (viewer == null) {
-			if (m_memento != null) // Keep the old state;
-				memento.putMemento(m_memento);
-			return;
-		}
-		memento.putString(TAG_URL, viewer.getURL());
-		IMemento scrollbarMemento = memento.createChild(TAG_SCROLLBARS);
-		ScrollBar horizontalBar = viewer.getHorizontalBar();
-		if (horizontalBar != null) {
-			IMemento horizontal = scrollbarMemento
-					.createChild(TAG_HORIZONTAL_BAR);
-			horizontal.putInteger(TAG_SELECTION, horizontalBar.getSelection());
-		}
-		ScrollBar verticalBar = viewer.getVerticalBar();
-		if (verticalBar != null) {
-			IMemento vertical = scrollbarMemento.createChild(TAG_VERTICAL_BAR);
-			vertical.putInteger(TAG_SELECTION, verticalBar.getSelection());
-		}
-	}
+  @Override
+  public void saveState( IMemento memento )
+  {
+    if( viewer == null )
+    {
+      if( m_memento != null ) // Keep the old state;
+        memento.putMemento( m_memento );
+      return;
+    }
+    memento.putString( TAG_URL, viewer.getURL() );
+    IMemento scrollbarMemento = memento.createChild( TAG_SCROLLBARS );
+    ScrollBar horizontalBar = viewer.getHorizontalBar();
+    if( horizontalBar != null )
+    {
+      IMemento horizontal = scrollbarMemento.createChild( TAG_HORIZONTAL_BAR );
+      horizontal.putInteger( TAG_SELECTION, horizontalBar.getSelection() );
+    }
+    ScrollBar verticalBar = viewer.getVerticalBar();
+    if( verticalBar != null )
+    {
+      IMemento vertical = scrollbarMemento.createChild( TAG_VERTICAL_BAR );
+      vertical.putInteger( TAG_SELECTION, verticalBar.getSelection() );
+    }
+  }
 
-	public boolean handlePerspectiveProtocol(String url) {
-		String[] split = url.split(IKalypsoLinkHandler.SEPERATOR);
-		// try {
-		// KalypsoPerspectiveHelper.nextPerspective(split[0], split[1]);
-		// } catch (WorkbenchException e) {
-		// e.printStackTrace();
-		// return false;
-		// } catch (FileNotFoundException e) {
-		// e.printStackTrace();
-		// return false;
-		// }
-		// return true;
-		return KalypsoPerspectiveHelper.nextPerspective(split[0], split[1],
-				getSite().getPage());
-	}
+  public boolean handlePerspectiveProtocol( String url )
+  {
+    String[] split = url.split( IKalypsoLinkHandler.SEPERATOR );
+    // try {
+    // KalypsoPerspectiveHelper.nextPerspective(split[0], split[1]);
+    // } catch (WorkbenchException e) {
+    // e.printStackTrace();
+    // return false;
+    // } catch (FileNotFoundException e) {
+    // e.printStackTrace();
+    // return false;
+    // }
+    // return true;
+    return KalypsoPerspectiveHelper.nextPerspective( split[0], split[1], getSite().getPage() );
+  }
 
 }
