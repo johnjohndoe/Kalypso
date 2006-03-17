a5187b0498996f903ab9d7cc1cec9f2c204bb84b
nobody <nobody@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-03-17 09:11:18 +0000

    This commit was manufactured by cvs2svn to create tag 'Profileditor_1_0_0_20060324'.

--- KalypsoDeegree/deegree/org/kalypsodeegree/model/feature/event/ModellEventListener.java
+++
@@ -1,9 +0,0 @@
-package org.kalypsodeegree.model.feature.event;
-
-/**
- * @author bce
- */
-public interface ModellEventListener
-{
-  public void onModellChange( final ModellEvent modellEvent );
-}
