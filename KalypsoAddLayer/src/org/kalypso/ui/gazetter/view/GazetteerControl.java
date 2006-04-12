b8599220b758b02e40aec5897f4002a884017671
kalypsosync <kalypsosync@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-04-12 09:33:28 +0000

    implementation of a gazetteer client

--- KalypsoAddLayer/src/org/kalypso/ui/gazetter/view/GazetteerControl.java
+++ KalypsoAddLayer/src/org/kalypso/ui/gazetter/view/GazetteerControl.java
@@ -40,64 +40,197 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ui.gazetter.view;
 
+import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.xml.namespace.QName;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Control;
+import org.kalypso.gmlschema.feature.IFeatureType;
+import org.kalypso.ogc.wfs.WFSUtilities;
 import org.kalypso.view.gazetter.GazetterLocationType;
+import org.kalypsodeegree.model.feature.Feature;
+import org.kalypsodeegree.model.feature.GMLWorkspace;
+import org.opengis.cs.CS_CoordinateSystem;
 
 /**
  * @author doemming
  */
-public class GazetteerControl implements ISelectionChangedListener, SelectionListener, IStructuredContentProvider
+public class GazetteerControl implements ISelectionChangedListener, IStructuredContentProvider
 {
 
   private final GazetterLocationType m_gazetteerLocation;
 
-  private String[] m_content = new String[] { "keine Auswahl" };
-
-  private Runnable m_enableRunnable = null;
-
-  private Runnable m_disableRunnable = null;
-
-  private Runnable m_refreshComboRunnable = null;
-
   private final GazetterView m_view;
 
-  public GazetteerControl( GazetterLocationType gazetteerLocation, GazetterView view )
+  final URL m_baseURL;
+
+  ComboViewer m_comboViewer;
+
+  Control m_button;
+
+  public GazetteerControl( GazetterLocationType gazetteerLocation, URL baseURL, GazetterView view )
   {
     m_gazetteerLocation = gazetteerLocation;
+    m_baseURL = baseURL;
     m_view = view;
   }
 
-  public void init( )
+  public void init( GazetterLocationType parent, Feature selectedFeature, String query )
   {
-//    WFSUtilities.createGMLWorkspaceFromGetFeature(baseURL, featureTypeToLoad, targetCRS, filter, maxFeatureAsString);
-    setContent( new String[] { "Eimsbuettel", "Altona", "Bergedorf", "Harburg" } );
-  }
+    initChilds();
+    if( parent != null && selectedFeature == null )
+    {
+      // parent exists but nothing is selected...
+      setEnable( false );
 
-  private void setContent( String[] content )
-  {
-    m_content = content;
-    enable( true );
-  }
+      return;
+    }
+    final QName featureTypeToLoad = m_gazetteerLocation.getFeatureType();
+    final CS_CoordinateSystem targetCRS = null;
+    final String filter;
+    final String maxFeatureAsString = "100";
+    // no parent and nothing seleced
+    if( parent == null && selectedFeature == null )
+    {
+      final QName labelProperty = m_gazetteerLocation.getLabelProperty();
+      final StringBuffer buffer = new StringBuffer();
+      // TODO replace all non xml-string characters... <
+      if( query != null && query.length() > 0 )
+      {
+        final StringBuffer queryBuffer = new StringBuffer( query );
+        if( query.indexOf( "*" ) < 0 )
+          queryBuffer.append( "*" );
+        buffer.append( "<ogc:Filter>\n" );
+        buffer.append( "<ogc:PropertyIsLike wildCard=\"*\" singleChar=\"#\" escape=\"!\">\n" );
+        buffer.append( "<ogc:PropertyName>" + labelProperty.getLocalPart() + "</ogc:PropertyName>\n" );
+        buffer.append( "<ogc:Literal>" + queryBuffer.toString() + "</ogc:Literal>\n" );
+        buffer.append( "</ogc:PropertyIsLike>\n" );
+        buffer.append( "</ogc:Filter>\n" );
+      }
+      filter = buffer.toString();
 
-  public void enable( boolean doEnable )
-  {
-    if( doEnable )
-      m_enableRunnable.run();
+    }
+    else if( parent != null && selectedFeature != null )
+    {
+      // build filter
+      final QName spacialIdentifier = parent.getSpacialIdentifierProperty();
+      final Object value = selectedFeature.getProperty( spacialIdentifier );
+      if( value == null )
+        return;
+      // TODO better use binding
+      String valueAsString = value.toString();
+      final StringBuffer buffer = new StringBuffer();
+      buffer.append( "<ogc:Filter>\n" );
+      // buffer.append( "<ogc:PropertyIsEqualTo wildCard=\"*\" singleChar=\"#\" escape=\"!\">\n" );
+      buffer.append( "<ogc:PropertyIsEqualTo>\n" );
+      buffer.append( "<ogc:PropertyName>parents/" + spacialIdentifier.getLocalPart() + "</ogc:PropertyName>\n" );
+      buffer.append( "<ogc:Literal>" + valueAsString + "</ogc:Literal>\n" );
+      buffer.append( "</ogc:PropertyIsEqualTo>\n" );
+      buffer.append( "</ogc:Filter>\n" );
+      filter = buffer.toString();
+    }
     else
-      m_disableRunnable.run();
+      return; // never happens
+
+    final Job job = new Job( "Gazetter: load" + featureTypeToLoad.getLocalPart() )
+    {
+      @Override
+      protected IStatus run( IProgressMonitor monitor )
+      {
+        try
+        {
+
+          setContent( WFSUtilities.createGMLWorkspaceFromGetFeature( m_baseURL, featureTypeToLoad, targetCRS, filter, maxFeatureAsString ) );
+          setEnable( true );
+        }
+        catch( Exception e )
+        {
+          e.printStackTrace();
+          setContent( null );
+          setEnable( false );
+        }
+        return Status.OK_STATUS;
+      }
+
+    };
+    if( filter.length() > 0 || !m_gazetteerLocation.isDoTextSearch() )
+      job.schedule();
+    else
+    {
+      setContent( null );
+      setEnable( false );
+    }
+
   }
 
-  public void query( String queryText )
+  void setEnable( final boolean enable )
   {
-    setContent( new String[] { "...Eim*", "A***a", "Bergedorf", "Harburg" } );
+    if( m_comboViewer != null && !m_comboViewer.getControl().isDisposed() )
+    {
+      m_comboViewer.getControl().getDisplay().asyncExec( new Runnable()
+      {
+
+        public void run( )
+        {
+          if( m_comboViewer != null && !m_comboViewer.getControl().isDisposed() )
+            m_comboViewer.getControl().setEnabled( enable );
+          if( m_button != null && !m_button.isDisposed() )
+            m_button.setEnabled( enable );
+        }
+      } );
+    }
+
+  }
+
+  private void initChilds( )
+  {
+
+    final List<GazetterLocationType> gChildLocations = m_gazetteerLocation.getGazetterLocation();
+    final Iterator<GazetterLocationType> iterator = gChildLocations.iterator();
+    while( iterator.hasNext() )
+    {
+      final GazetterLocationType gChildLocation = iterator.next();
+      final GazetteerControl control = m_view.getGControlForGLocation( gChildLocation );
+      try
+      {
+        control.init( m_gazetteerLocation, null, null );
+      }
+      catch( Exception e )
+      {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+  }
+
+  void setContent( final GMLWorkspace workspace )
+  {
+    if( m_comboViewer != null && !m_comboViewer.getControl().isDisposed() )
+    {
+      m_comboViewer.getControl().getDisplay().asyncExec( new Runnable()
+      {
+
+        public void run( )
+        {
+          m_comboViewer.setInput( workspace );
+          m_comboViewer.setSelection( new StructuredSelection( GazetteerConstants.NO_SELECTION_IN_COMBO ) );
+        }
+      } );
+    }
   }
 
   /**
@@ -107,30 +240,21 @@ public class GazetteerControl implements ISelectionChangedListener, SelectionLis
   {
     // combo invoked
     // let childs update
-    final List<GazetterLocationType> gazetterLocation = m_gazetteerLocation.getGazetterLocation();
-    final Iterator<GazetterLocationType> iterator = gazetterLocation.iterator();
+    final Feature selectedFeature;
+    IStructuredSelection selection = (IStructuredSelection) event.getSelection();
+    Object firstElement = selection.getFirstElement();
+    if( firstElement instanceof Feature )
+      selectedFeature = (Feature) firstElement;
+    else
+      selectedFeature = null;
+    final List<GazetterLocationType> gChildLocations = m_gazetteerLocation.getGazetterLocation();
+    final Iterator<GazetterLocationType> iterator = gChildLocations.iterator();
     while( iterator.hasNext() )
     {
-      final GazetterLocationType gLocation = iterator.next();
-      // TODO...
+      final GazetterLocationType gChildLocation = iterator.next();
+      final GazetteerControl gChildControl = m_view.getGControlForGLocation( gChildLocation );
+      gChildControl.init( m_gazetteerLocation, selectedFeature, null );
     }
-
-  }
-
-  /**
-   * @see org.eclipse.swt.events.SelectionListener#widgetSelected(org.eclipse.swt.events.SelectionEvent)
-   */
-  public void widgetSelected( SelectionEvent e )
-  {
-    // user invoked GOTO-Button
-  }
-
-  /**
-   * @see org.eclipse.swt.events.SelectionListener#widgetDefaultSelected(org.eclipse.swt.events.SelectionEvent)
-   */
-  public void widgetDefaultSelected( SelectionEvent e )
-  {
-    // nothing
   }
 
   /**
@@ -138,7 +262,20 @@ public class GazetteerControl implements ISelectionChangedListener, SelectionLis
    */
   public Object[] getElements( Object inputElement )
   {
-    return m_content;
+    if( inputElement instanceof GMLWorkspace )
+    {
+      final GMLWorkspace workspace = (GMLWorkspace) inputElement;
+      QName featureType = m_gazetteerLocation.getFeatureType();
+      final IFeatureType ft = workspace.getFeatureType( featureType );
+      final Feature[] features = workspace.getFeatures( ft );
+      final Object result[] = new Object[features.length + 1];
+      result[0] = GazetteerConstants.NO_SELECTION_IN_COMBO;
+      for( int i = 1; i < result.length; i++ )
+        result[i] = features[i - 1];
+      return result;
+    }
+    else
+      return new String[] { "gazetteer nicht erreichbar" };
   }
 
   /**
@@ -158,18 +295,9 @@ public class GazetteerControl implements ISelectionChangedListener, SelectionLis
     // TODO Auto-generated method stub
   }
 
-  public void setEnableRunnable( Runnable enableRunnable )
+  public void setViewer( ComboViewer comboViewer, Button control )
   {
-    m_enableRunnable = enableRunnable;
-  }
-
-  public void setDisableRunnable( Runnable disableRunnable )
-  {
-    m_disableRunnable = disableRunnable;
-  }
-
-  public void setRefreshComboRunnable( Runnable refreshComboRunnable )
-  {
-    m_refreshComboRunnable = refreshComboRunnable;
+    m_comboViewer = comboViewer;
+    m_button = control;
   }
 }
