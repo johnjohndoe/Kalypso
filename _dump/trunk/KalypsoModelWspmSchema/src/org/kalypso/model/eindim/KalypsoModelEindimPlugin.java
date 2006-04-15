4c07818b3b9504049f99f03af45e7d10553bc56d
belger <belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-04-15 00:45:53 +0000

    *** empty log message ***

---
+++ _dump/trunk/KalypsoModelWspmSchema/src/org/kalypso/model/eindim/KalypsoModelEindimPlugin.java
@@ -0,0 +1,50 @@
+package org.kalypso.model.eindim;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class KalypsoModelEindimPlugin extends Plugin
+{
+
+  // The shared instance.
+  private static KalypsoModelEindimPlugin plugin;
+
+  /**
+   * The constructor.
+   */
+  public KalypsoModelEindimPlugin( )
+  {
+    plugin = this;
+  }
+
+  /**
+   * This method is called upon plug-in activation
+   */
+  @Override
+  public void start( BundleContext context ) throws Exception
+  {
+    super.start( context );
+  }
+
+  /**
+   * This method is called when the plug-in is stopped
+   */
+  @Override
+  public void stop( BundleContext context ) throws Exception
+  {
+    super.stop( context );
+    plugin = null;
+  }
+
+  /**
+   * Returns the shared instance.
+   */
+  public static KalypsoModelEindimPlugin getDefault( )
+  {
+    return plugin;
+  }
+
+}
