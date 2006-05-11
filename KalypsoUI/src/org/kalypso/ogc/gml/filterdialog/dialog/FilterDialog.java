4c2d70a9ce345592fba6f5555abdd44584acae6f
kuepfer <kuepfer@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-05-11 14:30:27 +0000

    *** empty log message ***

--- KalypsoUI/src/org/kalypso/ogc/gml/filterdialog/dialog/FilterDialog.java
+++ KalypsoUI/src/org/kalypso/ogc/gml/filterdialog/dialog/FilterDialog.java
@@ -213,7 +213,7 @@ public class FilterDialog extends TitleAreaDialog implements IErrorMessageReciev
     {
       IDialogSettings dialogSettings = KalypsoGisPlugin.getDefault().getDialogSettings();
       String filterXML = dialogSettings.get( FILTER_KEY );
-      if( filterXML != null || filterXML.length() > 0 )
+      if( filterXML != null )
       {
         StringInputStream input = new StringInputStream( filterXML );
         try
