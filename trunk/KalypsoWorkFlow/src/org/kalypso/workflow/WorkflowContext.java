30b8f60016958e3539ed03bc6ca50865eaf8e57a
doemming <doemming@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-05-15 13:58:49 +0000

    add KalypsoWorkflow for browser based workflow support

---
+++ trunk/KalypsoWorkFlow/src/org/kalypso/workflow/WorkflowContext.java
@@ -0,0 +1,127 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.workflow;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.kalypso.contribs.java.net.IUrlResolver2;
+
+/**
+ * multi context for workflow<br>
+ * <li>context for active project </li>
+ * <li>context for active calccase </li>
+ * <li>context for actionURL</li>
+ * 
+ * @author doemming
+ */
+public class WorkflowContext implements IUrlResolver2
+{
+  private IProject m_contextProject = null;
+
+  private IFolder m_contextCalcDir = null;
+
+  private URL m_contextActionURL = null;
+
+  public void setContextProject( IProject contextProject )
+  {
+    m_contextProject = contextProject;
+  }
+
+  public IProject getContextProject( )
+  {
+    return m_contextProject;
+  }
+
+  public void setContextCalcDir( IFolder contextCalcDir )
+  {
+    m_contextCalcDir = contextCalcDir;
+  }
+
+  public IFolder getContextCalcDir( )
+  {
+    return m_contextCalcDir;
+  }
+
+  public void setContextActionURL( final URL contextActionURL )
+  {
+    m_contextActionURL = contextActionURL;
+    System.out.println( "context for actionURL: " + m_contextActionURL );
+  }
+
+  public URL getContextActionURL( )
+  {
+    return m_contextActionURL;
+  }
+
+  /**
+   * @see org.kalypso.contribs.java.net.IUrlResolver2#resolveURL(java.lang.String)
+   */
+  public URL resolveURL( final String relativeOrAbsolute ) throws MalformedURLException
+  {
+    if( relativeOrAbsolute == null ) // nothing to resolve
+      return getContextActionURL(); // return base
+
+    final IProject contextProject = getContextProject();
+    final IFolder contextCalcDir = getContextCalcDir();
+    final HashMap<String, String> replaceTokens = new HashMap<String, String>();
+    if( contextProject != null )
+      replaceTokens.put( "project:", "platform:/resource/" + contextProject.getName() + "/" );
+    if( contextCalcDir != null )
+      replaceTokens.put( "calcdir:", "platform:/resource/" + contextCalcDir.getFullPath().toString() + "/" );
+    final Set<Entry<String, String>> entrySet = replaceTokens.entrySet();
+    final Iterator<Entry<String, String>> iterator = entrySet.iterator();
+    String relative = relativeOrAbsolute;
+    while( iterator.hasNext() )
+    {
+      final Entry<String, String> entry = iterator.next();
+      relative = relative.replaceFirst( entry.getKey(), entry.getValue() );
+    }
+    return new URL( getContextActionURL(), relative );
+  }
+
+}
---
+++ trunk/KalypsoWorkFlow/src/org/kalypso/workflow/ui/browser/urlaction/URLActionActivateEditor.java
@@ -0,0 +1,116 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.workflow.ui.browser.urlaction;
+
+import java.net.URL;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IEditorRegistry;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.part.FileEditorInput;
+import org.kalypso.contribs.eclipse.core.resources.ResourceUtilities;
+import org.kalypso.workflow.ui.browser.AbstractURLAction;
+import org.kalypso.workflow.ui.browser.ICommandURL;
+
+/**
+ * example<br>
+ * kalypso://openEditor?input=project:file&activate=true&contentType=gmt
+ * 
+ * @author doemming
+ */
+public class URLActionActivateEditor extends AbstractURLAction
+{
+  private final static String COMMAND_NAME = "openEditor";
+
+  private final static String PARAM_INPUT = "input";
+
+  /**
+   * optional e.g. "gmt"
+   */
+  private final static String PARAM_CONTENT_TYPE = "contentType";
+
+  /**
+   * optional boolean to control if editor should be active after open<br>
+   * default is <code>false</code>
+   */
+  private final static String PARAM_ACTIVATE = "activate";
+
+  /**
+   * @see org.kalypso.contribs.eclipse.ui.browser.commandable.ICommandURLAction#run(org.kalypso.contribs.eclipse.ui.browser.commandable.ICommandURL)
+   */
+  public boolean run( ICommandURL commandURL )
+  {
+    final IWorkbenchPage activePage = getActivePage();
+    final String inputLocation = commandURL.getParameter( PARAM_INPUT );
+    final String activateEditorAsString = commandURL.getParameter( PARAM_ACTIVATE );
+    final String contentTypeAsString = commandURL.getParameter( PARAM_CONTENT_TYPE );
+    final boolean activateEditor = Boolean.parseBoolean( activateEditorAsString );
+    final IWorkbench workbench = getWorkbench();
+    final IEditorRegistry editorRegistry = workbench.getEditorRegistry();
+    try
+    {
+      final URL inputURL = getWorkFlowContext().resolveURL( inputLocation );
+      final IEditorDescriptor defaultDefaultDescriptor;
+      if( contentTypeAsString != null && contentTypeAsString.length() > 0 )
+        defaultDefaultDescriptor = editorRegistry.getDefaultEditor( contentTypeAsString );
+      else
+        defaultDefaultDescriptor = editorRegistry.getDefaultEditor( inputURL.toString() );
+      final IFile file = ResourceUtilities.findFileFromURL( inputURL );
+      final FileEditorInput input = new FileEditorInput( file );
+      activePage.openEditor( input, defaultDefaultDescriptor.getId(), activateEditor );
+    }
+    catch( Exception e )
+    {
+      e.printStackTrace();
+    }
+    return true;
+  }
+
+  /**
+   * @see org.kalypso.contribs.eclipse.ui.browser.commandable.ICommandURLAction#getActionName()
+   */
+  public String getActionName( )
+  {
+    return COMMAND_NAME;
+  }
+}
