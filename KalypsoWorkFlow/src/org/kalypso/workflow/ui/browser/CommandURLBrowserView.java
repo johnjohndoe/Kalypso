d87630f7ef7b531a0b1cf8184de4744e5eecee0f
doemming <doemming@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-05-15 13:58:49 +0000

    add KalypsoWorkflow for browser based workflow support

---
+++ KalypsoWorkFlow/src/org/kalypso/workflow/ui/browser/CommandURLBrowserView.java
@@ -0,0 +1,135 @@
+package org.kalypso.workflow.ui.browser;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.actions.ActionFactory;
+import org.kalypso.contribs.eclipse.ui.browser.AbstractBrowserView;
+import org.kalypso.workflow.WorkflowContext;
+import org.kalypso.workflow.ui.KalypsoWorkFlowPlugin;
+
+/**
+ * BrowserView that can handle and excecute CommandURLs via CommandLocationListener<br>
+ * 
+ * @author kuepfer
+ */
+public class CommandURLBrowserView extends AbstractBrowserView
+{
+  public static final String WEB_BROWSER_VIEW_ID = "org.kalypso.contribs.ui.browser.commandable.CommandURLBrowserView"; //$NON-NLS-1$
+
+  protected final CommandLocationListener m_listener;
+
+  private WorkflowContext m_workflowContext;
+
+  public CommandURLBrowserView( )
+  {
+    m_workflowContext = KalypsoWorkFlowPlugin.getDefault().getDefaultWorkflowContext();
+    // m_workflowContext = new WorkflowContext();
+    m_listener = new CommandLocationListener( m_workflowContext, this );
+  }
+
+  /**
+   * @see org.kalypso.contribs.eclipse.ui.browser.AbstractBrowserView#createPartControl(org.eclipse.swt.widgets.Composite)
+   */
+  @Override
+  public void createPartControl( Composite parent )
+  {
+    super.createPartControl( parent );
+    configuerActionBar();
+    updateNavigationActionsState();
+    addLocationListener( m_listener );
+    // setURL( "file:///C:/eclipse3.1_runtime_workspace/TEST_URLROWSER/test1.html" );
+    // setURL( "http://www.tu-harburg.de/wb" );
+  }
+
+  protected Browser getBrowser( )
+  {
+    return m_viewer.getBrowser();
+  }
+
+  private void configuerActionBar( )
+  {
+    final IActionBars actionBars = getActionBars();
+    final IToolBarManager toolBarManager = actionBars.getToolBarManager();
+    actionBars.setGlobalActionHandler( ActionFactory.FORWARD.getId(), forwardAction );
+    actionBars.setGlobalActionHandler( ActionFactory.BACK.getId(), backAction );
+    toolBarManager.add( backAction );
+    toolBarManager.add( forwardAction );
+    toolBarManager.update( true );
+    actionBars.updateActionBars();
+  }
+
+  protected Action backAction = new Action()
+  {
+
+    {
+      setToolTipText( "Zurück" );
+      setImageDescriptor( ImageDescriptor.createFromURL( getClass().getResource( "icons/backward_nav.gif" ) ) ); //$NON-NLS-1$
+      setDisabledImageDescriptor( ImageDescriptor.createFromURL( getClass().getResource( "icons/backward_nav_gray.gif" ) ) ); //$NON-NLS-1$
+    }
+
+    @Override
+    public void run( )
+    {
+      navigateBack();
+    }
+  };
+
+  protected Action forwardAction = new Action()
+  {
+
+    {
+      setToolTipText( "Vorwärts" );
+      setImageDescriptor( ImageDescriptor.createFromURL( getClass().getResource( "icons/forward_nav.gif" ) ) ); //$NON-NLS-1$
+      setDisabledImageDescriptor( ImageDescriptor.createFromURL( getClass().getResource( "icons/forward_nav_gray.gif" ) ) ); //$NON-NLS-1$
+    }
+
+    @Override
+    public void run( )
+    {
+      navigateForward();
+    }
+  };
+
+  protected void updateNavigationActionsState( )
+  {
+
+    // in static html intro, use browser history.
+    forwardAction.setEnabled( m_viewer.isForwardEnabled() );
+    backAction.setEnabled( m_viewer.isBackEnabled() );
+  }
+
+  protected void navigateBack( )
+  {
+    m_viewer.back();
+    try
+    {
+      m_workflowContext.setContextActionURL( new URL( m_viewer.getURL() ) );
+    }
+    catch( MalformedURLException e )
+    {
+      e.printStackTrace();
+    }
+    updateNavigationActionsState();
+  }
+
+  protected void navigateForward( )
+  {
+    m_viewer.forward();
+    try
+    {
+      m_workflowContext.setContextActionURL( new URL( m_viewer.getURL() ) );
+    }
+    catch( MalformedURLException e )
+    {
+      e.printStackTrace();
+    }
+    updateNavigationActionsState();
+  }
+}
---
+++ KalypsoWorkFlow/src/org/kalypso/workflow/ui/browser/urlaction/URLActionExtract.java
@@ -0,0 +1,123 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.workflow.ui.browser.urlaction;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.kalypso.commons.java.util.zip.ZipUtilities;
+import org.kalypso.contribs.eclipse.core.resources.ResourceUtilities;
+import org.kalypso.workflow.WorkflowContext;
+import org.kalypso.workflow.ui.browser.AbstractURLAction;
+import org.kalypso.workflow.ui.browser.ICommandURL;
+
+/**
+ * example<br>
+ * kalypso://copy?source=http://kalypso.org/archive.zip&target=project:/
+ * 
+ * @author doemming
+ */
+public class URLActionExtract extends AbstractURLAction
+{
+  private final static String COMMAND_NAME = "extract";
+
+  private final static String PARAM_SOURCE = "sourceURL";
+
+  private final static String PARAM_TARGET = "target";
+
+  /**
+   * @see org.kalypso.contribs.eclipse.ui.browser.commandable.ICommandURLAction#run(org.kalypso.contribs.eclipse.ui.browser.commandable.ICommandURL)
+   */
+  public boolean run( ICommandURL commandURL )
+  {
+    final String source = commandURL.getParameter( PARAM_SOURCE );
+    final String relativeTargetURL = commandURL.getParameter( PARAM_TARGET );
+    final URL sourceURL;
+    final WorkflowContext wfContext = getWorkFlowContext();
+    try
+    {
+      sourceURL = wfContext.resolveURL( source );
+    }
+    catch( MalformedURLException e )
+    {
+      e.printStackTrace();
+      return false;
+    }
+
+    InputStream resourceAsStream = null;
+    try
+    {
+      final URL targetURL = wfContext.resolveURL( relativeTargetURL );
+      // * note:
+      // * next line can not find IFile for a url that points to a project and not to a resource inside a project
+      // * IFile iFile ResourceUtilities.findFileFromURL(url);
+      final File targetFile = ResourceUtilities.findFileFromURL2( targetURL );
+      resourceAsStream = sourceURL.openStream();
+      ZipUtilities.unzip( resourceAsStream, targetFile );
+      // refresh on the whole project, cause we cannot refresh from IPath
+      final IProject project = ResourceUtilities.findProjectFromURL( targetURL );
+      project.refreshLocal( IResource.DEPTH_INFINITE, null );
+      return true;
+    }
+    catch( Exception e )
+    {
+      e.printStackTrace();
+      return false;
+    }
+    finally
+    {
+      IOUtils.closeQuietly( resourceAsStream );
+    }
+  }
+
+  /**
+   * @see org.kalypso.contribs.eclipse.ui.browser.commandable.ICommandURLAction#getActionName()
+   */
+  public String getActionName( )
+  {
+    return COMMAND_NAME;
+  }
+}
