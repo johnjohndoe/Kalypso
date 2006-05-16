332fc676f458ce2d96542f94491085774359c29f
kuepfer <kuepfer@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-05-16 18:10:49 +0000

    *** empty log message ***

--- trunk/KalypsoPortal/src/org/kalypso/portal/dialog/SiteDialog.java
+++ trunk/KalypsoPortal/src/org/kalypso/portal/dialog/SiteDialog.java
@@ -1,48 +1,38 @@
 package org.kalypso.portal.dialog;
 
-import org.eclipse.jface.dialogs.TitleAreaDialog;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.target.internal.ui.SiteExplorerView;
-import org.eclipse.ui.IViewSite;
-import org.eclipse.ui.IWorkbenchPartSite;
-import org.eclipse.ui.PartInitException;
 
-public class SiteDialog extends TitleAreaDialog {
-
-	private IWorkbenchPartSite m_site;
-
-	private final SiteExplorerView m_view;
-
-	public SiteDialog(Shell parentShell, IWorkbenchPartSite site) {
-		super(parentShell);
-		m_site = site;
-		m_view = new SiteExplorerView();
-		setTitle("Projekt Explorer");
-		setShellStyle(getShellStyle() | SWT.MAX);
-	}
-
-	protected Control createDialogArea(Composite parent) {
-		Composite main = (Composite) super.createDialogArea(parent);
-		GridLayout layout = new GridLayout(1, true);
-		GridData gd = new GridData();
-		gd.heightHint = 300;
-		gd.widthHint = 500;
-		main.setLayout(layout);
-		main.setLayoutData(gd);
-		if (m_site instanceof IWorkbenchPartSite) {
-			try {
-				m_view.init((IViewSite) m_site);
-			} catch (PartInitException e) {
-				e.printStackTrace();
-			}
-		}
-		m_view.createPartControl(main);
-
-		return main;
-	}
+public class SiteDialog //extends TitleAreaDialog
+{
+// TODO for testing please do not remove
+//	private IWorkbenchPartSite m_site;
+//
+//	private final SiteExplorerView m_view;
+//
+//	public SiteDialog(Shell parentShell, IWorkbenchPartSite site) {
+//		super(parentShell);
+//		m_site = site;
+//		m_view = new SiteExplorerView();
+//		setTitle("Projekt Explorer");
+//		setShellStyle(getShellStyle() | SWT.MAX);
+//	}
+//
+//	protected Control createDialogArea(Composite parent) {
+//		Composite main = (Composite) super.createDialogArea(parent);
+//		GridLayout layout = new GridLayout(1, true);
+//		GridData gd = new GridData();
+//		gd.heightHint = 300;
+//		gd.widthHint = 500;
+//		main.setLayout(layout);
+//		main.setLayoutData(gd);
+//		if (m_site instanceof IWorkbenchPartSite) {
+//			try {
+//				m_view.init((IViewSite) m_site);
+//			} catch (PartInitException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		m_view.createPartControl(main);
+//
+//		return main;
+//	}
 }
