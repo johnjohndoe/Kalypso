4a8fd0ad1854409896733a96d0ce8871ba069983
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-05-17 16:20:23 +0000

    Still prototyping

---
+++ trunk/KalypsoUI/src/org/kalypso/ogc/gml/om/ObservationGuiTypeHandler.java
@@ -0,0 +1,177 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.ogc.gml.om;
+
+import java.text.ParseException;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.swt.graphics.Image;
+import org.kalypso.commons.xml.NS;
+import org.kalypso.gmlschema.property.IPropertyType;
+import org.kalypso.observation.IObservation;
+import org.kalypso.observation.result.TupleResult;
+import org.kalypso.ogc.gml.featureview.IFeatureChangeListener;
+import org.kalypso.ogc.gml.featureview.IFeatureModifier;
+import org.kalypso.ogc.gml.featureview.dialog.IFeatureDialog;
+import org.kalypso.ogc.gml.gui.IGuiTypeHandler;
+import org.kalypso.ogc.gml.selection.IFeatureSelectionManager;
+import org.kalypso.template.featureview.ControlType;
+import org.kalypso.template.featureview.ObjectFactory;
+import org.kalypsodeegree.model.feature.Feature;
+
+/**
+ * TODO [marc] make this typehandler work
+ * 
+ * @author schlienger
+ */
+public class ObservationGuiTypeHandler implements IGuiTypeHandler
+{
+  /**
+   * @see org.kalypso.ogc.gml.gui.IGuiTypeHandler#createFeatureDialog(org.kalypsodeegree.model.feature.Feature, org.kalypso.gmlschema.property.IPropertyType)
+   */
+  public IFeatureDialog createFeatureDialog( Feature feature, IPropertyType ftp )
+  {
+    try
+    {
+      final IObservation<TupleResult> obs = ObservationFeatureFactory.observationFromFeature( feature.getParent() );
+      
+      return new ObservationFeatureDialog( obs );
+    }
+    catch( final ParseException e )
+    {
+      e.printStackTrace();
+      
+      return null;
+    }
+  }
+
+  /**
+   * @see org.kalypso.ogc.gml.gui.IGuiTypeHandler#createFeatureviewControl(org.kalypso.gmlschema.property.IPropertyType, org.kalypso.template.featureview.ObjectFactory)
+   */
+  public JAXBElement< ? extends ControlType> createFeatureviewControl( IPropertyType property, ObjectFactory factory )
+  {
+    return null;
+  }
+
+  /**
+   * @see org.kalypso.ogc.gml.gui.IGuiTypeHandler#createFeatureModifier(org.kalypso.gmlschema.property.IPropertyType, org.kalypso.ogc.gml.selection.IFeatureSelectionManager, org.kalypso.ogc.gml.featureview.IFeatureChangeListener)
+   */
+  public IFeatureModifier createFeatureModifier( IPropertyType ftp, IFeatureSelectionManager selectionManager, IFeatureChangeListener fcl )
+  {
+    return null;
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.ILabelProvider#getImage(java.lang.Object)
+   */
+  public Image getImage( Object element )
+  {
+    return null;
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.ILabelProvider#getText(java.lang.Object)
+   */
+  public String getText( Object element )
+  {
+    return null;
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.IBaseLabelProvider#addListener(org.eclipse.jface.viewers.ILabelProviderListener)
+   */
+  public void addListener( ILabelProviderListener listener )
+  {
+    // TODO Auto-generated method stub
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.IBaseLabelProvider#dispose()
+   */
+  public void dispose( )
+  {
+    // TODO Auto-generated method stub
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.IBaseLabelProvider#isLabelProperty(java.lang.Object, java.lang.String)
+   */
+  public boolean isLabelProperty( Object element, String property )
+  {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.IBaseLabelProvider#removeListener(org.eclipse.jface.viewers.ILabelProviderListener)
+   */
+  public void removeListener( ILabelProviderListener listener )
+  {
+    // TODO Auto-generated method stub
+  }
+
+  /**
+   * @see org.kalypso.gmlschema.types.ITypeHandler#getValueClass()
+   */
+  public Class getValueClass( )
+  {
+    return IObservation.class;
+  }
+
+  /**
+   * @see org.kalypso.gmlschema.types.ITypeHandler#getTypeName()
+   */
+  public QName getTypeName( )
+  {
+    return new QName(NS.OM, "result");
+  }
+
+  /**
+   * @see org.kalypso.gmlschema.types.ITypeHandler#isGeometry()
+   */
+  public boolean isGeometry( )
+  {
+    return false;
+  }
+}
