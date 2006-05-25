7237fbbde85c09571607c116a7dcb5239e1b9f25
devgernot <devgernot@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-05-25 17:15:25 +0000

    FeatureView: Templates with Combo-boxes without entries possible. Entries are generated from property type on the fly.

--- KalypsoCommons/src/org/kalypso/commons/xml/NSPrefixProvider.java
+++ KalypsoCommons/src/org/kalypso/commons/xml/NSPrefixProvider.java
@@ -71,12 +71,12 @@ public class NSPrefixProvider
     if( THE_NS_MAPPER == null )
     {
       THE_NS_MAPPER = new NSPrefixProvider();
-      THE_NS_MAPPER.getPreferredPrefix( NS.XLINK, "xlink" );
+//      THE_NS_MAPPER.getPreferredPrefix( NS.XLINK, "xlink" );
       THE_NS_MAPPER.getPreferredPrefix( NS.XSD, "xs" );
       THE_NS_MAPPER.getPreferredPrefix( NS.XSD_SCHEMA, "xsd" );
       THE_NS_MAPPER.getPreferredPrefix( NS.KALYPSO_MAPVIEW, "mapv" );
       THE_NS_MAPPER.getPreferredPrefix( NS.KALYPSO_OBSVIEW, "obsv" );
-      THE_NS_MAPPER.getPreferredPrefix( NS.GML2, "gml2" );
+//      THE_NS_MAPPER.getPreferredPrefix( NS.GML2, "gml2" );
       THE_NS_MAPPER.getPreferredPrefix( NS.KALYPSO_RRM, "rrm" );
       THE_NS_MAPPER.getPreferredPrefix( NS.KALYPSO_OBSLINK, "obslink" );
       THE_NS_MAPPER.getPreferredPrefix( NS.ADV, "adv" );
