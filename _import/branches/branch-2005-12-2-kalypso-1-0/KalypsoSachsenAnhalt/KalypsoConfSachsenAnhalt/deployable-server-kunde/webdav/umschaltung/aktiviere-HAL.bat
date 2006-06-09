8310c304bf1e68df6c3d3ff5578933128e6daea3
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-06-09 10:50:05 +0000

    Ergänzung für Tageswerte Umschaltung MD-HAL

---
+++ _import/branches/branch-2005-12-2-kalypso-1-0/KalypsoSachsenAnhalt/KalypsoConfSachsenAnhalt/deployable-server-kunde/webdav/umschaltung/aktiviere-HAL.bat
@@ -0,0 +1,5 @@
+d:
+cd d:\programme\kalypso\tomcat5.0\webapps\webdav\umschaltung
+copy /Y metadocService.properties_HAL ..\srvconf\IMetaDocService\metadocService.properties
+copy /Y repconf_server.xml_HAL ..\srvconf\IObservationService\repconf_server.xml
+copy /Y userService.properties_HAL ..\srvconf\IUserService\userService.properties
---
+++ _import/branches/branch-2005-12-2-kalypso-1-0/KalypsoSachsenAnhalt/KalypsoConfSachsenAnhalt/deployable-server-kunde/webdav/umschaltung/aktiviere-MD.bat
@@ -0,0 +1,5 @@
+d:
+cd d:\programme\kalypso\tomcat5.0\webapps\webdav\umschaltung
+copy /Y metadocService.properties_MD ..\srvconf\IMetaDocService\metadocService.properties
+copy /Y repconf_server.xml_MD ..\srvconf\IObservationService\repconf_server.xml
+copy /Y userService.properties_MD ..\srvconf\IUserService\userService.properties
