b441d703cc72582bd3dbe7815b5b610a681f3778
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-06-19 16:38:28 +0000

    Mem Leak ProfilChartView fixed? Bugfix im GmlTreeDropAdapter Prototyping new SWTChart

---
+++ KalypsoUI/src/org/kalypso/ogc/gml/om/chart/ObservationChartView.java
@@ -0,0 +1,195 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.ogc.gml.om.chart;
+
+import java.awt.Insets;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.views.navigator.LocalSelectionTransfer;
+import org.kalypso.commons.xml.XmlTypes;
+import org.kalypso.contribs.java.util.DoubleComparator;
+import org.kalypso.observation.IObservation;
+import org.kalypso.observation.result.IComponent;
+import org.kalypso.observation.result.TupleResult;
+import org.kalypso.ogc.gml.om.AbstractObservationView;
+import org.kalypso.swtchart.Chart;
+import org.kalypso.swtchart.axis.NumberAxis;
+import org.kalypso.swtchart.axis.IAxisConstants.DIRECTION;
+import org.kalypso.swtchart.axis.IAxisConstants.POSITION;
+import org.kalypso.swtchart.axis.IAxisConstants.PROPERTY;
+import org.kalypso.swtchart.axis.registry.IAxisRegistry;
+import org.kalypso.swtchart.axis.renderer.NumberAxisRenderer;
+import org.kalypso.swtchart.layer.IChartLayer;
+import org.kalypso.swtchart.layer.impl.TestLayer;
+
+/**
+ * @author schlienger
+ */
+public class ObservationChartView extends AbstractObservationView
+{
+  private final static int AXIS_WIDTH = 1;
+
+  private final static int TICK_LENGTH = 10;
+
+  private static final Insets LABEL_INSETS = new Insets( 5, 0, 5, 0 );
+
+  private static final Insets TICK_INSETS = new Insets( 2, 10, 10, 10 );
+
+  private Chart m_chart;
+
+  private Composite m_composite;
+
+  /**
+   * @see org.eclipse.ui.part.WorkbenchPart#dispose()
+   */
+  @Override
+  public void dispose( )
+  {
+    if( m_composite != null )
+      m_composite.dispose();
+
+    super.dispose();
+  }
+
+  /**
+   * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+   */
+  @Override
+  public void createPartControl( Composite parent )
+  {
+    m_composite = new Composite( parent, SWT.FILL );
+    m_composite.setLayout( new org.eclipse.swt.layout.GridLayout() );
+
+    m_chart = new Chart( m_composite, SWT.NONE );
+    m_chart.setLayoutData( new GridData( GridData.FILL_BOTH ) );
+
+    final Color cfg = parent.getDisplay().getSystemColor( SWT.COLOR_DARK_GRAY );
+    final Color cbg = parent.getDisplay().getSystemColor( SWT.COLOR_WHITE );
+
+    final NumberAxisRenderer numberAxisRenderer = new NumberAxisRenderer( cfg, cbg, AXIS_WIDTH, TICK_LENGTH, TICK_INSETS, 1, LABEL_INSETS, 4 );
+    final IAxisRegistry axisRegistry = m_chart.getAxisRegistry();
+    axisRegistry.setRenderer( Number.class, numberAxisRenderer );
+
+    final DoubleComparator dc = new DoubleComparator( 0.001 );
+    final NumberAxis botAxis = new NumberAxis( "1", "domain", PROPERTY.CONTINUOUS, POSITION.BOTTOM, DIRECTION.POSITIVE, dc );
+    final NumberAxis leftAxis = new NumberAxis( "2", "value", PROPERTY.CONTINUOUS, POSITION.LEFT, DIRECTION.POSITIVE, dc );
+
+    axisRegistry.addAxis( botAxis );
+    axisRegistry.addAxis( leftAxis );
+
+    addDropSupport( DND.DROP_COPY | DND.DROP_MOVE | DND.DROP_LINK, new Transfer[] { LocalSelectionTransfer.getInstance() } );
+
+    // testing
+    m_chart.addLayer( new TestLayer( botAxis, leftAxis ) );
+    
+    m_chart.setAutoscale( true );
+
+    m_composite.layout();
+  }
+
+  /**
+   * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
+   */
+  @Override
+  public void setFocus( )
+  {
+    m_chart.setFocus();
+  }
+
+  /**
+   * @see org.kalypso.ogc.gml.om.views.AbstractObservationView#getControl()
+   */
+  @Override
+  protected Control getControl( )
+  {
+    return m_composite;
+  }
+
+  /**
+   * @see org.kalypso.ogc.gml.om.views.AbstractObservationView#handleDrop(org.kalypso.observation.IObservation)
+   */
+  @Override
+  protected boolean handleDrop( final IObservation<TupleResult> obs )
+  {
+    final TupleResult result = obs.getResult();
+    final IComponent[] comps = result.getComponents();
+    final List<IComponent> list = new ArrayList<IComponent>( 2 );
+    for( int i = 0; i < comps.length; i++ )
+    {
+      final QName qn = comps[i].getValueTypeName();
+
+      if( XmlTypes.isNumber( qn ) /* || XmlTypes.isDate( qn ) */)
+        list.add( comps[i] );
+
+      if( list.size() == 2 )
+        break;
+    }
+
+    if( list.size() < 2 )
+      return false;
+
+    final IComponent domComp = list.get( 0 );
+    final IComponent valComp = list.get( 1 );
+
+    final DefaultTupleResultLayerProvider layerProvider = new DefaultTupleResultLayerProvider( m_chart.getAxisRegistry(), obs, domComp, valComp, "1", "2" );
+
+    final IChartLayer[] layers = layerProvider.getLayers();
+
+    for( int i = 0; i < layers.length; i++ )
+    {
+      final IChartLayer layer = layers[i];
+      System.out.println( "Would like to add layer: " + layer );
+      m_chart.addLayer( layer );
+    }
+
+    return true;
+  }
+}
--- KalypsoUI/src/org/kalypso/ogc/gml/om/chart/TupleResultChartLayer.java
+++ KalypsoUI/src/org/kalypso/ogc/gml/om/chart/TupleResultChartLayer.java
@@ -40,73 +40,155 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ogc.gml.om.chart;
 
+import org.kalypso.contribs.eclipse.swt.graphics.GCWrapper;
+import org.kalypso.observation.result.IComponent;
+import org.kalypso.observation.result.IRecord;
+import org.kalypso.observation.result.TupleResult;
+import org.kalypso.swtchart.axis.IAxis;
+import org.kalypso.swtchart.layer.IChartLayer;
+import org.kalypso.swtchart.layer.IDataRange;
+import org.kalypso.swtchart.legend.ILegendItem;
+import org.kalypso.swtchart.styles.ILayerStyle;
 
 /**
  * @author schlienger
  */
-public class TupleResultChartLayer //extends AbstractChartLayer
+public class TupleResultChartLayer implements IChartLayer
 {
-//  private final TupleResult m_result;
+  private final TupleResult m_result;
 
-//  public TupleResultChartLayer( TupleResult result, AxisRange domainRange, AxisRange valueRange )
-//  {
-//    //super( domainRange, valueRange );
-//    m_result = result;
-//  }
-//
-//  /**
-//   * @see de.belger.swtchart.layer.IChartLayer#getBounds()
-//   */
-//  public Rectangle2D getBounds( )
-//  {
-//    return null;
-//  }
-//
-//  /**
-//   * @see de.belger.swtchart.layer.IChartLayer#getHoverInfo(org.eclipse.swt.graphics.Point)
-//   */
-//  public EditInfo getHoverInfo( Point point )
-//  {
-//    return null;
-//  }
-//
-//  /**
-//   * @see de.belger.swtchart.layer.IChartLayer#paintDrag(org.kalypso.contribs.eclipse.swt.graphics.GCWrapper, org.eclipse.swt.graphics.Point, java.lang.Object)
-//   */
-//  public void paintDrag( GCWrapper gc, Point editing, Object hoverData )
-//  {
-//
-//  }
-//
-//  /**
-//   * @see de.belger.swtchart.layer.IChartLayer#paintLegend(org.kalypso.contribs.eclipse.swt.graphics.GCWrapper)
-//   */
-//  public void paintLegend( GCWrapper gc )
-//  {
-//
-//  }
-//
-//  /**
-//   * @see de.belger.swtchart.layer.IChartLayer#paint(org.kalypso.contribs.eclipse.swt.graphics.GCWrapper)
-//   */
-//  public void paint( GCWrapper gc )
-//  {
-//
-//  }
-//
-//  /**
-//   * @see de.belger.swtchart.layer.IChartLayer#edit(org.eclipse.swt.graphics.Point, java.lang.Object)
-//   */
-//  public void edit( Point point, Object data )
-//  {
-//
-//  }
-//
-//  /**
-//   * @see de.belger.swtchart.layer.IChartLayer#setActivePoint(java.lang.Object)
-//   */
-//  public void setActivePoint( Object data )
-//  {
-//
-//  }
+  private final IAxis m_valAxis;
+
+  private final IComponent m_domComp;
+
+  private final IComponent m_valComp;
+
+  private final IAxis m_domAxis;
+
+  public TupleResultChartLayer( TupleResult result, IComponent domComp, IComponent valComp, IAxis domAxis, IAxis valAxis )
+  {
+    m_result = result;
+    m_domComp = domComp;
+    m_valComp = valComp;
+    m_domAxis = domAxis;
+    m_valAxis = valAxis;
+  }
+
+  /**
+   * @see org.kalypso.swtchart.layer.IChartLayer#paint(org.kalypso.contribs.eclipse.swt.graphics.GCWrapper)
+   */
+  public void paint( final GCWrapper gc )
+  {
+    int size = m_result.size();
+    for( int i = 0, j = 1; j < size; i++, j++ )
+    {
+      final IRecord r1 = m_result.get( i );
+      final IRecord r2 = m_result.get( j );
+
+      Object x1 = m_result.getValue( r1, m_domComp );
+      Object y1 = m_result.getValue( r1, m_valComp );
+
+      Object x2 = m_result.getValue( r2, m_domComp );
+      Object y2 = m_result.getValue( r2, m_valComp );
+
+      int ix1 = m_domAxis.logicalToScreen( x1 );
+      int iy1 = m_valAxis.logicalToScreen( y1 );
+
+      int ix2 = m_domAxis.logicalToScreen( x2 );
+      int iy2 = m_valAxis.logicalToScreen( y2 );
+
+      System.out.println("drawing line...");
+      
+      gc.drawLine( ix1, iy1, ix2, iy2 );
+    }
+  }
+
+  /**
+   * @see org.kalypso.swtchart.layer.IChartLayer#getName()
+   */
+  public String getName( )
+  {
+    return "";
+  }
+
+  /**
+   * @see org.kalypso.swtchart.layer.IChartLayer#getDescription()
+   */
+  public String getDescription( )
+  {
+    return "";
+  }
+
+  /**
+   * @see org.kalypso.swtchart.layer.IChartLayer#getDomainAxis()
+   */
+  public IAxis getDomainAxis( )
+  {
+    return m_domAxis;
+  }
+
+  /**
+   * @see org.kalypso.swtchart.layer.IChartLayer#getRangeAxis()
+   */
+  public IAxis getValueAxis( )
+  {
+    return m_valAxis;
+  }
+
+  /**
+   * @see org.kalypso.swtchart.layer.IChartLayer#getLegendItem()
+   */
+  public ILegendItem getLegendItem( )
+  {
+    return null;
+  }
+
+  /**
+   * @see org.kalypso.swtchart.layer.IChartLayer#isActive()
+   */
+  public boolean isActive( )
+  {
+    return true;
+  }
+
+  /**
+   * @see org.kalypso.swtchart.layer.IChartLayer#isVisible()
+   */
+  public boolean isVisible( )
+  {
+    return true;
+  }
+
+  /**
+   * @see org.kalypso.swtchart.layer.IChartLayer#getStyle()
+   */
+  public ILayerStyle getStyle( )
+  {
+    return null;
+  }
+
+  /**
+   * @see org.kalypso.swtchart.layer.IChartLayer#setStyle(org.kalypso.swtchart.styles.ILayerStyle)
+   */
+  public void setStyle( ILayerStyle style )
+  {
+  }
+
+  /**
+   * @see org.kalypso.swtchart.layer.IChartLayer#getDomainRange()
+   */
+  public IDataRange getDomainRange( )
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /**
+   * @see org.kalypso.swtchart.layer.IChartLayer#getValueRange()
+   */
+  public IDataRange getValueRange( )
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
 }
