847a89ebb5b5422687de290550c9723c41ea0f7d
schlienger <schlienger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-06-20 10:55:03 +0000

    Eigenes Projekt für das neue Kalypso Chart API

--- KalypsoCommons/src/org/kalypso/commons/java/lang/MathUtils.java
+++ KalypsoCommons/src/org/kalypso/commons/java/lang/MathUtils.java
@@ -30,26 +30,158 @@
 package org.kalypso.commons.java.lang;
 
 /**
- * 
- * TODO: insert type comment here
- * 
- * @author doemming
+ * @author doemming,fichtner
  */
 public class MathUtils
 {
-
   /**
-   * 
-   * @param x1
-   * @param x2
-   * @param y1
-   * @param y2
-   * @param x
    * @return f(x) on line (x1,y1) (x2,y2)
    */
   public static double interpolate( long x1, long x2, double y1, double y2, long x )
   {
-    return y1 + ( y2 - y1 ) * ( ( (double)( x - x1 ) ) / ( (double)( x2 - x1 ) ) );
+    return y1 + (y2 - y1) * (((double) (x - x1)) / ((double) (x2 - x1)));
   }
 
+  public static enum RoundMethod
+  {
+    UP
+    {
+      @Override
+      public double round( final double d )
+      {
+        return Math.ceil( d );
+      }
+    },
+    DOWN
+    {
+      @Override
+      public double round( final double d )
+      {
+        return Math.floor( d );
+      }
+    },
+    HALF_UP
+    {
+      @Override
+      public double round( final double d )
+      {
+        return Math.rint( d );
+      }
+    };
+
+    public abstract double round( final double d );
+  }
+
+  public static double round( double d, final RoundMethod method )
+  {
+    int faktor = 1;
+    boolean negative = false;
+
+    if( d == 0 || Double.isInfinite( d ) )
+      return d;
+
+    if( d < 0 )
+    {
+      d = -d;
+      negative = true;
+    }
+
+    if( d < 1 )
+    {
+      while( d < 1 )
+      {
+        d = d * 10;
+        faktor = faktor * 10;
+      }
+
+      d = method.round( d );
+      d = d / faktor;
+    }
+    else if( d > 10 )
+    {
+      while( d > 10 )
+      {
+        d = d / 10;
+        faktor = faktor * 10;
+      }
+
+      d = method.round( d );
+
+      d = d * faktor;
+    }
+    else
+      d = method.round( d );
+
+    return negative ? -d : d;
+  }
+
+  /**
+   * Gibt die Anzahl der Stellen nach dem Komma bis zur ersten Zahl != 0
+   */
+  public static int scale( double d )
+  {
+    double abs = Math.abs( d );
+
+    if( abs > 1 )
+      return 0;
+
+    double bruchTeil = abs - Math.floor( abs );
+    int scale = 0;
+
+    if( bruchTeil > 0 )
+    {
+      while( bruchTeil < 1 )
+      {
+        bruchTeil = bruchTeil * 10;
+        scale++;
+      }
+    }
+
+    return scale;
+  }
+
+  /**
+   * Rundet die Zahl d auf die Position scale mit einer bestimmten Rundungsmethode roundMethod
+   */
+  public static double setScale( double d, final int scale, final RoundMethod method )
+  {
+    int c = 0;
+    long faktor = 1;
+
+    while( c < scale )
+    {
+      d = d * 10;
+      faktor = faktor * 10;
+      c++;
+    }
+
+    d = method.round( d );
+
+    // den Originalwert wiederherstellen
+    d = d / faktor;
+
+    return d;
+  }
+
+  public static final double nanMin( final double v1, final double v2 )
+  {
+    if( Double.isNaN( v1 ) )
+      return v2;
+
+    if( Double.isNaN( v2 ) )
+      return v1;
+
+    return Math.min( v1, v2 );
+  }
+
+  public static final double nanMax( final double v1, final double v2 )
+  {
+    if( Double.isNaN( v1 ) )
+      return v2;
+
+    if( Double.isNaN( v2 ) )
+      return v1;
+
+    return Math.max( v1, v2 );
+  }
 }
