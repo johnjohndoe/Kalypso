705124e84e3d9a46a7c2519014297c538eaf7515
thuel <thuel@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-07-05 15:26:12 +0000

    ungültige Zeichen in Temp-Dateinamen

--- _import/branches/branch-2005-12-2-kalypso-1-0/Common/EclipseRCPContributions/src/org/kalypso/contribs/eclipse/core/runtime/TempFileUtilities.java
+++ _import/branches/branch-2005-12-2-kalypso-1-0/Common/EclipseRCPContributions/src/org/kalypso/contribs/eclipse/core/runtime/TempFileUtilities.java
@@ -1,43 +1,43 @@
 /*--------------- Kalypso-Header ------------------------------------------
 
-This file is part of kalypso.
-Copyright (C) 2004, 2005 by:
+ This file is part of kalypso.
+ Copyright (C) 2004, 2005 by:
 
-Technical University Hamburg-Harburg (TUHH)
-Institute of River and coastal engineering
-Denickestr. 22
-21073 Hamburg, Germany
-http://www.tuhh.de/wb
+ Technical University Hamburg-Harburg (TUHH)
+ Institute of River and coastal engineering
+ Denickestr. 22
+ 21073 Hamburg, Germany
+ http://www.tuhh.de/wb
 
-and
+ and
 
-Bjoernsen Consulting Engineers (BCE)
-Maria Trost 3
-56070 Koblenz, Germany
-http://www.bjoernsen.de
+ Bjoernsen Consulting Engineers (BCE)
+ Maria Trost 3
+ 56070 Koblenz, Germany
+ http://www.bjoernsen.de
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-Contact:
+ Contact:
 
-E-Mail:
-belger@bjoernsen.de
-schlienger@bjoernsen.de
-v.doemming@tuhh.de
+ E-Mail:
+ belger@bjoernsen.de
+ schlienger@bjoernsen.de
+ v.doemming@tuhh.de
 
---------------------------------------------------------------------------*/
+ --------------------------------------------------------------------------*/
 
 package org.kalypso.contribs.eclipse.core.runtime;
 
@@ -49,14 +49,14 @@ import org.eclipse.core.runtime.Plugin;
 
 /**
  * Utilities for temp files stored within work directories of plugins
- *
+ * 
  * @author schlienger
  */
 public class TempFileUtilities
 {
   private TempFileUtilities()
   {
-    // not intended to be instanciated
+  // not intended to be instanciated
   }
 
   /**
@@ -64,17 +64,22 @@ public class TempFileUtilities
    * Uses File.createTempFile() so as written in the File javadoc, you should call .deleteOnExit() on the returned file
    * instance to make it a real 'temp' file.
    */
-  public static File createTempFile( final Plugin plugin, final String subDirName, String prefix, final String suffix ) throws IOException
+  public static File createTempFile( final Plugin plugin, final String subDirName, String prefix, final String suffix )
+      throws IOException
   {
     if( prefix.length() < 3 )
       prefix += "___";
-    
+
     final IPath path = plugin.getStateLocation();
     final File dir = new File( path.toFile(), subDirName );
     if( !dir.exists() )
       dir.mkdir();
 
-    final File file = File.createTempFile( prefix, suffix, dir );
+    // TODO as org.kalypso.commons.java.io.FileUtilities.validateName() should be moved to JavaApiContribs and could be
+    // used here
+    final String cleanPrefix = prefix.replaceAll( "[\\\\/:\\*\\?\"<>|]", "_" );
+
+    final File file = File.createTempFile( cleanPrefix, suffix, dir );
     return file;
   }
 
@@ -89,7 +94,7 @@ public class TempFileUtilities
     if( dir.exists() )
       cleanDirectory( dir );
   }
-  
+
   private static void cleanDirectory( final File dir )
   {
     final File[] files = dir.listFiles();
@@ -100,7 +105,7 @@ public class TempFileUtilities
       else
         files[i].delete();
     }
-    
+
     dir.delete();
   }
 }
