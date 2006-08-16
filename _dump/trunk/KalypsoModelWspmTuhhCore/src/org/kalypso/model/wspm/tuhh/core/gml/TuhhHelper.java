0a6e3ef40e51faa93aa23dce5f04263c0582f3a0
devgernot <devgernot@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-08-16 10:30:54 +0000

    Restord missing zip with model structure (.model directory and such)

--- _dump/trunk/KalypsoModelWspmTuhhCore/src/org/kalypso/model/wspm/tuhh/core/gml/TuhhHelper.java
+++ _dump/trunk/KalypsoModelWspmTuhhCore/src/org/kalypso/model/wspm/tuhh/core/gml/TuhhHelper.java
@@ -1,16 +1,22 @@
 package org.kalypso.model.wspm.tuhh.core.gml;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 
 import javax.xml.namespace.QName;
 
+import org.apache.commons.io.IOUtils;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.SubProgressMonitor;
+import org.kalypso.commons.java.util.zip.ZipUtilities;
 import org.kalypso.gmlschema.GMLSchemaException;
 import org.kalypso.model.wspm.core.IWspmConstants;
 import org.kalypso.model.wspm.core.gml.WspmWaterBody;
@@ -27,7 +33,10 @@ public class TuhhHelper implements IWspmConstants, IWspmTuhhConstants
   }
 
   /**
-   * TODO: just name it createTuhh-Project Ensures that the given container holds a valid wspm-tuhh modell.
+   * TODO: just name it createTuhh-Project
+   * <p>
+   * Ensures that the given container holds a valid wspm-tuhh modell.
+   * </p>
    * <p>
    * If it is not the case, it creates the structure
    * </p>
@@ -36,8 +45,9 @@ public class TuhhHelper implements IWspmConstants, IWspmTuhhConstants
    */
   public static IFile ensureValidWspmTuhhStructure( final IContainer wspmContainer, final IProgressMonitor monitor ) throws CoreException, InvocationTargetException
   {
-    monitor.beginTask( "Validierung der Modellstruktur", 1000 );
+    monitor.beginTask( "Validierung der Modellstruktur", 2000 );
 
+    InputStream zipInputStream = null;
     try
     {
       if( !wspmContainer.exists() )
@@ -48,7 +58,7 @@ public class TuhhHelper implements IWspmConstants, IWspmTuhhConstants
           ((IFolder) wspmContainer).create( false, true, new SubProgressMonitor( monitor, 100 ) );
         }
         else
-          monitor.worked(100);
+          monitor.worked( 100 );
         // else if( wspmContainer instanceof IProject )
         // {
         // monitor.subTask( "Projekt wird angelegt" );
@@ -59,17 +69,30 @@ public class TuhhHelper implements IWspmConstants, IWspmTuhhConstants
         // }
       }
 
-      monitor.subTask( " - erzeuge Modellstruktur" );
+      monitor.subTask( " - erzeuge Datenmodell" );
 
       final IFile targetFile = wspmContainer.getFile( new Path( "modell.gml" ) );
       // Add the tuhh namespace to the known namespaces, so it is later known for adding new feature
       final String[] additionalNamespaces = new String[] { IWspmTuhhConstants.NS_WSPM_TUHH };
       GmlSerializer.createGmlFile( new QName( IWspmConstants.NS_WSPMPROJ, "WspmProject" ), additionalNamespaces, targetFile, new SubProgressMonitor( monitor, 900 ) );
 
+      monitor.subTask( " - kopiere Modellstruktur" );
+      final File containerDir = wspmContainer.getLocation().toFile();
+
+      zipInputStream = TuhhHelper.class.getResourceAsStream( "resources/wspmTuhh_template.zip" );
+      ZipUtilities.unzip( zipInputStream, containerDir, false );
+      monitor.worked( 500 );
+      wspmContainer.refreshLocal( IResource.DEPTH_INFINITE, new SubProgressMonitor( monitor, 500 ) );
+
       return targetFile;
+    }
+    catch( final IOException e )
+    {
+      throw new InvocationTargetException( e );
     }
     finally
     {
+      IOUtils.closeQuietly( zipInputStream );
       monitor.done();
     }
 
