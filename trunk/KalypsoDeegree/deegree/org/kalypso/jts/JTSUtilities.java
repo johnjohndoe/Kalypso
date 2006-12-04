215cf131c07710c2c0e4ae58f455bbc5a41a263a
albert <albert@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2006-12-04 16:29:03 +0000

    - kalypso-degree:some new jts functions - inform:  length-section

--- trunk/KalypsoDeegree/deegree/org/kalypso/jts/JTSUtilities.java
+++ trunk/KalypsoDeegree/deegree/org/kalypso/jts/JTSUtilities.java
@@ -40,12 +40,16 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.jts;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.kalypso.commons.math.LinearEquation;
 import org.kalypso.commons.math.LinearEquation.SameXValuesException;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
 import com.vividsolutions.jts.geom.Point;
 
 /**
@@ -100,6 +104,9 @@ public class JTSUtilities
       return null;
 
     int numPoints = lineJTS.getNumPoints();
+
+    if( numPoints == 0 )
+      return null;
 
     /* Only loop until the point before the last point. */
     LineString line = null;
@@ -163,7 +170,7 @@ public class JTSUtilities
    *          The distance in percent at which the point should be placed on the line.
    * @return The newly created point on the line or null, if something was wrong.
    */
-  public static com.vividsolutions.jts.geom.Point pointOnLinePercent( LineString lineJTS, int percent )
+  public static Point pointOnLinePercent( LineString lineJTS, int percent )
   {
     if( percent < 0 || percent > 100 )
       return null;
@@ -172,5 +179,186 @@ public class JTSUtilities
     double distance = length / 100.0 * percent;
 
     return pointOnLine( lineJTS, (int) distance );
+  }
+
+  /**
+   * This function creates a line segment (JTS) of a line from a given start point to an end point, including all points
+   * on the given line.
+   * 
+   * @param line
+   *          The original line.
+   * @param start
+   *          The start point of the new line (it has to be one point that lies on the original line).
+   * @param end
+   *          The end point of the new line (it has to be one point that lies on the original line).
+   */
+  public static LineString createLineSegment( Geometry line, Point start, Point end )
+  {
+    // if( !line.crosses( start ) )
+    // return null;
+    //
+    // if( !line.crosses( end ) )
+    // return null;
+
+    if( line instanceof LineString )
+      return createLineSegmentFromLine( (LineString) line, start, end );
+    else if( line instanceof MultiLineString )
+      return createLineSegmentFromMultiLine( (MultiLineString) line, start, end );
+
+    return null;
+  }
+
+  /**
+   * This function creates a line segment (JTS) of a LineString from a given start point to an end point, including all
+   * points on the given LineString.
+   * 
+   * @param line
+   *          The original LineString.
+   * @param start
+   *          The start point of the new line (it has to be one point that lies on the original LineString).
+   * @param end
+   *          The end point of the new line (it has to be one point that lies on the original LineString).
+   */
+  private static LineString createLineSegmentFromLine( LineString line, Point start, Point end )
+  {
+    List<Point> points = new LinkedList<Point>();
+
+    boolean add = false;
+
+    points.add( start );
+
+    for( int i = 0; i < line.getNumPoints() - 1; i++ )
+    {
+      Point pointN = line.getPointN( i );
+      Point pointN1 = line.getPointN( i + 1 );
+
+      if( add )
+        points.add( pointN );
+
+      /* Build a line with the two points to check the flag. */
+      Coordinate[] coordinates = new Coordinate[] { new Coordinate( pointN.getCoordinate() ), new Coordinate( pointN1.getCoordinate() ) };
+      LineString testLine = new LineString( coordinates, line.getPrecisionModel(), line.getSRID() );
+
+      if( testLine.intersects( start ) )
+        add = true;
+
+      if( testLine.intersects( end ) )
+      {
+        add = false;
+        break;
+      }
+    }
+
+    points.add( end );
+
+    /* Create the coordinates for the new line string. */
+    Coordinate[] coordinates = new Coordinate[points.size()];
+
+    for( int i = 0; i < points.size(); i++ )
+      coordinates[i] = new Coordinate( points.get( i ).getCoordinate() );
+
+    LineString newLine = new LineString( coordinates, line.getPrecisionModel(), line.getSRID() );
+
+    return newLine;
+  }
+
+  /**
+   * This function creates a line segment (JTS) of a MultiLineString from a given start point to an end point, including
+   * all points on the given MultiLineString.
+   * 
+   * @param line
+   *          The original MultiLineString.
+   * @param start
+   *          The start point of the new line (it has to be one point one the original MultiLineString).
+   * @param end
+   *          The end point of the new line (it has to be one point one the original MultiLineString).
+   */
+  private static LineString createLineSegmentFromMultiLine( MultiLineString line, Point start, Point end )
+  {
+    List<Point> points = new LinkedList<Point>();
+
+    boolean add = false;
+    boolean endPointFound = false;
+
+    points.add( start );
+
+    for( int i = 0; i < line.getNumGeometries(); i++ )
+    {
+      LineString lineN = (LineString) line.getGeometryN( i );
+
+      for( int j = 0; j < lineN.getNumPoints() - 1; j++ )
+      {
+        Point pointN = lineN.getPointN( j );
+        Point pointN1 = lineN.getPointN( j + 1 );
+
+        if( add )
+          points.add( pointN );
+
+        /* Build a line with the two points to check the flag. */
+        Coordinate[] coordinates = new Coordinate[] { new Coordinate( pointN.getCoordinate() ), new Coordinate( pointN1.getCoordinate() ) };
+        LineString testLine = new LineString( coordinates, lineN.getPrecisionModel(), lineN.getSRID() );
+
+        if( testLine.intersects( start ) )
+          add = true;
+
+        if( testLine.intersects( end ) )
+        {
+          add = false;
+          endPointFound = true;
+          break;
+        }
+      }
+
+      if( endPointFound )
+        break;
+    }
+
+    points.add( end );
+
+    /* Create the coordinates for the new line string. */
+    Coordinate[] coordinates = new Coordinate[points.size()];
+
+    for( int i = 0; i < points.size(); i++ )
+      coordinates[i] = new Coordinate( points.get( i ).getCoordinate() );
+
+    LineString newLine = new LineString( coordinates, line.getPrecisionModel(), line.getSRID() );
+
+    return newLine;
+  }
+
+  /**
+   * Returns a vector of this line.
+   * 
+   * @param start
+   *          The start point of the line.
+   * @param end
+   *          The end point of the line.
+   * @return A vector of the line between this two points as point.
+   */
+  public static Point getVector( Point start, Point end )
+  {
+    Coordinate coords = new Coordinate( start.getX() - end.getX(), start.getY() - end.getY() );
+    Point vector = new Point( coords, start.getPrecisionModel(), start.getSRID() );
+
+    return vector;
+  }
+
+  /**
+   * Calculates a normalized vector.
+   * 
+   * @param vector
+   *          The vector to be normalized.
+   * @return The normalized vector.
+   */
+  public static Point getNormalizedVector( Point vector )
+  {
+    double x = vector.getX();
+    double y = vector.getY();
+
+    double laenge = Math.sqrt( x * x + y * y );
+
+    Coordinate coord = new Coordinate( x / laenge, y / laenge );
+
+    return new Point( coord, vector.getPrecisionModel(), vector.getSRID() );
   }
 }
