5f4f6b9d7cc6db58db23c1598f88dbc74fd2f482
dejan <dejan@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-01-04 18:49:00 +0000

    *** empty log message ***

---
+++ _import/trunk/Kalypso/sh/Kalypso1d2dProject/src/org/kalypso/kalypso1d2d/pjt/util/DejanTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.kalypso.kalypso1d2d.pjt.util;
+
+import junit.framework.TestCase;
+
+/**
+ * @author antanas
+ *
+ */
+public class DejanTest extends TestCase {
+
+	/**
+	 * Test method for {@link org.kalypso.kalypso1d2d.pjt.util.ImportUtils#convertShp2Gml(java.lang.String, org.opengis.cs.CS_CoordinateSystem, java.lang.String)}.
+	 */
+	public final void testConvertShp2Gml() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
