35003c3d46472bbe4e0382ed94569194a0972265
alibu <alibu@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-01-08 13:06:14 +0000

    * adjusted lengthSection.kod to new chart configuration * adjusted ChartUI and ModelWSPMTuhhSchema to use renamed chart configuration classes * new Layer TupleResultPointChartLayer

--- _dump/trunk/KalypsoModelWspmTuhhSchema/src/org/kalypso/model/wspm/tuhh/schema/simulation/LaengsschnittHelper.java
+++ _dump/trunk/KalypsoModelWspmTuhhSchema/src/org/kalypso/model/wspm/tuhh/schema/simulation/LaengsschnittHelper.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.wspm.tuhh.schema.simulation;
 
@@ -57,15 +57,15 @@ import org.eclipse.core.runtime.Platform;
 import org.kalypso.jwsdp.JaxbUtilities;
 import org.kalypso.observation.IObservation;
 import org.kalypso.observation.result.TupleResult;
-import org.kalypso.swtchart.configuration.NewConfigLoader;
+import org.kalypso.swtchart.configuration.ConfigLoader;
 import org.ksp.chart.configuration.AxisType;
 import org.ksp.chart.configuration.ChartType;
 import org.ksp.chart.configuration.ConfigurationType;
 
 /**
- * 
+ *
  * @author Gernot Belger, Alex Burtscher
- * 
+ *
  */
 public class LaengsschnittHelper
 {
@@ -82,9 +82,9 @@ public class LaengsschnittHelper
       return;
 
     /* We just load the template and tweak the direction of the station-axis */
-    final Unmarshaller unmarshaller = NewConfigLoader.JC.createUnmarshaller();
+    final Unmarshaller unmarshaller = ConfigLoader.JC.createUnmarshaller();
     final JAXBElement<ConfigurationType> config = (JAXBElement<ConfigurationType>) unmarshaller.unmarshal( LaengsschnittHelper.class.getResource( "resources/lengthSection.kod" ) );
-    
+
     List<Object> chartOrLayerOrAxis = config.getValue().getChartOrLayerOrAxis();
     for( Object object : chartOrLayerOrAxis )
     {
@@ -107,7 +107,7 @@ public class LaengsschnittHelper
       }
     }
 
-    final Marshaller marshaller = JaxbUtilities.createMarshaller( NewConfigLoader.JC, true );
+    final Marshaller marshaller = JaxbUtilities.createMarshaller( ConfigLoader.JC, true );
 
     OutputStream os = null;
 
