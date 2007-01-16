fe7b95a19b34b4f4df7f3cf24d981f581655e756
devgernot <devgernot@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-01-16 18:50:44 +0000

    Improved perfomance tool TimeLogger

--- KalypsoCommons/src/org/kalypso/commons/performance/TimeLogger.java
+++ KalypsoCommons/src/org/kalypso/commons/performance/TimeLogger.java
@@ -40,37 +40,95 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.commons.performance;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeConstants;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.Duration;
+
 /**
- * A small helper class wich shows usthe duraction of some code.<p>
- * Usage:</p>
- * <p>The class remebers its time of construction</p>
- * <p>Call #showElapsedTime( String ) to print a message, indicating how long it took since construction</p>.
+ * A small helper class wich shows usthe duraction of some code.
+ * <p>
+ * Usage:
+ * </p>
+ * <p>
+ * The class remebers its time of construction
+ * </p>
+ * <p>
+ * Call #showElapsedTime( String ) to print a message, indicating how long it took since construction
+ * </p>.
  * 
  * @author Belger
- *
  */
 public class TimeLogger
 {
-  private long m_constructionTime;
+  private final static DatatypeConstants.Field[] FIELDS_HOURS = { DatatypeConstants.HOURS, DatatypeConstants.MINUTES, DatatypeConstants.SECONDS };
   
+  private DatatypeFactory m_factory = null;
+  
+  private final long m_constructionTime;
+
   private long m_lastCall;
+
+  private Duration m_currentInterimDuration;
+
+  private Duration m_currentTotalDuration;
 
   public TimeLogger( )
   {
     this( null );
   }
-  
+
+  /**
+   * Create a time logger and start measuring. Prints the given message.
+   */
   public TimeLogger( final String message )
   {
     System.out.println( message );
+    
     m_constructionTime = System.currentTimeMillis();
+    try
+    {
+      m_factory = DatatypeFactory.newInstance();
+    }
+    catch( final DatatypeConfigurationException e )
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    
     m_lastCall = m_constructionTime;
   }
 
+  public void takeInterimTime( )
+  {
+    final long current = System.currentTimeMillis();
+
+    final long sinceLastCall = current - m_lastCall;
+    final long total = current - m_constructionTime;
+
+    m_currentInterimDuration = m_factory.newDuration( sinceLastCall );
+    m_currentTotalDuration = m_factory.newDuration( total );
+    
+    m_lastCall = current;
+  }
+
+  public void printCurrentInterim( final String message )
+  {
+    printDuration( message, m_currentInterimDuration, FIELDS_HOURS );
+  }
+
+  public void printCurrentTotal( final String message )
+  {
+    printDuration( message, m_currentTotalDuration, FIELDS_HOURS);
+  }
+  
+  /**
+   *  @deprecated Use {@link #takeInterimTime()} and instead.
+   */
   public void showElapsedTime( final String message )
   {
     final long current = System.currentTimeMillis();
-    
+
     final long sinceLastCall = current - m_lastCall;
     final long total = current - m_constructionTime;
     System.out.print( message );
@@ -79,8 +137,25 @@ public class TimeLogger
     System.out.print( " millis / SinceLast " );
     System.out.print( sinceLastCall );
     System.out.println( " millis." );
-    
+
     m_lastCall = current;
   }
 
+  private static void printDuration( final String message, final Duration duration, final DatatypeConstants.Field[] fields )
+  {
+    System.out.print( message );
+
+    for( final DatatypeConstants.Field field : fields )
+    {
+      if( duration.isSet( field ) )
+      {
+        System.out.print( duration.getField( field ) );
+        System.out.print( " " );
+        System.out.print( field.toString() );
+        System.out.print( ", " );
+      }
+    }
+    System.out.println();
+  }
+  
 }
