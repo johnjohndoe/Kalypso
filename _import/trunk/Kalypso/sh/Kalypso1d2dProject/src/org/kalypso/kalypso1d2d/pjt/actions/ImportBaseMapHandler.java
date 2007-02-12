4b5680d5c8fe1609e330def24ab02c7d2dfa8813
dejan <dejan@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-02-12 14:58:17 +0000

    *** empty log message ***

---
+++ _import/trunk/Kalypso/sh/Kalypso1d2dProject/src/org/kalypso/kalypso1d2d/pjt/actions/ImportBaseMapHandler.java
@@ -0,0 +1,67 @@
+package org.kalypso.kalypso1d2d.pjt.actions;
+
+import java.util.HashMap;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.window.Window;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.ISources;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.wizards.IWizardDescriptor;
+import org.kalypso.kalypsosimulationmodel.core.terrainmodel.IRoughnessPolygonCollection;
+import org.kalypso.ui.wizards.imports.INewWizardKalypsoImport;
+
+import de.renew.workflow.WorkflowCommandHandler;
+
+/**
+ * Starts the import roughness wizard
+ * 
+ * @author Stefan Kurzbach
+ */
+public class ImportBaseMapHandler extends WorkflowCommandHandler
+{
+  private static final String WIZARD_ID = "org.kalypso.ui.wizards.imports.baseMap.ImportBaseMapWizard";
+
+  /**
+   * @see org.kalypso.kalypsomodel1d2d.ui.WorkflowCommandHandler#executeInternal(org.eclipse.core.commands.ExecutionEvent)
+   */
+  @Override
+  protected IStatus executeInternal( final ExecutionEvent event ) throws CoreException
+  {
+    final IEvaluationContext context = (IEvaluationContext) event.getApplicationContext();
+    IStructuredSelection selection = (IStructuredSelection) context.getVariable( ISources.ACTIVE_CURRENT_SELECTION_NAME );
+    if(selection == null)
+    {
+      final IResource currentFolder = (IFolder) context.getVariable( "activeSimulationModelBaseFolder" );
+      selection = new StructuredSelection(currentFolder);
+    }
+    final IWorkbenchWindow workbenchWindow = (IWorkbenchWindow) context.getVariable( ISources.ACTIVE_WORKBENCH_WINDOW_NAME );
+    final IWorkbench workbench = (workbenchWindow).getWorkbench();
+
+    final IWizardDescriptor wizardDescriptor = workbench.getNewWizardRegistry().findWizard( WIZARD_ID );
+    final INewWizardKalypsoImport wizard = (INewWizardKalypsoImport) wizardDescriptor.createWizard();
+    final WizardDialog wizardDialog = new WizardDialog( workbenchWindow.getShell(), wizard );
+    
+    final IFolder currentFolder = (IFolder) context.getVariable( "activeSimulationModelBaseFolder" );
+    final HashMap<String, Object> data = new HashMap<String, Object>();
+    data.put( "ScenarioFolder", currentFolder.getFullPath().toOSString() );
+    // data.put( "ActiveSimulationModelBaseFolder", currentFolder.getFullPath() );
+
+    wizard.init( workbench, selection );
+    wizard.initModelProperties( data );
+    if( wizardDialog.open() == Window.OK )
+      return Status.OK_STATUS;
+    return Status.CANCEL_STATUS;
+
+  }
+}
