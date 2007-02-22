d92a29b256ddffc2cc5ec0bd5c2c9e1aca44c75c
congo <congo@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-02-22 11:50:01 +0000

    change to support staring widget funtionality into the worflow

--- KalypsoUI/src/org/kalypso/ogc/gml/map/widgets/SelectWidgetHandler.java
+++ KalypsoUI/src/org/kalypso/ogc/gml/map/widgets/SelectWidgetHandler.java
@@ -1,17 +1,23 @@
 package org.kalypso.ogc.gml.map.widgets;
 
+import java.util.Map;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.ISources;
 import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
 import org.kalypso.ogc.gml.map.MapPanel;
 import org.kalypso.ogc.gml.widgets.IWidget;
 import org.kalypso.ui.editor.mapeditor.GisMapEditor;
@@ -23,30 +29,60 @@ import org.osgi.framework.Bundle;
  * 
  * @author Stefan Kurzbach
  */
-public class SelectWidgetHandler extends AbstractHandler implements IHandler
+public class SelectWidgetHandler extends AbstractHandler implements IHandler, IExecutableExtension
 {
+  private String m_widgetClassFromExtension;
 
+  private String m_pluginIdFromExtension;
+
+  @Override
   public Object execute( final ExecutionEvent event )
   {
-    final String widgetParameter = event.getParameter( SelectWidgetCommandActionDelegate.PARAM_WIDGET_CLASS );
-    final String pluginParameter = event.getParameter( SelectWidgetCommandActionDelegate.PARAM_PLUGIN_ID );
+    final String widgetFromEvent = event.getParameter( SelectWidgetCommandActionDelegate.PARAM_WIDGET_CLASS );
+    final String widgetParameter;
+    if( widgetFromEvent != null )
+    {
+      widgetParameter = widgetFromEvent;
+    }
+    else
+    {
+      widgetParameter = m_widgetClassFromExtension;
+    }
+
+    final String pluginFromEvent = event.getParameter( SelectWidgetCommandActionDelegate.PARAM_PLUGIN_ID );
+    final String pluginParameter;
+    if( pluginFromEvent != null )
+    {
+      pluginParameter = pluginFromEvent;
+    }
+    else
+    {
+      pluginParameter = m_pluginIdFromExtension;
+    }
     final IWidget widget = getWidgetFromBundle( pluginParameter, widgetParameter );
-    final IEvaluationContext applicationContext = (IEvaluationContext) event.getApplicationContext();    
+    final IEvaluationContext applicationContext = (IEvaluationContext) event.getApplicationContext();
     final IEditorPart editor = (IEditorPart) applicationContext.getVariable( ISources.ACTIVE_EDITOR_NAME );
-    final IWorkbenchPart workbenchPart;
+    IWorkbenchPart workbenchPart = null;
+    final IWorkbenchWindow workbenchWindow = (IWorkbenchWindow) applicationContext.getVariable( ISources.ACTIVE_WORKBENCH_WINDOW_NAME );
+    if( workbenchWindow == null )
+    {
+      return null;
+    }
+
+    final IWorkbenchPage activePage = workbenchWindow.getActivePage();
     if( editor != null && editor.getEditorSite().getId().equals( GisMapEditor.ID ) )
     {
       workbenchPart = editor;
     }
     else
     {
-      final IWorkbenchWindow workbenchWindow = (IWorkbenchWindow) applicationContext.getVariable( ISources.ACTIVE_WORKBENCH_WINDOW_NAME );
-      final IViewPart mapView = workbenchWindow.getActivePage().findView( MapView.ID );
+      final IViewPart mapView = activePage.findView( MapView.ID );
       workbenchPart = mapView;
     }
-    
+
     if( workbenchPart != null )
     {
+      activePage.activate( workbenchPart );
       final MapPanel mapPanel = (MapPanel) workbenchPart.getAdapter( MapPanel.class );
       if( mapPanel != null && widget != null )
       {
@@ -69,6 +105,20 @@ public class SelectWidgetHandler extends AbstractHandler implements IHandler
       e.printStackTrace();
     }
     return null;
+  }
+
+  /**
+   * @see org.kalypso.ui.GenericCommandActionDelegate#setInitializationData(org.eclipse.core.runtime.IConfigurationElement,
+   *      java.lang.String, java.lang.Object)
+   */
+  public void setInitializationData( final IConfigurationElement config, final String propertyName, final Object data ) throws CoreException
+  {
+    if( data != null && data instanceof Map )
+    {
+      Map parameterMap = (Map) data;
+      m_widgetClassFromExtension = (String) parameterMap.get( SelectWidgetCommandActionDelegate.PARAM_WIDGET_CLASS );
+      m_pluginIdFromExtension = (String) parameterMap.get( SelectWidgetCommandActionDelegate.PARAM_PLUGIN_ID );
+    }
   }
 
 }
