2b5d1667cc13a41b4c042f107656bff3be645952
thomas <thomas@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-03-08 08:59:02 +0000

    added image descriptor

--- _import/trunk/Kalypso/sh/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/KalypsoModel1D2DPlugin.java
+++ _import/trunk/Kalypso/sh/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/KalypsoModel1D2DPlugin.java
@@ -3,43 +3,49 @@
  */
 package org.kalypso.kalypsomodel1d2d;
 
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.kalypso.contribs.eclipse.core.runtime.PluginUtilities;
 import org.osgi.framework.BundleContext;
 
 /**
  * @author Gernot Belger
  */
-public class KalypsoModel1D2DPlugin extends AbstractUIPlugin {
+public class KalypsoModel1D2DPlugin extends AbstractUIPlugin
+{
 
-	// The shared instance.
-	private static KalypsoModel1D2DPlugin plugin;
+  // The shared instance.
+  private static KalypsoModel1D2DPlugin plugin;
 
-	public KalypsoModel1D2DPlugin() {
-		plugin = this;
-	}
+  public KalypsoModel1D2DPlugin( )
+  {
+    plugin = this;
+  }
 
-	/**
-	 * This method is called upon plug-in activation
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
+  /**
+   * This method is called upon plug-in activation
+   */
+  @Override
+  public void start( BundleContext context ) throws Exception
+  {
+    super.start( context );
+  }
 
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
-		plugin = null;
-	}
+  /**
+   * This method is called when the plug-in is stopped
+   */
+  @Override
+  public void stop( BundleContext context ) throws Exception
+  {
+    super.stop( context );
+    plugin = null;
+  }
 
-	/**
-	 * Returns the shared instance.
-	 */
-	public static KalypsoModel1D2DPlugin getDefault() {
-		return plugin;
-	}
-
+  /**
+   * Returns the shared instance.
+   */
+  public static KalypsoModel1D2DPlugin getDefault( )
+  {
+    return plugin;
+  }
 }
