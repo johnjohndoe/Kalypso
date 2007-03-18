3e41094e9401732132bcaaae9a74c1ddf02ecf5c
devgernot <devgernot@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-03-18 10:24:24 +0000

    Better error handliing for feature-control. ON any error a text-field with the stack appears, the stack can be copy-pasted.

--- trunk/KalypsoUI/src/org/kalypso/ogc/gml/featureview/control/FeatureComposite.java
+++ trunk/KalypsoUI/src/org/kalypso/ogc/gml/featureview/control/FeatureComposite.java
@@ -218,8 +218,9 @@ public class FeatureComposite extends AbstractFeatureControl implements IFeature
     }
     catch( final Throwable t )
     {
-      final org.eclipse.swt.widgets.Text text = new org.eclipse.swt.widgets.Text( parent, SWT.NONE );
-      text.setEnabled( false );
+      final org.eclipse.swt.widgets.Text text = new org.eclipse.swt.widgets.Text( parent, SWT.MULTI );
+      text.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true ) );
+      text.setEditable( false );
       final String trace = ExceptionUtils.getStackTrace( t );
       text.setText( trace );
       return text;
@@ -514,7 +515,7 @@ public class FeatureComposite extends AbstractFeatureControl implements IFeature
     {
       final KalypsoGisPlugin plugin = KalypsoGisPlugin.getDefault();
       final TableFeatureContol fc = new TableFeatureContol( ftp, plugin.createFeatureTypeCellEditorFactory(), m_selectionManager, this );
-      
+
       final Gistableview gistableview = ((Table) controlType).getGistableview();
       if( gistableview != null )
       {
@@ -522,12 +523,12 @@ public class FeatureComposite extends AbstractFeatureControl implements IFeature
       }
 
       fc.setFeature( feature );
-      
+
       addFeatureControl( fc );
 
       final Control control = fc.createControl( parent, SWT.NONE );
       control.setLayoutData( new GridData() );
-      
+
       return control;
     }
 
