2de7f839cdc50376d92e1dc84a6f14a86e20722b
congo <congo@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-05-04 19:58:00 +0000

    trying to improve roughness fem layer

--- KalypsoCore/src/org/kalypso/ogc/gml/KalypsoFeatureTheme.java
+++ KalypsoCore/src/org/kalypso/ogc/gml/KalypsoFeatureTheme.java
@@ -117,7 +117,13 @@ public class KalypsoFeatureTheme extends AbstractKalypsoTheme implements IKalyps
 
     if( m_featureList != null && m_featureList.getParentFeature() == null )
     {
-      System.out.println( "BAD!" );
+      String message =
+        String.format( 
+            "BAD: layer with no feature list or feature listList.getParent is empty: "+
+            "\n\tfeaturePath = %s\n\tfeatureFromPath = %s", 
+            featurePath,
+            featureFromPath );
+      System.out.println( message );
     }
 
     m_workspace.addModellListener( this );
