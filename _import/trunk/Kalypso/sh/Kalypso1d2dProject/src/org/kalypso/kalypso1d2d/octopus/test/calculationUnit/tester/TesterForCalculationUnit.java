69ea3a6f513469aa2c4f0093b04fc69f418be77c
madan <madan@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-06-25 14:39:35 +0000

    Calculation Error Checks

--- _import/trunk/Kalypso/sh/Kalypso1d2dProject/src/org/kalypso/kalypso1d2d/octopus/test/calculationUnit/tester/TesterForCalculationUnit.java
+++ _import/trunk/Kalypso/sh/Kalypso1d2dProject/src/org/kalypso/kalypso1d2d/octopus/test/calculationUnit/tester/TesterForCalculationUnit.java
@@ -1,22 +1,24 @@
 package org.kalypso.kalypso1d2d.octopus.test.calculationUnit.tester;
 
-import org.kalypso.kalypso1d2d.octopus.test.calculationUnit.BoundaryCondition;
-import org.kalypso.kalypso1d2d.octopus.test.calculationUnit.BoundaryLine;
-import org.kalypso.kalypso1d2d.octopus.test.calculationUnit.CalculationUnit;
+import org.kalypso.kalypso1d2d.octopus.test.calculationUnit.MergeBoundaryCondition;
+import org.kalypso.kalypso1d2d.octopus.test.calculationUnit.MergeBoundaryLine;
+import org.kalypso.kalypso1d2d.octopus.test.calculationUnit.MergeCalculationUnit;
+
+
 
 public class TesterForCalculationUnit {
 	public static void main(String args[]){
-		BoundaryLine bline = new BoundaryLine();
-		BoundaryLine bline1 = new BoundaryLine();
-		BoundaryLine bline2 = new BoundaryLine();
+		MergeBoundaryLine bline = new MergeBoundaryLine();
+		MergeBoundaryLine bline1 = new MergeBoundaryLine();
+		MergeBoundaryLine bline2 = new MergeBoundaryLine();
 		
-		bline.addToBoundaryCondition(new BoundaryCondition());
-		bline.addToBoundaryCondition(new BoundaryCondition());
-		bline.addToBoundaryCondition(new BoundaryCondition());
+		MergeCalculationUnit calc = new MergeCalculationUnit();
+		bline.addToBoundaryCondition(calc,new MergeBoundaryCondition());
+		//bline.addToBoundaryCondition(new BoundaryCondition());
+		//bline.addToBoundaryCondition(new BoundaryCondition());
 		
-		CalculationUnit calc = new CalculationUnit();
 		calc.addToBoundaryLine(bline);
-		//calc.addToBoundaryLine(bline1);
+		calc.addToBoundaryLine(bline1);
 		//calc.addToBoundaryLine(bline2);
 		
 		System.out.println(calc.checkAllInvariants());
