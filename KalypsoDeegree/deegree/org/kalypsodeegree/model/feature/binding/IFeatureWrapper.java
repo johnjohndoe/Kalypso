82607d21b567b144833267ce5f64d72695299be4
devgernot <devgernot@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-07-17 14:29:02 +0000

    Fix for junction element writing

--- KalypsoDeegree/deegree/org/kalypsodeegree/model/feature/binding/IFeatureWrapper.java
+++ KalypsoDeegree/deegree/org/kalypsodeegree/model/feature/binding/IFeatureWrapper.java
@@ -1,19 +1,16 @@
 package org.kalypsodeegree.model.feature.binding;
 
-import org.kalypsodeegree.model.feature.Feature;
-
 /**
- * Classes implementing this interface signals their feature wrapper nature and provide an implementation of
- * {@link #getWrappedFeature()} to get the feature they are wrapping
+ * TODO: delete this interface! Classes implementing this interface signals their feature wrapper nature and provide an
+ * implementation of {@link #getWrappedFeature()} to get the feature they are wrapping
  * 
- * @deprecated Use {@link IFeatureWrapper2} instead. 
- * 
+ * @deprecated Use {@link IFeatureWrapper2} instead.
  * @author Patrice Congo
  */
 @Deprecated
-/*public*/ interface IFeatureWrapper
+/* public */interface IFeatureWrapper
 {
-//  public Feature getWrappedFeature( );
+// public Feature getWrappedFeature( );
 
-//  public String getGmlID( );
+// public String getGmlID( );
 }
