754b11b7d18371d8883a835a1d280feaded6438f
huebsch <huebsch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-08-02 14:51:12 +0000

    split of manuals

---
+++ _import/trunk/Kalypso/sh/KalypsoManual1D2D/src/org/kalypso/KalypsoManual1d2d/help/IKalypsoHelpContextIds.java
@@ -0,0 +1,73 @@
+package org.kalypso.KalypsoManual1d2d.help;			
+/**
+ * @see KalypsoManual common-eclipse.xsl and KalypsoBuild/publishing/docbook-xsl-1.69.1/eclipse/eclipse-ex.xsl
+ *
+ */				
+public interface IKalypsoHelpContextIds
+{
+  public static final String PREFIX = "org.kalypso.manual.";
+					
+				
+			
+  public static final String Kalypso1D2DAllgemeines =PREFIX + "Kalypso1D2DAllgemeines";
+		
+			
+  public static final String Kalypso1D2DProjektbearbeitung =PREFIX + "Kalypso1D2DProjektbearbeitung";
+		
+			
+  public static final String Kalypso1D2DNeuesProjekt =PREFIX + "Kalypso1D2DNeuesProjekt";
+		
+			
+  public static final String Kalypso1D2DVorhProjekt =PREFIX + "Kalypso1D2DVorhProjekt";
+		
+			
+  public static final String Kalypso1D2DPerspektive =PREFIX + "Kalypso1D2DPerspektive";
+		
+			
+  public static final String Kalypso1D2DWorkflow =PREFIX + "Kalypso1D2DWorkflow";
+		
+			
+  public static final String Kalypso1D2DDatenakquisition =PREFIX + "Kalypso1D2DDatenakquisition";
+		
+			
+  public static final String Kalypso1D2DPreprozessing =PREFIX + "Kalypso1D2DPreprozessing";
+		
+			
+  public static final String Kalypso1D2DDatenLaden =PREFIX + "Kalypso1D2DDatenLaden";
+		
+			
+  public static final String Kalypso1D2DImportBackgroundmaps =PREFIX + "Kalypso1D2DImportBackgroundmaps";
+		
+			
+  public static final String Kalypso1D2DImportDHM =PREFIX + "Kalypso1D2DImportDHM";
+		
+			
+  public static final String Kalypso1D2DImportRoughness =PREFIX + "Kalypso1D2DImportRoughness";
+		
+			
+  public static final String Kalypso1D2DImportTimeseries =PREFIX + "Kalypso1D2DImportTimeseries";
+		
+			
+  public static final String Kalypso1D2DImportProf =PREFIX + "Kalypso1D2DImportProf";
+		
+			
+  public static final String Kalypso1D2DNetzaufbau =PREFIX + "Kalypso1D2DNetzaufbau";
+		
+			
+  public static final String Kalypso1D2DRauheitsmodellierung =PREFIX + "Kalypso1D2DRauheitsmodellierung";
+		
+			
+  public static final String Kalypso1D2DProzessing =PREFIX + "Kalypso1D2DProzessing";
+		
+			
+  public static final String Kalypso1D2DBerechnungseinstellungen =PREFIX + "Kalypso1D2DBerechnungseinstellungen";
+		
+			
+  public static final String Kalypso1D2DPostprozessing =PREFIX + "Kalypso1D2DPostprozessing";
+		
+			
+  public static final String Kalypso1D2DModellkalibrierung =PREFIX + "Kalypso1D2DModellkalibrierung";
+		
+}
+				
+			
