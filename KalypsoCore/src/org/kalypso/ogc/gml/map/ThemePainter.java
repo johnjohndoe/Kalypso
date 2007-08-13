eb89d25c8f37bd4c38976bc76720faaa23bf10ab
devgernot <devgernot@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-08-13 06:33:23 +0000

    Bugfix: strange vsibility behaviour of cascaded themes should be fixed

--- KalypsoCore/src/org/kalypso/ogc/gml/map/ThemePainter.java
+++ KalypsoCore/src/org/kalypso/ogc/gml/map/ThemePainter.java
@@ -2,54 +2,54 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ogc.gml.map;
 
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
 import org.kalypso.ogc.gml.IKalypsoTheme;
-import org.kalypso.ogc.gml.mapmodel.IKalypsoThemePredicate;
 import org.kalypso.ogc.gml.mapmodel.IKalypsoThemeVisitor;
 import org.kalypso.ogc.gml.mapmodel.IMapModell;
-import org.kalypso.ogc.gml.mapmodel.visitor.KalypsoThemeVisitor;
 import org.kalypsodeegree.graphics.transformation.GeoTransform;
 import org.kalypsodeegree.model.geometry.GM_Envelope;
 
@@ -93,19 +93,33 @@ public class ThemePainter
 
   private IKalypsoTheme[] getVisibleThemes( final IMapModell model )
   {
-    /* Find all visible themes to be painted. */
-    final IKalypsoThemePredicate visiblePredicate = new IKalypsoThemePredicate()
+    final List<IKalypsoTheme> visibleThemes = new ArrayList<IKalypsoTheme>();
+    final IKalypsoThemeVisitor visitor = new IKalypsoThemeVisitor()
     {
-      public boolean decide( final IKalypsoTheme theme )
+      public boolean visit( final IKalypsoTheme theme )
       {
-        // Also filter out CascadinKalypsoThemes (aka IMapModell's), they would just paint their children.
-        return theme.isVisible() && !(theme instanceof IMapModell);
+        if( theme instanceof IMapModell )
+        {
+          if( theme.isVisible() )
+          {
+            // dont add it to the themes, we paint them ourselfes (the cascading modells paint method would also paint
+            // all is children)
+            return true;
+          }
+
+          // dont descent
+          return false;
+        }
+
+        if( theme.isVisible() )
+          visibleThemes.add( theme );
+
+        // if it is not a map modell we dont recurse
+        return false;
       }
     };
-    final KalypsoThemeVisitor visitor = new KalypsoThemeVisitor( visiblePredicate );
-    model.accept( visitor, IKalypsoThemeVisitor.DEPTH_INFINITE );
-    final IKalypsoTheme[] visibleThemes = visitor.getFoundThemes();
-    return visibleThemes;
-  }
 
+    model.accept( visitor, IKalypsoThemeVisitor.DEPTH_INFINITE );
+    return visibleThemes.toArray( new IKalypsoTheme[visibleThemes.size()] );
+  }
 }
