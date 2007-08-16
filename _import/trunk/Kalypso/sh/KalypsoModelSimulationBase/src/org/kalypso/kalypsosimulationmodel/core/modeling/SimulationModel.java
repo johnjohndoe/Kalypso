c9a41b39779116ff137446745b3aa170aaae1807
thomas <thomas@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-08-16 07:48:50 +0000

    resultdb update.

--- _import/trunk/Kalypso/sh/KalypsoModelSimulationBase/src/org/kalypso/kalypsosimulationmodel/core/modeling/SimulationModel.java
+++ _import/trunk/Kalypso/sh/KalypsoModelSimulationBase/src/org/kalypso/kalypsosimulationmodel/core/modeling/SimulationModel.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.kalypsosimulationmodel.core.modeling;
 
@@ -54,27 +54,14 @@ import org.kalypsodeegree_impl.model.feature.binding.AbstractFeatureBinder;
  * Default, {@link AbstractFeatureBinder} based implementation of {@link ISimulationModel}
  * 
  * @author Patrice Congo
- *
+ * 
  */
-public class SimulationModel<
-                StaticM extends IStaticModel,
-                OpM extends IOperationalModel,
-                CntlM extends IControlModel,
-                ResM extends IResultModel,
-                EvalM extends IEvaluationModel,
-                AdmP extends IFeatureWrapper2,
-                OverM extends IFeatureWrapper2>
-                extends AbstractFeatureBinder
-                implements ISimulationModel< 
-                                          StaticM, OpM, 
-                                          CntlM, ResM, 
-                                          EvalM, AdmP, OverM>
-                
+public class SimulationModel<StaticM extends IStaticModel, OpM extends IOperationalModel, CntlM extends IControlModel, EvalM extends IEvaluationModel, AdmP extends IFeatureWrapper2, OverM extends IFeatureWrapper2>
+    extends AbstractFeatureBinder implements ISimulationModel<StaticM, OpM, CntlM, EvalM, AdmP, OverM>
+
 {
-  
-  
   final private IFeatureWrapperCollection<AdmP> additionalProps;
-  
+
   final private IFeatureWrapperCollection<OverM> overridingModels;
 
   final private Class<StaticM> staticModelCls;
@@ -83,59 +70,48 @@ public class SimulationModel<
 
   final private Class<CntlM> controlModelCls;
 
-  final private Class<ResM> resultModelCls;
-
   final private Class<EvalM> evalModelCls;
 
   final private Class<AdmP> additionalModelPropsCls;
 
   final private Class<OverM> overridingModelCls;
-  
+
   /**
-   * Creates Simulation model wrapping the specified feature and 
-   * holding sumodel of the specified type
-   * @param featureToBind the simulation model feature to bind
-   * @param qnameToBind the q-name of the simulation feature to bind
-   * @param staticModelCls the wrapper class type for the static submodel
-   * @param operationalModelCls the wrapper class type for the operational submodel
-   * @param controlModelCls the wrapper class type for the control sub model
-   * @param resultModelCls the wrapper class type for the result sub model
-   * @param evalModelCls the wrapper class for the result sub model
-   * @param additionalModelPropsCls the wrapper class for additional model properties
-   * @param overridingModelCls the wrapper class for overriding model
+   * Creates Simulation model wrapping the specified feature and holding sumodel of the specified type
+   * 
+   * @param featureToBind
+   *            the simulation model feature to bind
+   * @param qnameToBind
+   *            the q-name of the simulation feature to bind
+   * @param staticModelCls
+   *            the wrapper class type for the static submodel
+   * @param operationalModelCls
+   *            the wrapper class type for the operational submodel
+   * @param controlModelCls
+   *            the wrapper class type for the control sub model
+   * @param resultModelCls
+   *            the wrapper class type for the result sub model
+   * @param evalModelCls
+   *            the wrapper class for the result sub model
+   * @param additionalModelPropsCls
+   *            the wrapper class for additional model properties
+   * @param overridingModelCls
+   *            the wrapper class for overriding model
    * 
    */
-  public SimulationModel(
-              Feature featureToBind,
-              QName qnameToBind,
-              Class<StaticM> staticModelCls, 
-              Class<OpM> operationalModelCls, 
-              Class<CntlM> controlModelCls, 
-              Class<ResM> resultModelCls, 
-              Class<EvalM> evalModelCls, 
-              Class<AdmP> additionalModelPropsCls, 
-              Class<OverM> overridingModelCls)
+  public SimulationModel( Feature featureToBind, QName qnameToBind, Class<StaticM> staticModelCls, Class<OpM> operationalModelCls, Class<CntlM> controlModelCls, Class<EvalM> evalModelCls, Class<AdmP> additionalModelPropsCls, Class<OverM> overridingModelCls )
   {
     super( featureToBind, qnameToBind );
     this.staticModelCls = staticModelCls;
     this.operationalModelCls = operationalModelCls;
     this.controlModelCls = controlModelCls;
-    this.resultModelCls = resultModelCls;
     this.evalModelCls = evalModelCls;
     this.additionalModelPropsCls = additionalModelPropsCls;
     this.overridingModelCls = overridingModelCls;
-    
-    additionalProps = 
-        new FeatureWrapperCollection<AdmP>(
-                featureToBind,
-                additionalModelPropsCls,
-                KalypsoModelSimulationBaseConsts.SIM_BASE_P_ADDITIONAL_MODEL_PROPS );
-    
-    overridingModels = 
-      new FeatureWrapperCollection<OverM>(
-              featureToBind,
-              overridingModelCls,
-              KalypsoModelSimulationBaseConsts.SIM_BASE_P_OVERRIDING_MODEL );
+
+    additionalProps = new FeatureWrapperCollection<AdmP>( featureToBind, additionalModelPropsCls, KalypsoModelSimulationBaseConsts.SIM_BASE_P_ADDITIONAL_MODEL_PROPS );
+
+    overridingModels = new FeatureWrapperCollection<OverM>( featureToBind, overridingModelCls, KalypsoModelSimulationBaseConsts.SIM_BASE_P_OVERRIDING_MODEL );
   }
 
   /**
@@ -143,7 +119,7 @@ public class SimulationModel<
    */
   public IFeatureWrapperCollection<AdmP> getAdditionalModelProps( )
   {
-    
+
     return additionalProps;
   }
 
@@ -152,11 +128,8 @@ public class SimulationModel<
    */
   public CntlM getControlModel( )
   {
-    
-    final CntlM controlModel =
-      getSubModel( 
-        controlModelCls, 
-        KalypsoModelSimulationBaseConsts.SIM_BASE_P_CONTROL_MODEL );
+
+    final CntlM controlModel = getSubModel( controlModelCls, KalypsoModelSimulationBaseConsts.SIM_BASE_P_CONTROL_MODEL );
     return controlModel;
   }
 
@@ -165,10 +138,7 @@ public class SimulationModel<
    */
   public EvalM getEvaluationModel( )
   {
-    final EvalM evaluationModel = 
-      getSubModel( 
-        evalModelCls, 
-        KalypsoModelSimulationBaseConsts.SIM_BASE_P_EVALUATION_MODEL );
+    final EvalM evaluationModel = getSubModel( evalModelCls, KalypsoModelSimulationBaseConsts.SIM_BASE_P_EVALUATION_MODEL );
     return evaluationModel;
   }
 
@@ -177,10 +147,7 @@ public class SimulationModel<
    */
   public OpM getOperationalModel( )
   {
-    final OpM operationalModel = 
-      getSubModel( 
-        operationalModelCls, 
-        KalypsoModelSimulationBaseConsts.SIM_BASE_P_OPERATIONAL_MODEL );
+    final OpM operationalModel = getSubModel( operationalModelCls, KalypsoModelSimulationBaseConsts.SIM_BASE_P_OPERATIONAL_MODEL );
     return operationalModel;
   }
 
@@ -197,34 +164,26 @@ public class SimulationModel<
    */
   public StaticM getStaticModel( )
   {
-    final StaticM staticModel=
-      getSubModel( 
-          staticModelCls, 
-          KalypsoModelSimulationBaseConsts.SIM_BASE_P_STATIC_MODEL );
+    final StaticM staticModel = getSubModel( staticModelCls, KalypsoModelSimulationBaseConsts.SIM_BASE_P_STATIC_MODEL );
     return staticModel;
   }
 
-  private <T extends IFeatureWrapper2> T getSubModel(
-                                          Class<T> adapterTargetClass,
-                                          QName propertyQName )
+  private <T extends IFeatureWrapper2> T getSubModel( Class<T> adapterTargetClass, QName propertyQName )
   {
-//     final Feature feature = getFeature();
-//     final Feature propFeature = FeatureHelper.resolveLink( feature, propertyQName );
-//     if( propFeature == null )
-//     {
-//       return null;
-//     }
-//     else
-//     {
-//       T adaptedFeature = (T) propFeature.getAdapter( adapterTargetClass );
-//       return adaptedFeature;
-//     }
-    
-    return FeatureHelper.resolveLink( 
-              getFeature(), 
-              propertyQName, 
-              adapterTargetClass );
-       
+    // final Feature feature = getFeature();
+    // final Feature propFeature = FeatureHelper.resolveLink( feature, propertyQName );
+    // if( propFeature == null )
+    // {
+    // return null;
+    // }
+    // else
+    // {
+    // T adaptedFeature = (T) propFeature.getAdapter( adapterTargetClass );
+    // return adaptedFeature;
+    // }
+
+    return FeatureHelper.resolveLink( getFeature(), propertyQName, adapterTargetClass );
+
   }
-  
+
 }
--- _import/trunk/Kalypso/sh/KalypsoModelSimulationBase/src/org/kalypso/kalypsosimulationmodel/core/resultmeta/ResultMeta.java
+++ _import/trunk/Kalypso/sh/KalypsoModelSimulationBase/src/org/kalypso/kalypsosimulationmodel/core/resultmeta/ResultMeta.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.kalypsosimulationmodel.core.resultmeta;
 
@@ -106,7 +106,7 @@ public abstract class ResultMeta extends AbstractFeatureBinder implements IResul
    */
   public void setParent( IResultMeta resultMeta )
   {
-    getFeature().setProperty( QNAME_PROP_PARENT, resultMeta );
+    getFeature().setProperty( QNAME_PROP_PARENT, resultMeta.getWrappedFeature() );
   }
 
   /**
@@ -122,7 +122,12 @@ public abstract class ResultMeta extends AbstractFeatureBinder implements IResul
    */
   public void setStatus( IStatus status )
   {
-    getFeature().setProperty( QNAME_PROP_STATUS, status );
+    if( status instanceof org.kalypsodeegree_impl.gml.binding.commons.IStatus )
+      getFeature().setProperty( QNAME_PROP_STATUS, ((org.kalypsodeegree_impl.gml.binding.commons.IStatus) status).getWrappedFeature() );
+    else
+    {
+      // TODO: create new Status and copy values to it
+    }
   }
 
 }
