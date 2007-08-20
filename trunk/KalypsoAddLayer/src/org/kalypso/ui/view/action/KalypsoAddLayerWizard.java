20be3267733f2cc56baf7d98de5f58bfd0140122
devgernot <devgernot@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-08-20 06:24:23 +0000

    Complete - task 42: Put add layer button into workflow

--- trunk/KalypsoAddLayer/src/org/kalypso/ui/view/action/KalypsoAddLayerWizard.java
+++ trunk/KalypsoAddLayer/src/org/kalypso/ui/view/action/KalypsoAddLayerWizard.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraße 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ui.view.action;
 
@@ -56,7 +56,7 @@ import org.kalypso.ui.KalypsoAddLayerPlugin;
 @SuppressWarnings("restriction")
 public class KalypsoAddLayerWizard extends Wizard
 {
-  private IMapModellView m_outlineviewer;
+  private final IMapModellView m_outlineviewer;
 
   private IWorkbench m_workbench;
 
@@ -69,6 +69,7 @@ public class KalypsoAddLayerWizard extends Wizard
     setWindowTitle( "Kalypso Daten Import" ); //$NON-NLS-1$
     setDefaultPageImageDescriptor( WorkbenchImages.getImageDescriptor( IWorkbenchGraphicConstants.IMG_WIZBAN_IMPORT_WIZ ) );
     setNeedsProgressMonitor( true );
+// setDialogSettings( PluginUtilities.getDialogSettings( KalypsoAddLayerPlugin.getDefault(), "addLayerWizard" ) );
   }
 
   /**
@@ -86,6 +87,11 @@ public class KalypsoAddLayerWizard extends Wizard
    * org.kalypso.ui are read. And not as specified from the Workbench ui -> see WizardsRegistryReader.
    */
   protected AdaptableList getAvailableImportWizards( )
+  {
+    return getAvailableWizards();
+  }
+
+  public static AdaptableList getAvailableWizards( )
   {
     final String pluginId = KalypsoAddLayerPlugin.getId();
     final String plugInpointId = KalypsoAddLayerPlugin.PL_IMPORT;
--- trunk/KalypsoAddLayer/src/org/kalypso/ui/view/action/KalypsoWizardSelectionPage.java
+++ trunk/KalypsoAddLayer/src/org/kalypso/ui/view/action/KalypsoWizardSelectionPage.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ui.view.action;
 
@@ -60,7 +60,7 @@ public class KalypsoWizardSelectionPage extends WorkbenchWizardListSelectionPage
 {
   protected IMapModellView m_outline;
 
-  public KalypsoWizardSelectionPage( final IWorkbench aWorkbench, IStructuredSelection selection, AdaptableList wizardElts, String message, IMapModellView outlineview )
+  public KalypsoWizardSelectionPage( final IWorkbench aWorkbench, final IStructuredSelection selection, final AdaptableList wizardElts, final String message, final IMapModellView outlineview )
   {
     super( aWorkbench, selection, wizardElts, message, null );
     m_outline = outlineview;
@@ -77,7 +77,7 @@ public class KalypsoWizardSelectionPage extends WorkbenchWizardListSelectionPage
       @Override
       public IWorkbenchWizard createWizard( ) throws CoreException
       {
-        IKalypsoDataImportWizard createdwizard = (IKalypsoDataImportWizard) element.createWizard();
+        final IKalypsoDataImportWizard createdwizard = (IKalypsoDataImportWizard) element.createWizard();
         // Set the outlineViewer in the import wizard
         createdwizard.setCommandTarget( m_outline );
         createdwizard.setMapModel( m_outline.getMapPanel().getMapModell() );
