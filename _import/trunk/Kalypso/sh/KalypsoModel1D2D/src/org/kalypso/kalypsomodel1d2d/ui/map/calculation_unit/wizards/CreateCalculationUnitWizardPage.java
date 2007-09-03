c0b35d7f32c0c2de926500b5ef60a3482a98205f
dejan <dejan@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-09-03 17:29:14 +0000

    Create/delete calculation unit widget reworked

--- _import/trunk/Kalypso/sh/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/ui/map/calculation_unit/wizards/CreateCalculationUnitWizardPage.java
+++ _import/trunk/Kalypso/sh/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/ui/map/calculation_unit/wizards/CreateCalculationUnitWizardPage.java
@@ -46,8 +46,6 @@ import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Combo;
