ba21fbf46e50f92cf84756558c04674ff1fd2e51
thomas <thomas@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-09-27 12:17:22 +0000

    small bug fix

--- _import/trunk/Kalypso/sh/Kalypso1d2dProject/src/org/kalypso/kalypso1d2d/pjt/Kalypso1d2dProjectImages.java
+++ _import/trunk/Kalypso/sh/Kalypso1d2dProject/src/org/kalypso/kalypso1d2d/pjt/Kalypso1d2dProjectImages.java
@@ -20,8 +20,9 @@ public class Kalypso1d2dProjectImages
 
     RESULT_META_DOCUMENT_NODES("icons/cview16/resViewDocNodes.gif"),
     RESULT_META_DOCUMENT_WSP("icons/cview16/resViewDocWSP.gif"),
-    RESULT_META_DOCUMENT_Depth("icons/cview16/resViewDocDepth.gif"),
-    RESULT_META_DOCUMENT_Velo("icons/cview16/resViewDocVelo.gif"),
+    RESULT_META_DOCUMENT_DEPTH("icons/cview16/resViewDocDepth.gif"),
+    RESULT_META_DOCUMENT_VELO("icons/cview16/resViewDocVelo.gif"),
+    RESULT_META_DOCUMENT_TERRAIN("icons/cview16/resViewDocTerrain.gif"),
     RESULT_META_DOCUMENT_TIN("icons/cview16/resViewDocTIN.gif"),
     RESULT_META_DOCUMENT_HYDRO("icons/cview16/resViewDocHydro.gif"),
     RESULT_META_DOCUMENT_LENGTH_SECTION("icons/cview16/resViewDocLengthSec.gif"),
@@ -35,7 +36,7 @@ public class Kalypso1d2dProjectImages
     RESULT_META_STEP_MAX("icons/cview16/resViewStepMax.gif"),
     RESULT_META_STEP_QSTEADY("icons/cview16/resViewStepQsteady.gif"),
 
-    RESULT_META_ERROR("icons/cview16/resViewDocError2.gif"),
+    RESULT_META_ERROR("icons/cview16/resViewDocError.gif"),
 
     RESULT_VIEWER_EDIT("icons/cview16/resViewEdit.gif"),
     RESULT_VIEWER_REMOVE("icons/cview16/resViewRemove.gif");
