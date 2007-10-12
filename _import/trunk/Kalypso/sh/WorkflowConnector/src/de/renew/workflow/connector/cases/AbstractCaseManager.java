22990b8d0ace8251dd6a8df24a0ef2deb8401ed4
thomas <thomas@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-10-12 07:57:19 +0000

    FloodModeller:  implementation started

--- _import/trunk/Kalypso/sh/WorkflowConnector/src/de/renew/workflow/connector/cases/AbstractCaseManager.java
+++ _import/trunk/Kalypso/sh/WorkflowConnector/src/de/renew/workflow/connector/cases/AbstractCaseManager.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package de.renew.workflow.connector.cases;
 
@@ -54,10 +54,8 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.ui.ide.ResourceUtil;
 import org.kalypso.contribs.eclipse.core.resources.FolderUtilities;
 
 import de.renew.workflow.cases.Case;
@@ -105,7 +103,7 @@ public abstract class AbstractCaseManager<T extends Case> implements ICaseManage
     m_project = project;
 
     final IFolder folder = project.getFolder( METADATA_FOLDER );
-    
+
     if( !folder.exists() )
     {
       try
@@ -114,7 +112,7 @@ public abstract class AbstractCaseManager<T extends Case> implements ICaseManage
       }
       catch( final CoreException e )
       {
-        //ignore
+        // ignore
       }
     }
 
@@ -245,7 +243,7 @@ public abstract class AbstractCaseManager<T extends Case> implements ICaseManage
         return Status.OK_STATUS;
       }
     };
-    job.setRule( m_metaDataFile.getParent() );
+    job.setRule( m_metaDataFile.getParent().getParent() );
     job.schedule();
   }
 
