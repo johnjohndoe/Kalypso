eb08d8e401a4cbbf9a1749b2ae1734c1bcc67ec0
dejan <dejan@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-10-18 21:37:49 +0000

    Creating the basic structure of KalypsoModelRisk plugin

--- trunk/KalypsoDeegree/deegree/org/kalypso/gis/doubleraster/grid/AsciiGrid.java
+++ trunk/KalypsoDeegree/deegree/org/kalypso/gis/doubleraster/grid/AsciiGrid.java
@@ -41,7 +41,6 @@
 package org.kalypso.gis.doubleraster.grid;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
