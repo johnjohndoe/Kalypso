e66a0409c46088d257c8ab988efda83441105572
dejan <dejan@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-10-19 14:45:45 +0000

    Implementing wizard for loading waterdepth raster data

--- _import/trunk/Kalypso/sh/Kalypso1d2dProject/src/org/kalypso/ui/wizards/imports/roughness/LinkedFeaturePropertyFunction.java
+++ _import/trunk/Kalypso/sh/Kalypso1d2dProject/src/org/kalypso/ui/wizards/imports/roughness/LinkedFeaturePropertyFunction.java
@@ -74,7 +74,7 @@ public class LinkedFeaturePropertyFunction extends FeaturePropertyFunction
    *      org.kalypso.gmlschema.property.IPropertyType, java.lang.Object)
    */
   @SuppressWarnings("unchecked")
-  public Object getValue( Feature feature, IPropertyType pt, Object currentValue )
+  public Object getValue( final Feature feature, final IPropertyType pt, final Object currentValue )
   {
     try
     {
