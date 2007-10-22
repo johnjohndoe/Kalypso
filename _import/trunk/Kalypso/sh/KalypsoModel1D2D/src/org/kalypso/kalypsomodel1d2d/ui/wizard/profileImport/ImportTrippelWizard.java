3a4ecf256673e1a8b5fd6a31e777210591b08ffb
kuch <kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-10-22 15:45:29 +0000

    [Fix] Fix trippel import wizard and small refactorings of tripple import wizard

--- _import/trunk/Kalypso/sh/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/ui/wizard/profileImport/ImportTrippelWizard.java
+++ _import/trunk/Kalypso/sh/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/ui/wizard/profileImport/ImportTrippelWizard.java
@@ -40,22 +40,15 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.kalypsomodel1d2d.ui.wizard.profileImport;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.StringTokenizer;
 
 import javax.xml.namespace.QName;
 
-import org.apache.commons.io.IOUtils;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -68,27 +61,20 @@ import org.kalypso.contribs.eclipse.jface.operation.ICoreRunnableWithProgress;
 import org.kalypso.contribs.eclipse.jface.operation.RunnableContextHelper;
 import org.kalypso.core.KalypsoCorePlugin;
 import org.kalypso.kalypsomodel1d2d.KalypsoModel1D2DPlugin;
+import org.kalypso.kalypsomodel1d2d.ui.wizard.utils.ImportTrippleHelper;
 import org.kalypso.kalypsosimulationmodel.core.terrainmodel.IRiverProfileNetwork;
 import org.kalypso.kalypsosimulationmodel.core.terrainmodel.IRiverProfileNetworkCollection;
 import org.kalypso.model.wspm.core.IWspmConstants;
 import org.kalypso.model.wspm.core.gml.ProfileFeatureFactory;
 import org.kalypso.model.wspm.core.gml.WspmProfile;
 import org.kalypso.model.wspm.core.profil.IProfil;
-import org.kalypso.model.wspm.core.profil.IProfilPoint;
-import org.kalypso.model.wspm.core.profil.ProfilFactory;
-import org.kalypso.model.wspm.core.profil.impl.points.ProfilPoint;
 import org.kalypsodeegree.model.feature.Feature;
 import org.kalypsodeegree.model.feature.FeatureVisitor;
 import org.kalypsodeegree.model.feature.GMLWorkspace;
 import org.kalypsodeegree.model.feature.event.FeatureStructureChangeModellEvent;
-import org.kalypsodeegree.model.geometry.GM_Curve;
 import org.kalypsodeegree_impl.model.feature.FeatureHelper;
 import org.kalypsodeegree_impl.model.feature.visitors.TransformVisitor;
 import org.opengis.cs.CS_CoordinateSystem;
-
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.Point;
 
 /**
  * A wizard to import profile data (right now jus as trippel) into a 1D2D Terrain Model.
@@ -102,8 +88,6 @@ public class ImportTrippelWizard extends Wizard implements IWizard
   private final List<Feature> m_terrainModelAdds = new ArrayList<Feature>();
 
   private final IRiverProfileNetworkCollection m_networkModel;
-
-  private List<IProfil> m_profiles;
 
   private ImportProfilePage m_ProfilePage;
 
@@ -133,236 +117,6 @@ public class ImportTrippelWizard extends Wizard implements IWizard
   }
 
   /**
-   * imports the profile trippel data and converts it into IProfils
-   */
-  protected IStatus importTrippelData( )
-  {
-    /* get file name from wizard */
-    final File trippelFile = m_ProfilePage.getFile();
-
-    if( trippelFile == null )
-      return StatusUtilities.createWarningStatus( Messages.getString( "ImportTrippelWizard.4" ) ); //$NON-NLS-1$
-
-    /* read profiles, show warnings */
-    final List<IProfil> profiles = new ArrayList<IProfil>();
-    final List<IProfilPoint> profilPointList = new ArrayList<IProfilPoint>();
-
-    /* file loading */
-    BufferedReader fileReader = null;
-    try
-    {
-      fileReader = new BufferedReader( new InputStreamReader( new FileInputStream( trippelFile ) ) );
-
-      String line = null;
-      final String separator = ";"; //$NON-NLS-1$
-      StringTokenizer tokenizer;
-
-      /* parameter */
-      double station = 0;
-
-      double currentStation = Double.NaN;
-      int numStations = 0;
-
-      /* File Header */
-      fileReader.readLine();
-
-      while( (line = fileReader.readLine()) != null )
-      {
-        /* trippel-format should be: station, x, y, z */
-        tokenizer = new StringTokenizer( line, separator );
-
-        /* continue just if there are enough values in the trippel file */
-        if( tokenizer.countTokens() == 4 )
-        {
-
-          /* first value = profile station */
-          station = Double.parseDouble( tokenizer.nextToken() );
-
-          if( station != currentStation )
-          {
-            if( !Double.isNaN( currentStation ) )
-            {
-              // store current profile points as IProfil
-              storeProfile( profilPointList, currentStation, profiles );
-              profilPointList.clear();
-              numStations = numStations + 1;
-            }
-
-            // update profile station
-            currentStation = station;
-          }
-
-          IProfilPoint point = createProfilePoint( profilPointList, tokenizer );
-          if( point != null )
-            profilPointList.add( point );
-
-        }
-        else
-        {
-          // inform the user that his profile has not enough values...
-          String message = Messages.getString( "ImportTrippelWizard.6" ) + (numStations + 1) + Messages.getString( "ImportTrippelWizard.7" ); //$NON-NLS-1$ //$NON-NLS-2$
-          return StatusUtilities.createWarningStatus( message );
-        }
-      }
-
-      fileReader.close();
-
-      /* store the last profile */
-      storeProfile( profilPointList, station, profiles );
-      profilPointList.clear();
-      numStations = numStations + 1;
-
-      m_profiles = profiles;
-
-    }
-    catch( FileNotFoundException e )
-    {
-      e.printStackTrace();
-      return StatusUtilities.createWarningStatus( Messages.getString( "ImportTrippelWizard.8" ) ); //$NON-NLS-1$
-    }
-    catch( IOException e )
-    {
-      e.printStackTrace();
-      return StatusUtilities.createWarningStatus( Messages.getString( "ImportTrippelWizard.9" ) ); //$NON-NLS-1$
-    }
-
-    finally
-    {
-      IOUtils.closeQuietly( fileReader );
-    }
-
-    if( profiles.size() == 0 )
-    {
-      return StatusUtilities.createWarningStatus( Messages.getString( "ImportTrippelWizard.10" ) ); //$NON-NLS-1$
-    }
-
-    return Status.OK_STATUS;
-
-  }
-
-  /**
-   * stores all gathered profile points in a new profile and adds it to the profile list
-   * 
-   * @param profilPointList
-   *            the list of the profile points of one profile
-   * @param station
-   *            the current profile station
-   * @param profiles
-   *            the list of the allready imported profiles
-   */
-  private void storeProfile( List<IProfilPoint> profilPointList, double station, List<IProfil> profiles )
-  {
-    final String profiletype = "org.kalypso.model.wspm.tuhh.profiletype"; //$NON-NLS-1$
-    final IProfil profile = ProfilFactory.createProfil( profiletype );
-
-    profile.addPointProperty( IWspmConstants.POINT_PROPERTY_BREITE );
-    profile.addPointProperty( IWspmConstants.POINT_PROPERTY_HOEHE );
-    profile.addPointProperty( IWspmConstants.POINT_PROPERTY_HOCHWERT );
-    profile.addPointProperty( IWspmConstants.POINT_PROPERTY_RECHTSWERT );
-
-    for( IProfilPoint point : profilPointList )
-    {
-      profile.addPoint( point );
-    }
-
-    profile.setStation( station );
-    profile.setName( Messages.getString( "ImportTrippelWizard.12" ) ); //$NON-NLS-1$
-
-    profiles.add( profile );
-
-  }
-
-  /**
-   * creates a new profile point and adds it to the point list of the current profile
-   * 
-   * @param profilPointList
-   *            point list of the current profile
-   * @param tokenizer
-   *            holds the point data (x, y, z)
-   */
-  private IProfilPoint createProfilePoint( final List<IProfilPoint> profilPointList, StringTokenizer tokenizer )
-  {
-    double x;
-    double y;
-    double z;
-
-    final ProfilPoint profilPoint = new ProfilPoint();
-    profilPoint.addProperty( IWspmConstants.POINT_PROPERTY_RECHTSWERT );
-    profilPoint.addProperty( IWspmConstants.POINT_PROPERTY_HOCHWERT );
-    profilPoint.addProperty( IWspmConstants.POINT_PROPERTY_HOEHE );
-    profilPoint.addProperty( IWspmConstants.POINT_PROPERTY_BREITE );
-
-    if( tokenizer.hasMoreElements() )
-    {
-      x = Double.parseDouble( tokenizer.nextToken() );
-      profilPoint.setValueFor( IWspmConstants.POINT_PROPERTY_RECHTSWERT, x );
-    }
-    else
-      return null;
-
-    if( tokenizer.hasMoreElements() )
-    {
-      y = Double.parseDouble( tokenizer.nextToken() );
-      profilPoint.setValueFor( IWspmConstants.POINT_PROPERTY_HOCHWERT, y );
-    }
-    else
-      return null;
-
-    if( tokenizer.hasMoreElements() )
-    {
-      z = Double.parseDouble( tokenizer.nextToken() );
-      profilPoint.setValueFor( IWspmConstants.POINT_PROPERTY_HOEHE, z );
-    }
-    else
-      return null;
-
-    final double width;
-    /* calculate width coordinate */
-    if( profilPointList.size() > 0 )
-    {
-      width = calculateSegmentLength( profilPointList, profilPoint );
-    }
-    else
-      width = 0;
-
-    profilPoint.setValueFor( IWspmConstants.POINT_PROPERTY_BREITE, width );
-
-    return profilPoint;
-
-  }
-
-  /**
-   * calculates the width coordinate by the segment length (2-dim distance of the profile points)
-   * 
-   * @param profilPointList
-   *            point list of the current profile
-   */
-  private double calculateSegmentLength( List<IProfilPoint> profilPointList, ProfilPoint profilPoint )
-  {
-    double distance = 0;
-    GeometryFactory factory = new GeometryFactory();
-
-    /* get the segment length of the allready imported profile points */
-    distance = profilPointList.get( profilPointList.size() - 1 ).getValueFor( IWspmConstants.POINT_PROPERTY_BREITE );
-
-    /* add the segment length of the segment defined by the last imported profile point and the new to add profile point */
-    double x1 = profilPointList.get( profilPointList.size() - 1 ).getValueFor( IWspmConstants.POINT_PROPERTY_RECHTSWERT );
-    double y1 = profilPointList.get( profilPointList.size() - 1 ).getValueFor( IWspmConstants.POINT_PROPERTY_HOCHWERT );
-    double x2 = profilPoint.getValueFor( IWspmConstants.POINT_PROPERTY_RECHTSWERT );
-    double y2 = profilPoint.getValueFor( IWspmConstants.POINT_PROPERTY_HOCHWERT );
-
-    Coordinate coordinates1 = new Coordinate( x1, y1 );
-    Coordinate coordinates2 = new Coordinate( x2, y2 );
-
-    Point point1 = factory.createPoint( coordinates1 );
-    Point point2 = factory.createPoint( coordinates2 );
-
-    distance = distance + point1.distance( point2 );
-
-    return distance;
-  }
-
-  /**
    * @see org.eclipse.jface.wizard.Wizard#performFinish()
    */
   @Override
@@ -383,36 +137,27 @@ public class ImportTrippelWizard extends Wizard implements IWizard
         {
           /* Import Trippel Data */
           monitor.subTask( Messages.getString( "ImportTrippelWizard.14" ) ); //$NON-NLS-1$
-          try
-          {
-            final IStatus status = importTrippelData();
-            if( status != Status.OK_STATUS )
-            {
-              return status;
-            }
-          }
-          catch( final Exception e )
-          {
-            return StatusUtilities.statusFromThrowable( e, Messages.getString( "ImportTrippelWizard.15" ) ); //$NON-NLS-1$
-          }
+
+          /* get file name from wizard */
+          final File trippelFile = m_ProfilePage.getFile();
+          List<IProfil> profiles = ImportTrippleHelper.importTrippelData( trippelFile );
 
           monitor.worked( 1 );
 
           /* Convert Trippel Data */
           monitor.subTask( Messages.getString( "ImportTrippelWizard.16" ) ); //$NON-NLS-1$
 
-          final IStatus status = doImportNetwork( profNetworkColl, terrainModelAdds );
+          final IStatus status = doImportNetwork( profNetworkColl, terrainModelAdds, profiles );
 
           monitor.worked( 1 );
 
           return status;
         }
-        catch( final Throwable t )
+        catch( final Exception e )
         {
-          // TODO: remove all added features from terrainModel
-
-          throw new InvocationTargetException( t );
+          return StatusUtilities.statusFromThrowable( e, Messages.getString( "ImportTrippelWizard.15" ) ); //$NON-NLS-1$
         }
+
         finally
         {
           monitor.done();
@@ -436,43 +181,31 @@ public class ImportTrippelWizard extends Wizard implements IWizard
    * @param addedFeatures
    */
   @SuppressWarnings("deprecation")//$NON-NLS-1$
-  protected IStatus doImportNetwork( final IRiverProfileNetworkCollection networkCollection, final List<Feature> addedFeatures ) throws Exception
+  protected IStatus doImportNetwork( final IRiverProfileNetworkCollection networkCollection, final List<Feature> addedFeatures, final List<IProfil> profiles ) throws Exception
   {
     final IRiverProfileNetwork network = networkCollection.addNew( IRiverProfileNetwork.QNAME );
     final Feature networkFeature = network.getWrappedFeature();
     addedFeatures.add( networkFeature );
 
     /* Set user friendly name and descrption */
-    final String desc = String.format( Messages.getString( "ImportTrippelWizard.19" ), m_ProfilePage.getFileName(), DF.format( new Date() ), m_ProfilePage.getFilePath() ); //$NON-NLS-1$
+    final String desc = String.format( Messages.getString( "ImportTrippelWizard.19" ), m_ProfilePage.getFileName(), ImportTrippelWizard.DF.format( new Date() ), m_ProfilePage.getFilePath() ); //$NON-NLS-1$
     network.setName( FileUtilities.nameWithoutExtension( m_ProfilePage.getFileName() ) );
     network.setDescription( desc );
-    CS_CoordinateSystem crs = m_ProfilePage.getCoordinateSystem();
+    final CS_CoordinateSystem crs = m_ProfilePage.getCoordinateSystem();
 
-    GMLWorkspace workspace = networkFeature.getWorkspace();
+    final GMLWorkspace workspace = networkFeature.getWorkspace();
 
-    CS_CoordinateSystem coordinatesSystem = KalypsoCorePlugin.getDefault().getCoordinatesSystem();
-    CS_CoordinateSystem targetCRS = coordinatesSystem;
-    if( crs != coordinatesSystem )
-    {
-      targetCRS = crs;
-    }
+    final CS_CoordinateSystem coordinatesSystem = KalypsoCorePlugin.getDefault().getCoordinatesSystem();
     workspace.accept( new TransformVisitor( coordinatesSystem ), networkFeature, FeatureVisitor.DEPTH_INFINITE );
 
-    for( IProfil profile : m_profiles )
+    for( final IProfil profile : profiles )
     {
-
       profile.getPointProperty( desc );
       final Feature profileFeature = FeatureHelper.addFeature( network.getWrappedFeature(), IRiverProfileNetwork.QNAME_PROP_RIVER_PROFILE, new QName( IWspmConstants.NS_WSPMPROF, Messages.getString( "ImportTrippelWizard.20" ) ) ); //$NON-NLS-1$
       ProfileFeatureFactory.toFeature( profile, profileFeature );
-      // profileFeature.getDefaultGeometryProperty().getCoordinateDimension();
-      GM_Curve property = (GM_Curve) profileFeature.getProperty( WspmProfile.QNAME_LINE );
-      // profileFeature.setProperty( propertyType, property )
-      property.setCoordinateSystem( targetCRS );
-      profileFeature.setProperty( WspmProfile.QNAME_LINE, property );
+      new WspmProfile( profileFeature ).setSrsName( crs.getName() );
+      profileFeature.invalidEnvelope();
       addedFeatures.add( profileFeature );
-      // property.setCoordinateSystem( targetCRS );
-      // profileFeature.setProperty( WspmProfile.QNAME_LINE, newProperty );
-
     }
 
     final GMLWorkspace workspace2 = network.getWrappedFeature().getWorkspace();
