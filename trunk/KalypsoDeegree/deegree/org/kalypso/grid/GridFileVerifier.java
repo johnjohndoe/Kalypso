562109bbbe1770d95ee89770581ab28dc0367c24
kuch <kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-10-24 10:18:13 +0000

    added jgw world file extension

--- trunk/KalypsoDeegree/deegree/org/kalypso/grid/GridFileVerifier.java
+++ trunk/KalypsoDeegree/deegree/org/kalypso/grid/GridFileVerifier.java
@@ -81,25 +81,19 @@ public class GridFileVerifier
   public boolean verify( final URL urlImage )
   {
     /* file exists? */
-    if( (urlImage == null) )
-    {
+    if( urlImage == null )
       return false;
-    }
 
     final String file = urlImage.toString().toLowerCase();
     final int index = file.lastIndexOf( "." );
 
     /* valid file extension */
     if( !ArrayUtils.contains( GridFileVerifier.validFileExtensions, file.substring( index + 1 ) ) )
-    {
       return false;
-    }
 
     final RASTER_TYPE raster_type = determineType( urlImage );
     if( raster_type == null )
-    {
       return false;
-    }
 
     return true;
   }
@@ -108,9 +102,7 @@ public class GridFileVerifier
   {
     final RASTER_TYPE raster_type = determineType( urlImage );
     if( raster_type == null )
-    {
-      throw (new IllegalStateException());
-    }
+      throw new IllegalStateException();
 
     switch( raster_type )
     {
@@ -137,34 +129,24 @@ public class GridFileVerifier
     final IMAGE_TYPE image_type = determineImageType( urlImage );
 
     /* if it is an image, it can maybe have an world file with coordinates */
-    if( !(IMAGE_TYPE.eNoImage.equals( image_type )) )
+    if( !IMAGE_TYPE.eNoImage.equals( image_type ) )
     {
       final URL worldFile = getWorldFile( urlImage );
       if( worldFile != null )
-      {
         return RASTER_TYPE.eImageWorldFile;
-      }
 
       if( worldFile == null )
-      {
         /* is it an geodata image, like geotif? ATM only geotif is supported */
         if( IMAGE_TYPE.eTIFF.equals( image_type ) && isGeoTiff( urlImage ) )
-        {
           return RASTER_TYPE.eImageGeo;
-        }
         else
-        {
           /* only a simple image */
           return RASTER_TYPE.eImage;
-        }
-      }
 
     }
 
     if( isAsciGrid( urlImage ) )
-    {
       return RASTER_TYPE.eAsciiGrid;
-    }
 
     return null;
   }
@@ -172,9 +154,7 @@ public class GridFileVerifier
   private boolean isAsciGrid( final URL urlImage )
   {
     if( urlImage == null )
-    {
-      throw (new IllegalStateException());
-    }
+      throw new IllegalStateException();
 
     // TODO: mke a real inspection of file
     final String[] ascExtensions = new String[] { "asc", "dat" };
@@ -183,9 +163,7 @@ public class GridFileVerifier
     final int index = file.lastIndexOf( "." );
 
     if( ArrayUtils.contains( ascExtensions, file.substring( index + 1 ) ) )
-    {
       return true;
-    }
 
     return false;
   }
@@ -193,9 +171,7 @@ public class GridFileVerifier
   private boolean isGeoTiff( final URL urlImage )
   {
     if( urlImage == null )
-    {
-      throw (new IllegalStateException());
-    }
+      throw new IllegalStateException();
     SeekableStream stream;
     try
     {
@@ -204,9 +180,7 @@ public class GridFileVerifier
 
       final XTIFFField[] geoKeys = directory.getGeoKeys();
       if( geoKeys.length <= 0 )
-      {
         return false;
-      }
 
       return true;
     }
@@ -225,11 +199,9 @@ public class GridFileVerifier
   private URL getWorldFile( final URL urlImage )
   {
     if( urlImage == null )
-    {
-      throw (new IllegalStateException());
-    }
+      throw new IllegalStateException();
 
-    final String[] worldFileExtensions = new String[] { "tfw", "gfw", "jpw", "pgw" };
+    final String[] worldFileExtensions = new String[] { "tfw", "gfw", "jpw", "jgw", "pgw" };
 
     for( final String extension : worldFileExtensions )
     {
@@ -262,9 +234,7 @@ public class GridFileVerifier
   private IMAGE_TYPE determineImageType( final URL urlImage )
   {
     if( urlImage == null )
-    {
-      throw (new IllegalStateException());
-    }
+      throw new IllegalStateException();
 
     final String file = urlImage.toString().toLowerCase();
     final int index = file.lastIndexOf( "." );
@@ -275,21 +245,13 @@ public class GridFileVerifier
     final String[] tifTypes = new String[] { "tif", "tiff" };
 
     if( ArrayUtils.contains( jpgTypes, fileExtension ) )
-    {
       return IMAGE_TYPE.eJPG;
-    }
     else if( ArrayUtils.contains( tifTypes, fileExtension ) )
-    {
       return IMAGE_TYPE.eTIFF;
-    }
     else if( "gif".equals( fileExtension ) )
-    {
       return IMAGE_TYPE.eGIF;
-    }
     else if( "png".equals( fileExtension ) )
-    {
       return IMAGE_TYPE.ePNG;
-    }
 
     return IMAGE_TYPE.eNoImage;
   }
