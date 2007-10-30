210230ebecb142d9c71ebf16859bc67a012eda13
devgernot <devgernot@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-10-30 10:08:47 +0000

    In work: [FLood] Implement Extrapolation-Polygon editing

--- temp/trunk/KalypsoModelRisk/src/org/kalypso/risk/model/services/LanduseStyleUpdateListener.java
+++ temp/trunk/KalypsoModelRisk/src/org/kalypso/risk/model/services/LanduseStyleUpdateListener.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.risk.model.services;
 
@@ -58,7 +58,6 @@ import de.renew.workflow.contexts.ICaseHandlingSourceProvider;
 
 /**
  * @author Dejan Antanaskovic
- * 
  */
 public class LanduseStyleUpdateListener implements IResourceChangeListener
 {
@@ -67,23 +66,29 @@ public class LanduseStyleUpdateListener implements IResourceChangeListener
   /**
    * @see org.eclipse.core.resources.IResourceChangeListener#resourceChanged(org.eclipse.core.resources.IResourceChangeEvent)
    */
-  public void resourceChanged( IResourceChangeEvent event )
+  public void resourceChanged( final IResourceChangeEvent event )
   {
     final IWorkbench workbench = PlatformUI.getWorkbench();
     final IHandlerService handlerService = (IHandlerService) workbench.getService( IHandlerService.class );
     final IEvaluationContext context = handlerService.getCurrentState();
     final IFolder scenarioFolder = (IFolder) context.getVariable( ICaseHandlingSourceProvider.ACTIVE_CASE_FOLDER_NAME );
-    final IPath resourcePath = scenarioFolder.getProjectRelativePath().append( "/models/RasterizationControlModel.gml");
+    // if no scenario is active, just return
+    if( scenarioFolder == null )
+      return;
+
+    final IPath resourcePath = scenarioFolder.getProjectRelativePath().append( "/models/RasterizationControlModel.gml" );
 
     final IResourceDelta rootDelta = event.getDelta();
     if( rootDelta == null )
       return;
 
     final IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
-    for( IProject project : projects )
+    for( final IProject project : projects )
     {
       final IPath roughnessPath = project.getFullPath().append( resourcePath );
       final IResourceDelta fileDelta = rootDelta.findMember( roughnessPath );
+      // TODO: check if it is a 1d2d project, there may be other projects with the same file
+
       if( fileDelta != null )
       {
         if( (fileDelta.getFlags() & IResourceDelta.CONTENT) != 0 )
@@ -100,7 +105,7 @@ public class LanduseStyleUpdateListener implements IResourceChangeListener
 
     m_jobStyleUpdate = new LanduseStyleUpdateService( gmlDatabaseFile );
 
-// m_job.setSystem( true );
+    // m_job.setSystem( true );
     m_jobStyleUpdate.setUser( false );
     m_jobStyleUpdate.setPriority( Job.LONG );
     m_jobStyleUpdate.setRule( m_jobStyleUpdate.getPolygonSymbolzerSldFile().getProject() );
