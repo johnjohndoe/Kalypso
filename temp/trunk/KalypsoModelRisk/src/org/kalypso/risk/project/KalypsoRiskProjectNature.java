db0cab3fa176fbdf21d5e2d192e507be3badd197
devgernot <devgernot@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-11-05 16:43:42 +0000

    Incomplete - task 52: [FLOOD] Projekt-Management

--- temp/trunk/KalypsoModelRisk/src/org/kalypso/risk/project/KalypsoRiskProjectNature.java
+++ temp/trunk/KalypsoModelRisk/src/org/kalypso/risk/project/KalypsoRiskProjectNature.java
@@ -40,28 +40,15 @@
  ---------------------------------------------------------------------------------------------------*/
 package org.kalypso.risk.project;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
-import java.util.logging.Logger;
 
-import org.apache.commons.io.IOUtils;
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IProjectNature;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.SubProgressMonitor;
 import org.kalypso.commons.java.util.zip.ZipUtilities;
-import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
-import org.kalypso.risk.plugin.KalypsoRiskPlugin;
+import org.kalypso.contribs.eclipse.core.resources.ProjectUtilities;
 
 /**
  * Project Nature for 1d 2d simulation, requires {@link org.kalypso.afgui.ScenarioHandlingProjectNature}
@@ -70,20 +57,24 @@ import org.kalypso.risk.plugin.KalypsoRiskPlugin;
  */
 public class KalypsoRiskProjectNature implements IProjectNature
 {
-  public static final String ID = "org.kalypso.risk.project.KalypsoRiskProjectNature";  //$NON-NLS-1$
+  public static final String ID = "org.kalypso.risk.project.KalypsoRiskProjectNature"; //$NON-NLS-1$
 
-  private final static Logger logger = Logger.getLogger( KalypsoRiskProjectNature.class.getName() );
+  private static final String EMPTY_PROJECT_ZIP_PATH = "RiskProjectTemplate.zip"; //$NON-NLS-1$
 
-  // TODO: change tracing option id (use Debug helper class, see KalypsoUIDebug example)
-  private static final boolean log = Boolean.parseBoolean( Platform.getDebugOption( "org.kalypso.risk/debug" ) );  //$NON-NLS-1$
-
-  static
+  public static final boolean isOfThisNature( final IProject project ) throws CoreException
   {
-    if( !log )
-      logger.setUseParentHandlers( false );
+    return project == null ? false : project.hasNature( ID );
   }
 
-  private static final String EMPTY_PROJECT_ZIP_PATH = "RiskProjectTemplate.zip";  //$NON-NLS-1$
+  public static final void addNature( final IProject project ) throws CoreException
+  {
+    ProjectUtilities.addNature( project, ID, new NullProgressMonitor() );
+  }
+
+  public static KalypsoRiskProjectNature getNature( final IProject project ) throws CoreException
+  {
+    return (KalypsoRiskProjectNature) project.getNature( ID );
+  }
 
   private IProject m_project;
 
@@ -98,75 +89,8 @@ public class KalypsoRiskProjectNature implements IProjectNature
     if( !basisFolder.exists() )
     {
       final URL zipLocation = getClass().getResource( EMPTY_PROJECT_ZIP_PATH );
-      unzipToContainer( zipLocation, getProject(), monitor );
+      ZipUtilities.unzipToContainer( zipLocation, getProject(), monitor );
     }
-  }
-
-  public static final boolean isOfThisNature( final IProject project ) throws CoreException
-  {
-    return project == null ? false : project.hasNature( ID );
-  }
-
-  public static final void addNature( final IProject project ) throws CoreException
-  {
-    // TODO: move that to a central place (i.e. platform contribs)
-    if( project.hasNature( ID ) )
-    {
-      return;
-    }
-    else
-    {
-      IProjectDescription description = project.getDescription();
-      String[] natures = description.getNatureIds();
-      String[] newNatures = new String[natures.length + 1];
-      System.arraycopy( natures, 0, newNatures, 0, natures.length );
-      newNatures[natures.length] = ID;
-      description.setNatureIds( newNatures );
-      project.setDescription( description, new NullProgressMonitor() );
-    }
-  }
-
-  /**
-   * TODO: move this method into common helper class inside KalypsoCommons.
-   * 
-   * @see org.eclipse.core.resources.IProjectNature#configure()
-   */
-  private void unzipToContainer( final URL zipLocation, final IContainer targetContainer, final IProgressMonitor monitor ) throws CoreException
-  {
-    monitor.beginTask( "", 2 );  //$NON-NLS-1$
-
-    InputStream zipStream = null;
-    try
-    {
-      // unpack other file structure from zip
-      zipStream = new BufferedInputStream( zipLocation.openStream() );
-
-      // REMARK: unzipping files with non UTF-8 filename encoding is really awesome in java.
-      // We do use the apache tools, which let us at least set the encoding.
-      // Try and error led to use the encoding: "IBM850" for WinZippes .zip's.
-      // REMARK: This doesn´t work in the deploy (I don´t know why???) -use simple unzip instead works!!!
-      // ZipUtilities.unzipApache( zipStream, targetContainer.getLocation().toFile(), true, "IBM850" );
-      ZipUtilities.unzip( zipStream, targetContainer.getLocation().toFile() );
-      zipStream.close();
-      monitor.worked( 1 );
-
-      targetContainer.refreshLocal( IResource.DEPTH_INFINITE, new SubProgressMonitor( monitor, 1 ) );
-    }
-    catch( final IOException e )
-    {
-      final IStatus status = StatusUtilities.statusFromThrowable( e );
-      KalypsoRiskPlugin.getDefault().getLog().log( status );
-      throw new CoreException( status );
-    }
-    finally
-    {
-      IOUtils.closeQuietly( zipStream );
-    }
-  }
-
-  public static KalypsoRiskProjectNature getNature( final IProject project ) throws CoreException
-  {
-    return (KalypsoRiskProjectNature) project.getNature( ID );
   }
 
   /**
