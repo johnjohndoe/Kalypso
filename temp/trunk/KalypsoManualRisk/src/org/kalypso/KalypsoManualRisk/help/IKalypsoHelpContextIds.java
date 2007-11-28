6885ab253388468351f63acc9a48fcf8ee281df4
dejan <dejan@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2007-11-28 08:24:41 +0000

    Kalypso Risk Manual plugin

---
+++ temp/trunk/KalypsoManualRisk/src/org/kalypso/KalypsoManualRisk/help/IKalypsoHelpContextIds.java
@@ -0,0 +1,73 @@
+package org.kalypso.KalypsoManualRisk.help;			
+/**
+ * @see KalypsoManual common-eclipse.xsl and KalypsoBuild/publishing/docbook-xsl-1.69.1/eclipse/eclipse-ex.xsl
+ *
+ */				
+public interface IKalypsoHelpContextIds
+{
+  public static final String PREFIX = "org.kalypso.manual.";
+					
+				
+			
+  public static final String KalypsoRiskAllgemeines =PREFIX + "KalypsoRiskAllgemeines";
+		
+			
+  public static final String KalypsoRiskProjektbearbeitung =PREFIX + "KalypsoRiskProjektbearbeitung";
+		
+			
+  public static final String KalypsoRiskNeuesProjekt =PREFIX + "KalypsoRiskNeuesProjekt";
+		
+			
+  public static final String KalypsoRiskVorhProjekt =PREFIX + "KalypsoRiskVorhProjekt";
+		
+			
+  public static final String KalypsoRiskPerspektive =PREFIX + "KalypsoRiskPerspektive";
+		
+			
+  public static final String KalypsoRiskWorkflow =PREFIX + "KalypsoRiskWorkflow";
+		
+			
+  public static final String KalypsoRiskDatenakquisition =PREFIX + "KalypsoRiskDatenakquisition";
+		
+			
+  public static final String KalypsoRiskPreprozessing =PREFIX + "KalypsoRiskPreprozessing";
+		
+			
+  public static final String KalypsoRiskDatenLaden =PREFIX + "KalypsoRiskDatenLaden";
+		
+			
+  public static final String KalypsoRiskImportBackgroundmaps =PREFIX + "KalypsoRiskImportBackgroundmaps";
+		
+			
+  public static final String KalypsoRiskImportDHM =PREFIX + "KalypsoRiskImportDHM";
+		
+			
+  public static final String KalypsoRiskImportRoughness =PREFIX + "KalypsoRiskImportRoughness";
+		
+			
+  public static final String KalypsoRiskImportTimeseries =PREFIX + "KalypsoRiskImportTimeseries";
+		
+			
+  public static final String KalypsoRiskImportProf =PREFIX + "KalypsoRiskImportProf";
+		
+			
+  public static final String KalypsoRiskNetzaufbau =PREFIX + "KalypsoRiskNetzaufbau";
+		
+			
+  public static final String KalypsoRiskRauheitsmodellierung =PREFIX + "KalypsoRiskRauheitsmodellierung";
+		
+			
+  public static final String KalypsoRiskProzessing =PREFIX + "KalypsoRiskProzessing";
+		
+			
+  public static final String KalypsoRiskBerechnungseinstellungen =PREFIX + "KalypsoRiskBerechnungseinstellungen";
+		
+			
+  public static final String KalypsoRiskPostprozessing =PREFIX + "KalypsoRiskPostprozessing";
+		
+			
+  public static final String KalypsoRiskModellkalibrierung =PREFIX + "KalypsoRiskModellkalibrierung";
+		
+}
+				
+			
