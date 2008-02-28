ff79338ffb1470c50617e3a3000bc66c41a8a398
devgernot <devgernot@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-02-28 11:20:25 +0000

    Moved demo project stuff from wspm to contribs, making it very resuable.
    Changed worklfow-project stuff to new demo project concept; adding dem oproject to workflow projects is now very easy (as soon as the data is available).
    
    Also moved the workflow.xml's into /etc of each project.

--- temp/trunk/KalypsoModelRisk/src/org/kalypso/risk/project/KalypsoRiskProjectWizard.java
+++ temp/trunk/KalypsoModelRisk/src/org/kalypso/risk/project/KalypsoRiskProjectWizard.java
@@ -6,10 +6,8 @@ public class KalypsoRiskProjectWizard extends NewProjectWizard
 {
   final static public String ID = "org.kalypso.risk.project.KalypsoRiskProjectWizard"; //$NON-NLS-1$
 
-  private static final String EMPTY_PROJECT_ZIP_PATH = "RiskProjectTemplate.zip"; //$NON-NLS-1$
-
   public KalypsoRiskProjectWizard( )
   {
-    super( KalypsoRiskProjectWizard.class.getResource( EMPTY_PROJECT_ZIP_PATH ) );
+    super( "org.kalypso.risk.projectTemplate", false );
   }
 }
