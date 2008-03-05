69ee98108c75f71a393a6a50dfa6ca4bb0f2e9de
kimwerner <kimwerner@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-03-05 10:35:58 +0000

    fix tuhh profile-rules

--- KalypsoUI/src/org/kalypso/ogc/gml/om/table/command/AddRowHandler.java
+++ KalypsoUI/src/org/kalypso/ogc/gml/om/table/command/AddRowHandler.java
@@ -65,7 +65,7 @@ public class AddRowHandler extends AbstractHandler
   {
     final TableViewer viewer = TupleResultCommandUtils.findTableViewer( event );
     final TupleResult tupleResult = TupleResultCommandUtils.findTupleResult( event );
-    if(  viewer == null )
+    if( viewer == null )
       throw new ExecutionException( "No tuple result viewer available" );
 
     final IStructuredSelection selection = (IStructuredSelection) viewer.getSelection();
@@ -74,50 +74,65 @@ public class AddRowHandler extends AbstractHandler
     final int size = tupleResult == null ? 0 : tupleResult.size();
     final IRecord next = index < size - 2 ? tupleResult.get( index + 1 ) : null;
     final IRecord row = tupleResult.createRecord();
-    for( final IComponent component : tupleResult.getComponents() )
+    if( target == null )
     {
-      if( component.getValueTypeName().equals( new QName( NS.XSD_SCHEMA, "double" ) ) && (target instanceof IRecord) )
+      for( final IComponent component : tupleResult.getComponents() )
       {
-        if( "urn:ogc:gml:dict:kalypso:model:wspm:profilePointComponents#BREITE".equals( component.getId() ) )
+        if( component.getValueTypeName().equals( new QName( NS.XSD_SCHEMA, "double" ) ) )
+          row.setValue( component, 0.0 );
+      }
+    }
+    else
+    {
+      for( final IComponent component : tupleResult.getComponents() )
+      {
+        if( component.getValueTypeName().equals( new QName( NS.XSD_SCHEMA, "double" ) ) && (target instanceof IRecord) )
         {
-          if( next != null )
+          if( "urn:ogc:gml:dict:kalypso:model:wspm:profilePointComponents#BREITE".equals( component.getId() ) )
           {
-            final Object b1 = ((IRecord) target).getValue( component );
-            if( b1 == null )
-              continue;
-            final Object l = next.getValue( component );
-            if( l == null )
-              continue;
-            row.setValue( component, (Double) b1 - ((Double) b1 - (Double) l) / 2.0 );
+            if( next != null )
+            {
+              final Object b1 = ((IRecord) target).getValue( component );
+              if( b1 == null )
+                continue;
+              final Object l = next.getValue( component );
+              if( l == null )
+                continue;
+              row.setValue( component, (Double) b1 - ((Double) b1 - (Double) l) / 2.0 );
+            }
+            else
+            {
+              final Object value = ((IRecord) target).getValue( component );
+              if( value != null )
+                row.setValue( component, (Double) value + 10 );
+              else
+                row.setValue( component, component.getDefaultValue() );
+            }
+          }
+          else if( "urn:ogc:gml:dict:kalypso:model:wspm:profilePointComponents#HOEHE".equals( component.getId() ) )
+          {
+            if( next != null )
+            {
+              final Object h1 = ((IRecord) target).getValue( component );
+              if( h1 == null )
+                continue;
+              final Object z = next.getValue( component );
+              if( z == null )
+                continue;
+              row.setValue( component, (Double) h1 - ((Double) h1 - (Double) z) / 2.0 );
+            }
+            else
+            {
+              final Object value = ((IRecord) target).getValue( component );
+              if( value != null )
+                row.setValue( component, value );
+              else
+                row.setValue( component, component.getDefaultValue() );
+            }
           }
           else
-          {
-            final Object value = ((IRecord) target).getValue( component );
-            if( value != null )
-              row.setValue( component, (Double) value + 10 );
-          }
+            row.setValue( component, ((IRecord) target).getValue( component ) );
         }
-        else if( "urn:ogc:gml:dict:kalypso:model:wspm:profilePointComponents#HOEHE".equals( component.getId() ) )
-        {
-          if( next != null )
-          {
-            final Object h1 = ((IRecord) target).getValue( component );
-            if( h1 == null )
-              continue;
-            final Object z = next.getValue( component );
-            if( z == null )
-              continue;
-            row.setValue( component, (Double) h1 - ((Double) h1 - (Double) z) / 2.0 );
-          }
-          else
-          {
-            final Object value = ((IRecord) target).getValue( component );
-            if( value != null )
-              row.setValue( component, value );
-          }
-        }
-        else
-          row.setValue( component, ((IRecord) target).getValue( component ) );
       }
     }
     if( next != null )
