eb9eeae3a4098ccd41baa68ed428a2238702bbf1
donner <donner@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-03-12 15:22:22 +0000

    Kalypso 2.1.1.1r      10. März 2008 -------------------------------------
    1.)  Zurückholen der ursprünglichen Energiehöhenberechnung (hen) bzw. Verlusthöhen-Berechnung (hv):
         Die Energiehöhe wurde seit Version 2.0.4.3 (07. August 2007) mit einem vereinfachten Ansatz ueber
         hv = v²/2g aus Stabilitätsgründen berechnet. Durch weitere Stabilisierung des Energiehöhenberechnung
         wird nun der ursprüngliche Ansatz: hv = phion / phiun**3 * q * q / g / 2. nach BWK 1, S.15 zurückgeholt.
    
         Diese Korrektur wirkt sich minimal (im cm Bereich) auf alle Rechenvarianten und alle Ergebnisse aus!
         Betroffene SUBR: eb2ks
    
    2.)  Zurücksetzen der Korrektur vom 07. Februar2008 der uebergebenen Parameter aus br-konv: Energiehöhen waren richtig!
         (siehe Theorie zum Verlustbeiwert)
         Diese Aenderung fuehrte zu KEINEN Aenderungen in den Ergebnissen, verhindert jedoch den Programm-
         abbruch fuer den Abflusszustand: Druckabzufluss in der Bruecke ohne Wehrabfluss!
    
    							                Monika Donner, 10.03.2008

--- _import/trunk/fortran/kalypso-1d/source/globale_definitionen.f90
+++ _import/trunk/fortran/kalypso-1d/source/globale_definitionen.f90
@@ -1,4 +1,4 @@
-!     Last change:  MD   26 Feb 2008    1:43 pm
+!     Last change:  MD   12 Mar 2008    4:03 pm
 !--------------------------------------------------------------------------
 ! This code, globale_definitionen.f90, contains the shared memory modules
 ! and functions of the hydrodynamic modell for
@@ -42,8 +42,8 @@ implicit none
 
 save
 
-CHARACTER(LEN=29), parameter :: VERSIONNR   = ' KALYPSO-1D, VERSION 2.1.1.0 '
-CHARACTER(LEN=17), parameter :: VERSIONDATE = 'Stand: 26.02.2008'
+CHARACTER(LEN=29), parameter :: VERSIONNR   = ' KALYPSO-1D, VERSION 2.1.1.1r'
+CHARACTER(LEN=17), parameter :: VERSIONDATE = 'Stand: 10.03.2008'
 
 end module VERSION
 
