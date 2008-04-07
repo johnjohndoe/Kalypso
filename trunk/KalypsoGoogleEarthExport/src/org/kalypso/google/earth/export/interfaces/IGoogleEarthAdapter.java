c06260e90e7ff9adc1928893f17b537effbffcc2
kuch <kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-04-07 18:31:38 +0000

    added extension point for google earth export

--- trunk/KalypsoGoogleEarthExport/src/org/kalypso/google/earth/export/interfaces/IGoogleEarthAdapter.java
+++ trunk/KalypsoGoogleEarthExport/src/org/kalypso/google/earth/export/interfaces/IGoogleEarthAdapter.java
@@ -1,6 +1,19 @@
 package org.kalypso.google.earth.export.interfaces;
 
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+
+import org.eclipse.core.resources.IFile;
+
 public interface IGoogleEarthAdapter
 {
+  // TODO we really want to provide IFiles?!? hmmm... String handling is not even better...
+  /**
+   * Point geometries can define placemarkers, this means, which image will be displayed for an point TODO add name,
+   * description (additional informations to these point markers)
+   */
+  public Map<QName, IFile> getPlaceMarkers( );
 
+  // TODO add additional placemarkers, perhaps for measures and their details (images, weblinks, aso)...
 }
