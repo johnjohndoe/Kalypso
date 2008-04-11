ebcfcdc8ecf14c9b87d83e836eea5e17e880f029
kuch <kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-04-11 13:20:10 +0000

    NEW - # 428: [MAP  - Google Export] - NotImplementedException für TextSymbolizer, KMZ-Dateien sehen aber sonst gut aus https://dev.bjoernsen.de/nofdp/ticket/428

--- trunk/KalypsoGoogleEarthExport/src/org/kalypso/google/earth/export/interfaces/IGoogleEarthAdapter.java
+++ trunk/KalypsoGoogleEarthExport/src/org/kalypso/google/earth/export/interfaces/IGoogleEarthAdapter.java
@@ -4,6 +4,8 @@ import org.kalypsodeegree.model.feature.Feature;
 
 public interface IGoogleEarthAdapter
 {
+  public static String ID = "org.kalypso.google.earth.export.googleEarthAdapter";
+
   /**
    * Point geometries can define place markers, this means, which image will be displayed for an point TODO add name,
    * description (additional informations to these point markers)
@@ -15,5 +17,10 @@ public interface IGoogleEarthAdapter
    */
   public IPlacemarker[] getAdditionalPlacemarkers( );
 
-  // TODO handle special geometries...
+  /**
+   * needed for additional place marks - all rendered features will be registered.
+   */
+  public void registerExportedFeature( Feature feature );
+
+  public void cleanUp( );
 }
