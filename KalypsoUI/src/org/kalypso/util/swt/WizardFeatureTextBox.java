4b5630e51046d6e90a68876e0fd26a6781daa13e
kuch <kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-04-22 08:59:38 +0000

    CLOSED - # 480: Editieren von Name, Station und Description der Profile ermöglichen https://dev.bjoernsen.de/nofdp/ticket/480

--- KalypsoUI/src/org/kalypso/util/swt/WizardFeatureTextBox.java
+++ KalypsoUI/src/org/kalypso/util/swt/WizardFeatureTextBox.java
@@ -2,47 +2,49 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.util.swt;
 
+import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.util.GregorianCalendar;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.xml.datatype.XMLGregorianCalendar;
@@ -118,6 +120,29 @@ public class WizardFeatureTextBox
           m_textBox.setText( date );
           m_text = date;
 
+        }
+        else if( m_property instanceof List )
+        {
+          final List names = (List) m_property;
+          m_textBox.setText( names.get( 0 ).toString() );
+          m_text = names.get( 0 ).toString();
+
+        }
+        else if( m_property instanceof Double )
+        {
+          final Double value = (Double) m_property;
+
+          final String text = String.format( "%.4f", value );
+          m_textBox.setText( text );
+          m_text = text;
+        }
+        else if( m_property instanceof BigDecimal )
+        {
+          final BigDecimal value = (BigDecimal) m_property;
+
+          final String text = String.format( "%.4f", value );
+          m_textBox.setText( text );
+          m_text = text;
         }
         else
         {
