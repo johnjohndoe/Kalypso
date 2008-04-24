ebc5083ec6d5365c648588f43cd0260b60cc64cf
thomas <thomas@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-04-24 07:16:25 +0000

    [Gird] again bugfix for getValueChecked

--- trunk/KalypsoDeegree/deegree/org/kalypso/grid/AbstractGeoGrid.java
+++ trunk/KalypsoDeegree/deegree/org/kalypso/grid/AbstractGeoGrid.java
@@ -159,7 +159,7 @@ public abstract class AbstractGeoGrid implements IGeoGrid
    */
   public double getValueChecked( final int x, final int y ) throws GeoGridException
   {
-    if( (x < 0) || (x >= getSizeX()) || (y < 0) || (y >= getSizeX()) )
+    if( (x < 0) || (x >= getSizeX()) || (y < 0) || (y >= getSizeY()) )
       return Double.NaN;
 
     return getValue( x, y );
