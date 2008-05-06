8e80a309e78c37aac305f6689a83c3d4ffda1f87
thomas <thomas@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-05-06 08:32:21 +0000

    [GRID] raster to vector implementation

--- KalypsoDeegree/deegree/org/kalypso/grid/processes/raster2vector/LinkedCoordinateException.java
+++
@@ -1,9 +0,0 @@
-package org.kalypso.grid.processes.raster2vector;
-
-public class LinkedCoordinateException extends Exception
-{
-  public LinkedCoordinateException( final String message )
-  {
-    super( message );
-  }
-}
