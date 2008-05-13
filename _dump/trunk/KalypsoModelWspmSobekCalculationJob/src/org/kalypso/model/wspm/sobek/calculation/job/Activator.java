39a4140eb6fa114f66134756d590010aedc15a29
kuch <kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-05-13 12:42:26 +0000

    *** empty log message ***

--- _dump/trunk/KalypsoModelWspmSobekCalculationJob/src/org/kalypso/model/wspm/sobek/calculation/job/Activator.java
+++ _dump/trunk/KalypsoModelWspmSobekCalculationJob/src/org/kalypso/model/wspm/sobek/calculation/job/Activator.java
@@ -13,7 +13,7 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -22,25 +22,29 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext context) throws Exception {
+	@Override
+	public void start(final BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
-	public void stop(BundleContext context) throws Exception {
+	@Override
+	public void stop(final BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
