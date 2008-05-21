0750d661ec02421a51680154dff266538200f808
schrage <schrage@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-05-21 15:07:31 +0000

    compiled manuals for Flood and Risk

--- temp/trunk/KalypsoManualRisk/src/org/kalypso/KalypsoManualRisk/help/IKalypsoHelpContextIds.java
+++ temp/trunk/KalypsoManualRisk/src/org/kalypso/KalypsoManualRisk/help/IKalypsoHelpContextIds.java
@@ -1,53 +1,74 @@
 package org.kalypso.KalypsoManualRisk.help;
 
 /**
- * @see KalypsoManual common-eclipse.xsl and KalypsoBuild/publishing/docbook-xsl-1.69.1/eclipse/eclipse-ex.xsl
- *
- */				
-public interface IKalypsoHelpContextIds
-{
-  public static final String PREFIX = "org.kalypso.manual.";
-					
-				
-			
-  public static final String KalypsoRiskoPreprocessing =PREFIX + "KalypsoRiskoPreprocessing";
-		
-			
-  public static final String KalypsoRisikoDatenladen =PREFIX + "KalypsoRisikoDatenladen";
-		
-			
-  public static final String KalypsoRisikoImport_Hintergrunddaten =PREFIX + "KalypsoRisikoImport_Hintergrunddaten";
-		
-			
-  public static final String KalypsoRisikoImport_Hintergrunddaten_TIF_JPG =PREFIX + "KalypsoRisikoImport_Hintergrunddaten_TIF_JPG";
-		
-			
-  public static final String KalypsoRisikoImport_Hintergrunddaten_SHP =PREFIX + "KalypsoRisikoImport_Hintergrunddaten_SHP";
-		
-			
-  public static final String KalypsoRisikoImport_Hintergrunddaten_WMS =PREFIX + "KalypsoRisikoImport_Hintergrunddaten_WMS";
-		
-			
-  public static final String KalypsoRisikoImport_Landnutzungsdaten =PREFIX + "KalypsoRisikoImport_Landnutzungsdaten";
-		
-			
-  public static final String KalypsoRisikoImport_Wassertiefendaten =PREFIX + "KalypsoRisikoImport_Wassertiefendaten";
-		
-			
-  public static final String KalypsoRisikoBearbeiten_HQi =PREFIX + "KalypsoRisikoBearbeiten_HQi";
-		
-			
-  public static final String KalypsoRisikoBearbeiten_Rasterdaten =PREFIX + "KalypsoRisikoBearbeiten_Rasterdaten";
-		
-			
-  public static final String KalypsoRisikoBerechnungsparameter =PREFIX + "KalypsoRisikoBerechnungsparameter";
-		
-			
-  public static final String KalypsoRisikoBerechnungsparameter_festlegen =PREFIX + "KalypsoRisikoBerechnungsparameter_festlegen";
-		
-			
-  public static final String KalypsoRisikoBerechnungsparameter_Risikozonierung =PREFIX + "KalypsoRisikoBerechnungsparameter_Risikozonierung";
-		
+ * @see KalypsoManual common-eclipse.xsl and
+ *      KalypsoBuild/publishing/docbook-xsl-1.69.1/eclipse/eclipse-ex.xsl
+ * 
+ */
+public interface IKalypsoHelpContextIds {
+	public static final String PREFIX = "org.kalypso.manual.";
+
+	public static final String KalypsoRiskoPreprocessing = PREFIX
+			+ "KalypsoRiskoPreprocessing";
+
+	public static final String KalypsoRisikoDatenladen = PREFIX
+			+ "KalypsoRisikoDatenladen";
+
+	public static final String KalypsoRisikoImport_Hintergrunddaten = PREFIX
+			+ "KalypsoRisikoImport_Hintergrunddaten";
+
+	public static final String KalypsoRisikoImport_Hintergrunddaten_TIF_JPG = PREFIX
+			+ "KalypsoRisikoImport_Hintergrunddaten_TIF_JPG";
+
+	public static final String KalypsoRisikoImport_Hintergrunddaten_SHP = PREFIX
+			+ "KalypsoRisikoImport_Hintergrunddaten_SHP";
+
+	public static final String KalypsoRisikoImport_Hintergrunddaten_WMS = PREFIX
+			+ "KalypsoRisikoImport_Hintergrunddaten_WMS";
+
+	public static final String KalypsoRisikoImport_Landnutzungsdaten = PREFIX
+			+ "KalypsoRisikoImport_Landnutzungsdaten";
+
+	public static final String KalypsoRisikoImport_Wassertiefendaten = PREFIX
+			+ "KalypsoRisikoImport_Wassertiefendaten";
+
+	public static final String KalypsoRisikoBearbeiten_HQi = PREFIX
+			+ "KalypsoRisikoBearbeiten_HQi";
+
+	public static final String KalypsoRisikoBearbeiten_Rasterdaten = PREFIX
+			+ "KalypsoRisikoBearbeiten_Rasterdaten";
+
+	public static final String KalypsoRisikoBerechnungsparameter = PREFIX
+			+ "KalypsoRisikoBerechnungsparameter";
+
+	public static final String KalypsoRisikoBerechnungsparameter_festlegen = PREFIX
+			+ "KalypsoRisikoBerechnungsparameter_festlegen";
+
+	public static final String KalypsoRisikoBerechnungsparameter_Risikozonierung = PREFIX
+			+ "KalypsoRisikoBerechnungsparameter_Risikozonierung";
+
+	public static final String KalypsoRiskProcessing = PREFIX
+			+ "KalypsoRiskProcessing";
+
+	public static final String KalypsoRisikoRasterung_Landnutzung = PREFIX
+			+ "KalypsoRisikoRasterung_Landnutzung";
+
+	public static final String KalypsoRisikoBerechnung_Schadenspotential = PREFIX
+			+ "KalypsoRisikoBerechnung_Schadenspotential";
+
+	public static final String KalypsoRisikoAusweisung_HQ_Risiko = PREFIX
+			+ "KalypsoRisikoAusweisung_HQ_Risiko";
+
+	public static final String KalypsoRiskPostprocessing = PREFIX
+			+ "KalypsoRiskPostprocessing";
+
+	public static final String KalypsoRiskStatistische_Auswertung = PREFIX
+			+ "KalypsoRiskStatistische_Auswertung";
+
+	public static final String KalypsoRiskExport_Schadenspotential = PREFIX
+			+ "KalypsoRiskExport_Schadenspotential";
+
+	public static final String KalypsoRiskExport_Risikozonen = PREFIX
+			+ "KalypsoRiskExport_Risikozonen";
+
 }
-				
-			
