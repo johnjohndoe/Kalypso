b5e5533f37514d0e77334ec614ec83601c5d769c
kuch <kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-05-29 13:38:32 +0000

    NEW - # 627: Inundation Duration Event Based https://dev.bjoernsen.de/nofdp/ticket/627

--- trunk/KalypsoDeegree/deegree/org/kalypsodeegree_impl/tools/GeometryUtilities.java
+++ trunk/KalypsoDeegree/deegree/org/kalypsodeegree_impl/tools/GeometryUtilities.java
@@ -15,16 +15,16 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * history:
- * 
+ *
  * Files in this package are originally taken from deegree and modified here
  * to fit in kalypso. As goals of kalypso differ from that one in deegree
- * interface-compatibility to deegree is wanted but not retained always. 
- * 
- * If you intend to use this software in other ways than in kalypso 
+ * interface-compatibility to deegree is wanted but not retained always.
+ *
+ * If you intend to use this software in other ways than in kalypso
  * (e.g. OGC-web services), you should consider the latest version of deegree,
  * see http://www.deegree.org .
  *
- * all modifications are licensed as deegree, 
+ * all modifications are licensed as deegree,
  * original copyright:
  *
  * Copyright (C) 2001 by:
@@ -1107,6 +1107,9 @@ public class GeometryUtilities
     final int numVertices = posArray.length / 3;
     final TriangulationUtils triangulator = new TriangulationUtils();
     final int num = triangulator.triangulateConcavePolygon( posArray, 0, numVertices, output, normal );
+
+    if( num < 0 )
+      return new GM_Position[0][0];
 
     final GM_Position[][] triangles = new GM_Position[num][3];
 
