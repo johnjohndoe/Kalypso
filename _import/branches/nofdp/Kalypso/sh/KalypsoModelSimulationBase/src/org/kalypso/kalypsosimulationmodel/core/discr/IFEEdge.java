d88b05bc84f76474eace0086449f9624565d0da6
nobody <nobody@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-06-10 15:18:10 +0000

    This commit was manufactured by cvs2svn to create branch 'nofdp'.

---
+++ _import/branches/nofdp/Kalypso/sh/KalypsoModelSimulationBase/src/org/kalypso/kalypsosimulationmodel/core/discr/IFEEdge.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.kalypso.kalypsosimulationmodel.core.discr;
+
+import org.kalypsodeegree.model.feature.binding.IFeatureWrapper2;
+
+/**
+ * Base interface  for all classes abstracting a finite
+ * element element in the substitution hierarchy of simBase:_FEEdge 
+ * 
+ * @author Patrice Congo
+ */
+public interface IFEEdge extends IFeatureWrapper2
+{
+	
+	
+}
