73ab8b8fc8c9f960fa613f7040182a375282a4d4
nobody <nobody@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-06-11 09:33:54 +0000

    This commit was manufactured by cvs2svn to create tag 'nofdp_zwischenstand_juni'.

--- KalypsoPortal/src/org/kalypso/portal/perspective/IKalypsoPlanerPortalPerspectivConstants.java
+++
@@ -1,7 +0,0 @@
-package org.kalypso.portal.perspective;
-
-public interface IKalypsoPlanerPortalPerspectivConstants {
-
-	public static final String KalypsoPlanerPortalPerspectiv_ID = "org.kalypso.portal.perspective.planer";
-
-}
