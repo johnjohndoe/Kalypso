fd7e6e4431e6b4b5c010c7c42cf5852999eb145d
kuch <kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-06-16 12:47:59 +0000

    NEW - # 641: Inundation Duration - Hydropgrah https://dev.bjoernsen.de/nofdp/ticket/641

--- branches/nofdp/KalypsoDeegree/deegree/org/kalypso/grid/GridMetaReaderAscii.java
+++ branches/nofdp/KalypsoDeegree/deegree/org/kalypso/grid/GridMetaReaderAscii.java
@@ -61,25 +61,35 @@ public class GridMetaReaderAscii implements IGridMetaReader
 
   private RectifiedGridDomain m_domain;
 
-  private final URL m_urlImage;
-
   private String m_noDataValue;
 
   public GridMetaReaderAscii( final URL urlImage, final String cs )
   {
-    m_urlImage = urlImage;
     m_cs = cs;
-    setup();
-  }
 
-  private void setup( )
-  {
-    if( m_urlImage == null )
+    if( urlImage == null )
       throw (new IllegalStateException());
 
     try
     {
-      final AsciiGridReader reader = new AsciiGridReader( new File( m_urlImage.getFile() ) );
+      final AsciiGridReader reader = new AsciiGridReader( new File( urlImage.getFile() ) );
+      m_domain = reader.getGridDomain( m_cs );
+      m_noDataValue = reader.getNoDataValue();
+    }
+    catch( final Exception e )
+    {
+      e.printStackTrace();
+    }
+
+  }
+
+  public GridMetaReaderAscii( final File image, final String cs )
+  {
+    m_cs = cs;
+
+    try
+    {
+      final AsciiGridReader reader = new AsciiGridReader( image );
       m_domain = reader.getGridDomain( m_cs );
       m_noDataValue = reader.getNoDataValue();
     }
