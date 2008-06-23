426b46da3e91965b65e9c3c539d28c23de4d797f
thomas <thomas@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-06-23 13:51:29 +0000

    CLOSED - bug 1184: [1d2d Calculation Wizard] multiple processing of result files possible http://dev.bjoernsen.de/cgi-bin/bugzilla/show_bug.cgi?id=1184

--- _import/branches/branch_kalypsoProduct_v2_0/Kalypso/sh/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/sim/RMA10ResultPage.java
+++ _import/branches/branch_kalypsoProduct_v2_0/Kalypso/sh/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/sim/RMA10ResultPage.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraße 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.kalypsomodel1d2d.sim;
 
@@ -80,6 +80,8 @@ public class RMA10ResultPage extends WizardPage implements IWizardPage, ISimulat
   private StatusComposite m_statusComp;
 
   protected boolean m_deleteAllResults;
+
+  private boolean m_isProcessing = false;
 
   protected RMA10ResultPage( final String pageName, final ResultManager resultManager, final IContainer unitFolder, final ICaseDataProvider<IModel> caseDataProvider )
   {
@@ -169,6 +171,8 @@ public class RMA10ResultPage extends WizardPage implements IWizardPage, ISimulat
     /* Result processing */
     final ResultManagerOperation operation = new ResultManagerOperation( m_resultManager, m_unitFolder, m_simulationStatus, bean, m_caseDataProvider );
 
+    m_isProcessing = true;
+
     final IWizardContainer container = getContainer();
     if( container instanceof WizardDialog2 )
     {
@@ -186,12 +190,19 @@ public class RMA10ResultPage extends WizardPage implements IWizardPage, ISimulat
       m_statusComp.setStatus( m_resultStatus );
 
       setMessage( "Ergebnisauswertung beendet. Drücken Sie 'Beenden', um den Dialog zu schliesen." );
+
+      m_isProcessing = false;
     }
   }
 
   public IStatus getResultStatus( )
   {
     return m_resultStatus;
+  }
+
+  public boolean isProcessing( )
+  {
+    return m_isProcessing;
   }
 
 }
