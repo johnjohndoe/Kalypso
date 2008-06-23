a6aed224f6737c0ea53092fa2d8461e5c09bae71
kimwerner <kimwerner@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-06-23 10:12:10 +0000

    no interpolation calculated while inserting a point in cross section

--- branches/branch_kalypsoProduct_v2_0/KalypsoUI/src/org/kalypso/ogc/gml/om/table/command/AddRowHandler.java
+++ branches/branch_kalypsoProduct_v2_0/KalypsoUI/src/org/kalypso/ogc/gml/om/table/command/AddRowHandler.java
@@ -2,54 +2,55 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ogc.gml.om.table.command;
-
-import javax.xml.namespace.QName;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
-import org.kalypso.commons.xml.NS;
+import org.eclipse.ui.progress.UIJob;
 import org.kalypso.i18n.Messages;
-import org.kalypso.observation.result.IComponent;
 import org.kalypso.observation.result.IRecord;
 import org.kalypso.observation.result.TupleResult;
 
@@ -66,80 +67,37 @@ public class AddRowHandler extends AbstractHandler
   {
     final TableViewer viewer = TupleResultCommandUtils.findTableViewer( event );
     final TupleResult tupleResult = TupleResultCommandUtils.findTupleResult( event );
-    if( viewer == null )
-      throw new ExecutionException( Messages.getString("org.kalypso.ogc.gml.om.table.command.AddRowHandler.0") ); //$NON-NLS-1$
+    if( viewer == null || tupleResult==null)
+      throw new ExecutionException( Messages.getString( "org.kalypso.ogc.gml.om.table.command.AddRowHandler.0" ) ); //$NON-NLS-1$
 
     final IStructuredSelection selection = (IStructuredSelection) viewer.getSelection();
-    final Object target = selection.getFirstElement();
-    final int index = tupleResult == null ? -1 : tupleResult.indexOf( target );
-    final int size = tupleResult == null ? 0 : tupleResult.size();
-    final IRecord next = index < size - 2 ? tupleResult.get( index + 1 ) : null;
+    if( selection==null)
+      throw new ExecutionException(  "no selection in tuple result viewer available"  ); 
+
+    final Object obj = selection.getFirstElement();
+    final int index =  tupleResult.indexOf( obj );
+ 
     final IRecord row = tupleResult.createRecord();
-    if( target == null )
-    {
-      for( final IComponent component : tupleResult.getComponents() )
-      {
-        if( component.getValueTypeName().equals( new QName( NS.XSD_SCHEMA, "double" ) ) ) //$NON-NLS-1$
-          row.setValue( component, 0.0 );
-      }
-    }
-    else
-    {
-      for( final IComponent component : tupleResult.getComponents() )
-      {
-        if( component.getValueTypeName().equals( new QName( NS.XSD_SCHEMA, "double" ) ) && (target instanceof IRecord) ) //$NON-NLS-1$
-        {
-          if( "urn:ogc:gml:dict:kalypso:model:wspm:profilePointComponents#BREITE".equals( component.getId() ) ) //$NON-NLS-1$
-          {
-            if( next != null )
-            {
-              final Object b1 = ((IRecord) target).getValue( component );
-              if( b1 == null )
-                continue;
-              final Object l = next.getValue( component );
-              if( l == null )
-                continue;
-              row.setValue( component, (Double) b1 - ((Double) b1 - (Double) l) / 2.0 );
-            }
-            else
-            {
-              final Object value = ((IRecord) target).getValue( component );
-              if( value != null )
-                row.setValue( component, (Double) value + 10 );
-              else
-                row.setValue( component, component.getDefaultValue() );
-            }
-          }
-          else if( "urn:ogc:gml:dict:kalypso:model:wspm:profilePointComponents#HOEHE".equals( component.getId() ) ) //$NON-NLS-1$
-          {
-            if( next != null )
-            {
-              final Object h1 = ((IRecord) target).getValue( component );
-              if( h1 == null )
-                continue;
-              final Object z = next.getValue( component );
-              if( z == null )
-                continue;
-              row.setValue( component, (Double) h1 - ((Double) h1 - (Double) z) / 2.0 );
-            }
-            else
-            {
-              final Object value = ((IRecord) target).getValue( component );
-              if( value != null )
-                row.setValue( component, value );
-              else
-                row.setValue( component, component.getDefaultValue() );
-            }
-          }
-          else
-            row.setValue( component, ((IRecord) target).getValue( component ) );
-        }
-      }
-    }
-    if( next != null )
+    
+      final boolean success = tupleResult.doInterpolation( tupleResult, row, index ,0.5);
+   
+    if( success )
       tupleResult.add( index + 1, row );
     else
       tupleResult.add( row );
+
+    // select the new row; in ui job, as table is also updated in an ui event
+    new UIJob( "" )
+    {
+      @Override
+      public IStatus runInUIThread( final IProgressMonitor monitor )
+      {
+        viewer.setSelection( new StructuredSelection( row ) );
+        return Status.OK_STATUS;
+      }
+    }.schedule();
+
     return null;
   }
+
 }
