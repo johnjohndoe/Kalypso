db05c33789f043a764299b695eeaef02de4c9b54
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-07-28 15:36:56 +0000

    Major commit...: merged https://svn.wb.tu-harburg.de/kalypso/source/k-base/branches/branch-2005-12-2-kalypso-1-0 into head.

--- KalypsoUI/src/org/kalypso/ogc/sensor/view/actions/ConfigurePreviewHandler.java
+++ KalypsoUI/src/org/kalypso/ogc/sensor/view/actions/ConfigurePreviewHandler.java
@@ -54,6 +54,7 @@ import org.kalypso.contribs.eclipse.jface.dialog.DateRangeInputDialog;
 import org.kalypso.contribs.eclipse.swt.widgets.DateRangeInputControlStuct;
 import org.kalypso.i18n.Messages;
 import org.kalypso.ogc.sensor.cache.ObservationCache;
+import org.kalypso.ogc.sensor.timeseries.TimeserieUtils;
 import org.kalypso.ogc.sensor.view.ObservationChooser;
 import org.kalypso.repository.IRepository;
 import org.kalypso.ui.KalypsoGisPlugin;
@@ -79,7 +80,8 @@ public class ConfigurePreviewHandler extends AbstractHandler
     if( rep == null )
       return Status.OK_STATUS;
 
-    final DateRangeInputDialog dlg = new DateRangeInputDialog( shell, Messages.getString("org.kalypso.ogc.sensor.view.actions.ConfigurePreviewHandler.0"), Messages.getString("org.kalypso.ogc.sensor.view.actions.ConfigurePreviewHandler.1"), DateRangeInputControlStuct.create( rep.getProperties(), DateFormat.getDateTimeInstance() ) ); //$NON-NLS-1$ //$NON-NLS-2$
+    final DateFormat dateFormat = TimeserieUtils.getDateFormat();
+    final DateRangeInputDialog dlg = new DateRangeInputDialog( shell, Messages.getString("org.kalypso.ogc.sensor.view.actions.ConfigurePreviewHandler.0"), Messages.getString("org.kalypso.ogc.sensor.view.actions.ConfigurePreviewHandler.1"), DateRangeInputControlStuct.create( rep.getProperties(), dateFormat ) ); //$NON-NLS-1$ //$NON-NLS-2$
 
     if( dlg.open() == Window.OK )
     {
