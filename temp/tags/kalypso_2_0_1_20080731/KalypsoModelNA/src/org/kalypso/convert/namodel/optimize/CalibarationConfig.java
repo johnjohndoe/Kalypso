583381da4eaa3c7656ce5fb330ec77c3f42fd445
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-08-01 18:48:03 +0000

    Deployment of kalypso_2_0_1_20080731 finished.

---
+++ temp/tags/kalypso_2_0_1_20080731/KalypsoModelNA/src/org/kalypso/convert/namodel/optimize/CalibarationConfig.java
@@ -0,0 +1,127 @@
+/*--------------- Kalypso-Header --------------------------------------------------------------------
+
+ This file is part of kalypso.
+ Copyright (C) 2004, 2005 by:
+
+ Technical University Hamburg-Harburg (TUHH)
+ Institute of River and coastal engineering
+ Denickestr. 22
+ 21073 Hamburg, Germany
+ http://www.tuhh.de/wb
+
+ and
+ 
+ Bjoernsen Consulting Engineers (BCE)
+ Maria Trost 3
+ 56070 Koblenz, Germany
+ http://www.bjoernsen.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ Contact:
+
+ E-Mail:
+ belger@bjoernsen.de
+ schlienger@bjoernsen.de
+ v.doemming@tuhh.de
+ 
+ ---------------------------------------------------------------------------------------------------*/
+package org.kalypso.convert.namodel.optimize;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kalypso.optimize.transform.ParameterOptimizeContext;
+import org.kalypsodeegree.model.feature.Feature;
+import org.kalypsodeegree_impl.model.feature.FeatureHelper;
+
+/**
+ * @author doemming
+ */
+public class CalibarationConfig
+{
+
+  private final List<ParameterOptimizeContext> m_contexts;
+
+  public CalibarationConfig()
+  {
+    m_contexts = new ArrayList<ParameterOptimizeContext>();
+  }
+
+  public void addFromNAControl( Feature rootFeatureControl )
+  {
+    // Catchments
+    final String queryBaseCatchment = FeatureHelper.getAsString( rootFeatureControl, "Catchments" );
+    String[] xpathControl = new String[]
+    {
+        "CatchmentsBianf",
+        "CatchmentsFaktorRetobTetint",
+        "CatchmentsFaktn",
+        "CatchmentsFaktorAigw" };
+
+    String[][] xpathModel = new String[][]
+    {
+        new String[]
+//        { queryBaseCatchment + "/:bodenkorrekturmember/:bodenschichtkorrektur/:banf" },
+        { queryBaseCatchment + "/:faktorBianf" },
+        new String[]
+        { queryBaseCatchment + "/:faktorRetobRetint" },
+        new String[]
+        { queryBaseCatchment + "/:faktn" },
+        new String[]
+        { queryBaseCatchment + "/:faktorAigw" } };
+
+    generateAndAddContexts( rootFeatureControl, xpathModel, xpathControl );
+    // KMChannels
+    final String queryBaseKMChannel = FeatureHelper.getAsString( rootFeatureControl, "KMChannels" );
+    final String[] propNamesII = new String[]
+    {
+        "KMChannelsFaktorRkf",
+        "KMChannelsFaktorRnf" };
+
+    final String[][] queryKMChannels = new String[][]
+    {
+        new String[]
+        { queryBaseKMChannel + "/:faktorRkf" },
+        new String[]
+        { queryBaseKMChannel + "/:faktorRnf" } };
+    generateAndAddContexts( rootFeatureControl, queryKMChannels, propNamesII );
+  }
+
+  private void generateAndAddContexts( Feature rootFeatureControl, String[][] xpathModel, String[] xpathControl )
+  {
+    final int n = xpathControl.length;
+    for( int i = 0; i < n; i++ )
+    {
+      final String value = FeatureHelper.getAsString( rootFeatureControl, xpathControl[i] );
+      if( value == null || value.length() == 0 )
+        return;
+      final double initialValue = Double.parseDouble( value );
+      final String[] xPaths = xpathModel[i];
+      addContext( new ParameterOptimizeContext( initialValue, 1, 0, 2, ParameterOptimizeContext.MODE_DIRECT, xPaths ) );
+    }
+  }
+
+  public ParameterOptimizeContext[] getCalContexts()
+  {
+    return m_contexts.toArray( new ParameterOptimizeContext[m_contexts.size()] );
+  }
+
+  public void addContext( ParameterOptimizeContext context )
+  {
+    m_contexts.add( context );
+  }
+
+}
