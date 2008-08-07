116b8ccf9ef269b2c1bb9d8e1cf5dea687893b55
skurzbach <skurzbach@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-08-07 12:43:38 +0000

    Remove @override

--- trunk/KalypsoUI/src/org/kalypso/ogc/gml/table/wizard/ExportableLayerTable.java
+++ trunk/KalypsoUI/src/org/kalypso/ogc/gml/table/wizard/ExportableLayerTable.java
@@ -129,7 +129,6 @@ public class ExportableLayerTable implements IExportableObject
   /**
    * @see org.kalypso.metadoc.IExportableObject#getStationIDs()
    */
-  @Override
   public String getStationIDs( )
   {
     return "";
