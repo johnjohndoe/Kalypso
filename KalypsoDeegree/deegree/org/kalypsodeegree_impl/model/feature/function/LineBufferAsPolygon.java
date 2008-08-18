bc4cbff5008a19b4931e98f4a29049d077dce0eb
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-08-18 15:22:52 +0000

    CLOSED - # 765: riverBedElevationChange und BufferStrip haben laut Maßnahmen-Prüfen bei der Hydraulischen Berechnung https://dev.bjoernsen.de/nofdp/ticket/765 CLOSED - # 762: im linken Baum ist Mehrfachselektion möglich https://dev.bjoernsen.de/nofdp/ticket/762 CLOSED - # 764: Konstruktionshilfe-Tooltip für "Floodplain Earth Wall" verweist auf "adapted Agriculture" https://dev.bjoernsen.de/nofdp/ticket/764

--- KalypsoDeegree/deegree/org/kalypsodeegree_impl/model/feature/function/LineBufferAsPolygon.java
+++ KalypsoDeegree/deegree/org/kalypsodeegree_impl/model/feature/function/LineBufferAsPolygon.java
@@ -15,16 +15,16 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * history:
- * 
+ *
  * Files in this package are originally taken from deegree and modified here
  * to fit in kalypso. As goals of kalypso differ from that one in deegree
- * interface-compatibility to deegree is wanted but not retained always. 
- * 
- * If you intend to use this software in other ways than in kalypso 
+ * interface-compatibility to deegree is wanted but not retained always.
+ *
+ * If you intend to use this software in other ways than in kalypso
  * (e.g. OGC-web services), you should consider the latest version of deegree,
  * see http://www.deegree.org .
  *
- * all modifications are licensed as deegree, 
+ * all modifications are licensed as deegree,
  * original copyright:
  *
  * Copyright (C) 2001 by:
@@ -354,6 +354,8 @@ public class LineBufferAsPolygon extends FeaturePropertyFunction
       }
 
       final GM_Surface< ? > polygon = (GM_Surface< ? >) JTSAdapter.wrap( geometry );
+      polygon.setCoordinateSystem( lineValue.getCoordinateSystem() );
+
       return polygon;
     }
     catch( final GM_Exception e )
