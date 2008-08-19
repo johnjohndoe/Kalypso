9275acba99abd3283265b7bcafffeda23114a955
holger.albert <holger.albert@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-08-19 09:54:13 +0000

    - raster service process timeseries function

--- KalypsoDeegree/deegree/org/kalypso/grid/DefaultWalkingStrategy.java
+++ KalypsoDeegree/deegree/org/kalypso/grid/DefaultWalkingStrategy.java
@@ -44,17 +44,12 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 
 import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Envelope;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
 
 /**
  * @author Gernot Belger
  */
 public class DefaultWalkingStrategy implements IGeoWalkingStrategy
 {
-  private static GeometryFactory GF = new GeometryFactory();
-
   /**
    * Simple, straightforward implementation of the interface method.
    * <p>
@@ -62,49 +57,25 @@ public class DefaultWalkingStrategy implements IGeoWalkingStrategy
    * </p>
    * 
    * @see org.kalypso.grid.IGeoWalkingStrategy#walk(org.kalypso.grid.IGeoGrid, org.kalypso.grid.IGeoGridWalker,
-   *      com.vividsolutions.jts.geom.Geometry, org.eclipse.core.runtime.IProgressMonitor)
+   *      org.kalypso.grid.IGeoGridArea, org.eclipse.core.runtime.IProgressMonitor)
    */
-  public Object walk( final IGeoGrid grid, final IGeoGridWalker pwo, final Geometry walkingArea, final IProgressMonitor monitor ) throws GeoGridException, OperationCanceledException
+  public Object walk( final IGeoGrid grid, final IGeoGridWalker pwo, final IGeoGridArea walkingArea, final IProgressMonitor monitor ) throws GeoGridException, OperationCanceledException
   {
     final int sizeX = grid.getSizeX();
     final int sizeY = grid.getSizeY();
     if( monitor != null )
       monitor.beginTask( "Raster wird durchlaufen", sizeY );
 
-    final int yStart;
-    final int yEnd;
-    final int xStart;
-    final int xEnd;
-
-    if( walkingArea == null )
-    {
-      yStart = 0;
-      yEnd = sizeY;
-      xStart = 0;
-      xEnd = sizeX;
-    }
-    else
-    {
-      final Envelope envelope = walkingArea.getEnvelopeInternal();
-      final GeoGridCell minMinCell = GeoGridUtilities.cellFromPosition( grid, new Coordinate( envelope.getMinX(), envelope.getMinY() ) );
-      final GeoGridCell maxMaxCell = GeoGridUtilities.cellFromPosition( grid, new Coordinate( envelope.getMaxX(), envelope.getMaxY() ) );
-
-      yStart = Math.max( 0, Math.min( minMinCell.y, maxMaxCell.y ) );
-      yEnd = Math.min( sizeY, Math.max( minMinCell.y, maxMaxCell.y ) + 1 );
-      xStart = Math.max( 0, Math.min( minMinCell.x, maxMaxCell.x ) );
-      xEnd = Math.min( sizeX, Math.max( minMinCell.x, maxMaxCell.x ) + 1 );
-    }
-
     pwo.start( grid );
 
     final Coordinate tmpCrd = new Coordinate();
 
-    for( int y = yStart; y < yEnd; y++ )
+    for( int y = 0; y < sizeY; y++ )
     {
-      for( int x = xStart; x < xEnd; x++ )
+      for( int x = 0; x < sizeX; x++ )
       {
         final Coordinate coordinate = GeoGridUtilities.calcCoordinate( grid, x, y, tmpCrd );
-        if( walkingArea == null || walkingArea.contains( GF.createPoint( coordinate ) ) )
+        if( walkingArea == null || walkingArea.contains( x, y, coordinate ) )
           pwo.operate( x, y, coordinate );
       }
 
