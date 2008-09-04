e94ad20c06067b4debe32bcb773d4a1e39088f4e
holger.albert <holger.albert@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-09-04 12:15:06 +0000

    - new ini with server config (have to check, if okay), old config for use with the HwvServiceSachsen launch backuped in folder lokal

--- trunk/KalypsoServiceObs/src/org/kalypso/services/observation/WSProxyServlet.java
+++ trunk/KalypsoServiceObs/src/org/kalypso/services/observation/WSProxyServlet.java
@@ -20,14 +20,19 @@ public class WSProxyServlet extends WSServlet
 {
   private final WSServletContextListener m_servletContextListener = new WSServletContextListener();
 
+  /**
+   * @see com.sun.xml.ws.transport.http.servlet.WSServlet#init(javax.servlet.ServletConfig)
+   */
   @Override
   public void init( final ServletConfig servletConfig ) throws ServletException
   {
+    /* Get the servlet context. */
     final ServletContext context = servletConfig.getServletContext();
 
+    /* Debug. */
     System.out.println( "[NNNN] WS_Proxy_Servlet: Setting JAX-WS Runtime..." );
 
-    // We just simulate the initialization of the servlet
+    /* We just simulate the initialization of the servlet. */
     m_servletContextListener.contextInitialized( new ServletContextEvent( context ) );
 
     super.init( servletConfig );
