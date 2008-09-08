831644ba693070625751371bcfa65230ab29e271
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-09-08 18:23:22 +0000

    No more syste outs from command job

--- trunk/KalypsoUI/src/org/kalypso/util/command/CommandJob.java
+++ trunk/KalypsoUI/src/org/kalypso/util/command/CommandJob.java
@@ -10,7 +10,7 @@
  http://www.tuhh.de/wb
 
  and
- 
+
  Bjoernsen Consulting Engineers (BCE)
  Maria Trost 3
  56070 Koblenz, Germany
@@ -36,11 +36,9 @@
  belger@bjoernsen.de
  schlienger@bjoernsen.de
  v.doemming@tuhh.de
- 
+
  ---------------------------------------------------------------------------------------------------*/
 package org.kalypso.util.command;
-
-import java.util.logging.Logger;
 
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -68,7 +66,8 @@ public final class CommandJob extends Job
 
   private final ICommandManager myCommandManager;
 
-  private static final Logger LOGGER = Logger.getLogger( CommandJob.class.getName() );
+  // TODO: change to tracing option
+// private static final Logger LOGGER = Logger.getLogger( CommandJob.class.getName() );
 
   private final Runnable myRunnable;
 
@@ -100,7 +99,7 @@ public final class CommandJob extends Job
   {
     final String description = getCommandDescription( myCommandManager, myCommand, m_type );
 
-    LOGGER.info( m_type.toString() + ": " + description ); //$NON-NLS-1$
+//    LOGGER.info( m_type.toString() + ": " + description ); //$NON-NLS-1$
 
     try
     {
@@ -118,15 +117,12 @@ public final class CommandJob extends Job
     {
       e.printStackTrace();
 
-      LOGGER.warning( Messages.getString("org.kalypso.util.command.CommandJob.5") + m_type + ": " + description ); //$NON-NLS-1$ //$NON-NLS-2$
+//      LOGGER.warning( Messages.getString("org.kalypso.util.command.CommandJob.5") + m_type + ": " + description ); //$NON-NLS-1$ //$NON-NLS-2$
 
       return new Status( IStatus.ERROR, KalypsoGisPlugin.getDefault().getBundle().getSymbolicName(), 0, Messages.getString("org.kalypso.util.command.CommandJob.7") + m_type + ": " + description, e ); //$NON-NLS-1$ //$NON-NLS-2$
     }
 
-    if( description == null )
-      System.out.print( false );
-
-    LOGGER.info( Messages.getString("org.kalypso.util.command.CommandJob.9") + m_type + ": " + description ); //$NON-NLS-1$ //$NON-NLS-2$
+//    LOGGER.info( Messages.getString("org.kalypso.util.command.CommandJob.9") + m_type + ": " + description ); //$NON-NLS-1$ //$NON-NLS-2$
 
     return Status.OK_STATUS;
   }
