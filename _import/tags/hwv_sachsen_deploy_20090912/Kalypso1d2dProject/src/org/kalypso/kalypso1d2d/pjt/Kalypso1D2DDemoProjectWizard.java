231922d8d0f9b40870d91cf1a80cd6055eac991b
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-09-12 11:27:25 +0000

    Deploy of HWV (Hochwasservorhersage) Sachsen on 12.9.2008

---
+++ _import/tags/hwv_sachsen_deploy_20090912/Kalypso1d2dProject/src/org/kalypso/kalypso1d2d/pjt/Kalypso1D2DDemoProjectWizard.java
@@ -0,0 +1,18 @@
+package org.kalypso.kalypso1d2d.pjt;
+
+import org.kalypso.afgui.wizards.NewProjectWizard;
+
+/**
+ * Creates a demo Kalypso 1d 2d Project
+ * 
+ * @author Thomas Jung
+ */
+public class Kalypso1D2DDemoProjectWizard extends NewProjectWizard
+{
+  public final static String ID = "org.kalypso.kalypso1d2d.pjt.Kalypso1D2DDemoProjectWizard"; //$NON-NLS-1$
+
+  public Kalypso1D2DDemoProjectWizard( )
+  {
+    super( "org.kalypso.kalypso1d2d.pjt.demoProject", true );//$NON-NLS-1$
+  }
+}
