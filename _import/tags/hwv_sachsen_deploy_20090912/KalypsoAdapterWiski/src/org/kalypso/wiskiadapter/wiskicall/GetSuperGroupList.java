f039a892b805f227c1515bfdf3b4da0ae7f774cb
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-09-12 11:29:28 +0000

    Deploy of HWV (Hochwasservorhersage) Sachsen on 12.9.2008

---
+++ _import/tags/hwv_sachsen_deploy_20090912/KalypsoAdapterWiski/src/org/kalypso/wiskiadapter/wiskicall/GetSuperGroupList.java
@@ -0,0 +1,73 @@
+package org.kalypso.wiskiadapter.wiskicall;
+
+import java.rmi.NoSuchObjectException;
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.List;
+
+import de.kisters.tsmsystem.common.data.SimpleRequestFilterTerm;
+import de.kisters.tsmsystem.common.data.SimpleRequestSortTerm;
+import de.kisters.wiski.webdataprovider.common.net.KiWWDataProviderInterface;
+import de.kisters.wiski.webdataprovider.common.util.KiWWException;
+import de.kisters.wiski.webdataprovider.server.KiWWDataProviderRMIf;
+
+/**
+ * GetSuperGroupList
+ * 
+ * @author schlienger
+ */
+public class GetSuperGroupList implements IWiskiCall
+{
+  /** columns of GROUP */
+  public static final String[] COLUMNS =
+  { "supergroup_name" };
+
+  private final SimpleRequestFilterTerm filtergroup;
+
+  private final SimpleRequestSortTerm sort;
+
+  private List resultList;
+
+  public GetSuperGroupList( final String superGroupName )
+  {
+    filtergroup = new SimpleRequestFilterTerm();
+    filtergroup.addColumnReference( "supergroup_name" );
+    filtergroup.addOperator( "like" );
+    filtergroup.addValue( superGroupName );
+
+    sort = new SimpleRequestSortTerm();
+    sort.addColumnAscent( "supergroup_name" );
+  }
+
+  public GetSuperGroupList( final String[] superGroupNames )
+  {
+    filtergroup = new SimpleRequestFilterTerm();
+
+    for( int i = 0; i < superGroupNames.length; i++ )
+    {
+      filtergroup.addColumnReference( "supergroup_name" );
+      filtergroup.addOperator( "like" );
+      filtergroup.addValue( superGroupNames[i] );
+      
+      if( i < superGroupNames.length - 1 )
+        filtergroup.addOperator( "or" );
+    }
+
+    sort = new SimpleRequestSortTerm();
+    sort.addColumnAscent( "supergroup_name" );
+  }
+
+  public void execute( final KiWWDataProviderRMIf wiski, final HashMap userData ) throws NoSuchObjectException,
+      KiWWException, RemoteException
+  {
+    final HashMap superGrouplist = wiski.getGroupList( userData, COLUMNS, KiWWDataProviderInterface.TIMESERIES_GROUP,
+        sort, filtergroup, 0, 0, false, null );
+
+    resultList = (List)superGrouplist.get( KiWWDataProviderInterface.KEY_RESULT_LIST );
+  }
+
+  public List getResultList()
+  {
+    return resultList;
+  }
+}
