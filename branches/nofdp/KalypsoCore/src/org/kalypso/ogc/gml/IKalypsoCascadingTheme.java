f2fbdaa0d7a7dd108b5cefbb32553f13ac319972
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-09-16 15:16:25 +0000

    RESOLVED - bug 1284: Conflict Detection https://dev.bjoernsen.de/cgi-bin/bugzilla/show_bug.cgi?id=1284

--- branches/nofdp/KalypsoCore/src/org/kalypso/ogc/gml/IKalypsoCascadingTheme.java
+++ branches/nofdp/KalypsoCore/src/org/kalypso/ogc/gml/IKalypsoCascadingTheme.java
@@ -43,7 +43,7 @@ package org.kalypso.ogc.gml;
 /**
  * @author kuch
  */
-public interface IKalypsoCascadingTheme extends IKalypsoLayerModell
+public interface IKalypsoCascadingTheme extends IKalypsoLayerModell, IKalypsoTheme
 {
   IKalypsoTheme[] getChildThemes( );
 
