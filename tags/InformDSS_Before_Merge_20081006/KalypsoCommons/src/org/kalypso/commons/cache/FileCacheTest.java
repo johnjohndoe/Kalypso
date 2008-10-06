8e259c6c5eb7759753f575a4c238e5815c94d3e3
holger.albert <holger.albert@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-10-06 11:18:16 +0000

    before merge (branch)

---
+++ tags/InformDSS_Before_Merge_20081006/KalypsoCommons/src/org/kalypso/commons/cache/FileCacheTest.java
@@ -0,0 +1,105 @@
+package org.kalypso.commons.cache;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Comparator;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.io.IOUtils;
+import org.kalypso.commons.serializer.ISerializer;
+
+/**
+ * CacheTest
+ * 
+ * @author schlienger
+ */
+public class FileCacheTest extends TestCase
+{
+  public void testGetObject( ) throws InvocationTargetException
+  {
+    final StringKeyFactory fact = new StringKeyFactory();
+    final Comparator<String> kc = new StringComparator();
+    final ISerializer<String> ser = new StringSerializer();
+    final FileCache<String, String> cache = new FileCache<String, String>( fact, kc, ser, new File( System.getProperty( "java.io.tmpdir" ) ) );
+
+    cache.addObject( "A", "A" );
+    cache.addObject( "B", "B" );
+    cache.addObject( "C", "C" );
+    cache.addObject( "D", "D" );
+
+    assertEquals( cache.getObject( "A" ), "A" );
+    assertEquals( cache.getObject( "B" ), "B" );
+    assertEquals( cache.getObject( "C" ), "C" );
+    assertEquals( cache.getObject( "D" ), "D" );
+
+    cache.remove( "C" );
+
+    assertTrue( cache.getObject( "C" ) == null );
+
+    cache.clear();
+
+    assertTrue( cache.size() == 0 );
+    assertTrue( cache.getObject( "B" ) == null );
+  }
+
+  protected static class StringComparator implements Comparator<String>
+  {
+    public int compare( String s1, String s2 )
+    {
+      return s1.compareTo( s2 );
+    }
+  }
+
+  protected static class StringKeyFactory implements IKeyFactory<String>
+  {
+    public String createKey( final String string )
+    {
+      return string;
+    }
+
+    public String toString( final String key )
+    {
+      return key;
+    }
+  }
+
+  protected static class StringSerializer implements ISerializer<String>
+  {
+    public String read( final InputStream ins ) throws IOException
+    {
+      BufferedReader r = null;
+      try
+      {
+        r = new BufferedReader( new InputStreamReader( ins ) );
+
+        return r.readLine();
+      }
+      finally
+      {
+        IOUtils.closeQuietly( r );
+      }
+    }
+
+    public void write( final String object, final OutputStream os ) throws IOException
+    {
+      BufferedWriter w = null;
+      try
+      {
+        w = new BufferedWriter( new OutputStreamWriter( os ) );
+        w.write( object.toString() );
+      }
+      finally
+      {
+        IOUtils.closeQuietly( w );
+      }
+    }
+  }
+}
---
+++ tags/InformDSS_Before_Merge_20081006/KalypsoCommons/src/org/kalypso/commons/xml/XmlTypes.java
@@ -0,0 +1,164 @@
+/*--------------- Kalypso-Header --------------------------------------------------------------------
+
+ This file is part of kalypso.
+ Copyright (C) 2004, 2005 by:
+
+ Technical University Hamburg-Harburg (TUHH)
+ Institute of River and coastal engineering
+ Denickestr. 22
+ 21073 Hamburg, Germany
+ http://www.tuhh.de/wb
+
+ and
+ 
+ Bjoernsen Consulting Engineers (BCE)
+ Maria Trost 3
+ 56070 Koblenz, Germany
+ http://www.bjoernsen.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ Contact:
+
+ E-Mail:
+ belger@bjoernsen.de
+ schlienger@bjoernsen.de
+ v.doemming@tuhh.de
+ 
+ ---------------------------------------------------------------------------------------------------*/
+package org.kalypso.commons.xml;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.xml.namespace.QName;
+
+import org.kalypso.commons.parser.impl.DateParser;
+import org.kalypso.commons.parser.impl.DoubleParser;
+
+/**
+ * Utility class that contains the default java formaters for the types found in xml.
+ * 
+ * @author schlienger
+ */
+public final class XmlTypes
+{
+  private XmlTypes( )
+  {
+    // do not instanciate
+  }
+
+  public final static QName XS_BOOLEAN = new QName( NS.XSD_SCHEMA, "boolean" );
+
+  public final static QName XS_BYTE = new QName( NS.XSD_SCHEMA, "byte" );
+
+  public final static QName XS_DATE = new QName( NS.XSD_SCHEMA, "date" );
+
+  public final static QName XS_DATETIME = new QName( NS.XSD_SCHEMA, "dateTime" );
+
+  public final static QName XS_DECIMAL = new QName( NS.XSD_SCHEMA, "decimal" );
+
+  public final static QName XS_DOUBLE = new QName( NS.XSD_SCHEMA, "double" );
+
+  public final static QName XS_DURATION = new QName( NS.XSD_SCHEMA, "duration" );
+
+  public final static QName XS_FLOAT = new QName( NS.XSD_SCHEMA, "float" );
+
+  public final static QName XS_INT = new QName( NS.XSD_SCHEMA, "int" );
+
+  public final static QName XS_INTEGER = new QName( NS.XSD_SCHEMA, "integer" );
+
+  public final static QName XS_LONG = new QName( NS.XSD_SCHEMA, "long" );
+
+  public final static QName XS_SHORT = new QName( NS.XSD_SCHEMA, "short" );
+
+  public final static QName XS_STRING = new QName( NS.XSD_SCHEMA, "string" );
+
+  public final static QName XS_TIME = new QName( NS.XSD_SCHEMA, "time" );
+
+  public final static String XML_DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+
+  /**
+   * Parser for the type <code>double</code>. It uses the default behaviour of the java.lang.Double class.
+   */
+  public final static DoubleParser PDOUBLE = new DoubleParser();
+
+  /**
+   * Returns true if the given QName represents an xml-type which is a number
+   */
+  public static boolean isNumber( final QName valueTypeName )
+  {
+    return valueTypeName.equals( XmlTypes.XS_BYTE ) || valueTypeName.equals( XmlTypes.XS_DECIMAL ) || valueTypeName.equals( XmlTypes.XS_DOUBLE ) || valueTypeName.equals( XmlTypes.XS_FLOAT )
+        || valueTypeName.equals( XmlTypes.XS_INT ) || valueTypeName.equals( XmlTypes.XS_INTEGER ) || valueTypeName.equals( XmlTypes.XS_LONG ) || valueTypeName.equals( XmlTypes.XS_SHORT );
+  }
+
+  /**
+   * Returns true if the given QName represents an xml-type which is a date
+   */
+  public static boolean isDate( final QName valueTypeName )
+  {
+    return valueTypeName.equals( XmlTypes.XS_DATE ) || valueTypeName.equals( XmlTypes.XS_DATETIME ) || valueTypeName.equals( XmlTypes.XS_DURATION ) || valueTypeName.equals( XmlTypes.XS_TIME );
+  }
+
+  /**
+   * Parser for the type <code>date</code>. It uses following format string:
+   * 
+   * <pre>
+   *      yyyy-MM-dd'T'HH:mm:ss
+   * </pre>
+   */
+  public static DateParser getDateParser( final TimeZone timezone )
+  {
+    final DateParser parser = new DateParser( XML_DATETIME_FORMAT );
+    parser.setTimezone( timezone );
+
+    return parser;
+  }
+
+  /**
+   * @return the corresponding java class to the given QName representing an XML type.
+   */
+  public static Class< ? > toJavaClass( final QName typeName )
+  {
+    if( XS_BOOLEAN.equals( typeName ) )
+      return Boolean.class;
+    
+    if( XS_BYTE.equals( typeName ) )
+      return Byte.class;
+    
+    if( isDate( typeName ) )
+      return Date.class;
+      
+    if( XS_DECIMAL.equals( typeName ) || XS_DOUBLE.equals( typeName ) )
+      return Double.class;
+    
+    if( XS_FLOAT.equals( typeName ) )
+      return Float.class;
+    
+    if( XS_INT.equals( typeName ) || XS_INTEGER.equals( typeName ) )
+      return Integer.class;
+    
+    if( XS_LONG.equals( typeName ) )
+      return Long.class;
+    
+    if( XS_SHORT.equals( typeName ) )
+      return Short.class;
+    
+    if( XS_STRING.equals( typeName ) )
+      return String.class;
+    
+    return Object.class;
+  }
+}
