9259f06d8220fd07478e4d76b475ce53d09518a7
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-10-13 15:22:47 +0000

    Profile API refactoring - IProfileFeature

--- _import/trunk/Kalypso/sh/KalypsoModelSimulationBase/src/org/kalypso/kalypsosimulationmodel/core/terrainmodel/IRiverProfileNetwork.java
+++ _import/trunk/Kalypso/sh/KalypsoModelSimulationBase/src/org/kalypso/kalypsosimulationmodel/core/terrainmodel/IRiverProfileNetwork.java
@@ -3,16 +3,16 @@ package org.kalypso.kalypsosimulationmodel.core.terrainmodel;
 import javax.xml.namespace.QName;
 
 import org.kalypso.kalypsosimulationmodel.schema.UrlCatalogModelSimulationBase;
-import org.kalypso.model.wspm.core.gml.WspmProfile;
+import org.kalypso.model.wspm.core.gml.IProfileFeature;
 import org.kalypsodeegree.model.feature.binding.IFeatureWrapper2;
 import org.kalypsodeegree.model.feature.binding.IFeatureWrapperCollection;
 
-/**
+/*
  * Interface to be implemented by classes that represent a simBase:RiverProfileNetwork
- * 
+ *
  * @author Patrice Congo
  */
-public interface IRiverProfileNetwork extends IFeatureWrapperCollection<WspmProfile>, IFeatureWrapper2
+public interface IRiverProfileNetwork extends IFeatureWrapperCollection<IProfileFeature>, IFeatureWrapper2
 {
   public static QName QNAME = new QName( UrlCatalogModelSimulationBase.SIM_MODEL_NS, "RiverProfileNetwork" ); //$NON-NLS-1$
 
@@ -25,7 +25,7 @@ public interface IRiverProfileNetwork extends IFeatureWrapperCollection<WspmProf
    *          the reference profile
    * @return the river profile situated before the specified one
    */
-  public WspmProfile getPrevious( final WspmProfile riverProfile );
+  public IProfileFeature getPrevious( final IProfileFeature riverProfile );
 
   /**
    * To get the river profile which is located after the given river
@@ -34,6 +34,6 @@ public interface IRiverProfileNetwork extends IFeatureWrapperCollection<WspmProf
    *          the reference profile
    * @return the river profile situated after the specified one
    */
-  public WspmProfile getNext( final WspmProfile riverProfile );
+  public IProfileFeature getNext( final IProfileFeature riverProfile );
 
 }
