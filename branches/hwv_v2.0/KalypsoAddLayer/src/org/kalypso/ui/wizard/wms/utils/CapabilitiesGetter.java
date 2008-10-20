57cc03cead2163942591689d4ccb5875b17dcf4e
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-10-20 10:23:25 +0000

    Branching hwv

---
+++ branches/hwv_v2.0/KalypsoAddLayer/src/org/kalypso/ui/wizard/wms/utils/CapabilitiesGetter.java
@@ -0,0 +1,84 @@
+package org.kalypso.ui.wizard.wms.utils;
+
+import java.net.URL;
+
+import org.deegree.ogcwebservices.wms.capabilities.WMSCapabilities;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.kalypso.contribs.eclipse.jface.operation.ICoreRunnableWithProgress;
+import org.kalypso.ogc.gml.wms.deegree.DeegreeWMSUtilities;
+import org.kalypso.ogc.gml.wms.loader.ICapabilitiesLoader;
+import org.kalypso.ogc.gml.wms.provider.images.IKalypsoImageProvider;
+import org.kalypso.ogc.gml.wms.utils.KalypsoWMSUtilities;
+
+/**
+ * Small runnable, which loads the capabilities from a WMS.
+ * 
+ * @author Holger Albert
+ */
+public class CapabilitiesGetter implements ICoreRunnableWithProgress
+{
+  /**
+   * The service URL.
+   */
+  private final URL m_service;
+
+  /**
+   * The capabilities.
+   */
+  private WMSCapabilities m_capabilities = null;
+
+  /**
+   * The ID of the provider.
+   */
+  private final String m_providerID;
+
+  public CapabilitiesGetter( final URL service, final String providerID )
+  {
+    super();
+
+    m_service = service;
+    m_providerID = providerID;
+  }
+
+  /**
+   * This function starts the loading of the capabilities.
+   * 
+   * @param monitor
+   *            A progress monitor.
+   * @return A status containing the result.
+   */
+  public IStatus execute( final IProgressMonitor monitor ) throws CoreException
+  {
+    /*
+     * This image provider should do nothing, but create the necessary image loader, so we need not to provide anything,
+     * except the providerID.
+     */
+    final IKalypsoImageProvider imageProvider = KalypsoWMSUtilities.getImageProvider( "", null, null, "", m_providerID );
+
+    /* Get the loader. */
+    final ICapabilitiesLoader loader = imageProvider.getLoader();
+
+    /* Init the loader. */
+    loader.init( m_service );
+
+    /* Load the capabilities. */
+    final WMSCapabilities capabilities = DeegreeWMSUtilities.loadCapabilities( loader, monitor );
+
+    m_capabilities = capabilities;
+
+    return Status.OK_STATUS;
+  }
+
+  /**
+   * This function returns the last retrieved capabilities.
+   * 
+   * @return The last retrieved capabilities.
+   */
+  public WMSCapabilities getCapabilities( )
+  {
+    return m_capabilities;
+  }
+}
