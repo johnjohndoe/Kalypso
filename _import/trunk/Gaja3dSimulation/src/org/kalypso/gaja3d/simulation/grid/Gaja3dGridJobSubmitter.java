50eaff5772c1d5f9b442a4646afcaa7225b15ba5
skurzbach <skurzbach@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-10-21 13:35:10 +0000

    initial commit

--- _import/trunk/Gaja3dSimulation/src/org/kalypso/gaja3d/simulation/grid/Gaja3dGridJobSubmitter.java
+++ _import/trunk/Gaja3dSimulation/src/org/kalypso/gaja3d/simulation/grid/Gaja3dGridJobSubmitter.java
@@ -31,9 +31,9 @@ public class Gaja3dGridJobSubmitter extends SimulationGridJobSubmitter {
 	 * (zip) and script (sh).
 	 */
 	private static final URL EXEC_ZIP_URL = Gaja3dGridJobSubmitter.class
-			.getResource("resources/Gaja3dService.zip");
+			.getResource("Gaja3dService.zip");
 	private static final URL EXEC_SCRIPT_URL = Gaja3dGridJobSubmitter.class
-			.getResource("resources/Gaja3dService.sh");
+			.getResource("Gaja3dService.sh");
 
 	/**
 	 * The working directory of Gaja3d
