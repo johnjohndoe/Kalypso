5375d82e92e564152bc2006a0672b1dd0b3f453a
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-10-22 11:42:02 +0000

    ASSIGNED - # 34: Bereitstellen von Projektvorlagen im Modelldatendienst https://dev.bjoernsen.de/planerclient/ticket/34

---
+++ KalypsoProjectDatabaseClient/src/org/kalypso/project/database/client/core/project/ProjectWrapper.java
@@ -0,0 +1,47 @@
+package org.kalypso.project.database.client.core.project;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.Assert;
+import org.kalypso.project.database.common.interfaces.IKalypsoProject;
+import org.kalypso.project.database.sei.beans.KalypsoProjectBean;
+
+public class ProjectWrapper
+{
+
+  private KalypsoProjectBean m_bean = null;
+
+  private IProject m_local = null;
+
+  public ProjectWrapper( final KalypsoProjectBean bean )
+  {
+    m_bean = bean;
+  }
+
+  public ProjectWrapper( final IProject local )
+  {
+    m_local = local;
+  }
+
+  public boolean isLocal( )
+  {
+    if( m_local != null && m_bean == null )
+      return true;
+
+    return false;
+  }
+
+  public boolean isRemote( )
+  {
+    if( m_local == null && m_bean != null )
+      return true;
+
+    return false;
+  }
+
+  public IKalypsoProject getBean( )
+  {
+    Assert.isNotNull( m_bean );
+
+    return m_bean;
+  }
+}
