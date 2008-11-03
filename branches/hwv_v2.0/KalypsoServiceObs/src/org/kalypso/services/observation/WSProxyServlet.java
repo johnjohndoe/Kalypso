90c2150deb607f2bbcedc19b6c464b69092f33d1
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-11-03 13:54:38 +0000

    Nur ein Kommentar

--- branches/hwv_v2.0/KalypsoServiceObs/src/org/kalypso/services/observation/WSProxyServlet.java
+++ branches/hwv_v2.0/KalypsoServiceObs/src/org/kalypso/services/observation/WSProxyServlet.java
@@ -32,7 +32,7 @@ public class WSProxyServlet extends WSServlet
     /* Debug. */
     System.out.println( "[NNNN] WS_Proxy_Servlet: Setting JAX-WS Runtime..." );
 
-    /* We just simulate the initialization of the servlet. */
+    /* We just simulate the initialisation of the servlet. */
     m_servletContextListener.contextInitialized( new ServletContextEvent( context ) );
 
     super.init( servletConfig );
