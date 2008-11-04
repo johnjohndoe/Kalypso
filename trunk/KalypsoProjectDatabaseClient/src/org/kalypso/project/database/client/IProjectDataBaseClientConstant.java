6dd6c47ca0e9c7a7972f4c641e4bcbc66bc7b340
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-11-04 10:31:29 +0000

    CLOSED - # 41: Data model for handling remote and local ModelDataService Projects https://dev.bjoernsen.de/planerclient/ticket/41 NEW - # 33: Refactoring Welcome Page https://dev.bjoernsen.de/planerclient/ticket/33

--- trunk/KalypsoProjectDatabaseClient/src/org/kalypso/project/database/client/IProjectDataBaseClientConstant.java
+++ trunk/KalypsoProjectDatabaseClient/src/org/kalypso/project/database/client/IProjectDataBaseClientConstant.java
@@ -5,4 +5,6 @@ public interface IProjectDataBaseClientConstant
   public static String CLIENT_READABLE_PATH = "org.kalypso.project.database.client.readable.path";
 
   public static String CLIENT_WRITEABLE_PATH = "org.kalypso.project.database.client.writeable.path";
+
+  public static String SERVER_WSDL_LOCATION = "org.kalypso.project.database.server.wsdl";
 }
