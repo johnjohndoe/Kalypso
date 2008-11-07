3d432f3e2fafc7d981ee7fae42e0cad0fcb6515b
thomas.jung <thomas.jung@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-11-07 12:24:05 +0000

    ShapeApi: polylineZ shapes are now always getting converted into GM_MultiCurves.

--- KalypsoDeegree/deegree/org/kalypsodeegree_impl/io/shpapi/ShapeFile.java
+++ KalypsoDeegree/deegree/org/kalypsodeegree_impl/io/shpapi/ShapeFile.java
@@ -15,16 +15,16 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * history:
- * 
+ *
  * Files in this package are originally taken from deegree and modified here
  * to fit in kalypso. As goals of kalypso differ from that one in deegree
- * interface-compatibility to deegree is wanted but not retained always. 
- * 
- * If you intend to use this software in other ways than in kalypso 
+ * interface-compatibility to deegree is wanted but not retained always.
+ *
+ * If you intend to use this software in other ways than in kalypso
  * (e.g. OGC-web services), you should consider the latest version of deegree,
  * see http://www.deegree.org .
  *
- * all modifications are licensed as deegree, 
+ * all modifications are licensed as deegree,
  * original copyright:
  *
  * Copyright (C) 2001 by:
@@ -393,19 +393,13 @@ public class ShapeFile
     {
       final SHPPolyLinez shppolyline = (SHPPolyLinez) shp.getByRecNo( RecNo );
 
-      // TODO!
       final GM_Curve[] curves = shpwks.transformPolyLinez( null, shppolyline );
 
-      if( (curves != null) && (curves.length > 1) )
+      if( curves != null )
       {
         // create multi curve
         final GM_MultiCurve mc = GeometryFactory.createGM_MultiCurve( curves );
         geom = mc;
-      }
-      else if( (curves != null) && (curves.length == 1) )
-      {
-        // single curve
-        geom = curves[0];
       }
       else
       {
