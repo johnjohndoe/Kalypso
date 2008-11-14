7462f82b8ef15ace04b0b162dcd8c8fa1852de29
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-11-14 16:16:55 +0000

    First basic refactoring of Kalypso Welcome Page [still work in progress]

---
+++ temp/trunk/KalypsoUIRRM/src/org/kalypso/ui/rrm/extension/KalypsoModuleRRM.java
@@ -0,0 +1,193 @@
+package org.kalypso.ui.rrm.extension;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import kalypsoUIRRM.KalypsoUIRRMPlugin;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.swt.graphics.Image;
+import org.kalypso.commons.java.util.zip.ZipUtilities;
+import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
+import org.kalypso.kalypsosimulationmodel.extension.IKalypsoModule;
+import org.kalypso.kalypsosimulationmodel.extension.IKalypsoModuleEnteringPageHandler;
+import org.kalypso.kalypsosimulationmodel.extension.IKalypsoModuleWelcomePageHandler;
+import org.kalypso.kalypsosimulationmodel.extension.ILocalProjectTemplateDescription;
+import org.kalypso.kalypsosimulationmodel.extension.IRemoteProjectTemplateDescription;
+import org.kalypso.project.database.common.interfaces.IProjectDatabaseFilter;
+import org.kalypso.project.database.common.model.ProjectHandler;
+
+public class KalypsoModuleRRM implements IKalypsoModule
+{
+  protected static final Image IMG_MODULE = new Image( null, KalypsoModuleRRM.class.getResourceAsStream( "icons/button_na.gif" ) );
+
+  protected static boolean INFO_PAGE_EXTRACTED = false;
+
+  public KalypsoModuleRRM( )
+  {
+  }
+
+  @Override
+  public IKalypsoModuleWelcomePageHandler getWelcomePageHandler( )
+  {
+    return new IKalypsoModuleWelcomePageHandler()
+    {
+      @Override
+      public Image getIcon( )
+      {
+        return IMG_MODULE;
+      }
+
+      @Override
+      public String getLabel( )
+      {
+        return "Kalypso Hydrology";
+      }
+
+      @Override
+      public String getTooltip( )
+      {
+        return "Kalypso Hydrology öffnen";
+      }
+    };
+  }
+
+  /**
+   * @see org.kalypso.kalypsosimulationmodel.extension.IKalypsoModule#getModuleEnteringPage()
+   */
+  @Override
+  public IKalypsoModuleEnteringPageHandler getModuleEnteringPage( )
+  {
+    return new IKalypsoModuleEnteringPageHandler()
+    {
+      @Override
+      public IProjectDatabaseFilter getDatabaseFilter( )
+      {
+        return new IProjectDatabaseFilter()
+        {
+          @Override
+          public boolean select( final ProjectHandler handler )
+          {
+            return false;
+          }
+        };
+      }
+
+      @Override
+      public String getHeader( )
+      {
+        return "Kalypso Hydrology";
+      }
+
+      @Override
+      public URL getInfoURL( ) throws MalformedURLException
+      {
+        if( !INFO_PAGE_EXTRACTED )
+        {
+          try
+          {
+            /* info page of plugin */
+            final InputStream zipStream = getClass().getResourceAsStream( "infoPage.zip" );
+            try
+            {
+              final IPath stateLocation = KalypsoUIRRMPlugin.getDefault().getStateLocation();
+              final File targetDir = new File( stateLocation.toFile(), "infoPage" );
+              // final boolean mkdir = dir.mkdir();
+
+              ZipUtilities.unzip( zipStream, targetDir );
+
+              INFO_PAGE_EXTRACTED = true;
+            }
+            finally
+            {
+              zipStream.close();
+            }
+          }
+          catch( final Exception e )
+          {
+            KalypsoUIRRMPlugin.getDefault().getLog().log( StatusUtilities.statusFromThrowable( e ) );
+          }
+
+        }
+
+        final IPath stateLocation = KalypsoUIRRMPlugin.getDefault().getStateLocation();
+        final URL baseUrl = stateLocation.toFile().toURI().toURL();
+
+        final URL url = new URL( baseUrl, "infoPage/index.html" );
+        return url;
+      }
+
+      @Override
+      public IRemoteProjectTemplateDescription getRemoteProjectTemplateDescription( )
+      {
+        return new IRemoteProjectTemplateDescription()
+        {
+          @Override
+          public String getCommitType( )
+          {
+            return "KalypsRrmModel";
+          }
+
+          @Override
+          public String[] getNaturesToBeAdded( )
+          {
+            return new String[] {};
+          }
+
+          @Override
+          public String[] getRemoteTemlateIds( )
+          {
+            return new String[] {};
+          }
+        };
+      }
+
+      @Override
+      public ILocalProjectTemplateDescription getLocalProjectTemplateDescription( )
+      {
+        return new ILocalProjectTemplateDescription()
+        {
+          @Override
+          public String getCategoryId( )
+          {
+            return null;
+          }
+
+          @Override
+          public String getDemoCategoryId( )
+          {
+            return null;
+          }
+        };
+      }
+
+      @Override
+      public IRemoteProjectTemplateDescription getRemoteDemoTemplateDescription( )
+      {
+        return new IRemoteProjectTemplateDescription()
+        {
+          @Override
+          public String getCommitType( )
+          {
+            return "KalypsRrmModel";
+          }
+
+          @Override
+          public String[] getNaturesToBeAdded( )
+          {
+            return new String[] {};
+          }
+
+          @Override
+          public String[] getRemoteTemlateIds( )
+          {
+            return new String[] {};
+          }
+        };
+      }
+    };
+  }
+
+}
