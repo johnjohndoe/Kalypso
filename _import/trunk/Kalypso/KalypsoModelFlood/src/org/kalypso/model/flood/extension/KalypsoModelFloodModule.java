bbf02c97a976a13ee234a840a9d69f893a3b7e2d
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-11-14 16:16:47 +0000

    First basic refactoring of Kalypso Welcome Page [still work in progress]

---
+++ _import/trunk/Kalypso/KalypsoModelFlood/src/org/kalypso/model/flood/extension/KalypsoModelFloodModule.java
@@ -0,0 +1,217 @@
+package org.kalypso.model.flood.extension;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.swt.graphics.Image;
+import org.kalypso.commons.java.util.zip.ZipUtilities;
+import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
+import org.kalypso.kalypsosimulationmodel.extension.IKalypsoModule;
+import org.kalypso.kalypsosimulationmodel.extension.IKalypsoModuleEnteringPageHandler;
+import org.kalypso.kalypsosimulationmodel.extension.IKalypsoModuleWelcomePageHandler;
+import org.kalypso.kalypsosimulationmodel.extension.ILocalProjectTemplateDescription;
+import org.kalypso.kalypsosimulationmodel.extension.IRemoteProjectTemplateDescription;
+import org.kalypso.model.flood.KalypsoModelFloodPlugin;
+import org.kalypso.project.database.common.interfaces.IProjectDatabaseFilter;
+import org.kalypso.project.database.common.model.ProjectHandler;
+
+import de.renew.workflow.base.Workflow;
+import de.renew.workflow.connector.WorkflowProjectNature;
+
+public class KalypsoModelFloodModule implements IKalypsoModule
+{
+  protected static final Image IMG_MODEL_FLOOD_MODULE = new Image( null, KalypsoModelFloodModule.class.getResourceAsStream( "icons/button_flood.gif" ) );
+
+  protected static boolean INFO_PAGE_EXTRACTED = false;
+
+  public KalypsoModelFloodModule( )
+  {
+  }
+
+  @Override
+  public IKalypsoModuleWelcomePageHandler getWelcomePageHandler( )
+  {
+    return new IKalypsoModuleWelcomePageHandler()
+    {
+
+      @Override
+      public Image getIcon( )
+      {
+        return IMG_MODEL_FLOOD_MODULE;
+      }
+
+      @Override
+      public String getLabel( )
+      {
+        return "Kalypso Flood";
+      }
+
+      @Override
+      public String getTooltip( )
+      {
+        return "Kalypso Flood öffnen";
+      }
+    };
+  }
+
+  /**
+   * @see org.kalypso.kalypsosimulationmodel.extension.IKalypsoModule#getModuleEnteringPage()
+   */
+  @Override
+  public IKalypsoModuleEnteringPageHandler getModuleEnteringPage( )
+  {
+    return new IKalypsoModuleEnteringPageHandler()
+    {
+
+      @Override
+      public IProjectDatabaseFilter getDatabaseFilter( )
+      {
+        return new IProjectDatabaseFilter()
+        {
+          @Override
+          public boolean select( final ProjectHandler handler )
+          {
+            if( handler.isLocal() )
+            {
+              try
+              {
+                final WorkflowProjectNature nature = WorkflowProjectNature.toThisNature( handler.getProject() );
+                if( nature == null )
+                  return false;
+
+                final Workflow workflow = nature.getCurrentWorklist();
+                final String uri = workflow.getURI();
+
+                return uri.contains( "org.kalypso.model.flood.WF_KalypsoFlood" );
+              }
+              catch( final CoreException e )
+              {
+                KalypsoModelFloodPlugin.getDefault().getLog().log( StatusUtilities.statusFromThrowable( e ) );
+              }
+            }
+
+            return false;
+          }
+        };
+      }
+
+      @Override
+      public String getHeader( )
+      {
+        return "Kalypso Flood";
+      }
+
+      @Override
+      public URL getInfoURL( ) throws MalformedURLException
+      {
+        if( !INFO_PAGE_EXTRACTED )
+        {
+          try
+          {
+            /* info page of plugin */
+            final InputStream zipStream = getClass().getResourceAsStream( "infoPage.zip" );
+            try
+            {
+              final IPath stateLocation = KalypsoModelFloodPlugin.getDefault().getStateLocation();
+              final File targetDir = new File( stateLocation.toFile(), "infoPage" );
+              // final boolean mkdir = dir.mkdir();
+
+              ZipUtilities.unzip( zipStream, targetDir );
+
+              INFO_PAGE_EXTRACTED = true;
+            }
+            finally
+            {
+              zipStream.close();
+            }
+          }
+          catch( final Exception e )
+          {
+            KalypsoModelFloodPlugin.getDefault().getLog().log( StatusUtilities.statusFromThrowable( e ) );
+          }
+
+        }
+
+        final IPath stateLocation = KalypsoModelFloodPlugin.getDefault().getStateLocation();
+        final URL baseUrl = stateLocation.toFile().toURI().toURL();
+
+        final URL url = new URL( baseUrl, "infoPage/index.html" );
+        return url;
+      }
+
+      @Override
+      public IRemoteProjectTemplateDescription getRemoteProjectTemplateDescription( )
+      {
+        return new IRemoteProjectTemplateDescription()
+        {
+          @Override
+          public String getCommitType( )
+          {
+            return "KalypsoFloodModelType";
+          }
+
+          @Override
+          public String[] getNaturesToBeAdded( )
+          {
+            return new String[] {};
+          }
+
+          @Override
+          public String[] getRemoteTemlateIds( )
+          {
+            return new String[] {};
+          }
+        };
+      }
+
+      @Override
+      public ILocalProjectTemplateDescription getLocalProjectTemplateDescription( )
+      {
+        return new ILocalProjectTemplateDescription()
+        {
+          @Override
+          public String getCategoryId( )
+          {
+            return "org.kalypso.model.flood.projectTemplate";
+          }
+
+          @Override
+          public String getDemoCategoryId( )
+          {
+            return "org.kalypso.model.flood.demoProject";
+          }
+        };
+      }
+
+      @Override
+      public IRemoteProjectTemplateDescription getRemoteDemoTemplateDescription( )
+      {
+        return new IRemoteProjectTemplateDescription()
+        {
+          @Override
+          public String getCommitType( )
+          {
+            return "KalypsoFloodModelType";
+          }
+
+          @Override
+          public String[] getNaturesToBeAdded( )
+          {
+            return new String[] {};
+          }
+
+          @Override
+          public String[] getRemoteTemlateIds( )
+          {
+            return new String[] {};
+          }
+        };
+      }
+    };
+  }
+
+}
---
+++ _import/trunk/Kalypso/sh/Kalypso1d2dProject/src/org/kalypso/kalypso1d2d/extension/Kalypso1d2dModule.java
@@ -0,0 +1,211 @@
+package org.kalypso.kalypso1d2d.extension;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.swt.graphics.Image;
+import org.kalypso.commons.java.util.zip.ZipUtilities;
+import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
+import org.kalypso.kalypso1d2d.pjt.Kalypso1D2DProjectNature;
+import org.kalypso.kalypso1d2d.pjt.Kalypso1d2dProjectPlugin;
+import org.kalypso.kalypsosimulationmodel.extension.IKalypsoModule;
+import org.kalypso.kalypsosimulationmodel.extension.IKalypsoModuleEnteringPageHandler;
+import org.kalypso.kalypsosimulationmodel.extension.IKalypsoModuleWelcomePageHandler;
+import org.kalypso.kalypsosimulationmodel.extension.ILocalProjectTemplateDescription;
+import org.kalypso.kalypsosimulationmodel.extension.IRemoteProjectTemplateDescription;
+import org.kalypso.project.database.common.interfaces.IProjectDatabaseFilter;
+import org.kalypso.project.database.common.model.ProjectHandler;
+
+public class Kalypso1d2dModule implements IKalypsoModule
+{
+  protected static final Image IMG_1D_2D_MODULE = new Image( null, Kalypso1d2dModule.class.getResourceAsStream( "icons/button_1d2d.gif" ) );
+
+  protected static boolean INFO_PAGE_EXTRACTED = false;
+
+  public Kalypso1d2dModule( )
+  {
+  }
+
+  @Override
+  public IKalypsoModuleWelcomePageHandler getWelcomePageHandler( )
+  {
+    return new IKalypsoModuleWelcomePageHandler()
+    {
+
+      @Override
+      public Image getIcon( )
+      {
+        return IMG_1D_2D_MODULE;
+      }
+
+      @Override
+      public String getLabel( )
+      {
+        return "Kalypso 1D/2D";
+      }
+
+      @Override
+      public String getTooltip( )
+      {
+        return "Kalypso 1D/2D öffnen";
+      }
+    };
+  }
+
+  /**
+   * @see org.kalypso.kalypsosimulationmodel.extension.IKalypsoModule#getModuleEnteringPage()
+   */
+  @Override
+  public IKalypsoModuleEnteringPageHandler getModuleEnteringPage( )
+  {
+    return new IKalypsoModuleEnteringPageHandler()
+    {
+      @Override
+      public IProjectDatabaseFilter getDatabaseFilter( )
+      {
+        return new IProjectDatabaseFilter()
+        {
+          @Override
+          public boolean select( final ProjectHandler handler )
+          {
+            if( handler.isLocal() )
+            {
+              try
+              {
+                final IProject project = handler.getProject();
+                return Kalypso1D2DProjectNature.isOfThisNature( project );
+              }
+              catch( final CoreException e )
+              {
+                Kalypso1d2dProjectPlugin.getDefault().getLog().log( StatusUtilities.statusFromThrowable( e ) );
+              }
+            }
+
+            // TODO handle remote projects
+
+            return false;
+          }
+        };
+      }
+
+      @Override
+      public String getHeader( )
+      {
+        return "Kalypso 1D/2D";
+      }
+
+      @Override
+      public URL getInfoURL( ) throws MalformedURLException
+      {
+        if( !INFO_PAGE_EXTRACTED )
+        {
+          try
+          {
+            /* info page of plugin */
+            final InputStream zipStream = getClass().getResourceAsStream( "infoPage.zip" );
+            try
+            {
+              final IPath stateLocation = Kalypso1d2dProjectPlugin.getDefault().getStateLocation();
+              final File targetDir = new File( stateLocation.toFile(), "infoPage" );
+              // final boolean mkdir = dir.mkdir();
+
+              ZipUtilities.unzip( zipStream, targetDir );
+
+              INFO_PAGE_EXTRACTED = true;
+            }
+            finally
+            {
+              zipStream.close();
+            }
+          }
+          catch( final Exception e )
+          {
+            Kalypso1d2dProjectPlugin.getDefault().getLog().log( StatusUtilities.statusFromThrowable( e ) );
+          }
+
+        }
+
+        final IPath stateLocation = Kalypso1d2dProjectPlugin.getDefault().getStateLocation();
+        final URL baseUrl = stateLocation.toFile().toURI().toURL();
+
+        final URL url = new URL( baseUrl, "infoPage/index.html" );
+        return url;
+      }
+
+      @Override
+      public IRemoteProjectTemplateDescription getRemoteProjectTemplateDescription( )
+      {
+        return new IRemoteProjectTemplateDescription()
+        {
+
+          @Override
+          public String getCommitType( )
+          {
+            return "Kalypso1d2dModel";
+          }
+
+          @Override
+          public String[] getNaturesToBeAdded( )
+          {
+            return new String[] {};
+          }
+
+          @Override
+          public String[] getRemoteTemlateIds( )
+          {
+            return new String[] {};
+          }
+        };
+      }
+
+      @Override
+      public ILocalProjectTemplateDescription getLocalProjectTemplateDescription( )
+      {
+        return new ILocalProjectTemplateDescription()
+        {
+          @Override
+          public String getCategoryId( )
+          {
+            return "org.kalypso.kalypso1d2d.pjt.projectTemplate";
+          }
+
+          @Override
+          public String getDemoCategoryId( )
+          {
+            return "org.kalypso.kalypso1d2d.pjt.demoProject";
+          }
+        };
+      }
+
+      @Override
+      public IRemoteProjectTemplateDescription getRemoteDemoTemplateDescription( )
+      {
+        return new IRemoteProjectTemplateDescription()
+        {
+          @Override
+          public String getCommitType( )
+          {
+            return "KalypsoFloodModelType";
+          }
+
+          @Override
+          public String[] getNaturesToBeAdded( )
+          {
+            return new String[] {};
+          }
+
+          @Override
+          public String[] getRemoteTemlateIds( )
+          {
+            return new String[] {};
+          }
+        };
+      }
+    };
+  }
+}
