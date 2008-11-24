02758a72c85f1c54faca4db622c6275d6f582bb0
nico <nico@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-11-24 16:03:04 +0000

    stupid small change to prevent data mess later on

--- _import/branches/branch_kalypsoProduct_v2_0/Kalypso/sh/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/conv/Control1D2DConverter.java
+++ _import/branches/branch_kalypsoProduct_v2_0/Kalypso/sh/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/conv/Control1D2DConverter.java
@@ -232,7 +232,7 @@ public class Control1D2DConverter
     formatter.format( "C4%14.1f%8.1f%8.1f%40d%n", 0.0, 20.0, 0.0, artImpulsstromBeiwert ? 1 : 0 ); //$NON-NLS-1$
 
     // C5
-    formatter.format( "C5%14d%8d%16d%8d%8d%8d%8d%8d%8d%n", m_controlModel.getNITI(), m_controlModel.getNITN(), m_controlModel.getNCYC(), 0, 1, 1, 0, 1, 1 ); //$NON-NLS-1$
+    formatter.format( "C5%14d%8d%16d%8d%8d%8d%8d%8d%8d%n", m_controlModel.getNITI(), m_controlModel.getNITN(), m_controlModel.getNCYC(), 0, 0, 1, 0, 1, 1 ); //$NON-NLS-1$
 
     // C6
     if( m_controlModel.getIcpu() != 0 )
