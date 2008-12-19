5e1b96debb586e51cd3177ff394d59de772cdf3c
werner.kim <werner.kim@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2008-12-19 12:50:25 +0000

    internationalisierung der Kalypsomodule

---
+++ _dump/trunk/KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/i18n/createCSV.rb
@@ -0,0 +1,2 @@
+load("../i18nUtilities/src/mkeTable/mkeTable.rb")
+createCSV( "KalypsoModelWspmTuhhUI", "src/org/kalypso/model/wspm/tuhh/ui/i18n")
---
+++ _dump/trunk/KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/i18n/createProperties.rb
@@ -0,0 +1,2 @@
+load("../i18nUtilities/src/mkeTable/mkeTable.rb")
+createProperties( "KalypsoModelWspmTuhhUI", "src/org/kalypso/model/wspm/tuhh/ui/i18n","messages")
