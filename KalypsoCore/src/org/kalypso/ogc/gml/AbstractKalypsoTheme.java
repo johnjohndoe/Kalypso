37eb092b5372fcb49561b25871e331aac2f78a44
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-01-06 07:46:24 +0000

    NEW - # 111: ObjektArten verwalten https://dev.bjoernsen.de/planerclient/ticket/111

--- KalypsoCore/src/org/kalypso/ogc/gml/AbstractKalypsoTheme.java
+++ KalypsoCore/src/org/kalypso/ogc/gml/AbstractKalypsoTheme.java
@@ -85,7 +85,7 @@ import org.kalypsodeegree.model.geometry.GM_Envelope;
  * <p>
  * Implements common features to all KalypsoTheme's
  * </p>
- *
+ * 
  * @author Gernot Belger
  */
 public abstract class AbstractKalypsoTheme extends PlatformObject implements IKalypsoTheme
@@ -142,7 +142,7 @@ public abstract class AbstractKalypsoTheme extends PlatformObject implements IKa
 
   /**
    * The constructor.
-   *
+   * 
    * @param name
    *          The name of the theme.
    * @param type
@@ -224,6 +224,10 @@ public abstract class AbstractKalypsoTheme extends PlatformObject implements IKa
     {
       public void visit( final IKalypsoThemeListener l )
       {
+        // FIXME NPE
+        if( l == null )
+          return;
+
         l.contextChanged( AbstractKalypsoTheme.this );
       }
     } );
@@ -238,6 +242,10 @@ public abstract class AbstractKalypsoTheme extends PlatformObject implements IKa
     {
       public void visit( final IKalypsoThemeListener l )
       {
+        // FIXME NPE
+        if( l == null )
+          return;
+
         l.statusChanged( AbstractKalypsoTheme.this );
       }
     } );
@@ -252,6 +260,10 @@ public abstract class AbstractKalypsoTheme extends PlatformObject implements IKa
     {
       public void visit( final IKalypsoThemeListener l )
       {
+        // FIXME NPE
+        if( l == null )
+          return;
+
         l.visibilityChanged( AbstractKalypsoTheme.this, newVisibility );
       }
     } );
@@ -259,7 +271,7 @@ public abstract class AbstractKalypsoTheme extends PlatformObject implements IKa
 
   /**
    * Fire the given event to my registered listeners.<br>
-   *
+   * 
    * @param invalidExtent
    *          The extent that is no more valid; <code>null</code> indicating that the complete theme should be
    *          repainted.
@@ -270,6 +282,10 @@ public abstract class AbstractKalypsoTheme extends PlatformObject implements IKa
     {
       public void visit( final IKalypsoThemeListener l )
       {
+        // FIXME NPE
+        if( l == null )
+          return;
+
         l.repaintRequested( AbstractKalypsoTheme.this, invalidExtent );
       }
     } );
@@ -285,7 +301,7 @@ public abstract class AbstractKalypsoTheme extends PlatformObject implements IKa
 
   /**
    * Returns the type of the theme by default. Override if needed.
-   *
+   * 
    * @see org.kalypso.ogc.gml.IKalypsoTheme#getContext()
    */
   public String getTypeContext( )
@@ -320,7 +336,7 @@ public abstract class AbstractKalypsoTheme extends PlatformObject implements IKa
   /**
    * This function returns the icon set in the style (StyledLayerType), if any.<br>
    * This may be icons with a relative path or icons, which are defined via some URNs.<br>
-   *
+   * 
    * @return If an user icon or URN is defined, this icon will be returned.<br>
    *         If not, it checks the number of styles and rules.<br>
    *         If only one style and rule exists, there is a generated icon returned, representing the first rule.<br>
@@ -432,7 +448,7 @@ public abstract class AbstractKalypsoTheme extends PlatformObject implements IKa
 
   /**
    * This function returns the resolved URL for the legend icon or null, if none could be created.
-   *
+   * 
    * @return The resolved URL for the legend icon or null, if none could be created.
    */
   private URL getLegendIconURL( )
@@ -477,7 +493,7 @@ public abstract class AbstractKalypsoTheme extends PlatformObject implements IKa
    * <strong>Note:</strong><br>
    * <br>
    * This has only an effect, if the user does not define an URL or URN and the theme has more then one style or rule.
-   *
+   * 
    * @return The default image descriptor.
    */
   protected ImageDescriptor getDefaultIcon( )
@@ -734,7 +750,7 @@ public abstract class AbstractKalypsoTheme extends PlatformObject implements IKa
   /**
    * This function returns the URL or URN defined by the user for an icon, which should be displayed in a legend or an
    * outline.
-   *
+   * 
    * @return The URL or URN string. May be null.
    */
   public String getLegendIcon( )
@@ -744,7 +760,7 @@ public abstract class AbstractKalypsoTheme extends PlatformObject implements IKa
 
   /**
    * This function returns the context.
-   *
+   * 
    * @return The context, if the theme is part of a template loaded from a file. May be null.
    */
   protected URL getContext( )
@@ -755,7 +771,7 @@ public abstract class AbstractKalypsoTheme extends PlatformObject implements IKa
   /**
    * This function returns true, if the theme allows showing its children in an outline. Otherwise, it will return
    * false.
-   *
+   * 
    * @return True,if the theme allows showing its children in an outline. Otherwise, false.
    */
   public boolean shouldShowChildren( )
