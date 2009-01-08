c4fa54eff9d2f2fa9f069ae807bd0f89143bbe78
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-01-08 09:05:26 +0000

    Planer Client - dev.bjoernsen.de - deployment changes

--- trunk/KalypsoProjectDatabase/src/org/kalypso/project/database/server/ProjectDatabase.java
+++ trunk/KalypsoProjectDatabase/src/org/kalypso/project/database/server/ProjectDatabase.java
@@ -5,7 +5,7 @@
  *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
- *  Denickestraße 22
+ *  Denickestraï¿½e 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
  *
@@ -79,6 +79,7 @@ public class ProjectDatabase implements IProjectDatabase
 
   public ProjectDatabase( )
   {
+    //FIXME configuration file -> system property!!!
 // final URL url = this.getClass().getResource( "conf/hibernate.cfg.xml" );
     final AnnotationConfiguration configure = new AnnotationConfiguration().configure();
 
