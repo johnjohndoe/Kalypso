35c7126f579e7aff3a2dc4dd2f3bd11ed14f6f64
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-02-11 09:49:44 +0000

    Kalypso 2.1.0 RC4

---
+++ tags/kalypso_2_1_0_20090209/KalypsoCore/src/org/kalypso/ogc/gml/expressions/QNamePropertyTester.java
@@ -0,0 +1,26 @@
+package org.kalypso.ogc.gml.expressions;
+
+import javax.xml.namespace.QName;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.runtime.Assert;
+import org.kalypso.gmlschema.GMLSchemaUtilities;
+import org.kalypsodeegree.model.feature.Feature;
+
+public class QNamePropertyTester extends PropertyTester
+{
+  public boolean test( final Object receiver, final String property, final Object[] args, final Object expectedValue )
+  {
+    if( "qname".equals( property ) ) //$NON-NLS-1$
+    {
+      final Feature feature = (Feature) receiver;
+      final String excpectedStr = expectedValue.toString().replaceAll( "\"", "" ); // strip " //$NON-NLS-1$ //$NON-NLS-2$
+      final QName expectedQName = QName.valueOf( excpectedStr );
+      return GMLSchemaUtilities.substitutes( feature.getFeatureType(), expectedQName );
+    }
+
+    Assert.isTrue( false );
+    return false;
+  }
+
+}
