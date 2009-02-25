88f2c0bb97c27d74dce323c4318ef841ff8f66ae
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-02-25 18:32:32 +0000

    @dirk: please fix, see Fixme's in comment. Fixes 'resource tree locked messages' in console

--- trunk/KalypsoProjectDatabaseClient/src/org/kalypso/project/database/client/core/model/local/LocalProjectHandler.java
+++ trunk/KalypsoProjectDatabaseClient/src/org/kalypso/project/database/client/core/model/local/LocalProjectHandler.java
@@ -40,7 +40,6 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.project.database.client.core.model.local;
 
-import org.apache.commons.lang.ArrayUtils;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IProjectNature;
@@ -49,7 +48,6 @@ import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences.IPreferenceChangeListener;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences.PreferenceChangeEvent;
 import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
@@ -77,9 +75,10 @@ public class LocalProjectHandler extends AbstractProjectHandler implements ILoca
     m_project = project;
     m_model = localWorkspaceModel;
 
+    // FIXME: too have, one listener per project... too much business if we have lots of projects...
+    // Every project in the workspace gets this listener....!
     m_changeListener = new IResourceChangeListener()
     {
-
       @Override
       public void resourceChanged( final IResourceChangeEvent event )
       {
@@ -110,7 +109,11 @@ public class LocalProjectHandler extends AbstractProjectHandler implements ILoca
             {
               try
               {
-                getRemotePreferences().setModified( true );
+                // FIXME: with this call, every(!) project automatically gets the remote-nature...
+                // this is not allowed!
+                final IRemoteProjectPreferences remotePreferences = getRemotePreferences();
+                if( remotePreferences != null )
+                  remotePreferences.setModified( true );
                 break;
               }
               catch( final IllegalStateException e )
@@ -162,15 +165,19 @@ public class LocalProjectHandler extends AbstractProjectHandler implements ILoca
   {
     if( m_preferences == null )
     {
+      // FIXEM: sue m_project.isNatureEnabled( natureId );
       final IProjectNature nature = m_project.getNature( RemoteProjectNature.NATURE_ID );
       if( nature == null )
       {
-        final IProjectDescription description = m_project.getDescription();
-        final String[] natureIds = description.getNatureIds();
-        ArrayUtils.add( natureIds, RemoteProjectNature.NATURE_ID );
-
-        description.setNatureIds( natureIds );
-        m_project.setDescription( description, new NullProgressMonitor() );
+        // FIXME: does not work! setDescription always throws an exception, m_preferences stays forver null ->
+        // consolole output 'The resource tree is locked for modifications.'
+        //
+// final IProjectDescription description = m_project.getDescription();
+// final String[] natureIds = description.getNatureIds();
+// ArrayUtils.add( natureIds, RemoteProjectNature.NATURE_ID );
+//
+// description.setNatureIds( natureIds );
+// m_project.setDescription( description, new NullProgressMonitor() );
       }
 
       final RemoteProjectNature myNature = (RemoteProjectNature) nature;
