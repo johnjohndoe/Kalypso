ac19025f153a61d906a565ff5ea4b8e5fd534945
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-02-25 09:40:33 +0000

    Fixed possible NPE in TimeLogger

--- KalypsoCommons/src/org/kalypso/commons/performance/TimeLogger.java
+++ KalypsoCommons/src/org/kalypso/commons/performance/TimeLogger.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.commons.performance;
 
@@ -56,15 +56,15 @@ import javax.xml.datatype.Duration;
  * <p>
  * Call #showElapsedTime( String ) to print a message, indicating how long it took since construction
  * </p>.
- * 
+ *
  * @author Belger
  */
 public class TimeLogger
 {
   private final static DatatypeConstants.Field[] FIELDS_HOURS = { DatatypeConstants.HOURS, DatatypeConstants.MINUTES, DatatypeConstants.SECONDS };
-  
+
   private DatatypeFactory m_factory = null;
-  
+
   private final long m_constructionTime;
 
   private long m_lastCall;
@@ -84,7 +84,7 @@ public class TimeLogger
   public TimeLogger( final String message )
   {
     System.out.println( message );
-    
+
     m_constructionTime = System.currentTimeMillis();
     try
     {
@@ -95,7 +95,8 @@ public class TimeLogger
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
-    
+
+    m_currentInterimDuration = m_factory.newDuration( 0 );
     m_lastCall = m_constructionTime;
   }
 
@@ -108,7 +109,7 @@ public class TimeLogger
 
     m_currentInterimDuration = m_factory.newDuration( sinceLastCall );
     m_currentTotalDuration = m_factory.newDuration( total );
-    
+
     m_lastCall = current;
   }
 
@@ -121,7 +122,7 @@ public class TimeLogger
   {
     printDuration( message, m_currentTotalDuration, FIELDS_HOURS);
   }
-  
+
   /**
    *  @deprecated Use {@link #takeInterimTime()} and {@link #printCurrentInterim(String)} instead.
    */
@@ -158,5 +159,5 @@ public class TimeLogger
     }
     System.out.println();
   }
-  
+
 }
