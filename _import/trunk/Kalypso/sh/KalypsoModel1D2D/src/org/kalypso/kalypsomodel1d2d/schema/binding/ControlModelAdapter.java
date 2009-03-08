7ae087c30f89edcef7453919fc341dcccef35f50
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-03-08 13:08:58 +0000

    Correct progress for shape and gml loading.

--- _import/trunk/Kalypso/sh/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/schema/binding/ControlModelAdapter.java
+++ _import/trunk/Kalypso/sh/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/schema/binding/ControlModelAdapter.java
@@ -7,9 +7,7 @@ import javax.xml.namespace.QName;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
 import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
 import org.kalypso.core.util.pool.IModelAdaptor;
 import org.kalypso.gmlschema.property.IPropertyType;
@@ -21,18 +19,15 @@ import org.kalypso.kalypsosimulationmodel.schema.KalypsoModelSimulationBaseConst
 import org.kalypso.observation.IObservation;
 import org.kalypso.observation.result.IComponent;
 import org.kalypso.observation.result.TupleResult;
-import org.kalypso.ogc.gml.command.ChangeFeaturesCommand;
-import org.kalypso.ogc.gml.command.CompositeCommand;
-import org.kalypso.ogc.gml.command.FeatureChange;
-import org.kalypso.ogc.gml.mapmodel.CommandableWorkspace;
 import org.kalypso.ogc.gml.om.ObservationFeatureFactory;
 import org.kalypsodeegree.model.feature.Feature;
 import org.kalypsodeegree.model.feature.FeatureList;
+import org.kalypsodeegree.model.feature.GMLWorkspace;
 import org.kalypsodeegree_impl.model.feature.FeatureHelper;
 
 /**
  * Adapter from original 1d2d control model without ordinal number column and wrong sorting to version 1.0.
- * 
+ *
  */
 public class ControlModelAdapter implements IModelAdaptor
 {
@@ -40,7 +35,7 @@ public class ControlModelAdapter implements IModelAdaptor
 
   private IStatus m_result = Status.OK_STATUS;
 
-  public CommandableWorkspace adapt( final CommandableWorkspace workspace )
+  public GMLWorkspace adapt( final GMLWorkspace workspace, final IProgressMonitor monitor )
   {
     final Object property = workspace.getRootFeature().getProperty( KalypsoModelSimulationBaseConsts.SIM_BASE_PROP_VERSION );
     if( property != null && property.equals( VERSION_1_0 ) )
@@ -49,103 +44,69 @@ public class ControlModelAdapter implements IModelAdaptor
       return workspace;
     }
 
-    final String name = "Control model collection adapting";
-    final Job job = new Job( name )
-    {
+    m_result = execute( workspace, monitor );
 
-      @Override
-      protected IStatus run( IProgressMonitor monitor )
-      {
-        if( monitor == null )
-        {
-          monitor = new NullProgressMonitor();
-        }
-        final List<IStatus> statusList = new ArrayList<IStatus>();
-
-        final Feature controlModelGroup = workspace.getRootFeature();
-
-        final QName qnameProp1 = new QName( UrlCatalog1D2D.MODEL_1D2DControl_NS, "controlModelCollection" );
-        final QName qnameProp2 = new QName( UrlCatalog1D2D.MODEL_1D2DControl_NS, "controlModelMember" );
-        final IRelationType controlModelCollectionPropertyType = (IRelationType) controlModelGroup.getFeatureType().getProperty( qnameProp1 );
-        final Feature controlModelCollection = (Feature) controlModelGroup.getProperty( controlModelCollectionPropertyType );
-        if( controlModelCollection == null )
-        {
-          // happens if no control model is defined yet
-          return StatusUtilities.createOkStatus( "Empty control model collection, nothnig to adapt" );
-        }
-
-        final IRelationType controlModelPropertyType = (IRelationType) controlModelCollection.getFeatureType().getProperty( qnameProp2 );
-        final FeatureList controlModels = (FeatureList) controlModelCollection.getProperty( controlModelPropertyType );
-
-        try
-        {
-          final CompositeCommand cc = new CompositeCommand( name );
-
-          final int numberOfChanges = controlModels.size();
-          final List<FeatureChange> featureChanges = new ArrayList<FeatureChange>( numberOfChanges );
-          final int amountOfWork = numberOfChanges * 10;
-          monitor.beginTask( "Control model collection adapting", amountOfWork );
-
-          for( final Object modelObject : controlModels )
-          {
-            final Feature feature = FeatureHelper.getFeature( workspace, modelObject );
-            final Feature obsFeature = (Feature) feature.getProperty( ControlModel1D2D.WB1D2DCONTROL_PROP_TIMESTEPS_MEMBER );
-            final IObservation<TupleResult> obs = ObservationFeatureFactory.toObservation( obsFeature );
-            final TupleResult result = obs.getResult();
-            if( result.getOrdinalNumberComponent() == null )
-            {
-              final IComponent componentTime = ObservationFeatureFactory.createDictionaryComponent( obsFeature, Kalypso1D2DDictConstants.DICT_COMPONENT_TIME );
-              final IComponent componentOrdnialNumber = ObservationFeatureFactory.createDictionaryComponent( obsFeature, Kalypso1D2DDictConstants.DICT_COMPONENT_ORDINAL_NUMBER );
-              result.setSortComponents( new IComponent[] { componentTime } );
-              result.setOrdinalNumberComponent( componentOrdnialNumber );
-              ObservationFeatureFactory.toFeature( obs, obsFeature );
-            }
-            monitor.worked( 10 );
-          }
-
-          // set version
-          final IPropertyType versionProperty = controlModelGroup.getFeatureType().getProperty( KalypsoModelSimulationBaseConsts.SIM_BASE_PROP_VERSION );
-          featureChanges.add( new FeatureChange( controlModelGroup, versionProperty, VERSION_1_0 ) );
-
-          // create command
-          final ChangeFeaturesCommand changeFeaturesCommand = new ChangeFeaturesCommand( workspace, featureChanges.toArray( new FeatureChange[featureChanges.size()] ) );
-          cc.addCommand( changeFeaturesCommand );
-
-          workspace.postCommand( cc );
-        }
-        catch( final Exception e )
-        {
-          statusList.add( StatusUtilities.statusFromThrowable( e ) );
-        }
-        finally
-        {
-          monitor.done();
-        }
-        final IStatus resultStatus;
-        if( statusList.size() > 0 )
-          resultStatus = StatusUtilities.createStatus( statusList, "Problems during the control model adaptation." );
-        else
-          resultStatus = StatusUtilities.createInfoStatus( "Control model is adapted to version 1.0" );
-        return resultStatus;
-      }
-    };
-
-    job.schedule();
-    try
-    {
-      job.join();
-    }
-    catch( final InterruptedException e )
-    {
-      e.printStackTrace();
-    }
-
-    m_result = job.getResult();
     return workspace;
   }
 
   public IStatus getResult( )
   {
     return m_result;
+  }
+
+  public IStatus execute( final GMLWorkspace workspace, final IProgressMonitor monitor )
+  {
+    final List<IStatus> statusList = new ArrayList<IStatus>();
+
+    final Feature controlModelGroup = workspace.getRootFeature();
+
+    final QName qnameProp1 = new QName( UrlCatalog1D2D.MODEL_1D2DControl_NS, "controlModelCollection" );
+    final QName qnameProp2 = new QName( UrlCatalog1D2D.MODEL_1D2DControl_NS, "controlModelMember" );
+    final IRelationType controlModelCollectionPropertyType = (IRelationType) controlModelGroup.getFeatureType().getProperty( qnameProp1 );
+    final Feature controlModelCollection = (Feature) controlModelGroup.getProperty( controlModelCollectionPropertyType );
+
+    // set version
+    final IPropertyType versionProperty = controlModelGroup.getFeatureType().getProperty( KalypsoModelSimulationBaseConsts.SIM_BASE_PROP_VERSION );
+    controlModelCollection.setProperty( versionProperty, VERSION_1_0 );
+
+    final IRelationType controlModelPropertyType = (IRelationType) controlModelCollection.getFeatureType().getProperty( qnameProp2 );
+    final FeatureList controlModels = (FeatureList) controlModelCollection.getProperty( controlModelPropertyType );
+
+    try
+    {
+      final int numberOfChanges = controlModels.size();
+      final int amountOfWork = numberOfChanges * 10;
+      monitor.beginTask( "Adapting control model", amountOfWork );
+
+      for( final Object modelObject : controlModels )
+      {
+        final Feature feature = FeatureHelper.getFeature( workspace, modelObject );
+        final Feature obsFeature = (Feature) feature.getProperty( ControlModel1D2D.WB1D2DCONTROL_PROP_TIMESTEPS_MEMBER );
+        final IObservation<TupleResult> obs = ObservationFeatureFactory.toObservation( obsFeature );
+        final TupleResult result = obs.getResult();
+        if( result.getOrdinalNumberComponent() == null )
+        {
+          final IComponent componentTime = ObservationFeatureFactory.createDictionaryComponent( obsFeature, Kalypso1D2DDictConstants.DICT_COMPONENT_TIME );
+          final IComponent componentOrdnialNumber = ObservationFeatureFactory.createDictionaryComponent( obsFeature, Kalypso1D2DDictConstants.DICT_COMPONENT_ORDINAL_NUMBER );
+          result.setSortComponents( new IComponent[] { componentTime } );
+          result.setOrdinalNumberComponent( componentOrdnialNumber );
+          ObservationFeatureFactory.toFeature( obs, obsFeature );
+        }
+        monitor.worked( 10 );
+      }
+    }
+    catch( final Exception e )
+    {
+      statusList.add( StatusUtilities.statusFromThrowable( e ) );
+    }
+    finally
+    {
+      monitor.done();
+    }
+
+    if( statusList.size() > 0 )
+      return StatusUtilities.createStatus( statusList, "Problems during the control model adaptation." );
+
+    return StatusUtilities.createInfoStatus( "Control model has been adapted to version 1.0" );
   }
 }
