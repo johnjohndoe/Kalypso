befe70f695d2a3968edcd4e2eb0304b833801ec9
nico <nico@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-03-10 16:46:10 +0000

    Tagging Milestone 1 release made on March, 10th 2009

---
+++ KalypsoProjectDatabase/src/org/kalypso/project/database/KalypsoProjectDatabase.java
@@ -0,0 +1,56 @@
+package org.kalypso.project.database;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class KalypsoProjectDatabase extends Plugin
+{
+
+  // The plug-in ID
+  public static final String PLUGIN_ID = "org.kalypso.project.database";
+
+  // The shared instance
+  private static KalypsoProjectDatabase plugin;
+
+  /**
+   * The constructor
+   */
+  public KalypsoProjectDatabase( )
+  {
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+   */
+  @Override
+  public void start( final BundleContext context ) throws Exception
+  {
+    super.start( context );
+    plugin = this;
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+   */
+  @Override
+  public void stop( final BundleContext context ) throws Exception
+  {
+    plugin = null;
+    super.stop( context );
+  }
+
+  /**
+   * Returns the shared instance
+   * 
+   * @return the shared instance
+   */
+  public static KalypsoProjectDatabase getDefault( )
+  {
+    return plugin;
+  }
+}
---
+++ KalypsoProjectDatabase/src/org/kalypso/project/database/common/nature/RemoteProjectPreferencesHandler.java
@@ -0,0 +1,233 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ *
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraße 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ *
+ *  and
+ *
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Contact:
+ *
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.project.database.common.nature;
+
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+
+/**
+ * Remote project settings of {@link org.eclipse.core.resources.IProjectNature} -> {@link RemoteProjectNature}
+ * 
+ * @author Dirk Kuch
+ */
+public class RemoteProjectPreferencesHandler implements IRemoteProjectPreferences
+{
+  private static final String PROJECT_LOCK_TICKET = "project.lock";
+
+  private static final String PROJECT_IS_MODIFIED = "project.is.modified";
+
+  private static final String PROJECT_CHANGES_COMMITED = "project.changes.commited";
+
+  private static final String PROJECT_IS_ON_SERVER = "project.is.on.server";
+
+  private static final String PROJECT_DOWNLOADED_VERSION = "project.downloaded.version";
+
+  private static final String REMOTE_PROJECT_TYPE = "project.remote.type";
+
+  protected final IEclipsePreferences m_node;
+
+  public RemoteProjectPreferencesHandler( final IEclipsePreferences node )
+  {
+    m_node = node;
+  }
+
+  /**
+   * @see org.kalypso.project.database.common.nature.IRemoteProjectPreferences#isLocked()
+   */
+  @Override
+  public boolean isLocked( )
+  {
+    final String ticket = getEditTicket();
+    if( ticket == null || "".equals( ticket.trim() ) )
+    {
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * @see org.kalypso.project.database.common.nature.IRemoteProjectPreferences#setEditTicket(java.lang.String)
+   */
+  @Override
+  public void setEditTicket( final String ticket )
+  {
+    m_node.put( PROJECT_LOCK_TICKET, ticket );
+    flush();
+  }
+
+  /**
+   * @see org.kalypso.project.database.common.nature.IRemoteProjectPreferences#getEditTicket()
+   */
+  @Override
+  public String getEditTicket( )
+  {
+    return m_node.get( PROJECT_LOCK_TICKET, null );
+  }
+
+  /**
+   * @see org.kalypso.project.database.common.nature.IRemoteProjectPreferences#isOnServer()
+   */
+  @Override
+  public boolean isOnServer( )
+  {
+    return m_node.getBoolean( PROJECT_IS_ON_SERVER, false );
+  }
+
+  /**
+   * @see org.kalypso.project.database.common.nature.IRemoteProjectPreferences#isOnServer()
+   */
+  @Override
+  public Integer getVersion( )
+  {
+    return Integer.valueOf( m_node.get( PROJECT_DOWNLOADED_VERSION, "-1" ) );
+  }
+
+  /**
+   * @see org.kalypso.project.database.common.nature.IRemoteProjectPreferences#setIsOnServer(boolean)
+   */
+  @Override
+  public void setIsOnServer( final boolean onServer )
+  {
+    m_node.putBoolean( PROJECT_IS_ON_SERVER, Boolean.valueOf( onServer ) );
+    flush();
+  }
+
+  private void flush( )
+  {
+    new WorkspaceJob( "" )
+    {
+      @Override
+      public IStatus runInWorkspace( final IProgressMonitor monitor )
+      {
+        try
+        {
+          m_node.flush();
+        }
+        catch( final Exception e )
+        {
+        }
+        return Status.OK_STATUS;
+      }
+    }.schedule();
+
+  }
+
+  /**
+   * @see org.kalypso.project.database.common.nature.IRemoteProjectPreferences#setVersion(java.lang.Integer)
+   */
+  @Override
+  public void setVersion( final Integer version )
+  {
+    m_node.put( PROJECT_DOWNLOADED_VERSION, version.toString() );
+    flush();
+  }
+
+  /**
+   * @see org.kalypso.project.database.common.nature.IRemoteProjectPreferences#getProjectType()
+   */
+  @Override
+  public String getProjectType( )
+  {
+    return m_node.get( REMOTE_PROJECT_TYPE, "blub" );
+  }
+
+  /**
+   * @see org.kalypso.project.database.common.nature.IRemoteProjectPreferences#setProjectType(java.lang.String)
+   */
+  @Override
+  public void setProjectType( final String projectType )
+  {
+    m_node.put( REMOTE_PROJECT_TYPE, projectType );
+    flush();
+  }
+
+  /**
+   * @see org.kalypso.project.database.common.nature.IRemoteProjectPreferences#getChangesCommited()
+   */
+  @Override
+  public boolean getChangesCommited( )
+  {
+    final String value = m_node.get( PROJECT_CHANGES_COMMITED, Boolean.FALSE.toString() );
+
+    return Boolean.valueOf( value );
+  }
+
+  /**
+   * @see org.kalypso.project.database.common.nature.IRemoteProjectPreferences#setChangesCommited(boolean)
+   */
+  @Override
+  public void setChangesCommited( final boolean commited )
+  {
+    m_node.put( PROJECT_CHANGES_COMMITED, Boolean.valueOf( commited ).toString() );
+    if( Boolean.TRUE.equals( commited ) )
+    {
+      setModified( false );
+    }
+
+    flush();
+  }
+
+  /**
+   * @see org.kalypso.project.database.common.nature.IRemoteProjectPreferences#isModified()
+   */
+  @Override
+  public boolean isModified( )
+  {
+    final String value = m_node.get( PROJECT_IS_MODIFIED, Boolean.FALSE.toString() );
+
+    return Boolean.valueOf( value );
+  }
+
+  /**
+   * @see org.kalypso.project.database.common.nature.IRemoteProjectPreferences#setModified(boolean)
+   */
+  @Override
+  public void setModified( final boolean value )
+  {
+    m_node.put( PROJECT_IS_MODIFIED, Boolean.valueOf( value ).toString() );
+    flush();
+
+  }
+}
