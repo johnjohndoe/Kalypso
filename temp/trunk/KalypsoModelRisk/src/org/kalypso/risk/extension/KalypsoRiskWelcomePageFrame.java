10251ff08cb10c2c9d55b8adf0de4ba7064c89ed
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-03-27 13:07:03 +0000

    NEW - # 235: Mapping between model database and calculation chain https://dev.bjoernsen.de/planerclient/ticket/235

---
+++ temp/trunk/KalypsoModelRisk/src/org/kalypso/risk/extension/KalypsoRiskWelcomePageFrame.java
@@ -0,0 +1,82 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraße 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.risk.extension;
+
+import org.eclipse.swt.graphics.Image;
+import org.kalypso.project.database.client.extension.pages.welcome.IKalypsoModuleWelcomePageFrame;
+
+/**
+ * @author kuch
+ *
+ */
+public class KalypsoRiskWelcomePageFrame implements IKalypsoModuleWelcomePageFrame
+{
+
+  protected static final Image IMG = new Image( null, KalypsoRiskWelcomePageFrame.class.getResourceAsStream( "icons/risk_no.gif" ) );
+
+  protected static final Image IMG_HOVER = new Image( null, KalypsoRiskWelcomePageFrame.class.getResourceAsStream( "icons/risk_hover.gif" ) );
+
+  
+  @Override
+  public Image getIcon( )
+  {
+    return IMG;
+  }
+
+  @Override
+  public String getLabel( )
+  {
+    return "KalypsoRisk";
+  }
+
+  @Override
+  public String getTooltip( )
+  {
+    return "KalypsoRisk öffnen";
+  }
+
+  @Override
+  public Image getHoverIcon( )
+  {
+    return IMG_HOVER;
+  }
+
+}
