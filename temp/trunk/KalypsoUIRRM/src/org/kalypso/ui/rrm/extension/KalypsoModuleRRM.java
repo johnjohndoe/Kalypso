b1fd0b584a43e5658c6c491e2d3b3ac7be2e2eb6
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-03-27 13:03:52 +0000

    NEW - # 235: Mapping between model database and calculation chain https://dev.bjoernsen.de/planerclient/ticket/235

--- temp/trunk/KalypsoUIRRM/src/org/kalypso/ui/rrm/extension/KalypsoModuleRRM.java
+++ temp/trunk/KalypsoUIRRM/src/org/kalypso/ui/rrm/extension/KalypsoModuleRRM.java
@@ -1,37 +1,14 @@
 package org.kalypso.ui.rrm.extension;
 
-import java.io.File;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import kalypsoUIRRM.KalypsoUIRRMPlugin;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.jface.wizard.IWizard;
-import org.eclipse.swt.graphics.Image;
-import org.kalypso.afgui.wizards.INewProjectWizard;
-import org.kalypso.commons.java.util.zip.ZipUtilities;
-import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
-import org.kalypso.project.database.client.core.model.interfaces.ILocalProject;
 import org.kalypso.project.database.client.extension.IKalypsoModule;
-import org.kalypso.project.database.client.extension.database.IProjectDatabaseFilter;
-import org.kalypso.project.database.client.extension.database.IProjectHandler;
+import org.kalypso.project.database.client.extension.database.IKalypsoRemoteDatabaseSettings;
 import org.kalypso.project.database.client.extension.pages.module.IKalypsoModulePage;
 import org.kalypso.project.database.client.extension.pages.welcome.IKalypsoModuleWelcomePageFrame;
-import org.kalypso.project.database.client.extension.project.IKalypsoModuleProjectOpenAction;
-import org.kalypso.ui.rrm.wizards.NewNAAsciiProjectWizard;
 
 public class KalypsoModuleRRM implements IKalypsoModule
 {
-  protected static final Image IMG = new Image( null, KalypsoModuleRRM.class.getResourceAsStream( "icons/hydrology_no.gif" ) );
-
-  protected static final Image IMG_HOVER = new Image( null, KalypsoModuleRRM.class.getResourceAsStream( "icons/hydrology_hover.gif" ) );
-
-  protected static boolean INFO_PAGE_EXTRACTED = false;
-
+ 
+  // public constructor, needed because of declared extension point and java class loader
   public KalypsoModuleRRM( )
   {
   }
@@ -39,33 +16,7 @@ public class KalypsoModuleRRM implements IKalypsoModule
   @Override
   public IKalypsoModuleWelcomePageFrame getWelcomePageFrame( )
   {
-    return new IKalypsoModuleWelcomePageFrame()
-    {
-      @Override
-      public Image getIcon( )
-      {
-        return IMG;
-      }
-
-      @Override
-      public String getLabel( )
-      {
-        return "KalypsoHydrology";
-      }
-
-      @Override
-      public String getTooltip( )
-      {
-        return "KalypsoHydrology öffnen";
-      }
-
-      @Override
-      public Image getHoverIcon( )
-      {
-        return IMG_HOVER;
-      }
-
-    };
+    return new KalypsoRrmWelcomePageFrame();
   }
 
   /**
@@ -74,132 +25,17 @@ public class KalypsoModuleRRM implements IKalypsoModule
   @Override
   public IKalypsoModulePage getModulePage( )
   {
-    return new IKalypsoModulePage()
-    {
-      @Override
-      public IProjectDatabaseFilter getDatabaseFilter( )
-      {
-        return new IProjectDatabaseFilter()
-        {
-          @Override
-          public boolean select( final IProjectHandler handler )
-          {
-            if( handler instanceof ILocalProject )
-            {
-              final ILocalProject local = (ILocalProject) handler;
-              final IProject project = local.getProject();
-              final IFile file = project.getFile( "hydrotop.gml" ); //$NON-NLS-1$
-
-              return file.exists();
-            }
-
-            return false;
-          }
-        };
-      }
-
-      @Override
-      public String getHeader( )
-      {
-        return "KalypsoHydrology";
-      }
-
-      @Override
-      public URL getInfoURL( ) throws MalformedURLException
-      {
-        if( !INFO_PAGE_EXTRACTED )
-        {
-          try
-          {
-            /* info page of plugin */
-            final InputStream zipStream = getClass().getResourceAsStream( "infoPage.zip" );
-            try
-            {
-              final IPath stateLocation = KalypsoUIRRMPlugin.getDefault().getStateLocation();
-              final File targetDir = new File( stateLocation.toFile(), "infoPage" );
-              ZipUtilities.unzip( zipStream, targetDir );
-
-              INFO_PAGE_EXTRACTED = true;
-            }
-            finally
-            {
-              zipStream.close();
-            }
-          }
-          catch( final Exception e )
-          {
-            KalypsoUIRRMPlugin.getDefault().getLog().log( StatusUtilities.statusFromThrowable( e ) );
-          }
-
-        }
-
-        final IPath stateLocation = KalypsoUIRRMPlugin.getDefault().getStateLocation();
-        final URL baseUrl = stateLocation.toFile().toURI().toURL();
-
-        final URL url = new URL( baseUrl, "infoPage/index.html" );
-        return url;
-      }
-
-      @Override
-      public INewProjectWizard getDemoProjectWizard( )
-      {
-        return null;
-      }
-
-      /**
-       * @see org.kalypso.afgui.extension.IKalypsoModuleEnteringPageHandler#hasDemoProjectWizard()
-       */
-      @Override
-      public boolean hasDemoProjectWizard( )
-      {
-        return false;
-      }
-
-      @Override
-      public INewProjectWizard getProjectWizard( )
-      {
-        return new NewNAAsciiProjectWizard();
-      }
-
-      @Override
-      public String getRemoteCommitType( )
-      {
-        return "KalypsRrmModel";
-      }
-
-      /**
-       * @see org.kalypso.afgui.extension.IKalypsoModuleEnteringPageHandler#hasImportWizard()
-       */
-      @Override
-      public boolean hasImportWizard( )
-      {
-        return false;
-      }
-
-      @Override
-      public IWizard getImportWizard( )
-      {
-        return null;
-      }
-
-      @Override
-      public String getImportWizardLabel( )
-      {
-        return null;
-      }
-
-      @Override
-      public Integer getPriority( )
-      {
-        return 1;
-      }
-
-      @Override
-      public IKalypsoModuleProjectOpenAction getProjectOpenAction( )
-      {
-        return new KalypsoRRMOpenAction();
-      }
-    };
+    return new KalypsoRrmModulePage( this );
   }
 
+  
+  /**
+   * @see org.kalypso.project.database.client.extension.IKalypsoModule#getRemoteDatabaseSettings()
+   */
+  @Override
+  public IKalypsoRemoteDatabaseSettings getRemoteDatabaseSettings( )
+  {
+    return new KalypsoRrmRemoteDatabaseSettings();
+  }
+  
 }
