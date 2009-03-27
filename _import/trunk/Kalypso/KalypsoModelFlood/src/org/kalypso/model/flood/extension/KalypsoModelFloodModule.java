306a542ec1a2da28d64206b6ca59c7d0f533408c
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-03-27 13:03:42 +0000

    NEW - # 235: Mapping between model database and calculation chain https://dev.bjoernsen.de/planerclient/ticket/235

--- _import/trunk/Kalypso/KalypsoModelFlood/src/org/kalypso/model/flood/extension/KalypsoModelFloodModule.java
+++ _import/trunk/Kalypso/KalypsoModelFlood/src/org/kalypso/model/flood/extension/KalypsoModelFloodModule.java
@@ -1,40 +1,14 @@
 package org.kalypso.model.flood.extension;
 
-import java.io.File;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.jface.wizard.IWizard;
-import org.eclipse.swt.graphics.Image;
-import org.kalypso.afgui.wizards.INewProjectWizard;
-import org.kalypso.commons.java.util.zip.ZipUtilities;
-import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
-import org.kalypso.model.flood.KalypsoModelFloodPlugin;
-import org.kalypso.model.flood.ui.wizards.NewDemoProjectWizard;
-import org.kalypso.model.flood.ui.wizards.NewProjectWizard;
-import org.kalypso.project.database.client.core.model.interfaces.ILocalProject;
 import org.kalypso.project.database.client.extension.IKalypsoModule;
-import org.kalypso.project.database.client.extension.database.IProjectDatabaseFilter;
-import org.kalypso.project.database.client.extension.database.IProjectHandler;
+import org.kalypso.project.database.client.extension.database.IKalypsoRemoteDatabaseSettings;
 import org.kalypso.project.database.client.extension.pages.module.IKalypsoModulePage;
 import org.kalypso.project.database.client.extension.pages.welcome.IKalypsoModuleWelcomePageFrame;
-import org.kalypso.project.database.client.extension.project.IKalypsoModuleProjectOpenAction;
-import org.kalypso.project.database.client.extension.project.SzenarioProjectOpenAction;
-
-import de.renew.workflow.base.IWorkflow;
-import de.renew.workflow.connector.WorkflowProjectNature;
 
 public class KalypsoModelFloodModule implements IKalypsoModule
 {
-  protected static final Image IMG = new Image( null, KalypsoModelFloodModule.class.getResourceAsStream( "icons/flood_no.gif" ) );
 
-  protected static final Image IMG_HOVER = new Image( null, KalypsoModelFloodModule.class.getResourceAsStream( "icons/flood_hover.gif" ) );
-
-  protected static boolean INFO_PAGE_EXTRACTED = false;
-
+  // public constructor, needed because of declared extension point and java class loader
   public KalypsoModelFloodModule( )
   {
   }
@@ -42,33 +16,7 @@ public class KalypsoModelFloodModule implements IKalypsoModule
   @Override
   public IKalypsoModuleWelcomePageFrame getWelcomePageFrame( )
   {
-    return new IKalypsoModuleWelcomePageFrame()
-    {
-      @Override
-      public Image getIcon( )
-      {
-        return IMG;
-      }
-
-      @Override
-      public String getLabel( )
-      {
-        return "KalypsoFlood";
-      }
-
-      @Override
-      public String getTooltip( )
-      {
-        return "KalypsoFlood öffnen";
-      }
-
-      @Override
-      public Image getHoverIcon( )
-      {
-        return IMG_HOVER;
-      }
-
-    };
+    return new KalypsoFloodWelcomePageFrame();
   }
 
   /**
@@ -77,144 +25,16 @@ public class KalypsoModelFloodModule implements IKalypsoModule
   @Override
   public IKalypsoModulePage getModulePage( )
   {
-    return new IKalypsoModulePage()
-    {
-      @Override
-      public IProjectDatabaseFilter getDatabaseFilter( )
-      {
-        return new IProjectDatabaseFilter()
-        {
-          @Override
-          public boolean select( final IProjectHandler handler )
-          {
-            if( handler instanceof ILocalProject )
-            {
-              try
-              {
-                final ILocalProject local = (ILocalProject) handler;
-                final WorkflowProjectNature nature = WorkflowProjectNature.toThisNature( local.getProject() );
-                if( nature == null )
-                  return false;
+    return new KalypsoFloodModulePage( this );
+  }
 
-                final IWorkflow workflow = nature.getCurrentWorklist();
-                final String uri = workflow.getURI();
-
-                return uri.contains( "org.kalypso.model.flood.WF_KalypsoFlood" );
-              }
-              catch( final CoreException e )
-              {
-                KalypsoModelFloodPlugin.getDefault().getLog().log( StatusUtilities.statusFromThrowable( e ) );
-              }
-            }
-
-            return false;
-          }
-        };
-      }
-
-      @Override
-      public String getHeader( )
-      {
-        return "KalypsoFlood";
-      }
-
-      @Override
-      public URL getInfoURL( ) throws MalformedURLException
-      {
-        if( !INFO_PAGE_EXTRACTED )
-        {
-          try
-          {
-            /* info page of plugin */
-            final InputStream zipStream = getClass().getResourceAsStream( "infoPage.zip" );
-            try
-            {
-              final IPath stateLocation = KalypsoModelFloodPlugin.getDefault().getStateLocation();
-              final File targetDir = new File( stateLocation.toFile(), "infoPage" );
-              // final boolean mkdir = dir.mkdir();
-
-              ZipUtilities.unzip( zipStream, targetDir );
-
-              INFO_PAGE_EXTRACTED = true;
-            }
-            finally
-            {
-              zipStream.close();
-            }
-          }
-          catch( final Exception e )
-          {
-            KalypsoModelFloodPlugin.getDefault().getLog().log( StatusUtilities.statusFromThrowable( e ) );
-          }
-        }
-
-        final IPath stateLocation = KalypsoModelFloodPlugin.getDefault().getStateLocation();
-        final URL baseUrl = stateLocation.toFile().toURI().toURL();
-
-        final URL url = new URL( baseUrl, "infoPage/index.html" );
-        return url;
-      }
-
-      @Override
-      public INewProjectWizard getDemoProjectWizard( )
-      {
-        return new NewDemoProjectWizard();
-      }
-
-      /**
-       * @see org.kalypso.afgui.extension.IKalypsoModuleEnteringPageHandler#hasDemoProjectWizard()
-       */
-      @Override
-      public boolean hasDemoProjectWizard( )
-      {
-        return true;
-      }
-
-      @Override
-      public INewProjectWizard getProjectWizard( )
-      {
-        return new NewProjectWizard();
-      }
-
-      @Override
-      public String getRemoteCommitType( )
-      {
-        return "KalypsoFloodModelType";
-      }
-
-      @Override
-      public IWizard getImportWizard( )
-      {
-        return null;
-      }
-
-      /**
-       * @see org.kalypso.afgui.extension.IKalypsoModuleEnteringPageHandler#hasImportWizard()
-       */
-      @Override
-      public boolean hasImportWizard( )
-      {
-        return false;
-      }
-
-      @Override
-      public String getImportWizardLabel( )
-      {
-        return null;
-      }
-
-      @Override
-      public Integer getPriority( )
-      {
-        return 4;
-      }
-
-      @Override
-      public IKalypsoModuleProjectOpenAction getProjectOpenAction( )
-      {
-        return new SzenarioProjectOpenAction();
-      }
-    };
+  /**
+   * @see org.kalypso.project.database.client.extension.IKalypsoModule#getRemoteDatabaseSettings()
+   */
+  @Override
+  public IKalypsoRemoteDatabaseSettings getRemoteDatabaseSettings( )
+  {
+    return new KalypsoFloodRemoteDatabaseSettings();
   }
 
 }
