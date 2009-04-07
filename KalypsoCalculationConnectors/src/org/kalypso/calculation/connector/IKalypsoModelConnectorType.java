10429515faeb1d08fab0af40500ebb02018c59f3
dejan <dejan@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-04-07 14:57:26 +0000

    Necessary models modifications for chain calculation (backward compatible, will not affect regular calculations)

--- KalypsoCalculationConnectors/src/org/kalypso/calculation/connector/IKalypsoModelConnectorType.java
+++ KalypsoCalculationConnectors/src/org/kalypso/calculation/connector/IKalypsoModelConnectorType.java
@@ -2,55 +2,35 @@ package org.kalypso.calculation.connector;
 
 import org.apache.commons.lang.NotImplementedException;
 
-public interface IKalypsoModelConnectorType
-{
-  public enum MODEL_CONNECTOR_TYPEID
-  {
-    CONNECTOR_NA_WSPM,
-    CONNECTOR_WSPM_FLOOD,
-    CONNECTOR_FLOOD_RISK;
-    public String getValue( )
-    {
-      final MODEL_CONNECTOR_TYPEID kind = MODEL_CONNECTOR_TYPEID.valueOf( name() );
+public interface IKalypsoModelConnectorType {
+    public enum MODEL_CONNECTOR_TYPEID {
+	CONNECTOR_NA_WSPM, CONNECTOR_WSPM_FLOOD, CONNECTOR_FLOOD_RISK;
+	public String getValue() {
+	    final MODEL_CONNECTOR_TYPEID kind = MODEL_CONNECTOR_TYPEID.valueOf(name());
 
-      switch( kind )
-      {
-        case CONNECTOR_NA_WSPM:
-          return "KalypsoModelConnector_NA_WSPM";
-        case CONNECTOR_WSPM_FLOOD:
-          return "KalypsoModelConnector_WSPM_FM";
-        case CONNECTOR_FLOOD_RISK:
-          return "KalypsoModelConnector_FM_RM";
-        default:
-          throw new NotImplementedException();
-      }
+	    switch (kind) {
+	    case CONNECTOR_NA_WSPM:
+		return "KalypsoModelConnector_NA_WSPM";
+	    case CONNECTOR_WSPM_FLOOD:
+		return "KalypsoModelConnector_WSPM_FM";
+	    case CONNECTOR_FLOOD_RISK:
+		return "KalypsoModelConnector_FM_RM";
+	    default:
+		throw new NotImplementedException();
+	    }
+	}
     }
-  }
 
-  public enum MODELSPEC_CONNECTOR_NA_WSPM
-  {
-    NA_Model,
-    NA_StatisticalReport,
-    NA_RiverCode,
-    WSPM_Model,
-    WSPM_RunoffEventID;
-  }
+    public enum MODELSPEC_CONNECTOR_NA_WSPM {
+	NA_Model, NA_ControlModel, NA_StatisticalReport, NA_RiverCode, WSPM_Model, WSPM_RunoffEventID;
+    }
 
-  public enum MODELSPEC_CONNECTOR_WSPM_FM
-  {
-    WSPM_TinFile,
-    FM_Model,
-    OPT_DeleteExistingRunoffEvents,
-    WSPM_TinReference;
-  }
+    public enum MODELSPEC_CONNECTOR_WSPM_FM {
+	WSPM_Model, WSPM_RunoffEventID, FM_Model, WSPM_TinFile, OPT_DeleteExistingRunoffEvents, WSPM_TinReference;
+    }
 
-  public enum MODELSPEC_CONNECTOR_FM_RM
-  {
-    FM_Model,
-    FM_EventsFolder,
-    RM_Model,
-    RM_InputRasterFolder;
-  }
-  
+    public enum MODELSPEC_CONNECTOR_FM_RM {
+	FM_Model, FM_EventsFolder, RM_Model, RM_InputRasterFolder;
+    }
 
 }
--- KalypsoCalculationConnectors/src/org/kalypso/calculation/connector/na_wspm/Connector_NA_WSPM_Job.java
+++ KalypsoCalculationConnectors/src/org/kalypso/calculation/connector/na_wspm/Connector_NA_WSPM_Job.java
@@ -33,178 +33,150 @@ import org.kalypso.simulation.core.SimulationException;
 import org.kalypsodeegree.model.feature.Feature;
 import org.kalypsodeegree.model.feature.GMLWorkspace;
 
-public class Connector_NA_WSPM_Job extends AbstractInternalStatusJob implements ISimulation
-{
-  public Connector_NA_WSPM_Job( )
-  {
-  }
-  
-  
-  @Override
-  public URL getSpezifikation( )
-  {
-    return getClass().getResource( "resources/modelSpecification.xml" );
-  }
-
-  @Override
-  public void run( final File tmpdir, final ISimulationDataProvider inputProvider, final ISimulationResultEater resultEater, final ISimulationMonitor monitor ) throws SimulationException
-  {
-    final URL urlModelNA = (URL) inputProvider.getInputForID( MODELSPEC_CONNECTOR_NA_WSPM.NA_Model.name() );
-    final URL urlModelWSPM = (URL) inputProvider.getInputForID( MODELSPEC_CONNECTOR_NA_WSPM.WSPM_Model.name() );
-    final URL urlStatisticalReportNA = (URL) inputProvider.getInputForID( MODELSPEC_CONNECTOR_NA_WSPM.NA_StatisticalReport.name() );
-    final Object riverCode = inputProvider.getInputForID( MODELSPEC_CONNECTOR_NA_WSPM.NA_RiverCode.name() );
-    final String naRiverCode = riverCode == null ? "" : riverCode.toString();
-    final String runOffEventID = inputProvider.getInputForID( MODELSPEC_CONNECTOR_NA_WSPM.WSPM_RunoffEventID.name() ).toString();
-    try
-    {
-      final File outputFile = File.createTempFile( "outTempWspmModel", ".gml", tmpdir );
-      final File gmlModelFileNA = new File( urlModelNA.getPath() );
-      if( gmlModelFileNA.exists() )
-      {
-        try
-        {
-          TupleResult wspmRunoffEventTupleResult = null;
-          final List<BigDecimal> kilometerValuesList = new ArrayList<BigDecimal>();
-          final GMLWorkspace workspaceNA = GmlSerializer.createGMLWorkspace( urlModelNA, null );
-          final GMLWorkspace workspaceWSPM = GmlSerializer.createGMLWorkspace( urlModelWSPM, null );
-
-          Feature wspmObservationFeature = null;
-          org.kalypso.observation.IObservation<TupleResult> wspmObservation = null;
-          final IFeatureType wspmRunoffEventFeatureType = workspaceWSPM.getGMLSchema().getFeatureType( new QName( IWspmConstants.NS_WSPMRUNOFF, "RunOffEvent" ) );
-          final Feature[] wspmRunoffEvents = workspaceWSPM.getFeatures( wspmRunoffEventFeatureType );
-          for( final Feature feature : wspmRunoffEvents )
-          {
-            if( runOffEventID.equals( feature.getId() ) )
-            {
-              wspmObservationFeature = feature;
-              break;
-            }
-          }
-          if( wspmObservationFeature != null )
-          {
-            wspmObservation = ObservationFeatureFactory.toObservation( wspmObservationFeature );
-            wspmRunoffEventTupleResult = wspmObservation.getResult();
-          }
-          wspmRunoffEventTupleResult.clear();
-
-          int stationComp = -1;
-          int abflussComp = -1;
-          final IComponent[] wspmRunoffEventTupleResultComponents = wspmRunoffEventTupleResult.getComponents();
-          for( int i = 0; i < wspmRunoffEventTupleResultComponents.length; i++ )
-          {
-            final IComponent comp = wspmRunoffEventTupleResultComponents[i];
-            // TODO: get component via phenomenon
-            if( comp.getName().startsWith( "Abfluss" ) )
-            {
-              abflussComp = i;
-            }
-            if( comp.getName().startsWith( "Station" ) ) //$NON-NLS-1$
-            {
-              stationComp = i;
-              wspmRunoffEventTupleResult.setSortComponents( new IComponent[] { comp } );
-            }
-          }
-
-          final IObservation maxDischargesObservation = ZmlFactory.parseXML( urlStatisticalReportNA, null );
-          final IAxis[] axisList = maxDischargesObservation.getAxisList();
-          IAxis axisNodeNr = null;
-          IAxis axisMaxDischarge = null;
-          // Observation structure:
-          // "Knoten - Nr.", Integer
-          // "Stationierung", String
-          // "Datum", Date
-          // "max. Abfluss", Double
-          // (unfortunately, this values are hard-coded in the
-          // observation...)
-          for( final IAxis element : axisList )
-          {
-            if( "Knoten - Nr.".equals( element.getName() ) )
-            {
-              axisNodeNr = element;
-            }
-            if( "max. Abfluss".equals( element.getName() ) )
-            {
-              axisMaxDischarge = element;
-            }
-          }
-          if( axisNodeNr == null || axisMaxDischarge == null )
-          {
-            // TODO error report & exit
-            setStatus( STATUS.ERROR, "No proper axis found." );
-            return;
-          }
-
-          final ITuppleModel tuppleModel = maxDischargesObservation.getValues( null );
-
-          final IFeatureType naNodeFeatureType = workspaceNA.getGMLSchema().getFeatureType( NaModelConstants.NODE_ELEMENT_FT );
-          final Feature[] naNodes = workspaceNA.getFeatures( naNodeFeatureType );
-
-          for( int i = 0; i < tuppleModel.getCount(); i++ )
-          {
-            final String nodeNr = tuppleModel.getElement( i, axisNodeNr ).toString();
-            final Double maxDischarge = Double.parseDouble( tuppleModel.getElement( i, axisMaxDischarge ).toString() );
-            for( final Feature feature : naNodes )
-            {
-              if( feature.getName().equals( nodeNr ) )
-              {
-                if( naRiverCode != null && naRiverCode.length() > 0 )
-                {
-                  final Object nodeRiverCodeProperty = feature.getProperty( NaModelConstants.NODE_RIVER_CODE_PROP );
-                  if( nodeRiverCodeProperty == null || !naRiverCode.equals( nodeRiverCodeProperty ) )
-                  {
-                    continue;
-                  }
-                }
-                final Object riverKilometerProp = feature.getProperty( NaModelConstants.NODE_RIVER_KILOMETER_PROP );
-                if( riverKilometerProp != null )
-                {
-                  final Double nodeRiverKilometerProperty = Double.parseDouble( riverKilometerProp.toString() );
-                  final BigDecimal bigDecimalRiverKilometer = new BigDecimal( nodeRiverKilometerProperty ).setScale( IProfileFeature.STATION_SCALE, RoundingMode.HALF_UP );
-                  final BigDecimal bigDecimalDischarge = new BigDecimal( maxDischarge ).setScale( 3, RoundingMode.HALF_UP );
-
-                  if( kilometerValuesList.contains( bigDecimalRiverKilometer ) )
-                  {
-                    // TODO: what to do if there are
-                    // duplicate values for river kilometer?
-                    // It is not allowed as WSPM input!
-                    setStatus( STATUS.ERROR, "Duplicate value for river kilometer (formatted to 0.0000): " + bigDecimalRiverKilometer );
-                    return;
-                  }
-                  else
-                  {
-                    kilometerValuesList.add( bigDecimalRiverKilometer );
-                  }
-
-                  final IRecord record = wspmRunoffEventTupleResult.createRecord();
-                  wspmRunoffEventTupleResult.add( record );
-                  record.setValue( stationComp, bigDecimalRiverKilometer );
-                  record.setValue( abflussComp, bigDecimalDischarge );
-                }
-              }
-            }
-          }
-          ObservationFeatureFactory.toFeature( wspmObservation, wspmObservationFeature );
-          GmlSerializer.serializeWorkspace( outputFile, workspaceWSPM, "UTF-8" );
-          setStatus( STATUS.OK, "Success" );
-        }
-        catch( final Exception e )
-        {
-          setStatus( STATUS.ERROR, e.getLocalizedMessage() );
-        }
-      }
-      else
-      {
-        setStatus( STATUS.ERROR, "no input file" );
-      }
-      if( isOkStatus() )
-      {
-        resultEater.addResult( MODELSPEC_CONNECTOR_NA_WSPM.WSPM_Model.name(), outputFile );
-      }
+public class Connector_NA_WSPM_Job extends AbstractInternalStatusJob implements ISimulation {
+    public Connector_NA_WSPM_Job() {
     }
-    catch( final IOException e )
-    {
-      e.printStackTrace();
+
+    @Override
+    public URL getSpezifikation() {
+	return getClass().getResource("resources/modelSpecification.xml");
     }
-  }
+
+    @Override
+    public void run(final File tmpdir, final ISimulationDataProvider inputProvider, final ISimulationResultEater resultEater, final ISimulationMonitor monitor) throws SimulationException {
+	final URL urlModelNA = (URL) inputProvider.getInputForID(MODELSPEC_CONNECTOR_NA_WSPM.NA_Model.name());
+	final URL urlControlModelNA = (URL) inputProvider.getInputForID(MODELSPEC_CONNECTOR_NA_WSPM.NA_ControlModel.name());
+	final URL urlModelWSPM = (URL) inputProvider.getInputForID(MODELSPEC_CONNECTOR_NA_WSPM.WSPM_Model.name());
+	final URL urlStatisticalReportNA = (URL) inputProvider.getInputForID(MODELSPEC_CONNECTOR_NA_WSPM.NA_StatisticalReport.name());
+	final Object riverCode = inputProvider.getInputForID(MODELSPEC_CONNECTOR_NA_WSPM.NA_RiverCode.name());
+	final String naRiverCode = riverCode == null ? "" : riverCode.toString();
+	final String runOffEventID = inputProvider.getInputForID(MODELSPEC_CONNECTOR_NA_WSPM.WSPM_RunoffEventID.name()).toString();
+	try {
+	    final File outputFile = File.createTempFile("outTempWspmModel", ".gml", tmpdir);
+	    final File gmlModelFileNA = new File(urlModelNA.getPath());
+	    if (gmlModelFileNA.exists()) {
+		try {
+		    final TupleResult wspmRunoffEventTupleResult;
+		    final List<BigDecimal> kilometerValuesList = new ArrayList<BigDecimal>();
+		    final GMLWorkspace workspaceNA = GmlSerializer.createGMLWorkspace(urlModelNA, null);
+		    final GMLWorkspace workspaceControlModelNA = GmlSerializer.createGMLWorkspace(urlControlModelNA, null);
+		    final GMLWorkspace workspaceWSPM = GmlSerializer.createGMLWorkspace(urlModelWSPM, null);
+
+		    Feature wspmObservationFeature = null;
+		    org.kalypso.observation.IObservation<TupleResult> wspmObservation = null;
+		    final IFeatureType wspmRunoffEventFeatureType = workspaceWSPM.getGMLSchema().getFeatureType(new QName(IWspmConstants.NS_WSPMRUNOFF, "RunOffEvent"));
+		    final Feature[] wspmRunoffEvents = workspaceWSPM.getFeatures(wspmRunoffEventFeatureType);
+		    for (final Feature feature : wspmRunoffEvents) {
+			if (runOffEventID.equals(feature.getId())) {
+			    wspmObservationFeature = feature;
+			    break;
+			}
+		    }
+		    if (wspmObservationFeature != null) {
+			wspmObservation = ObservationFeatureFactory.toObservation(wspmObservationFeature);
+			wspmRunoffEventTupleResult = wspmObservation.getResult();
+		    } else
+			throw new SimulationException("Undefined WSPM runoff event ID specified.");
+
+		    int stationComp = -1;
+		    int abflussComp = -1;
+		    final IComponent[] wspmRunoffEventTupleResultComponents = wspmRunoffEventTupleResult.getComponents();
+		    for (int i = 0; i < wspmRunoffEventTupleResultComponents.length; i++) {
+			final IComponent comp = wspmRunoffEventTupleResultComponents[i];
+			// TODO: get component via phenomenon
+			if (comp.getName().startsWith("Abfluss")) {
+			    abflussComp = i;
+			}
+			if (comp.getName().startsWith("Station")) //$NON-NLS-1$
+			{
+			    stationComp = i;
+			    wspmRunoffEventTupleResult.setSortComponents(new IComponent[] { comp });
+			}
+		    }
+
+		    final IObservation maxDischargesObservation = ZmlFactory.parseXML(urlStatisticalReportNA, null);
+		    final IAxis[] axisList = maxDischargesObservation.getAxisList();
+		    IAxis axisNodeNr = null;
+		    IAxis axisMaxDischarge = null;
+		    // Observation structure:
+		    // "Knoten - Nr.", Integer
+		    // "Stationierung", String
+		    // "Datum", Date
+		    // "max. Abfluss", Double
+		    // (unfortunately, this values are hard-coded in the
+		    // observation...)
+		    for (final IAxis element : axisList) {
+			if ("Knoten - Nr.".equals(element.getName())) {
+			    axisNodeNr = element;
+			}
+			if ("max. Abfluss".equals(element.getName())) {
+			    axisMaxDischarge = element;
+			}
+		    }
+		    if (axisNodeNr == null || axisMaxDischarge == null) {
+			// TODO error report & exit
+			setStatus(STATUS.ERROR, "No proper axis found.");
+			return;
+		    }
+
+		    final ITuppleModel tuppleModel = maxDischargesObservation.getValues(null);
+
+		    final IFeatureType naNodeFeatureType = workspaceNA.getGMLSchema().getFeatureType(NaModelConstants.NODE_ELEMENT_FT);
+		    final Feature[] naNodes = workspaceNA.getFeatures(naNodeFeatureType);
+
+		    for (int i = 0; i < tuppleModel.getCount(); i++) {
+			final String nodeNr = tuppleModel.getElement(i, axisNodeNr).toString();
+			final Double maxDischarge = Double.parseDouble(tuppleModel.getElement(i, axisMaxDischarge).toString());
+			for (final Feature feature : naNodes) {
+			    if (feature.getName().equals(nodeNr)) {
+				if (naRiverCode != null && naRiverCode.length() > 0) {
+				    final Object nodeRiverCodeProperty = feature.getProperty(NaModelConstants.NODE_RIVER_CODE_PROP);
+				    if (nodeRiverCodeProperty == null || !naRiverCode.equals(nodeRiverCodeProperty)) {
+					continue;
+				    }
+				}
+				final Object riverKilometerProp = feature.getProperty(NaModelConstants.NODE_RIVER_KILOMETER_PROP);
+				if (riverKilometerProp != null) {
+				    final Double nodeRiverKilometerProperty = Double.parseDouble(riverKilometerProp.toString());
+				    final BigDecimal bigDecimalRiverKilometer = new BigDecimal(nodeRiverKilometerProperty).setScale(IProfileFeature.STATION_SCALE, RoundingMode.HALF_UP);
+				    final BigDecimal bigDecimalDischarge = new BigDecimal(maxDischarge).setScale(3, RoundingMode.HALF_UP);
+
+				    if (kilometerValuesList.contains(bigDecimalRiverKilometer)) {
+					// TODO: what to do if there are
+					// duplicate values for river kilometer?
+					// It is not allowed as WSPM input!
+					setStatus(STATUS.ERROR, "Duplicate value for river kilometer (formatted to 0.0000): " + bigDecimalRiverKilometer);
+					return;
+				    } else {
+					kilometerValuesList.add(bigDecimalRiverKilometer);
+				    }
+
+				    final IRecord record = wspmRunoffEventTupleResult.createRecord();
+				    wspmRunoffEventTupleResult.add(record);
+				    record.setValue(stationComp, bigDecimalRiverKilometer);
+				    record.setValue(abflussComp, bigDecimalDischarge);
+				}
+			    }
+			}
+		    }
+		    ObservationFeatureFactory.toFeature(wspmObservation, wspmObservationFeature);
+		    final Object returnPeriod = workspaceControlModelNA.getRootFeature().getProperty(NaModelConstants.CONTROL_RETURN_PERIOD_PROP);
+		    final QName returnPeriodQName = new QName(IWspmConstants.NS_WSPMRUNOFF, "returnPeriod");
+		    wspmObservationFeature.setProperty(returnPeriodQName, (returnPeriod != null && ((Integer) returnPeriod) > 0) ? returnPeriod : 1);
+		    GmlSerializer.serializeWorkspace(outputFile, workspaceWSPM, "UTF-8");
+		    setStatus(STATUS.OK, "Success");
+		} catch (final Exception e) {
+		    setStatus(STATUS.ERROR, e.getLocalizedMessage());
+		}
+	    } else {
+		setStatus(STATUS.ERROR, "no input file");
+	    }
+	    if (isOkStatus()) {
+		resultEater.addResult(MODELSPEC_CONNECTOR_NA_WSPM.WSPM_Model.name(), outputFile);
+	    }
+	} catch (final IOException e) {
+	    e.printStackTrace();
+	}
+    }
 
 }
