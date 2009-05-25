d3c68bf0bba85f358acd9ad7e3292ea35a8c2a5d
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-05-25 16:11:25 +0000

    fixed file not found

--- KalypsoCalculationConnectors/src/org/kalypso/calculation/connector/lzNA_kzNA/Connector_lzNA_kzNA_Job.java
+++ KalypsoCalculationConnectors/src/org/kalypso/calculation/connector/lzNA_kzNA/Connector_lzNA_kzNA_Job.java
@@ -16,32 +16,40 @@ import org.kalypso.simulation.core.ISimulationResultEater;
 import org.kalypso.simulation.core.SimulationException;
 import org.kalypsodeegree.model.feature.GMLWorkspace;
 
-public class Connector_lzNA_kzNA_Job extends AbstractInternalStatusJob implements ISimulation {
+public class Connector_lzNA_kzNA_Job extends AbstractInternalStatusJob implements ISimulation
+{
 
-    public Connector_lzNA_kzNA_Job() {
-    }
-
-    @Override
-    public URL getSpezifikation() {
-	return getClass().getResource("resources/modelSpecification.xml");
-    }
+    public Connector_lzNA_kzNA_Job( )
+  {
+  }
 
     @Override
-    public void run(final File tmpdir, final ISimulationDataProvider inputProvider, final ISimulationResultEater resultEater, final ISimulationMonitor monitor) throws SimulationException {
-	final URL urlControlModelKZ = (URL) inputProvider.getInputForID(MODELSPEC_CONNECTOR_LZNA_KZNA.KZNA_CALCULATION.name());
-	try {
-	    final File resultsFolderLZ = new File(inputProvider.getInputForID(MODELSPEC_CONNECTOR_LZNA_KZNA.LZNA_ERGEBNISSE_AKTUEL_ANFANGWERTE.name()).toString());
-	    final GMLWorkspace workspaceControlModelKZ = GmlSerializer.createGMLWorkspace(urlControlModelKZ, null);
-	    
-	    // TODO @dejan - unknown property exception - startsimulation should be {http://www.tuhh.de/initialValues}iniDate
+  public URL getSpezifikation( )
+  {
+    return getClass().getResource( "resources/modelSpecification.xml" );
+  }
+
+    @Override
+  public void run( final File tmpdir, final ISimulationDataProvider inputProvider, final ISimulationResultEater resultEater, final ISimulationMonitor monitor ) throws SimulationException
+  {
+    final URL urlControlModelKZ = (URL) inputProvider.getInputForID( MODELSPEC_CONNECTOR_LZNA_KZNA.KZNA_CALCULATION.name() );
+    try
+    {
+      final URL urlResultFolderLz = (URL) inputProvider.getInputForID( MODELSPEC_CONNECTOR_LZNA_KZNA.LZNA_ERGEBNISSE_AKTUEL_ANFANGWERTE.name() );
+      final File resultsFolderLZ = new File( urlResultFolderLz.getFile() );
+      final GMLWorkspace workspaceControlModelKZ = GmlSerializer.createGMLWorkspace( urlControlModelKZ, null );
+
+      // TODO @dejan - unknown property exception - startsimulation should be {http://www.tuhh.de/initialValues}iniDate
       // ?!?
-	    final XMLGregorianCalendar calendar = (XMLGregorianCalendar) workspaceControlModelKZ.getRootFeature().getProperty(NaModelConstants.CONTROL_STARTSIMULATION);
-	    final String fileName = String.format("%1$tY%1$tm%1$td(%1$tH).gml", calendar.toGregorianCalendar().getTime());
-	    final File lzSim = new File(resultsFolderLZ, fileName);
-	    if (lzSim.exists())
-		resultEater.addResult(MODELSPEC_CONNECTOR_LZNA_KZNA.KZNA_ANFANGWERTE_LZSIM.name(), lzSim);
-	} catch (final Exception e) {
-	    throw new SimulationException(e.getLocalizedMessage());
-	}
+      final XMLGregorianCalendar calendar = (XMLGregorianCalendar) workspaceControlModelKZ.getRootFeature().getProperty( NaModelConstants.CONTROL_STARTSIMULATION );
+      final String fileName = String.format( "%1$tY%1$tm%1$td(%1$tH).gml", calendar.toGregorianCalendar().getTime() );
+      final File lzSim = new File( resultsFolderLZ, fileName );
+      if( lzSim.exists() )
+        resultEater.addResult( MODELSPEC_CONNECTOR_LZNA_KZNA.KZNA_ANFANGWERTE_LZSIM.name(), lzSim );
+    }
+    catch( final Exception e )
+    {
+      throw new SimulationException( e.getLocalizedMessage() );
+    }
     }
 }
