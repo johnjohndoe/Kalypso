03bcea2aa27b93b4cc7ad093917de1381268d602
nico <nico@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-08-17 08:07:18 +0000

    Tagging for Milestone Release 2 of Kalypso version 2.2.0

---
+++ _dump/tags/Kalypso_2.2.0_Milestone2_20090817/KalypsoModelWspmCore/src/org/kalypso/model/wspm/core/util/pointpropertycalculator/AddFixValueOperator.java
@@ -0,0 +1,89 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.model.wspm.core.util.pointpropertycalculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.Status;
+import org.kalypso.model.wspm.core.KalypsoModelWspmCorePlugin;
+import org.kalypso.model.wspm.core.i18n.Messages;
+import org.kalypso.model.wspm.core.profil.IProfilChange;
+import org.kalypso.model.wspm.core.profil.changes.PointPropertyEdit;
+import org.kalypso.model.wspm.core.profil.util.ProfilUtil;
+import org.kalypso.observation.result.IComponent;
+import org.kalypso.observation.result.IRecord;
+import org.kalypso.observation.result.TupleResult;
+
+/**
+ * @author kimwerner
+ */
+public class AddFixValueOperator implements IPointPropertyCalculator
+{
+
+  /**
+   * @see org.kalypso.model.wspm.core.util.pointpropertycalculator.IPointPropertyCalculator#calculate(java.lang.Double,
+   *      java.lang.String[], java.util.List)
+   */
+  public IProfilChange[] calculate( final Double operand, final IComponent[] properties, final List<IRecord> points )
+  {
+    final List<IProfilChange> changes = new ArrayList<IProfilChange>();
+    for( final IRecord point : points )
+    {
+      for( final IComponent property : properties )
+      {
+        final TupleResult result = point.getOwner();
+
+        if( result.hasComponent( property ) )
+        {
+          final Double oldValue = ProfilUtil.getDoubleValueFor( property.getId(), point );
+          final double newValue = oldValue.isNaN() ? operand : oldValue + operand;
+          changes.add( new PointPropertyEdit( point, property, newValue ) );
+        }
+        else
+          KalypsoModelWspmCorePlugin.getDefault().getLog().log( new Status( Status.CANCEL, KalypsoModelWspmCorePlugin.getID(),
+              Messages.getFormatString( "org.kalypso.model.wspm.core.util.pointpropertycalculator.AddFixValueOperator.0",property, point ) ) ); //$NON-NLS-1$
+      }
+    }
+    return changes.toArray( new IProfilChange[changes.size()] );
+  }
+
+}
