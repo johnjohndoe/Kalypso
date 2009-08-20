153c847e3529b419ae76f3b1ce7516758d87b710
donner <donner@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-08-20 15:00:30 +0000

    Kalypso 2.1.5.0      20. August 2009 ------------------------------------- 1) BUGFIX fuer negative Wasserspiegellagen bzw. negative GOK im Profil    (alle Berechnungen: Wasserspiegellage bis Polynomberechnungen)

--- _import/trunk/fortran/kalypso-1d/source/globale_definitionen.f90
+++ _import/trunk/fortran/kalypso-1d/source/globale_definitionen.f90
@@ -1,4 +1,4 @@
-!     Last change:  MD    7 Nov 2008   11:29 am
+!     Last change:  MD   20 Aug 2009    4:35 pm
 !--------------------------------------------------------------------------
 ! This code, globale_definitionen.f90, contains the shared memory modules
 ! and functions of the hydrodynamic modell for
@@ -42,8 +42,8 @@ implicit none
 
 save
 
-CHARACTER(LEN=29), parameter :: VERSIONNR   = ' KALYPSO-1D, VERSION 2.1.4.1'
-CHARACTER(LEN=17), parameter :: VERSIONDATE = 'Stand: 07.11.2008'
+CHARACTER(LEN=29), parameter :: VERSIONNR   = ' KALYPSO-1D, VERSION 2.1.5.0'
+CHARACTER(LEN=17), parameter :: VERSIONDATE = 'Stand: 20.08.2009'
 
 end module VERSION
 
