626851b34f09d596aa766cd2a4a816828abcf567
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-09-04 16:09:20 +0000

    Initializing Kalypso 2_2 branch

---
+++ branches/kalypso_2_2/KalypsoUIRRM/src/org/kalypso/ui/rrm/extension/KalypsoModuleRRM.java
@@ -0,0 +1,46 @@
+package org.kalypso.ui.rrm.extension;
+
+import org.kalypso.project.database.client.extension.IKalypsoModule;
+import org.kalypso.project.database.client.extension.database.IKalypsoModuleDatabaseSettings;
+import org.kalypso.project.database.client.extension.pages.module.IKalypsoModulePage;
+import org.kalypso.project.database.client.extension.pages.welcome.IKalypsoModuleWelcomePageFrame;
+
+public class KalypsoModuleRRM implements IKalypsoModule
+{
+  public static final String ID = "KalypsRrmModel"; //$NON-NLS-1$
+  // public constructor, needed because of declared extension point and java class loader
+  public KalypsoModuleRRM( )
+  {
+  }
+
+  @Override
+  public IKalypsoModuleWelcomePageFrame getWelcomePageFrame( )
+  {
+    return new KalypsoRrmWelcomePageFrame();
+  }
+
+  /**
+   * @see org.kalypso.kalypsosimulationmodel.extension.IKalypsoModule#getModuleEnteringPage()
+   */
+  @Override
+  public IKalypsoModulePage getModulePage( )
+  {
+    return new KalypsoRrmModulePage( this );
+  }
+
+  
+  /**
+   * @see org.kalypso.project.database.client.extension.IKalypsoModule#getRemoteDatabaseSettings()
+   */
+  @Override
+  public IKalypsoModuleDatabaseSettings getDatabaseSettings( )
+  {
+    return new KalypsoRrmRemoteDatabaseSettings();
+  }
+
+  @Override
+  public String getId( )
+  {
+    return ID;
+  }
+}
