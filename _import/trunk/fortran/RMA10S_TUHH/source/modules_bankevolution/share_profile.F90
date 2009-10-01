bde0cf51259f942cc746bc03b1c327a64fa4bde3
hasan <hasan@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-10-01 16:10:18 +0000

    add a save statement

--- _import/trunk/fortran/RMA10S_TUHH/source/modules_bankevolution/share_profile.F90
+++ _import/trunk/fortran/RMA10S_TUHH/source/modules_bankevolution/share_profile.F90
@@ -6,6 +6,6 @@ USE types
 ! INTEGER :: number_of_profiles
  TYPE (profile)            ,allocatable , DIMENSION (:)        :: old_pr !,old_profile
  TYPE (PROFILE )           ,allocatable , DIMENSION (:),TARGET :: BANKPROFILES
- TYPE (finite_element_node),allocatable , DIMENSION (:)        :: fenodes  ! it will conflicts with subroutines argument (fenodes) in bank evolution
+ TYPE (finite_element_node),allocatable , DIMENSION (:),save   :: fenodes  ! it will conflicts with subroutines argument (fenodes) in bank evolution
  logical                                                       :: BANKEVOLUTION
 end module
