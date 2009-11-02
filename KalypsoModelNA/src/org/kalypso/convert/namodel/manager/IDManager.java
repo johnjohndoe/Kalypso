cefc955e0ab8876143e36bd76e81e23b839f7b67
dejan <dejan@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2009-11-02 13:49:45 +0000

    Additional info for NA core debugging

--- KalypsoModelNA/src/org/kalypso/convert/namodel/manager/IDManager.java
+++ KalypsoModelNA/src/org/kalypso/convert/namodel/manager/IDManager.java
@@ -186,21 +186,22 @@ public class IDManager
       }
     } );
     sort.addAll( m_idMapFeature.keySet() );
+    writer.write( String.format( "%-10s%-6s%-16s %-32s %-32s %-32s\n\n", "ASCII ID", "", "GML Type", "GML ID", "GML Name", "GML Description" ) ); //$NON-NLS-1$
     for( Iterator<IDMap> iter = sort.iterator(); iter.hasNext(); )
     {
-      IDMap idmap = iter.next();
-      writer.write( idmap.toString() );
-      writer.write( "\t" ); //$NON-NLS-1$
+      final IDMap idmap = iter.next();
+      writer.write( String.format( "%-10s -->  ", idmap.toString() ) ); //$NON-NLS-1$
       final Object value = m_idMapFeature.get( idmap );
       if( value instanceof Feature )
       {
-        Feature feature = (Feature) value;
-//        feature.getFeatureType().getQName().getLocalPart().toString();
-        writer.write( feature.getId() );
+        final Feature feature = (Feature) value;
+        final String type = feature.getFeatureType().getQName().getLocalPart().toString();
+        writer.write( String.format( "%-16s %-32s %-32s %-32s\n", type, feature.getId(), feature.getName(), feature.getDescription() ) ); //$NON-NLS-1$
       }
       else
-        writer.write( "[control entry]" ); //$NON-NLS-1$
-      writer.write( "\n" ); //$NON-NLS-1$
+      {
+        writer.write( "[control entry]\n" ); //$NON-NLS-1$
+      }
     }
   }
 
