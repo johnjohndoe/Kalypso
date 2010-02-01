f14b041454791b24c58d5c3975da970db6958160
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2010-02-01 12:07:39 +0000

    [fix] NPE WspmImport/NewProject-Wizard

--- KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/extension/KalypsoWspmModulePage.java
+++ KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/extension/KalypsoWspmModulePage.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.wspm.tuhh.ui.extension;
 
@@ -44,6 +44,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.ui.PlatformUI;
 import org.kalypso.afgui.wizards.INewProjectWizard;
 import org.kalypso.model.wspm.tuhh.ui.KalypsoModelWspmTuhhUIPlugin;
 import org.kalypso.model.wspm.tuhh.ui.i18n.Messages;
@@ -78,7 +79,10 @@ public class KalypsoWspmModulePage extends AbstractKalypsoModulePage
   @Override
   public INewProjectWizard getDemoProjectWizard( )
   {
-    return new DemoProjectWizard();
+    final DemoProjectWizard wizard = new DemoProjectWizard();
+    wizard.init( PlatformUI.getWorkbench(), null );
+
+    return wizard;
   }
 
   /**
@@ -108,7 +112,10 @@ public class KalypsoWspmModulePage extends AbstractKalypsoModulePage
   @Override
   public IWizard getImportWizard( )
   {
-    return new NewProjectImportWspwinWizard();
+    final NewProjectImportWspwinWizard wizard = new NewProjectImportWspwinWizard();
+    wizard.init( PlatformUI.getWorkbench(), null );
+
+    return wizard;
   }
 
   @Override
@@ -123,6 +130,6 @@ public class KalypsoWspmModulePage extends AbstractKalypsoModulePage
     return 2;
   }
 
- 
+
 
 }
