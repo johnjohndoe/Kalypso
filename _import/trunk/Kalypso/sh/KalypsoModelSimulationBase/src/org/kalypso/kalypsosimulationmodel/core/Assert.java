97159c4e09e47820d40c8ee0b08f2a45c3bd4555
skurzbach <skurzbach@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2010-02-15 09:48:40 +0000

    clean up unused methods

--- _import/trunk/Kalypso/sh/KalypsoModelSimulationBase/src/org/kalypso/kalypsosimulationmodel/core/Assert.java
+++ _import/trunk/Kalypso/sh/KalypsoModelSimulationBase/src/org/kalypso/kalypsosimulationmodel/core/Assert.java
@@ -1,17 +1,10 @@
 package org.kalypso.kalypsosimulationmodel.core;
 
-import java.io.File;
-import java.util.Collection;
-
-import javax.xml.namespace.QName;
-
-import org.kalypso.afgui.model.Util;
 import org.kalypso.kalypsosimulationmodel.i18n.Messages;
-import org.kalypsodeegree.model.feature.Feature;
 
 /**
  * Provides assertion methods
- *
+ * 
  * @author Patrice Congo
  */
 public class Assert
@@ -24,13 +17,13 @@ public class Assert
   /**
    * Assert the given object for null value. This method throws concequently an illegal argument exception if the passed
    * object is null
-   *
+   * 
    * @param obj
-   *            the object to be asserted
+   *          the object to be asserted
    * @param message
-   *            the exception message
+   *          the exception message
    * @throws IllegalArgumentException
-   *             if the passed object is null
+   *           if the passed object is null
    */
   public static final void throwIAEOnNull( final Object obj, final String message ) throws IllegalArgumentException
   {
@@ -43,13 +36,13 @@ public class Assert
   /**
    * Assert the given object for null value. This method throws concequently an illegal argument exception if the passed
    * object is null
-   *
+   * 
    * @param obj
-   *            the object to be asserted
+   *          the object to be asserted
    * @param message
-   *            the exception message
+   *          the exception message
    * @throws IllegalArgumentException
-   *             if the passed object is null
+   *           if the passed object is null
    */
   public static final void throwIAEOnNullParam( final Object param, final String paramName ) throws IllegalArgumentException
   {
@@ -60,53 +53,6 @@ public class Assert
       buf.append( paramName );
       throw new IllegalArgumentException( buf.toString() );
     }
-  }
-
-  /**
-   * Assert the given object for null value. This method throws concequently an illegal argument exception if the passed
-   * object is null
-   *
-   * @param obj
-   *            the object to be asserted
-   * @param message
-   *            the exception message
-   * @throws IllegalArgumentException
-   *             if the passed object is null
-   */
-  public static final void throwIAEOnCollectionNullOrHasNullElements( final Collection param, final String paramName ) throws IllegalArgumentException
-  {
-    if( param == null )
-    {
-      final StringBuffer buf = new StringBuffer( 128 );
-      buf.append( Messages.getString( "org.kalypso.kalypsosimulationmodel.core.Assert.1" ) ); //$NON-NLS-1$
-      buf.append( paramName );
-      throw new IllegalArgumentException( buf.toString() );
-    }
-
-    for( final Object ele : param )
-    {
-      if( ele == null )
-      {
-        final String message = Messages.getString( "org.kalypso.kalypsosimulationmodel.core.Assert.2"  + Messages.getString("org.kalypso.kalypsosimulationmodel.core.Assert.3"), paramName, param ); //$NON-NLS-1$ //$NON-NLS-2$
-        throw new IllegalArgumentException( message );
-      }
-    }
-  }
-
-  public static final void throwIAEOnCollectionNullOrEmpty( final Collection param, final String paramName ) throws IllegalArgumentException
-  {
-    if( param == null )
-    {
-      final String message =  Messages.getString("org.kalypso.kalypsosimulationmodel.core.Assert.4", paramName ); //$NON-NLS-1$
-      throw new IllegalArgumentException( message );
-    }
-
-    if( param.isEmpty() )
-    {
-      final String message =  Messages.getString("org.kalypso.kalypsosimulationmodel.core.Assert.5", paramName ); //$NON-NLS-1$
-      throw new IllegalArgumentException( message );
-    }
-
   }
 
   public static final String throwIAEOnNullOrEmpty( String str ) throws IllegalArgumentException
@@ -147,38 +93,5 @@ public class Assert
       }
       throw new IllegalArgumentException( message );
     }
-  }
-
-  public static final void throwIAEOnNotDirectInstanceOf( final Feature feature, final QName expectedType ) throws IllegalArgumentException
-  {
-    if( !Util.directInstanceOf( feature, expectedType ) )
-    {
-      throw new IllegalArgumentException( Messages.getString( "org.kalypso.kalypsosimulationmodel.core.Assert.12" ) + //$NON-NLS-1$
-          expectedType + Messages.getString( "org.kalypso.kalypsosimulationmodel.core.Assert.13" ) + feature.getFeatureType().getQName() ); //$NON-NLS-1$
-    }
-  }
-
-  public static final void throwIAEOnNulOrIsDirOrNotExistsOrNotReadable( final File file ) throws IllegalArgumentException
-  {
-    if( file == null )
-    {
-      throw new IllegalArgumentException( Messages.getString( "org.kalypso.kalypsosimulationmodel.core.Assert.14" ) ); //$NON-NLS-1$
-    }
-
-    if( file.isDirectory() )
-    {
-      throw new IllegalArgumentException( Messages.getString( "org.kalypso.kalypsosimulationmodel.core.Assert.15" ) + file ); //$NON-NLS-1$
-    }
-
-    if( !file.exists() )
-    {
-      throw new IllegalArgumentException( Messages.getString( "org.kalypso.kalypsosimulationmodel.core.Assert.16" ) + file ); //$NON-NLS-1$
-    }
-
-    if( !file.canRead() )
-    {
-      throw new IllegalArgumentException( Messages.getString( "org.kalypso.kalypsosimulationmodel.core.Assert.17" ) + file ); //$NON-NLS-1$
-    }
-
   }
 }
