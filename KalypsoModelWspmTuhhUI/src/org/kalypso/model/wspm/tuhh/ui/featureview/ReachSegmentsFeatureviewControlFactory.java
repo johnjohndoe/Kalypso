5aaf4b1c8dd335c3745f9b83ce1c1709f1b46518
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2010-02-18 14:25:00 +0000

    Introduced new feature control factory in order to refaktor FeatureComposite. Moved lots of code into the new factories.

--- KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/featureview/ReachSegmentsFeatureviewControlFactory.java
+++ KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/featureview/ReachSegmentsFeatureviewControlFactory.java
@@ -44,13 +44,13 @@ import java.util.Properties;
 
 import org.kalypso.gmlschema.property.IPropertyType;
 import org.kalypso.ogc.gml.featureview.control.IFeatureControl;
-import org.kalypso.ogc.gml.featureview.control.IFeatureviewControlFactory;
+import org.kalypso.ogc.gml.featureview.control.IExtensionsFeatureControlFactory;
 import org.kalypsodeegree.model.feature.Feature;
 
 /**
  * @author Gernot Belger
  */
-public class ReachSegmentsFeatureviewControlFactory implements IFeatureviewControlFactory
+public class ReachSegmentsFeatureviewControlFactory implements IExtensionsFeatureControlFactory
 {
   /**
    * @see org.kalypso.ogc.gml.featureview.control.IFeatureviewControlFactory#createFeatureControl(org.kalypsodeegree.model.feature.Feature,
