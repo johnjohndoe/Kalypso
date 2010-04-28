4c91e56f71cee74b86943653d88505fc78989cc6
nico <nico@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2010-04-28 13:02:29 +0000

    Clear first Setup

--- _import/trunk/fortran/HydroLib/FlowResistance/FlowResistances/source/globalconstants.f90
+++
@@ -1,6 +0,0 @@
-module globalconstants
-REAL (KIND = 8), parameter :: g = 9.81d0      !Erdbeschleunigung
-REAL (KIND = 8), parameter :: rho = 1000.0d0  !Dichte des Wassers
-REAL (KIND = 8), parameter :: nu_10 =  1.3e-06 ! viscosity of water for 10 degrees.
-Real (KIND = 8), parameter :: Pi = 3.1415926535897932384626433d0
-end module
