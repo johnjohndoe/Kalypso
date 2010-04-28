ddad9dd4859b10e5055f5b8ac82b8bc1b955b8c9
riedel <riedel@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2010-04-28 10:17:17 +0000

    Initial setup of HydroLib Sources

---
+++ _import/trunk/fortran/FlowResistance/source/Rauheiten_1_0.f90
@@ -0,0 +1,16 @@
+!  Rauheiten_1_0.f90 
+!
+!  FUNCTIONS/SUBROUTINES exported from Rauheiten_1_0.dll:
+!  Rauheiten_1_0 - subroutine 
+!
+subroutine Rauheiten_1_0
+
+  ! Expose subroutine Rauheiten_1_0 to users of this DLL
+  !
+  !DEC$ ATTRIBUTES DLLEXPORT::Rauheiten_1_0
+
+  ! Variables
+
+ ! Body of Rauheiten_1_0
+
+end subroutine Rauheiten_1_0
