303f43da0d6e58c2d8f7625d8054850aaaabb0b9
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2010-04-29 16:49:05 +0000

    Tag for 2.2 stable release.

---
+++ _import/tags/Kalypso_2.2.0.20100426/KalypsoModelSimulationBase/src/org/kalypso/kalypsosimulationmodel/KalypsoModelSimulationBase.java
@@ -0,0 +1,8 @@
+package org.kalypso.kalypsosimulationmodel;
+
+import org.eclipse.core.runtime.Plugin;
+
+public class KalypsoModelSimulationBase extends Plugin
+{
+  public static String ID = "org.kalypso.ModelSimulationBase"; //$NON-NLS-1$
+}
