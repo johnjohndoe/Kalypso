995f3ad3ba5f3d8de654967d88afda9f5690b397
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2010-04-29 16:49:19 +0000

    Tag for 2.2 stable release.

---
+++ _import/tags/Kalypso_2.2.0.20100426/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/ui/map/flowrel/wizardPageZmlImportWithPreview/FileTreeContentProvider.java
@@ -0,0 +1,38 @@
+package org.kalypso.kalypsomodel1d2d.ui.map.flowrel.wizardPageZmlImportWithPreview;
+
+import java.io.File;
+
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+public class FileTreeContentProvider implements ITreeContentProvider
+{
+  public Object[] getChildren(Object element)
+  {
+    Object[] kids = ((File) element).listFiles();
+    return kids == null ? new Object[0] : kids;
+  }
+
+  public Object[] getElements(Object element)
+  {
+    return getChildren(element);
+  }
+
+  public boolean hasChildren(Object element)
+  {
+    return getChildren(element).length > 0;
+  }
+
+  public Object getParent(Object element)
+  {
+    return ((File)element).getParent();
+  }
+  
+  public void dispose()
+  {
+  }
+
+  public void inputChanged(Viewer viewer, Object old_input, Object new_input)
+  {
+  }
+}
