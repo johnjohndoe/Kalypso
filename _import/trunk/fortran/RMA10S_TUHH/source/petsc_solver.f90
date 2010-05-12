acfd4d6426c9099106eda0467a342b78214ccb1f
skurzbach <skurzbach@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2010-05-12 13:12:12 +0000

    from cnodix

--- _import/trunk/fortran/RMA10S_TUHH/source/petsc_solver.f90
+++ _import/trunk/fortran/RMA10S_TUHH/source/petsc_solver.f90
@@ -1,11 +1,11 @@
-subroutine petsc_solver (noOfEq, Apetsc, bpetsc, x)
-  USE petsc
-  USE petscvec
-  USE petscmat
-  USE petscksp
-  USE petscpc
-  USE petscsnes
-  use PardisoParams, only: mfwsiz
+subroutine petsc_solver (nszf, Apetsc, bpetsc, x)
+USE petsc
+USE petscvec
+USE petscmat
+USE petscksp
+USE petscpc
+USE petscsnes
+USE PardisoParams, only: mfwsiz
 
 implicit none
 !
@@ -31,57 +31,48 @@ implicit none
 
 !arguments
 !---------------
-integer (kind = 4), intent (inout) :: noOfEq
-!integer (kind = 4) :: irow(*), icol(*)
-!real (kind = 8) :: a(*), b(*), x(*)
-real (kind = 8), intent (inout) :: x(*)
-real (kind = 8), allocatable :: x_buf(:)
+integer (kind = 4), intent (inout)  :: nszf
+Vec, intent (inout)                 :: bpetsc
+Mat, intent (inout)                 :: Apetsc
+real (kind = 8), intent (inout)     :: x(*)
 
 !local variables
 !---------------
+integer (kind = 4) :: i
+integer (kind = 4), save :: nszfold
 
 !PETSC variables
-PetscErrorCode ierr
-PetscMPIInt     rank,size
-Vec         xpetsc,xlocalpetsc
-Vec, intent (inout) :: bpetsc
+PetscErrorCode       :: ierr
+Vec                  :: xpetsc, xlocalpetsc
 PetscScalar, pointer :: xx_v(:)
-PetscInt, pointer, save :: nnz(:)
-PetscInt noOfEqLocal, eqStart, eqEnd
-VecScatter vscat
-PetscTruth checkequal
-IS ix, ixlocal
-Mat, intent (inout) ::      Apetsc
-KSP ::      ksp
+VecScatter           :: vscat
+KSP, save            :: ksp
 
-!dimension of the equation system
-integer (kind = 8) :: nonZeros
+data nszfold/0/
 
-!execution and control of the routine
-integer (kind = 4), save :: nzzold = 0, nszfold = 0
-integer (kind = 8) :: nsort, maxnnz
-integer (kind = 4), allocatable :: jalocal(:)
-
-!counters
-integer (kind = 4) :: i, k, kk
-
-  call VecCreateSeq(PETSC_COMM_SELF,noOfEq,xlocalpetsc,ierr)
-  call VecDuplicate(bpetsc,xpetsc,ierr)
-  call VecGetOwnershipRange(xpetsc,eqStart,eqEnd,ierr)
-  call VecGetLocalSize(xpetsc,noOfEqLocal,ierr)
-
-  call KSPCreate(PETSC_COMM_WORLD,ksp,ierr)
-  call KSPSetOperators(ksp,Apetsc,Apetsc,DIFFERENT_NONZERO_PATTERN,ierr)
-  call KSPSetFromOptions(ksp, ierr)
-
+call VecCreateSeq(PETSC_COMM_SELF,nszf,xlocalpetsc,ierr)
+call VecDuplicate(bpetsc,xpetsc,ierr)
 ! initialize result vector to all zeros
-call VecSet(xpetsc,0,ierr)
-call KSPSolve(ksp,bpetsc,xpetsc,ierr)
+call VecZeroEntries(xpetsc,ierr)
 
+if (nszfold /= nszf) then
+    write(*,*) 'Creating new solver context'
+
+    if(nszfold /= 0) then
+		call KSPDestroy(ksp,ierr)
+	endif
+
+	call KSPCreate(PETSC_COMM_WORLD,ksp,ierr)
+	call KSPSetFromOptions(ksp, ierr)
+endif
+
+call KSPSetOperators(ksp,Apetsc,Apetsc,SAME_NONZERO_PATTERN,ierr)
+call KSPSetUp(ksp,ierr)
+call KSPSolve(ksp,bpetsc,xpetsc,ierr)
 
 if ( ierr .ne. 0 ) then
   call ErrorMessageAndStop (4203, ierr, 0.0d0, 0.0d0)
-endif
+end if
 
 call VecScatterCreateToAll(xpetsc,vscat,xlocalpetsc,ierr)
 call VecScatterBegin(vscat,xpetsc,xlocalpetsc,INSERT_VALUES,SCATTER_FORWARD,ierr)
@@ -89,13 +80,14 @@ call VecScatterEnd(vscat,xpetsc,xlocalpetsc,INSERT_VALUES,SCATTER_FORWARD,ierr)
 call VecScatterDestroy(vscat,ierr)
 
 call VecGetArrayF90(xlocalpetsc,xx_v,ierr)
-do i=1,noOfEq
+do i=1,nszf
   x(i) = xx_v(i)
-enddo
+end do
 call VecRestoreArrayF90(xlocalpetsc,xx_v,ierr)
+
+nszfold = nszf
 
 call VecDestroy(xpetsc,ierr)
 call VecDestroy(xlocalpetsc,ierr)
-call KSPDestroy(ksp,ierr)
 
 end subroutine petsc_solver
