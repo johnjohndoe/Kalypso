e69c9dfb9a8b53c01fb11c281e74c4320893d8c8
ilya <ilya@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2010-06-29 12:55:56 +0000

    ChooseExeControlFactory + controls are moved to the KalypsoModelSimulationBase plugin

--- KalypsoModelSimulationBase/src/org/kalypso/kalypsosimulationmodel/ui/calccore/ChooseAdditionalFileControl.java
+++ KalypsoModelSimulationBase/src/org/kalypso/kalypsosimulationmodel/ui/calccore/ChooseAdditionalFileControl.java
@@ -38,27 +38,25 @@
  *  v.doemming@tuhh.de
  *   
  *  ---------------------------------------------------------------------------*/
-package org.kalypso.kalypsomodel1d2d.ui.featurecontrols;
+package org.kalypso.kalypsosimulationmodel.ui.calccore;
 
 import java.io.File;
-import java.io.FilenameFilter;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.net.MalformedURLException;
 
-import org.apache.commons.lang.ObjectUtils;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.window.Window;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.dialogs.ElementListSelectionDialog;
 import org.kalypso.gmlschema.property.IPropertyType;
-import org.kalypso.kalypsomodel1d2d.ui.i18n.Messages;
+import org.kalypso.kalypsosimulationmodel.i18n.Messages;
 import org.kalypso.ogc.gml.command.ChangeFeatureCommand;
 import org.kalypso.ogc.gml.featureview.control.AbstractFeatureControl;
 import org.kalypso.ogc.gml.featureview.control.IFeatureControl;
@@ -66,20 +64,24 @@ import org.kalypsodeegree.model.feature.Feature;
 
 /**
  * @author belger
+ * @author ig
  * 
  */
-public class ChooseExeControl extends AbstractFeatureControl implements IFeatureControl
+public class ChooseAdditionalFileControl extends AbstractFeatureControl implements IFeatureControl
 {
-  private final File m_exeDir;
+  private File m_file;
 
-  private final String m_exePattern;
+  @SuppressWarnings("unused")
+  private final File m_dir;
 
-  public ChooseExeControl( final Feature feature, final IPropertyType ftp, final File exeDir, final String exePattern )
+  private final String m_fileExt;
+
+  public ChooseAdditionalFileControl( final Feature feature, final IPropertyType ftp, final File dir, final String fileExt )
   {
     super( feature, ftp );
-
-    m_exeDir = exeDir;
-    m_exePattern = exePattern;
+    m_dir = dir;
+    m_fileExt = fileExt;
+    m_file = new File( (String) feature.getProperty( ftp ) );
   }
 
   /**
@@ -95,7 +97,7 @@ public class ChooseExeControl extends AbstractFeatureControl implements IFeature
   public Control createControl( final Composite parent, final int style )
   {
     final Button button = new Button( parent, style );
-    button.setText( Messages.getString("org.kalypso.kalypsomodel1d2d.ui.featurecontrols.ChooseExeControl.0") ); //$NON-NLS-1$
+    button.setText( Messages.getString("org.kalypso.ogc.gml.featureview.control.ChooseExeControl.0") ); //$NON-NLS-1$
     button.addSelectionListener( new SelectionAdapter()
     {
       /**
@@ -111,78 +113,70 @@ public class ChooseExeControl extends AbstractFeatureControl implements IFeature
     return button;
   }
 
-  protected void handleButtonPressed( final SelectionEvent e )
+  /**
+   * Answer the source file location or <code>null</code> if unspecified
+   */
+  public IPath getSourceLocation( )
+  {
+    final String text = m_file == null? "": m_file.getAbsoluteFile().toString(); //$NON-NLS-1$
+    if( text.length() == 0 )
+      return null;
+    final IPath path = new Path( text );
+    if( !path.isAbsolute() )
+      return ResourcesPlugin.getWorkspace().getRoot().getLocation().append( path );
+    else
+      return path;
+  }
+  
+  /**
+   * Open a file browser dialog to locate a source file
+   */
+  protected void browseForSourceFile( final SelectionEvent e )
   {
     final Shell shell = e.display.getActiveShell();
-
-    // find all possible exes
-    final Pattern pattern = Pattern.compile( m_exePattern, Pattern.CASE_INSENSITIVE );
-    final File[] exeFiles = m_exeDir.listFiles( new FilenameFilter()
-    {
-      public boolean accept( final File dir, final String name )
-      {
-        return pattern.matcher( name ).matches();
-      }
-    } );
-
-    if( exeFiles == null || exeFiles.length == 0 )
-    {
-      final String msg = Messages.getString("org.kalypso.kalypsomodel1d2d.ui.featurecontrols.ChooseExeControl.1", m_exeDir.getAbsolutePath(), m_exePattern ); //$NON-NLS-1$
-      MessageDialog.openWarning( shell, Messages.getString("org.kalypso.kalypsomodel1d2d.ui.featurecontrols.ChooseExeControl.2"), msg ); //$NON-NLS-1$
+    IPath path = browse( shell, getSourceLocation() );
+    if( path == null )
       return;
-    }
+    final IPath rootLoc = ResourcesPlugin.getWorkspace().getRoot().getLocation();
+    if( rootLoc.isPrefixOf( path ) )
+      path = path.setDevice( null ).removeFirstSegments( rootLoc.segmentCount() );
+    m_file = new File( path.toString() );
 
-    // Find the currently chosen file, if any
-    final String oldVersion = (String) getFeature().getProperty( getFeatureTypeProperty() );
-    File selectedFile = null;
-    for( final File file : exeFiles )
-    {
-      final String newVersion = versionFromFile( pattern, file );
-      if( ObjectUtils.equals( oldVersion, newVersion ) )
-      {
-        selectedFile = file;
-        break;
-      }
-    }
-
-    // let user choose a new version/file
-    final ElementListSelectionDialog dialog = new ElementListSelectionDialog( shell, new LabelProvider()
-    {
-      /**
-       * @see org.eclipse.jface.viewers.LabelProvider#getText(java.lang.Object)
-       */
-      @Override
-      public String getText( Object element )
-      {
-        return versionFromFile( pattern, (File) element );
-      }
-    } );
-    dialog.setMultipleSelection( false );
-    dialog.setAllowDuplicates( false );
-    dialog.setElements( exeFiles );
-    dialog.setMessage( Messages.getString("org.kalypso.kalypsomodel1d2d.ui.featurecontrols.ChooseExeControl.3") ); //$NON-NLS-1$
-    dialog.setTitle( Messages.getString("org.kalypso.kalypsomodel1d2d.ui.featurecontrols.ChooseExeControl.4") ); //$NON-NLS-1$
-    if( selectedFile != null )
-      dialog.setInitialSelections( new File[] { selectedFile } );
-    if( dialog.open() != Window.OK )
-      return;
-
-    final Object[] result = dialog.getResult();
-    if( result.length == 0 )
-      return;
-
-    final File chosenFile = (File) result[0];
-    final String newVersion = versionFromFile( pattern, chosenFile );
-
-    final ChangeFeatureCommand command = new ChangeFeatureCommand( getFeature(), getFeatureTypeProperty(), newVersion );
-    fireFeatureChange( command );
   }
 
-  protected String versionFromFile( final Pattern pattern, final File file )
+  private IPath browse( final Shell shell, final IPath path )
   {
-    final Matcher matcher = pattern.matcher( file.getName() );
-    matcher.matches();
-    return matcher.group( 1 );
+    final FileDialog dialog = new FileDialog( shell, SWT.OPEN );
+    dialog.setFilterExtensions( new String[]{ m_fileExt } );
+    if( path != null )
+    {
+      if( path.segmentCount() > 1 )
+        dialog.setFilterPath( path.removeLastSegments( 1 ).toOSString() );
+      if( path.segmentCount() > 0 )
+        dialog.setFileName( path.lastSegment() );
+    }
+    final String result = dialog.open();
+    if( result == null )
+      return null;
+    return new Path( result );
+  }
+
+  
+  protected void handleButtonPressed( final SelectionEvent e )
+  {
+    browseForSourceFile( e );
+    String newFile = null;
+    try
+    {
+      newFile = m_file.toURI().toURL().toExternalForm();
+    }
+    catch( MalformedURLException e1 )
+    {
+      e1.printStackTrace();
+    } 
+
+    final ChangeFeatureCommand command = new ChangeFeatureCommand( getFeature(), getFeatureTypeProperty(), newFile );
+    fireFeatureChange( command );
   }
 
   /**
