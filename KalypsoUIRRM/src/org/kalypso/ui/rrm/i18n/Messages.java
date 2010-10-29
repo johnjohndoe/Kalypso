5739a97db169f989a929c74bd580c708b39abbc5
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2010-10-29 16:34:58 +0000

    Fixed: message class must contain package name in order to let eclipse recognize it...

--- KalypsoUIRRM/src/org/kalypso/ui/rrm/i18n/Messages.java
+++ KalypsoUIRRM/src/org/kalypso/ui/rrm/i18n/Messages.java
@@ -49,7 +49,7 @@ import org.kalypso.contribs.java.i18n.I18nUtils;
  */
 public class Messages
 {
-  private static final String BUNDLE_NAME = Messages.class.getPackage().getName() + ".messages"; //$NON-NLS-1$
+  private static final String BUNDLE_NAME = "org.kalypso.ui.rrm.i18n.messages"; //$NON-NLS-1$
 
   private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle( BUNDLE_NAME );
 
