0553113c1c938d7093e8134b002f1d77de0c8f62
dejan <dejan@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2010-11-08 17:11:42 +0000

    GUI support for defining sea evaporation params (for NA core calculation)

--- trunk/KalypsoModelNA/src/org/kalypso/model/hydrology/binding/model/StorageChannel.java
+++ trunk/KalypsoModelNA/src/org/kalypso/model/hydrology/binding/model/StorageChannel.java
@@ -72,6 +72,12 @@ public class StorageChannel extends Channel
 
   private static final QName PROP_SEA_EVAPORATION_FACTOR = new QName( NS_NAMODELL, "faktorSeaEvaporation" ); //$NON-NLS-1$
 
+  private static final QName PROP_INITIAL_CAPACITY = new QName( NS_NAMODELL, "sv" ); //$NON-NLS-1$
+
+  private static final QName PROP_VOLUME_MAX = new QName( NS_NAMODELL, "vmax" ); //$NON-NLS-1$
+
+  private static final QName PROP_VOLUME_MIN = new QName( NS_NAMODELL, "vmin" ); //$NON-NLS-1$
+
   public StorageChannel( final Object parent, final IRelationType parentRelation, final IFeatureType ft, final String id, final Object[] propValues )
   {
     super( parent, parentRelation, ft, id, propValues );
@@ -115,6 +121,21 @@ public class StorageChannel extends Channel
   public double getSeaEvaporationFactor( )
   {
     return getDoubleProperty( PROP_SEA_EVAPORATION_FACTOR, 1.0 );
+  }
+
+  public double getInitialCapacity( )
+  {
+    return getDoubleProperty( PROP_INITIAL_CAPACITY, 0.0 );
+  }
+
+  public double getVolumeMax( )
+  {
+    return getDoubleProperty( PROP_VOLUME_MAX, 0.0 );
+  }
+
+  public double getVolumeMin( )
+  {
+    return getDoubleProperty( PROP_VOLUME_MIN, 0.0 );
   }
 
 }
