7d4e9bbe0c98360295c4ca44dd80f84e4cc9c91d
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2010-12-23 09:30:36 +0000

    Tag vor release of kalypso_10.10.1.20101222

---
+++ KalypsoModelWspmTuhhCore/src/org/kalypso/model/wspm/tuhh/core/profile/buildings/BuildingUtil.java
@@ -0,0 +1,73 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.model.wspm.tuhh.core.profile.buildings;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.kalypso.model.wspm.core.KalypsoModelWspmCorePlugin;
+import org.kalypso.observation.result.IComponent;
+
+/**
+ * @author Dirk Kuch
+ */
+public class BuildingUtil
+{
+  public static double getDoubleValueFor( final String componentID, final IProfileBuilding building )
+  {
+    final IComponent property = building.getObjectProperty( componentID );
+
+    try
+    {
+      final Object value = building.getValue( property );
+      if( value == null )
+        return Double.NaN;
+      if( value instanceof Double )
+        return (Double) value;
+      return Double.NaN;
+    }
+    catch( final IllegalArgumentException e )
+    {
+      KalypsoModelWspmCorePlugin.getDefault().getLog().log( new Status( IStatus.ERROR, componentID, e.getLocalizedMessage(), null ) );
+      return Double.NaN;
+    }
+
+  }
+}
