1c2b943f93b55a356e08d571e1c15c200bdd3d09
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2011-02-23 11:55:43 +0000

    Introduced IRepositoryItemVisitor
    NEW - # 729: ungereimtheiten in der lokalen datenhaltung beseitigen 
    https://dev.bjoernsen.de/prognose/ticket/729

--- KalypsoAdapterDatacenter/src/org/kalypso/dcadapter/DataCenterLevelItem.java
+++ KalypsoAdapterDatacenter/src/org/kalypso/dcadapter/DataCenterLevelItem.java
@@ -6,6 +6,8 @@ import java.util.Iterator;
 
 import org.kalypso.repository.IRepository;
 import org.kalypso.repository.IRepositoryItem;
+import org.kalypso.repository.IRepositoryItemVisitor;
+import org.kalypso.repository.RepositoryException;
 
 import com.bce.datacenter.db.common.Level;
 import com.bce.datacenter.db.timeseries.Channel;
@@ -134,5 +136,18 @@ public class DataCenterLevelItem implements IRepositoryItem
   public boolean isMultipleSourceItem( )
   {
     return false;
+  }
+
+  /**
+   * @see org.kalypso.repository.IRepositoryItem#accept(org.kalypso.repository.IRepositoryItemVisitor)
+   */
+  @Override
+  public void accept( final IRepositoryItemVisitor visitor ) throws RepositoryException
+  {
+    final IRepositoryItem[] children = getChildren();
+    for( final IRepositoryItem child : children )
+    {
+      visitor.visit( child );
+    }
   }
 }
