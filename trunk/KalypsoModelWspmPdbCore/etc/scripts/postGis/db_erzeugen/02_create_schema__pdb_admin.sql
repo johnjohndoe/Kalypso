e48e7c6f5c5ff0a210510a4fa302def0693e173c
monika.thuel <monika.thuel@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2011-06-09 14:21:01 +0000

    PDB: auch DB Skripte gehören in die Source-Code-Verwaltung

---
+++ trunk/KalypsoModelWspmPdbCore/etc/scripts/postGis/db_erzeugen/02_create_schema__pdb_admin.sql
@@ -0,0 +1,2 @@
+CREATE SCHEMA pdb_admin
+  AUTHORIZATION pdb_admin;
---
+++ trunk/KalypsoModelWspmPdbCore/etc/scripts/postGis/db_erzeugen/05_register_geometries.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO geometry_columns(f_table_catalog, f_table_schema, f_table_name, f_geometry_column, coord_dimension, srid, "type")
+VALUES ( '', 'pdb_admin', 'cross_section', 'line', 3, 31468, 'LINESTRING' );
+
+INSERT INTO geometry_columns(f_table_catalog, f_table_schema, f_table_name, f_geometry_column, coord_dimension, srid, "type")
+VALUES ( '', 'pdb_admin', 'cross_section_part', 'line', 3, 31468, 'LINESTRING' );
+
+INSERT INTO geometry_columns(f_table_catalog, f_table_schema, f_table_name, f_geometry_column, coord_dimension, srid, "type")
+VALUES ( '', 'pdb_admin', 'point', 'location', 3, 31468, 'POINT' );
+
+INSERT INTO geometry_columns(f_table_catalog, f_table_schema, f_table_name, f_geometry_column, coord_dimension, srid, "type")
+VALUES ( '', 'pdb_admin', 'water_body', 'riverline', 3, 31468, 'LINESTRING' );
+
+INSERT INTO geometry_columns(f_table_catalog, f_table_schema, f_table_name, f_geometry_column, coord_dimension, srid, "type")
+VALUES ( '', 'pdb_admin', 'waterlevel_fixation', 'location', 3, 31468, 'POINT' );
