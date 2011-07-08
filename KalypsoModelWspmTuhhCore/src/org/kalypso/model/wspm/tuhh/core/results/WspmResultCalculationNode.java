2120810300757f95577a7bc99e46a7d56c4cc974
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2011-07-08 09:02:06 +0000

    [fix] KalypsoWspm - Theme Water Level - Panel - duplicated wspm result nodes in tree

--- KalypsoModelWspmTuhhCore/src/org/kalypso/model/wspm/tuhh/core/results/WspmResultCalculationNode.java
+++ KalypsoModelWspmTuhhCore/src/org/kalypso/model/wspm/tuhh/core/results/WspmResultCalculationNode.java
@@ -41,8 +41,8 @@
 package org.kalypso.model.wspm.tuhh.core.results;
 
 import java.io.FileFilter;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import org.apache.commons.io.filefilter.NameFileFilter;
 import org.apache.commons.lang.ArrayUtils;
@@ -50,11 +50,13 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.kalypso.commons.java.lang.Objects;
+import org.kalypso.commons.resources.FileUtilities;
 import org.kalypso.contribs.eclipse.core.resources.CollectFolderVisitor;
 import org.kalypso.contribs.eclipse.core.resources.FileFilterVisitor;
 import org.kalypso.contribs.eclipse.core.resources.ResourceUtilities;
+import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
 import org.kalypso.model.wspm.tuhh.core.IWspmTuhhConstants;
 import org.kalypso.model.wspm.tuhh.core.KalypsoModelWspmTuhhCorePlugin;
 import org.kalypso.model.wspm.tuhh.core.gml.CalculationWspmTuhhSteadyState;
@@ -82,44 +84,73 @@ public class WspmResultCalculationNode extends AbstractWspmResultNode implements
   @Override
   public IWspmResultNode[] getChildResults( )
   {
-    final Collection<IWspmResultNode> result = new ArrayList<IWspmResultNode>();
-
     final IFolder resultFolder = findResultFolder( m_calculation );
-    if( resultFolder != null )
-    {
-      try
-      {
-        /* Collect all results with length sections. */
-        final FileFilter filter = new NameFileFilter( IWspmTuhhConstants.FILE_RESULT_LENGTH_SECTION_GML );
-        final FileFilterVisitor visitor = new FileFilterVisitor( filter );
-        resultFolder.accept( visitor );
-        final IFile[] files = visitor.getFiles();
-        for( final IFile file : files )
-        {
-          final String label = file.getParent().getParent().getName();
-          result.add( new WspmResultContainer( this, file, label ) );
-        }
+    if( Objects.isNull( resultFolder ) )
+      return new IWspmResultNode[] {};
 
-        /* Collect all folders. */
-        final CollectFolderVisitor dirVisitor = new CollectFolderVisitor( new IFolder[] { resultFolder.getFolder( ".svn" ) } ); //$NON-NLS-1$
-        resultFolder.accept( dirVisitor, IResource.DEPTH_ONE, false );
-        final IFolder[] folders = dirVisitor.getFolders();
-        for( final IFolder folder : folders )
-        {
-          if( !resultFolder.equals( folder ) )
-          {
-            final WspmResultFolderNode folderNode = new WspmResultFolderNode( this, folder );
-            result.add( folderNode );
-          }
-        }
-      }
-      catch( final CoreException e )
+    final Set<IWspmResultNode> result = new LinkedHashSet<IWspmResultNode>();
+
+    /* Collect all results with length sections. */
+    try
+    {
+      final FileFilter filter = new NameFileFilter( IWspmTuhhConstants.FILE_RESULT_LENGTH_SECTION_GML );
+      final FileFilterVisitor visitor = new FileFilterVisitor( filter );
+      resultFolder.accept( visitor );
+
+      final IFile[] files = visitor.getFiles();
+      for( final IFile file : files )
       {
-        KalypsoModelWspmTuhhCorePlugin.getDefault().getLog().log( e.getStatus() );
+        final String label = file.getParent().getParent().getName();
+        result.add( new WspmResultContainer( this, file, label ) );
       }
+    }
+    catch( final Throwable t )
+    {
+      KalypsoModelWspmTuhhCorePlugin.getDefault().getLog().log( StatusUtilities.statusFromThrowable( t ) );
+    }
+
+    /* Collect all folders. */
+    try
+    {
+      final CollectFolderVisitor dirVisitor = new CollectFolderVisitor( new IFolder[] { resultFolder.getFolder( ".svn" ) } ); //$NON-NLS-1$
+      resultFolder.accept( dirVisitor, IResource.DEPTH_ONE, false );
+
+      final IFolder[] folders = dirVisitor.getFolders();
+      for( final IFolder folder : folders )
+      {
+        if( resultFolder.equals( folder ) )
+          continue;
+        else if( wasAdded( result, folder ) )
+          continue;
+
+        final WspmResultFolderNode folderNode = new WspmResultFolderNode( this, folder );
+        result.add( folderNode );
+      }
+    }
+    catch( final Throwable t )
+    {
+      KalypsoModelWspmTuhhCorePlugin.getDefault().getLog().log( StatusUtilities.statusFromThrowable( t ) );
     }
 
     return result.toArray( new IWspmResultNode[result.size()] );
+  }
+
+  private boolean wasAdded( final Set<IWspmResultNode> result, final IFolder current )
+  {
+    for( final IWspmResultNode node : result )
+    {
+      if( !(node instanceof WspmResultContainer) )
+        continue;
+
+      final WspmResultContainer container = (WspmResultContainer) node;
+      final IFile lengthsection = container.getLengthSectionFile();
+      if( current.equals( lengthsection ) )
+        return true;
+
+      return FileUtilities.isParent( current, lengthsection );
+    }
+
+    return false;
   }
 
   public static IFolder findResultFolder( final TuhhCalculation calculation )
