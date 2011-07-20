95d19e57fd26f4ff114cd974af0d2f1b72080599
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2011-07-20 17:10:50 +0000

    Allow user to specifiy sleep time when printing profiles to plotter

--- KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/export/wspwin/PlotterExportPage.java
+++ KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/export/wspwin/PlotterExportPage.java
@@ -40,15 +40,23 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.wspm.tuhh.ui.export.wspwin;
 
-import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.core.databinding.beans.BeansObservables;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jface.databinding.swt.ISWTObservableValue;
+import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.kalypso.commons.databinding.DataBinder;
+import org.kalypso.commons.databinding.jface.wizard.DatabindingWizardPage;
+import org.kalypso.commons.databinding.validation.NotNullValidator;
+import org.kalypso.commons.databinding.validation.NumberNotNegativeValidator;
 import org.kalypso.contribs.eclipse.jface.wizard.IFileChooserDelegate;
 import org.kalypso.model.wspm.tuhh.ui.i18n.Messages;
 
@@ -57,64 +65,67 @@ import org.kalypso.model.wspm.tuhh.ui.i18n.Messages;
  */
 public class PlotterExportPage extends ExportPrfFileChooserPage
 {
-  private static final String SETTINGS_DO_PRINT = "directPrint"; //$NON-NLS-1$
+  private final PlotterExportData m_data;
 
-  private boolean m_doPrint;
+  private DatabindingWizardPage m_binding;
 
-  public PlotterExportPage( final IFileChooserDelegate fileChooser )
+  public PlotterExportPage( final IFileChooserDelegate fileChooser, final PlotterExportData data )
   {
     super( fileChooser );
+
+    m_data = data;
   }
 
-  /**
-   * @see org.kalypso.model.wspm.tuhh.ui.export.ExportPrfFileChooserPage#createPageContent(org.eclipse.swt.widgets.Composite)
-   */
   @Override
   protected void createPageContent( final Composite parent )
   {
     super.createPageContent( parent );
+
+    m_binding = new DatabindingWizardPage( this, null );
 
     createOptionsGroup( parent );
   }
 
   private void createOptionsGroup( final Composite parent )
   {
-    final IDialogSettings dialogSettings = getDialogSettings();
-    if( dialogSettings != null )
-    {
-      m_doPrint = dialogSettings.getBoolean( SETTINGS_DO_PRINT );
-    }
-
     final Group group = new Group( parent, SWT.NONE );
-    group.setLayout( new GridLayout() );
+    group.setLayout( new GridLayout( 2, false ) );
     group.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, false ) );
     group.setText( Messages.getString( "PlotterExportPage_0" ) ); //$NON-NLS-1$
 
-    final Button button = new Button( group, SWT.CHECK );
-    button.setLayoutData( new GridData( SWT.FILL, SWT.CENTER, true, false ) );
+    createFields( group );
+  }
+
+  private void createFields( final Composite parent )
+  {
+    /* Print Button */
+    final Button button = new Button( parent, SWT.CHECK );
+    button.setLayoutData( new GridData( SWT.FILL, SWT.CENTER, true, false, 2, 1 ) );
     button.setText( Messages.getString( "PlotterExportPage_1" ) ); //$NON-NLS-1$
     button.setToolTipText( Messages.getString( "PlotterExportPage_2" ) ); //$NON-NLS-1$
-    button.setSelection( m_doPrint );
-    button.addSelectionListener( new SelectionAdapter()
-    {
-      /**
-       * @see org.eclipse.swt.events.SelectionAdapter#widgetSelected(org.eclipse.swt.events.SelectionEvent)
-       */
-      @Override
-      public void widgetSelected( final SelectionEvent e )
-      {
-        handlePrintSelected( button.getSelection() );
-      }
-    } );
-  }
 
-  protected void handlePrintSelected( final boolean selection )
-  {
-    m_doPrint = selection;
-  }
+    final ISWTObservableValue targetPrint = SWTObservables.observeSelection( button );
+    final IObservableValue modelPrint = BeansObservables.observeValue( m_data, PlotterExportData.PROPERTY_DO_PRINT );
 
-  public boolean getDoPrint( )
-  {
-    return m_doPrint;
+    m_binding.bindValue( targetPrint, modelPrint );
+
+    /* Sleep */
+    new Label( parent, SWT.NONE ).setText( "Sleep Time [ms]" );
+
+    final Text field = new Text( parent, SWT.BORDER | SWT.SINGLE );
+    field.setLayoutData( new GridData( SWT.FILL, SWT.CENTER, true, false ) );
+    field.setToolTipText( "Sleep time [milliseconds] between two consecutive print jobs. Enter longer time if too many prints at once cause trouble." );
+
+    final ISWTObservableValue targetSleep = SWTObservables.observeText( field, SWT.Modify );
+    final IObservableValue modelSleep = BeansObservables.observeValue( m_data, PlotterExportData.PROPERTY_SLEEP_TIME );
+    final DataBinder binder = new DataBinder( targetSleep, modelSleep );
+    binder.addTargetAfterConvertValidator( new NotNullValidator<Long>( Long.class, IStatus.ERROR, "Sleep Time may not be empty" ) );
+    binder.addTargetAfterConvertValidator( new NumberNotNegativeValidator( IStatus.ERROR ) );
+    m_binding.bindValue( binder );
+
+    /* Enable/Disable slepp field */
+    final ISWTObservableValue targetEnabled = SWTObservables.observeEnabled( field );
+    final IObservableValue modelEnabled = modelPrint;
+    m_binding.bindValue( targetEnabled, modelEnabled );
   }
 }
