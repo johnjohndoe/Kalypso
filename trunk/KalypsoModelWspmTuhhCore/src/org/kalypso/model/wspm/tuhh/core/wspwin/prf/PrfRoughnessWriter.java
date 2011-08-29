e3592004c0703083dc731b1a7751c528ce24b270
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2011-08-29 06:50:38 +0000

    PrfWriter handling of roughness classes

--- trunk/KalypsoModelWspmTuhhCore/src/org/kalypso/model/wspm/tuhh/core/wspwin/prf/PrfRoughnessWriter.java
+++ trunk/KalypsoModelWspmTuhhCore/src/org/kalypso/model/wspm/tuhh/core/wspwin/prf/PrfRoughnessWriter.java
@@ -40,15 +40,24 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.wspm.tuhh.core.wspwin.prf;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang3.ArrayUtils;
 import org.kalypso.commons.java.lang.Objects;
+import org.kalypso.commons.java.lang.Strings;
 import org.kalypso.model.wspm.core.IWspmPointProperties;
+import org.kalypso.model.wspm.core.gml.classifications.IRoughnessClass;
+import org.kalypso.model.wspm.core.gml.classifications.IWspmClassification;
+import org.kalypso.model.wspm.core.gml.classifications.helper.WspmClassifications;
 import org.kalypso.model.wspm.core.profil.IProfil;
 import org.kalypso.model.wspm.core.profil.IProfileObject;
 import org.kalypso.model.wspm.tuhh.core.IWspmTuhhConstants;
 import org.kalypso.model.wspm.tuhh.core.profile.buildings.Buildings;
 import org.kalypso.model.wspm.tuhh.core.profile.buildings.IProfileBuilding;
 import org.kalypso.observation.result.IComponent;
+import org.kalypso.observation.result.IRecord;
 import org.kalypso.wspwin.core.prf.DataBlockWriter;
 import org.kalypso.wspwin.core.prf.datablock.CoordDataBlock;
 import org.kalypso.wspwin.core.prf.datablock.DataBlockHeader;
@@ -146,7 +155,7 @@ public class PrfRoughnessWriter
 
         final DataBlockHeader dbhr = PrfHeaders.createHeader( component.getId() ); //$NON-NLS-1$
         final CoordDataBlock dbr = new CoordDataBlock( dbhr );
-        m_prfWriter.writeCoords( component, dbr, (Double) Objects.firstNonNull( building, 0.0 ) );
+        writeCoords( component, dbr, (Double) Objects.firstNonNull( building, 0.0 ) );
         if( Objects.isNotNull( building ) )
         {
           dbr.getY()[0] = building;
@@ -156,7 +165,74 @@ public class PrfRoughnessWriter
       }
 
     }
+  }
 
+  void writeCoords( final IComponent component, final CoordDataBlock db, final Double nullValue )
+  {
+    final IRecord[] points = m_profile.getPoints();
+
+    final List<Double> arrX = new ArrayList<Double>( points.length );
+    final List<Double> arrY = new ArrayList<Double>( points.length );
+
+    final int indexWidth = m_profile.indexOfProperty( IWspmPointProperties.POINT_PROPERTY_BREITE );
+
+    for( final IRecord point : points )
+    {
+      final Double x = (Double) point.getValue( indexWidth );
+      final Double roughness = applyFactor( point, getValue( point, component ) );
+
+      arrX.add( x );
+      arrY.add( (Double) Objects.firstNonNull( roughness, nullValue ) );
+    }
+
+    final Double[] xArray = arrX.toArray( new Double[arrX.size()] );
+    final Double[] yArray = arrY.toArray( new Double[arrY.size()] );
+    db.setCoords( xArray, yArray );
+  }
+
+  private Double applyFactor( final IRecord point, final Double value )
+  {
+    if( Objects.isNull( value ) )
+      return null;
+
+    final IComponent componentFactor = m_profile.hasPointProperty( IWspmPointProperties.POINT_PROPERTY_ROUGHNESS_FACTOR );
+    if( Objects.isNull( componentFactor ) )
+      return value;
+
+    final Double factor = (Double) point.getValue( componentFactor );
+    if( Objects.isNull( factor ) )
+      return value;
+
+    return value * factor;
+  }
+
+  private Double getValue( final IRecord point, final IComponent component )
+  {
+    final Double plainValue = (Double) point.getValue( component );
+    if( !m_prefersRoughnessClasses )
+      return plainValue;
+
+    final IComponent componentRoughnessClass = m_profile.hasPointProperty( IWspmPointProperties.POINT_PROPERTY_ROUGHNESS_CLASS );
+    if( Objects.isNull( componentRoughnessClass ) )
+      return plainValue;
+
+    final IWspmClassification classification = WspmClassifications.getClassification( m_profile );
+    if( Objects.isNull( classification ) )
+      return plainValue;
+
+    final String clazzName = (String) point.getValue( componentRoughnessClass );
+    if( Strings.isEmpty( clazzName ) )
+      return plainValue;
+
+    final IRoughnessClass clazz = classification.findRoughnessClass( clazzName );
+    if( Objects.isNull( clazz ) )
+      return plainValue;
+
+    final BigDecimal value = clazz.getValue( component.getId() );
+    if( Objects.isNotNull( value ) )
+      return value.doubleValue();
+
+    return plainValue;
   }
 
   private void writeEmptyRauheit( )
