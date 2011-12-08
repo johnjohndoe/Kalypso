bbe93844078ade953aa1ef255922e9e23aa281d3
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2011-12-08 09:37:25 +0000

    Removed some deprecation

--- KalypsoUIRRM/src/org/kalypso/ui/rrm/wizards/SourceMappingComposite.java
+++ KalypsoUIRRM/src/org/kalypso/ui/rrm/wizards/SourceMappingComposite.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ui.rrm.wizards;
 
@@ -222,7 +222,7 @@ public class SourceMappingComposite extends Composite
     if( ftp == null )
       input = new Object[] { EMPTY_KEY };
     else
-      input = ArrayUtils.addAll( new Object[] { NULL_KEY }, ftp );
+      input = ArrayUtils.addAll( new Object[] { NULL_KEY }, (Object[]) ftp );
 
     final Set<Entry<ComboViewer, IValuePropertyType>> entrySet = m_combos.entrySet();
     for( final Entry<ComboViewer, IValuePropertyType> entry : entrySet )
@@ -252,7 +252,7 @@ public class SourceMappingComposite extends Composite
 
     final List<IValuePropertyType> result = new ArrayList<IValuePropertyType>();
 
-    final IFeatureType srcFT = m_sourceData.getParentFeatureTypeProperty().getTargetFeatureType();
+    final IFeatureType srcFT = m_sourceData.getPropertyType().getTargetFeatureType();
     final IPropertyType[] ftp = srcFT.getProperties();
 
     for( final IPropertyType element : ftp )
