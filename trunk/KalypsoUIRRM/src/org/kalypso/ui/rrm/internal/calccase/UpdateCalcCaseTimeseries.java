29fa72617d2b7bcb9a973465d50a7ba311373ffa
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2011-12-09 08:27:53 +0000

    Regorganized KalypsoUIRrm package structure.

--- trunk/KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/calccase/UpdateCalcCaseTimeseries.java
+++ trunk/KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/calccase/UpdateCalcCaseTimeseries.java
@@ -38,7 +38,7 @@
  v.doemming@tuhh.de
 
  ---------------------------------------------------------------------------------------------------*/
-package org.kalypso.ui.rrm.internal.handler;
+package org.kalypso.ui.rrm.internal.calccase;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
