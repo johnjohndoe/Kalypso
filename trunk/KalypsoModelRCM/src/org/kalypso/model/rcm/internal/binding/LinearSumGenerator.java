f2ca9ebfbbe63856b62a146c4fcf52e25d938d75
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2011-12-13 15:21:05 +0000

    Some small changes to schema

--- trunk/KalypsoModelRCM/src/org/kalypso/model/rcm/internal/binding/LinearSumGenerator.java
+++ trunk/KalypsoModelRCM/src/org/kalypso/model/rcm/internal/binding/LinearSumGenerator.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.rcm.internal.binding;
 
@@ -53,7 +53,7 @@ import org.kalypso.gmlschema.feature.IFeatureType;
 import org.kalypso.gmlschema.property.relation.IRelationType;
 import org.kalypso.model.rcm.binding.AbstractRainfallGenerator;
 import org.kalypso.model.rcm.binding.ICatchment;
-import org.kalypso.model.rcm.binding.ICatchmentGenerator;
+import org.kalypso.model.rcm.binding.ILinearSumGenerator;
 import org.kalypso.model.rcm.binding.IFactorizedTimeseries;
 import org.kalypso.model.rcm.internal.KalypsoModelRcmActivator;
 import org.kalypso.model.rcm.util.RainfallGeneratorUtilities;
@@ -66,31 +66,20 @@ import org.kalypsodeegree_impl.model.feature.gmlxpath.GMLXPath;
 
 /**
  * The catchment generator.
- * 
+ *
  * @author Holger Albert
  */
-public class CatchmentGenerator extends AbstractRainfallGenerator implements ICatchmentGenerator
+public class LinearSumGenerator extends AbstractRainfallGenerator implements ILinearSumGenerator
 {
-  /**
-   * The constructor.
-   * 
-   * @param parent
-   * @param parentRelation
-   * @param ft
-   * @param id
-   * @param propValues
-   */
-  public CatchmentGenerator( final Object parent, final IRelationType parentRelation, final IFeatureType ft, final String id, final Object[] propValues )
+  public LinearSumGenerator( final Object parent, final IRelationType parentRelation, final IFeatureType ft, final String id, final Object[] propValues )
   {
     super( parent, parentRelation, ft, id, propValues );
   }
 
   /**
-   * @see org.kalypso.model.rcm.binding.IRainfallGenerator#createRainfall(org.kalypsodeegree.model.feature.Feature[],
-   *      org.kalypso.ogc.sensor.DateRange, org.eclipse.core.runtime.ILog, org.eclipse.core.runtime.IProgressMonitor)
    * @param catchmentFeatures
    *          The catchment features will be taken from the generator itself, so they are not needed here. Leave them
-   *          null.
+   *          <code>null</code>.
    */
   @Override
   public IObservation[] createRainfall( final Feature[] catchmentFeatures, final DateRange range, final ILog log, IProgressMonitor monitor ) throws CoreException
@@ -230,7 +219,7 @@ public class CatchmentGenerator extends AbstractRainfallGenerator implements ICa
 
   /**
    * This function converts an array of Doubles to an array of doubles.
-   * 
+   *
    * @param factors
    *          The array of Doubles.
    * @return A array of doubles.
