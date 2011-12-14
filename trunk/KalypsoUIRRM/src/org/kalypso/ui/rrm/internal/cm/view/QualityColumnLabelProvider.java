a35d6907bdb1af74b450a70cf0f54d4ac834368d
holger.albert <holger.albert@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2011-12-14 13:41:38 +0000

    label provider

--- trunk/KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/cm/view/QualityColumnLabelProvider.java
+++ trunk/KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/cm/view/QualityColumnLabelProvider.java
@@ -2,101 +2,72 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- *
+ * 
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- *
+ * 
  *  and
- *
+ *  
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- *
+ * 
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- *
+ * 
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *
+ * 
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
+ * 
  *  Contact:
- *
+ * 
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *
+ *   
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ui.rrm.internal.cm.view;
 
-import org.kalypso.model.rcm.binding.IFactorizedTimeseries;
-import org.kalypso.ui.rrm.internal.timeseries.view.TimeseriesBean;
-import org.kalypso.ui.rrm.internal.utils.featureBinding.FeatureBean;
-import org.kalypsodeegree.model.feature.Feature;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
 
 /**
- * @author Gernot Belger
+ * A column label provider.
+ * 
  * @author Holger Albert
  */
-public class FactorizedTimeseriesBean extends FeatureBean<IFactorizedTimeseries>
+public class QualityColumnLabelProvider extends ColumnLabelProvider
 {
-  private TimeseriesBean m_timeseriesBean;
-
-  public FactorizedTimeseriesBean( )
+  /**
+   * The constructor.
+   */
+  public QualityColumnLabelProvider( )
   {
-    super( IFactorizedTimeseries.FEATURE_FACTORIZED_TIMESERIES );
-
-    m_timeseriesBean = null;
   }
 
-  public FactorizedTimeseriesBean( final IFactorizedTimeseries factorizedTimeseries )
+  /**
+   * @see org.eclipse.jface.viewers.ColumnLabelProvider#getText(java.lang.Object)
+   */
+  @Override
+  public String getText( Object element )
   {
-    super( factorizedTimeseries );
+    if( element instanceof FactorizedTimeseriesBean )
+    {
+      FactorizedTimeseriesBean bean = (FactorizedTimeseriesBean) element;
+      return bean.getQualityText();
+    }
 
-    m_timeseriesBean = null;
-  }
-
-  public String getLabel( )
-  {
-    return (String) getProperty( Feature.QN_DESCRIPTION );
-  }
-
-  public String getStationText( )
-  {
-    return getLabel();
-  }
-
-  public String getTimestepText( )
-  {
-    // TODO
-
-    return "";
-  }
-
-  public String getQualityText( )
-  {
-    // TODO
-
-    return "";
-  }
-
-  public String getFactorText( )
-  {
-    Double factor = getFeature().getFactor();
-    if( factor != null && !factor.isNaN() && !factor.isInfinite() )
-      return String.format( "%f", factor.doubleValue() );
-
-    return "0.0";
+    return super.getText( element );
   }
 }
