fdc1634515f70201d12e1f19b9db317bc7de6565
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-01-03 19:33:40 +0000

    First part of Layout wizard refactoring; moved base stuff to KalypsoCore

--- trunk/KalypsoWspWinCore/src/org/kalypso/wspwin/core/ProfileBean.java
+++ trunk/KalypsoWspWinCore/src/org/kalypso/wspwin/core/ProfileBean.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.wspwin.core;
 
@@ -57,6 +57,10 @@ import org.kalypso.wspwin.core.i18n.Messages;
  */
 public class ProfileBean
 {
+  static final int MAX_WATERNAME_LENGTH = 9;
+
+  static final int MAX_STATENAME_LENGTH = 10;
+
   private final String m_waterName;
   private final String m_stateName;
   private final double m_station;
@@ -129,15 +133,15 @@ public class ProfileBean
 
   public String formatLine( )
   {
-    final String waterName = shortenName( getWaterName() );
-    final String stateName = shortenName( getStateName() );
+    final String waterName = shortenName( getWaterName(), MAX_WATERNAME_LENGTH );
+    final String stateName = shortenName( getStateName(), MAX_STATENAME_LENGTH );
 
     return String.format( Locale.US, "%-9s%9.4f 0        0    %-10s %-12s", waterName, m_station, stateName, m_fileName );
   }
 
-  public static String shortenName( final String name )
+  public static String shortenName( final String name, final int maxLength )
   {
     final String noSpaces = StringUtils.remove( name, ' ' ); //$NON-NLS-1$
-    return StringUtils.abbreviateMiddle( noSpaces, "_", 10 ); //$NON-NLS-1$
+    return StringUtils.abbreviateMiddle( noSpaces, "_", maxLength ); //$NON-NLS-1$
   }
 }
