11a5158f64519552ef7790ddccd1ddb2dfc0e5ab
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-01-04 15:07:50 +0000

    Started implementation of Thiesen Wiezard

--- trunk/KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/cm/view/ThiessenGeneratorWizard.java
+++ trunk/KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/cm/view/ThiessenGeneratorWizard.java
@@ -40,26 +40,123 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ui.rrm.internal.cm.view;
 
+import java.net.URL;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
-import org.kalypsodeegree.model.feature.Feature;
+import org.kalypso.afgui.scenarios.ScenarioHelper;
+import org.kalypso.commons.arguments.Arguments;
+import org.kalypso.commons.command.DefaultCommandManager;
+import org.kalypso.contribs.eclipse.core.resources.ResourceUtilities;
+import org.kalypso.core.layoutwizard.LayoutWizardPage;
+import org.kalypso.util.command.JobExclusiveCommandTarget;
 
 /**
  * @author Gernot Belger
  */
 public class ThiessenGeneratorWizard extends Wizard
 {
+  private static final String URN_PAGE_LAYOUT = "urn:sourceforge:kalypso:hydrology:wizard:page:thiessen"; //$NON-NLS-1$
+
+  private final JobExclusiveCommandTarget m_commandTarget = new JobExclusiveCommandTarget( new DefaultCommandManager(), null );
+
   private final LinearSumBean m_bean;
 
   public ThiessenGeneratorWizard( final LinearSumBean bean )
   {
     m_bean = bean;
+
+    m_commandTarget.dispose();
+
+    final URL argumentLocation = getContext();
+
+    final Arguments arguments = createPageDefinition();
+
+    final IWizardPage thiessenPage = new LayoutWizardPage( "thiessenPage", m_commandTarget, argumentLocation, arguments );
+
+    thiessenPage.setTitle( "Thiessen Method" );
+    thiessenPage.setDescription( "Please select the timeseries tht should be used to generate the catchment model." );
+
+    addPage( thiessenPage );
   }
+
+  private URL getContext( )
+  {
+    try
+    {
+      final IContainer currentScenario = ScenarioHelper.getScenarioDataProvider().getScenarioFolder();
+      return ResourceUtilities.createQuietURL( currentScenario );
+    }
+    catch( final CoreException e )
+    {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  private Arguments createPageDefinition( )
+  {
+    final Arguments arguments = new Arguments();
+
+    arguments.put( "pageLayout", URN_PAGE_LAYOUT );
+
+//    <arg name="pageLayout" value="project://.model/wizard/pages/10_Layout.xml" />
+//    <!-- MAP -->
+//    <arg name="gisMap.1">
+//      <arg name="mapTemplate" value="project://.model/wizard/pages/10_Ombrometer.gmt" />
+//      <arg name="maximizeMap" value="true" />
+//      <arg name="mapToolbar">
+//        <arg name="uri" value="toolbar:org.kalypso.hwv.calcWizard.map.basicNavigation" />
+//      </arg>
+//    </arg>
+//    <!-- FEATURE-VIEW -->
+//    <arg name="gisTable.1">
+//      <arg name="tableTemplate" value="project://.model/wizard/pages/10_Ombrometer.gtt" />
+//    </arg>
+//    <arg name="featureView.1">
+//      <arg name="featureTemplate" value="project://.model/wizard/pages/10_Ombrometer.gft" />
+//    </arg>
+//    <!-- Tabelle -->
+//    <arg name="zmlNewTable.1">
+//      <arg name="zmlTableTemplate" value="project://.model/wizard/pages/10_Tabelle.kot" />
+//      <arg name="timeserie1" value="id=N#nameColumn=name#linkColumn=precipitationLink1#nameString=%featureprop% [%axisunit%]" />
+//    </arg>
+//    <!-- Diagramm -->
+//    <arg name="zmlNewDiagram.1">
+//      <arg name="zmlDiagramTemplate" value="project://.model/wizard/pages/10_Diagramm.kod" />
+//      <arg name="executeCommand_1_1" value="org.kalypso.chart.zml.ui.commands.zoom_pan_maximize?enabled=true" />
+//      <arg name="zmlDiagramMenuContribution_1" value="toolbar:org.kalypso.hwv.product.sachsen.zml.diagram.menu.general" />
+//      <arg name="zmlDiagramMenuContribution_1a" value="toolbar:org.kalypso.hwv.product.sachsen.zml.diagram.menu.separator1" />
+//      <arg name="timeserie1" value="id=N#nameColumn=name#linkColumn=precipitationLink1#nameString=%featureprop% [%axisunit%]" />
+//      <arg name="timeserie1nZr" value="id=N_OriZR#linkColumn=precipitationLink1" />
+//    </arg>
+
+    return arguments;
+  }
+
+  @Override
+  public void dispose( )
+  {
+    m_commandTarget.dispose();
+
+    super.dispose();
+  }
+
+  // create gml layer for timeseries (aka stations)
+  // - fill all timeseries of current parameter type
+  // - group somehow by station
+  // - preselect all, that are already referenced in current bean
+  // - on every feature change, recalculate thiessen polygons
 
   @Override
   public boolean performFinish( )
   {
-    final String label = m_bean.getLabel();
-    m_bean.setProperty( Feature.QN_DESCRIPTION, label + "X" );
+    // TODO: calculate thiessen weights and set to catchments
+
+// final String label = m_bean.getLabel();
+// m_bean.setProperty( Feature.QN_DESCRIPTION, label + "X" );
 
     return true;
   }
