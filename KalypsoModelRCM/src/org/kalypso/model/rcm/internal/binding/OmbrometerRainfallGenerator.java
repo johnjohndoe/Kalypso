393c9ed6762ca761179903a96c385084948667f7
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-01-05 20:07:56 +0000

    Some refactorings of thiessen stuff, so it can be used in other context.

--- KalypsoModelRCM/src/org/kalypso/model/rcm/internal/binding/OmbrometerRainfallGenerator.java
+++ KalypsoModelRCM/src/org/kalypso/model/rcm/internal/binding/OmbrometerRainfallGenerator.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- * 
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- * 
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.rcm.internal.binding;
 
@@ -63,6 +63,8 @@ import org.kalypso.model.rcm.binding.IOmbrometer;
 import org.kalypso.model.rcm.binding.IOmbrometerCollection;
 import org.kalypso.model.rcm.internal.KalypsoModelRcmActivator;
 import org.kalypso.model.rcm.internal.UrlCatalogRcm;
+import org.kalypso.model.rcm.util.BufferBoundaryCalculator;
+import org.kalypso.model.rcm.util.IBoundaryCalculator;
 import org.kalypso.model.rcm.util.OmbrometerUtils;
 import org.kalypso.model.rcm.util.RainfallGeneratorUtilities;
 import org.kalypso.ogc.sensor.DateRange;
@@ -91,7 +93,7 @@ import com.vividsolutions.jts.geom.Polygon;
 
 /**
  * Implementation that generates rainfall from ombrometer stations with Thiessen.
- * 
+ *
  * @author Gernot Belger
  */
 public class OmbrometerRainfallGenerator extends AbstractRainfallGenerator
@@ -283,13 +285,15 @@ public class OmbrometerRainfallGenerator extends AbstractRainfallGenerator
 
     // REMARK: huge buffer ratio. Does not really change the result (as long as all catchments are covered), but we do
     // not need to worry where to get the buffer as we don't save the result here. Mainly needed for the visualisation.
-    final double bufferRatio = 10;
+    final IBoundaryCalculator bufferCalculator = new BufferBoundaryCalculator( 10.0 );
 
+    final QName propertyLocation = IOmbrometer.QNAME_PROP_STATIONLOCATION;
+    final QName propertyActive = IOmbrometer.QNAME_PROP_ISUSED;
     /* Recalculate Thiessen */
-    final Map<IOmbrometer, GM_Surface<GM_SurfacePatch>> areas = OmbrometerUtils.thiessenPolygons( Arrays.asList( ombrometerFeatures ), bufferRatio, monitor );
-    for( final Entry<IOmbrometer, GM_Surface<GM_SurfacePatch>> entry : areas.entrySet() )
+    final Map<Feature, GM_Surface<GM_SurfacePatch>> areas = OmbrometerUtils.thiessenPolygons( Arrays.asList( ombrometerFeatures ), propertyLocation, propertyActive, bufferCalculator, monitor );
+    for( final Entry<Feature, GM_Surface<GM_SurfacePatch>> entry : areas.entrySet() )
     {
-      final IOmbrometer ombrometer = entry.getKey();
+      final IOmbrometer ombrometer = (IOmbrometer) entry.getKey();
       ombrometer.setAffectedArea( entry.getValue() );
     }
   }
