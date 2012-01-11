95401f26395e51cf4405006dca35bb8103f3e6bd
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-01-11 10:03:16 +0000

    Fixed: import waterbodies could not handle non-string shape properties

--- trunk/KalypsoModelWspmPdbUI/src/org/kalypso/model/wspm/pdb/ui/internal/admin/waterbody/imports/ReadWaterBodiesOperation.java
+++ trunk/KalypsoModelWspmPdbUI/src/org/kalypso/model/wspm/pdb/ui/internal/admin/waterbody/imports/ReadWaterBodiesOperation.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- * 
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- * 
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.wspm.pdb.ui.internal.admin.waterbody.imports;
 
@@ -45,6 +45,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.databinding.beans.BeanProperties;
 import org.eclipse.core.runtime.CoreException;
@@ -68,7 +69,7 @@ import org.kalypsodeegree_impl.model.geometry.JTSAdapter;
 
 /**
  * Reads water bodies from a shape file.
- * 
+ *
  * @author Gernot Belger
  */
 public class ReadWaterBodiesOperation implements ICoreRunnableWithProgress
@@ -176,6 +177,7 @@ public class ReadWaterBodiesOperation implements ICoreRunnableWithProgress
 
   private Object findValue( final ImportAttributeInfo< ? > info, final IDBFField[] fields, final Object[] data ) throws CoreException
   {
+    final String property = info.getProperty();
     final IDBFField field = info.getField();
     if( field == ImportAttributeInfo.FIELD_USE_DEFAULT )
       return info.getDefaultValue();
@@ -183,12 +185,29 @@ public class ReadWaterBodiesOperation implements ICoreRunnableWithProgress
     final int fieldIndex = findFieldIndex( field.getName(), fields );
     final Object value = data[fieldIndex];
 
-    if( WaterBody.PROPERTY_DIRECTION_OF_STATIONING.equals( info.getProperty() ) )
-      return parseDirection( value );
-    if( WaterBody.PROPERTY_RANK.equals( info.getProperty() ) )
-      return parseRank( value );
+    switch( property )
+    {
+      case WaterBody.PROPERTY_DIRECTION_OF_STATIONING:
+        return parseDirection( value );
+
+      case WaterBody.PROPERTY_RANK:
+        return parseRank( value );
+
+      case WaterBody.PROPERTY_DESCRIPTION:
+      case WaterBody.PROPERTY_LABEL:
+      case WaterBody.PROPERTY_NAME:
+        return parseAsString( value );
+    }
 
     return value;
+  }
+
+  private String parseAsString( final Object value )
+  {
+    if( value instanceof String )
+      return (String) value;
+
+    return ObjectUtils.toString( value );
   }
 
   private Integer parseRank( final Object value ) throws CoreException
