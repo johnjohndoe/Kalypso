3bc77983f7689e45e1e4d582b065134ae6806d74
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-02-08 10:56:15 +0000

    Using profile-transaction for cross section converter

--- trunk/KalypsoModelWspmPdbCore/src/org/kalypso/model/wspm/pdb/internal/wspm/CrossSectionConverter.java
+++ trunk/KalypsoModelWspmPdbCore/src/org/kalypso/model/wspm/pdb/internal/wspm/CrossSectionConverter.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraße 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- * 
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- * 
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.wspm.pdb.internal.wspm;
 
@@ -90,17 +90,26 @@ public class CrossSectionConverter
 
   public void execute( )
   {
-    m_profile.setName( m_section.getName() );
-    m_profile.setDescription( m_section.getDescription() );
+    m_profile.startTransaction( this );
 
-    final BigDecimal station = m_section.getStation();
-    /* [m] -> [km] */
-    m_profile.setStation( station.movePointLeft( 3 ).doubleValue() );
+    try
+    {
+      m_profile.setName( m_section.getName() );
+      m_profile.setDescription( m_section.getDescription() );
 
-    convertP();
-    convertBuilding();
-    // TODO: other kinds...
-    // Other building types (tubes, ...)
+      final BigDecimal station = m_section.getStation();
+      /* [m] -> [km] */
+      m_profile.setStation( station.movePointLeft( 3 ).doubleValue() );
+
+      convertP();
+      convertBuilding();
+      // TODO: other kinds...
+      // Other building types (tubes, ...)
+    }
+    finally
+    {
+      m_profile.stopTransaction( this );
+    }
   }
 
   private void convertP( )
