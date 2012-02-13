8e91dd088d861181539635da8bf353ae28e9340e
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-02-13 09:28:59 +0000

    StringUtils instead of ""

--- trunk/KalypsoModelRCM/src/org/kalypso/model/rcm/RainfallGenerationOp.java
+++ trunk/KalypsoModelRCM/src/org/kalypso/model/rcm/RainfallGenerationOp.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraße 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- * 
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- * 
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.rcm;
 
@@ -45,6 +45,7 @@ import java.util.List;
 
 import javax.xml.namespace.QName;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.ILog;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -79,7 +80,7 @@ import org.kalypsodeegree_impl.model.feature.FeatureHelper;
 
 /**
  * This class does the real generation stuff.
- * 
+ *
  * @author Gernot Belger
  */
 public class RainfallGenerationOp
@@ -134,7 +135,7 @@ public class RainfallGenerationOp
   public IStatus execute( final IProgressMonitor monitor ) throws CoreException
   {
     /* Monitor. */
-    final SubMonitor progress = SubMonitor.convert( monitor, "", m_generators.length * 10 + 2 * 3 + 10 );
+    final SubMonitor progress = SubMonitor.convert( monitor, StringUtils.EMPTY, m_generators.length * 10 + 2 * 3 + 10 );
 
     /* Generate the rainfall. */
     for( final IRainfallGenerator generator : m_generators )
@@ -326,7 +327,7 @@ public class RainfallGenerationOp
     {
       final Period currentTimestep = MetadataHelper.getTimestep( observation.getMetadataList() );
 
-      boolean hasValues = hasValues( observation );
+      final boolean hasValues = hasValues( observation );
       if( currentTimestep == null && hasValues )
       {
         final IStatus status = new Status( IStatus.ERROR, KalypsoModelRcmActivator.PLUGIN_ID, "All timeseries involved in rainfall generation must have a timestep." );
@@ -349,13 +350,13 @@ public class RainfallGenerationOp
     }
   }
 
-  private static boolean hasValues( IObservation observation )
+  private static boolean hasValues( final IObservation observation )
   {
     try
     {
       return observation.getValues( null ).size() > 0;
     }
-    catch( SensorException e )
+    catch( final SensorException e )
     {
       e.printStackTrace();
       return false;
