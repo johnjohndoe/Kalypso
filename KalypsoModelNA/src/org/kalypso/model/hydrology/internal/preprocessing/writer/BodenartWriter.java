c51ef28dbe7643032c2614ea283db0e3f1e43564
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-03-14 17:39:42 +0000

    Introduced binding classes for soiltype and its layers

--- KalypsoModelNA/src/org/kalypso/model/hydrology/internal/preprocessing/writer/BodenartWriter.java
+++ KalypsoModelNA/src/org/kalypso/model/hydrology/internal/preprocessing/writer/BodenartWriter.java
@@ -2,48 +2,47 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 
 package org.kalypso.model.hydrology.internal.preprocessing.writer;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -79,10 +78,9 @@ public class BodenartWriter extends AbstractCoreFileWriter
     writer.append( "                [mm/dm] [mm/dm] [mm/dm]  [mm/d] [-]\n" ); //$NON-NLS-1$
     final List<String> names = new ArrayList<String>();
 
-    final Iterator< ? > iter = list.iterator();
-    while( iter.hasNext() )
+    for( final Object object : list )
     {
-      final Feature bodenartFE = (Feature) iter.next();
+      final Feature bodenartFE = (Feature) object;
       // TODO: nur die schreiben, die auch in Bodentyp verwendet werden.
       writeFeature( writer, bodenartFE );
       names.add( bodenartFE.getName() );
@@ -108,7 +106,7 @@ public class BodenartWriter extends AbstractCoreFileWriter
     if( !names.contains( "Drain" ) ) //$NON-NLS-1$
       writer.append( "Drain kap 4.0 30.5 40.0 1720.0 0.10\n" ); //$NON-NLS-1$
     if( !names.contains( "basem" ) ) //$NON-NLS-1$
-      writer.append( "basem kap 6.5 24.0 41.0 1570.0 0.50\n" ); //$NON-NLS-1$    
+      writer.append( "basem kap 6.5 24.0 41.0 1570.0 0.50\n" ); //$NON-NLS-1$
   }
 
   private void writeFeature( final PrintWriter writer, final Feature feature )
