de82f390b0da3cbc334ad5a359c03491c98a0473
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-03-27 11:18:36 +0000

    Edit Station Quality - Text / Combo Viewer (text completion)

--- KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/timeseries/view/edit/EditTimeseriesQualityComposite.java
+++ KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/timeseries/view/edit/EditTimeseriesQualityComposite.java
@@ -43,6 +43,7 @@ package org.kalypso.ui.rrm.internal.timeseries.view.edit;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import org.apache.commons.io.IOCase;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.swt.widgets.Composite;
@@ -50,10 +51,13 @@ import org.kalypso.commons.databinding.IDataBinding;
 import org.kalypso.commons.databinding.validation.FileNameIsUniqueValidator;
 import org.kalypso.commons.databinding.validation.MultiValidator;
 import org.kalypso.commons.databinding.validation.StringFilenameValidator;
+import org.kalypso.commons.java.lang.Objects;
 import org.kalypso.model.hydrology.timeseries.binding.IStation;
+import org.kalypso.model.hydrology.timeseries.binding.IStationCollection;
 import org.kalypso.model.hydrology.timeseries.binding.ITimeseries;
 import org.kalypso.ui.rrm.internal.utils.featureBinding.FeatureBean;
 import org.kalypso.ui.rrm.internal.utils.featureBinding.FeatureBeanComposite;
+import org.kalypsodeegree.model.feature.Feature;
 import org.kalypsodeegree.model.feature.IFeatureBindingCollection;
 
 /**
@@ -72,14 +76,56 @@ public class EditTimeseriesQualityComposite extends FeatureBeanComposite<ITimese
     final FeatureBean<ITimeseries> bean = getBean();
     final ITimeseries timeseries = bean.getFeature();
 
-    final String[] qualities = getQualities( timeseries.getStation(), timeseries.getParameterType() );
+    final String[] stationQualities = getQualities( timeseries.getStation(), timeseries.getParameterType() );
+    final String[] textCompletionQualities = findQualities( timeseries.getStation(), timeseries.getParameterType(), stationQualities );
 
     final StringFilenameValidator filenameValidator = new StringFilenameValidator( IStatus.ERROR, "Stationsname enthält ungültige Zeichen" );
-    final FileNameIsUniqueValidator uniqueValudator = new FileNameIsUniqueValidator( qualities, timeseries.getQuality(), IStatus.ERROR, "Quality bereits vorhanden." );
+    final FileNameIsUniqueValidator uniqueValudator = new FileNameIsUniqueValidator( stationQualities, timeseries.getQuality(), IStatus.ERROR, "Quality bereits vorhanden." );
 
     final MultiValidator validator = new MultiValidator( filenameValidator, uniqueValudator );
 
-    createPropertyControl( ITimeseries.PROPERTY_QUALITY, validator );
+    createPropertyComboTextControl( ITimeseries.PROPERTY_QUALITY, textCompletionQualities, validator );
+  }
+
+  /**
+   * @return possible existing qualities used for editing text completion
+   */
+  private String[] findQualities( final IStation current, final String parameterType, final String[] stationQualities )
+  {
+    final Feature parent = current.getOwner();
+    if( !(parent instanceof IStationCollection) )
+      return new String[] {};
+
+    final Set<String> found = new LinkedHashSet<>();
+
+    final IStationCollection collection = (IStationCollection) parent;
+    final IFeatureBindingCollection<IStation> stations = collection.getStations();
+    for( final IStation station : stations )
+    {
+      if( Objects.equal( station, current ) )
+        continue;
+
+      final String[] qualities = getQualities( station, parameterType );
+      for( final String quality : qualities )
+      {
+        if( notExists( stationQualities, quality ) )
+          found.add( quality );
+      }
+
+    }
+
+    return found.toArray( new String[] {} );
+  }
+
+  private boolean notExists( final String[] existing, final String quality )
+  {
+    for( final String exists : existing )
+    {
+      if( IOCase.SYSTEM.checkEquals( exists, quality ) )
+        return false;
+    }
+
+    return true;
   }
 
   private String[] getQualities( final IStation station, final String parameterType )
