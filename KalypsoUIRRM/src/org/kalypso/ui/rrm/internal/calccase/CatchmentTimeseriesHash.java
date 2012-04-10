ddccdb7252ae6d46c11bf72bc645b75df361cda3
holger.albert <holger.albert@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-04-10 12:07:06 +0000

    optimization of the multi catchment generator for perfomance reasons

--- KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/calccase/CatchmentTimeseriesHash.java
+++ KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/calccase/CatchmentTimeseriesHash.java
@@ -43,6 +43,7 @@ package org.kalypso.ui.rrm.internal.calccase;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -63,9 +64,20 @@ import org.kalypso.zml.obslink.TimeseriesLinkType;
 public class CatchmentTimeseriesHash
 {
   /**
-   * Hash catchment->timeseries link. One catchment may have more than one timeseries link.
+   * The metadata key of the hash code.
+   */
+  public static final String MD_HASH_CODE = "HASH_CODE";
+
+  /**
+   * Hash catchment->timeseries links. One catchment may have more than one timeseries link.
    */
   private final Map<String, List<TimeseriesLinkType>> m_hash;
+
+  /**
+   * Hash hash code->merged observation. The hash code represents the links of the observations, that were used to
+   * create the merged observation.
+   */
+  private final Map<String, IObservation> m_merged;
 
   /**
    * The constructor.
@@ -73,6 +85,7 @@ public class CatchmentTimeseriesHash
   public CatchmentTimeseriesHash( )
   {
     m_hash = new HashMap<String, List<TimeseriesLinkType>>();
+    m_merged = new HashMap<String, IObservation>();
   }
 
   /**
@@ -102,8 +115,8 @@ public class CatchmentTimeseriesHash
    */
   public Map<String, IObservation> merge( final URL context ) throws CoreException, MalformedURLException, SensorException
   {
-    /* Memory for the results. */
-    final HashMap<String, IObservation> results = new HashMap<String, IObservation>();
+    /* Hash catchment->merged observation. */
+    final Map<String, IObservation> results = new HashMap<String, IObservation>();
 
     /* Get the ids of the catchments. */
     final String[] keys = m_hash.keySet().toArray( new String[] {} );
@@ -112,17 +125,64 @@ public class CatchmentTimeseriesHash
       /* Get the timeseries links for the catchment. */
       final List<TimeseriesLinkType> timeseriesLinks = m_hash.get( key );
 
-      /* Load them all. */
-      final List<IObservation> observations = loadObservations( context, timeseriesLinks );
+      /* Build the hash code of the timeseries links for the catchment. */
+      final String hashCode = buildHashCode( timeseriesLinks );
 
-      /* Merge them. */
-      final IObservation observation = RainfallGenerationOp.combineObservations( observations );
+      /* If the hash code is already available, reuse the merged observation. */
+      if( m_merged.containsKey( hashCode ) )
+      {
+        /* Get the merged observation for the hash code. */
+        final IObservation mergedObservation = m_merged.get( hashCode );
+
+        /* Store the result. */
+        results.put( key, mergedObservation );
+
+        continue;
+      }
+
+      /* If the hash code is not available, merge the observations. */
+      final List<IObservation> observations = loadObservations( context, timeseriesLinks );
+      final IObservation mergedObservation = RainfallGenerationOp.combineObservations( observations );
+
+      /* In this case also put the hash code into the metadata of the merged observation. */
+      /* It may be later used, to find out, if one observation of the results equals another. */
+      mergedObservation.getMetadataList().put( MD_HASH_CODE, hashCode );
+
+      /* Update the merged hash. */
+      m_merged.put( hashCode, mergedObservation );
 
       /* Store the result. */
-      results.put( key, observation );
+      results.put( key, mergedObservation );
     }
 
     return results;
+  }
+
+  /**
+   * This function builds the hash code for the list of timeseries links.
+   * 
+   * @param timeseriesLinks
+   *          The list timeseries links.
+   * @return The hash code.
+   */
+  private String buildHashCode( final List<TimeseriesLinkType> timeseriesLinks )
+  {
+    /* The string builder. */
+    final StringBuilder hashCode = new StringBuilder();
+
+    /* The timeseries link should be already sorted by the way they were added. */
+    /* But to be sure, sort them again by their href. */
+    final TimeseriesLinkType[] links = timeseriesLinks.toArray( new TimeseriesLinkType[] {} );
+    Arrays.sort( links, new TimeseriesLinkComparator() );
+
+    /* Build the hash code. */
+    for( final TimeseriesLinkType link : links )
+    {
+      hashCode.append( link.getHref() );
+      hashCode.append( ";" );
+    }
+
+    return hashCode.toString();
   }
 
   /**
