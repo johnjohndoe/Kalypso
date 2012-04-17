1c23e50008f740d61539673cddeed79289dbd5be
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-04-17 05:52:28 +0000

    land base evaporation calculation

--- KalypsoModelNA/src/org/kalypso/model/hydrology/operation/evaporation/LandbasedEvaporationCalculator.java
+++ KalypsoModelNA/src/org/kalypso/model/hydrology/operation/evaporation/LandbasedEvaporationCalculator.java
@@ -53,7 +53,7 @@ public class LandbasedEvaporationCalculator extends AbstractEvaporationCalculato
   private static final double LATITUDE_DEGREE = 54.00;
 
   /* Albedo */
-  private static final double ALBEDO = 0.23;
+// private static final double ALBEDO = 0.23;
 
   /* Stefan Boltzmann Konstante (DVWK - Formel 5.27) */
 // private static final double BOLTZMANN_CONSTANT = 0.49 * Math.pow( 10.0, -6.0 );
@@ -64,7 +64,7 @@ public class LandbasedEvaporationCalculator extends AbstractEvaporationCalculato
   }
 
   @Override
-  protected Double doCalculate( final Double humidity, final Double sunshine, final Double temperature, final Double windVelocity, final Calendar date )
+  protected Double doCalculate( final double humidity, final double sunshine, final double temperature, final double windVelocity, final Calendar date )
   {
     final double es = 6.11 * Math.pow( 10.0, 7.48 * temperature / (237.0 + temperature) );
     final double e = es * (humidity / 100.0);
@@ -72,17 +72,17 @@ public class LandbasedEvaporationCalculator extends AbstractEvaporationCalculato
     final double s = es * (4284.0 / Math.pow( 243.12 + temperature, 2.0 ));
 
     final double roh = 0.0172 * date.get( Calendar.DAY_OF_YEAR ) - 1.39;
-    final double r0 = 245.0 * (9.9 + 7.08 * Math.sin( roh ) + 0.18 * (LATITUDE_DEGREE - 51.0) * (Math.sin( roh ) - 1));
-    final double s0 = 12.3 + Math.sin( roh ) * (4.3 + (LATITUDE_DEGREE - 51.0) / 6.0);
+    final double rohSinus = Math.sin( roh );
+
+    final double r0 = 245.0 * (9.9 + 7.08 * rohSinus + 0.18 * (LATITUDE_DEGREE - 51.0) * (rohSinus - 1.0));
+    final double s0 = 12.3 + rohSinus * (4.3 + (LATITUDE_DEGREE - 51.0) / 6.0);
     final double rg = r0 * (0.19 + 0.55 * (sunshine / s0));
 
 // final double rn = (1.0 - ALBEDO) * rg - BOLTZMANN_CONSTANT * Math.pow( 237.15 + temperature, 4.0 ) * (0.1 + 0.9
 // * (sunshine / s0)) * (0.34 - 0.044 * Math.pow( e, 0.5 ));
 
-    final double v2 = getV2( windVelocity );
-
-    final double etN = s * (0.6 * rg / l) + 0.655 * (3.75 / (temperature + 273.0)) * v2 * (es - e);
-    final double etT = s + 0.655 * (1 + 0.34 * v2);
+    final double etN = s * (0.6 * rg / l) + 0.655 * (3.75 / (temperature + 273.0)) * windVelocity * (es - e);
+    final double etT = s + 0.655 * (1 + 0.34 * windVelocity);
 
     final double et0 = etN / etT;
 
@@ -92,13 +92,4 @@ public class LandbasedEvaporationCalculator extends AbstractEvaporationCalculato
     return 0.0;
   }
 
-  /**
-   * Beaufort-Conversion of wind velocity
-   */
-  private double getV2( final Double v )
-  {
-    final double beaufort = 0.0009 * Math.pow( v, 6.0 ) - 0.0173 * Math.pow( v, 5.0 ) + 0.1219 * Math.pow( v, 4.0 ) - 0.3874 * Math.pow( v, 3.0 ) + 0.8597 * Math.pow( v, 2.0 ) + 0.2767 * v - 0.0186;
-
-    return beaufort;
-  }
 }
