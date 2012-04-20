2b8e8e6e84a8995de81e60da7f6e3c8779b8d250
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-04-20 12:12:03 +0000

    NEW - # 62: ZML Fehlwerte beim Import behandeln https://dev.bjoernsen.de/hydrology/ticket/62

--- KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/timeseries/view/imports/ValidateRuecksprungVisitor.java
+++ KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/timeseries/view/imports/ValidateRuecksprungVisitor.java
@@ -2,66 +2,76 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraße 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ui.rrm.internal.timeseries.view.imports;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TimeZone;
 
-import org.kalypso.commons.exception.CancelVisitorException;
+import org.apache.commons.lang3.Range;
+import org.eclipse.core.runtime.IStatus;
 import org.kalypso.commons.java.lang.Objects;
+import org.kalypso.contribs.eclipse.core.runtime.StatusCollector;
+import org.kalypso.core.KalypsoCorePlugin;
 import org.kalypso.ogc.sensor.IAxis;
 import org.kalypso.ogc.sensor.SensorException;
+import org.kalypso.ogc.sensor.metadata.MetadataHelper;
 import org.kalypso.ogc.sensor.timeseries.AxisUtils;
 import org.kalypso.ogc.sensor.visitor.IObservationValueContainer;
 import org.kalypso.ogc.sensor.visitor.IObservationVisitor;
+import org.kalypso.ui.rrm.internal.KalypsoUIRRMPlugin;
+import org.kalypso.ui.rrm.internal.i18n.Messages;
 
-/**
- * @author Dirk Kuch
- */
 public class ValidateRuecksprungVisitor implements IObservationVisitor
 {
+  private final Set<Range<Integer>> m_rueckspruenge = new LinkedHashSet<>();
+
+  private final StatusCollector m_stati = new StatusCollector( KalypsoUIRRMPlugin.getID() );
 
   private Date m_lastDate;
 
-  private boolean m_ruecksprung = false;
+  private int m_ptrRuecksprung = -1;
 
   @Override
-  public void visit( final IObservationValueContainer container ) throws CancelVisitorException, SensorException
+  public void visit( final IObservationValueContainer container ) throws SensorException
   {
     final IAxis dateAxis = AxisUtils.findDateAxis( container.getAxes() );
     final Date date = (Date) container.get( dateAxis );
@@ -70,21 +80,50 @@ public class ValidateRuecksprungVisitor implements IObservationVisitor
     {
       if( date.before( m_lastDate ) )
       {
-        m_ruecksprung = true;
-        throw new CancelVisitorException();
+        if( m_ptrRuecksprung == -1 )
+          m_ptrRuecksprung = container.getIndex();
       }
-    }
+      else
+      {
+        if( m_ptrRuecksprung != -1 )
+        {
+          m_rueckspruenge.add( Range.between( m_ptrRuecksprung, container.getIndex() ) );
+          m_ptrRuecksprung = -1;
 
-    m_lastDate = date;
+          final SimpleDateFormat sdf = new SimpleDateFormat( Messages.getString( "dd.MM.yyyy HH:mm:ss" ) );
+          final TimeZone timezone = MetadataHelper.getTimeZone( container.getMetaData(), KalypsoCorePlugin.getDefault().getTimeZone().getID() );
+          sdf.setTimeZone( timezone );
+
+          final String msg = String.format( "Zeitlicher Rücksprung im Bereich: %s - %s", sdf.format( m_lastDate ), sdf.format( date ) );
+          m_stati.add( IStatus.WARNING, msg );
+        }
+
+        m_lastDate = date;
+      }
+
+    }
+    else
+      m_lastDate = date;
+
   }
 
   public boolean hasRuecksprung( )
   {
-    return m_ruecksprung;
+    return !m_rueckspruenge.isEmpty();
   }
 
   public Date getLastDate( )
   {
     return m_lastDate;
+  }
+
+  public Set<Range<Integer>> getRueckspruenge( )
+  {
+    return m_rueckspruenge;
+  }
+
+  public IStatus getStatus( )
+  {
+    return m_stati.asMultiStatus( "Es wurden zeitliche Rücksprünge entdeckt" );
   }
 }
