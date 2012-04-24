1e658a0942f4dcd87b9482649c1afadfe6300205
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-04-24 09:02:10 +0000

    Incomplete - task : grasverdunstung

--- trunk/KalypsoModelNA/src/org/kalypso/model/hydrology/operation/evaporation/LandbasedEvaporationCalculator.java
+++ trunk/KalypsoModelNA/src/org/kalypso/model/hydrology/operation/evaporation/LandbasedEvaporationCalculator.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.hydrology.operation.evaporation;
 
@@ -54,10 +54,10 @@ public class LandbasedEvaporationCalculator extends AbstractEvaporationCalculato
   private double m_latitude = 54.00;
 
   /* Albedo */
-// private static final double ALBEDO = 0.23;
+  private static final double ALBEDO = 0.23;
 
   /* Stefan Boltzmann Konstante (DVWK - Formel 5.27) */
-// private static final double BOLTZMANN_CONSTANT = 0.49 * Math.pow( 10.0, -6.0 );
+  private static final double BOLTZMANN_CONSTANT = 0.49 * Math.pow( 10.0, -6.0 );
 
   public LandbasedEvaporationCalculator( final ITimeseriesCache humidity, final ITimeseriesCache sunshine, final ITimeseriesCache temperature, final ITimeseriesCache windVelocity, final DateRange daterange )
   {
@@ -79,10 +79,11 @@ public class LandbasedEvaporationCalculator extends AbstractEvaporationCalculato
     final double s0 = 12.3 + rohSinus * (4.3 + (m_latitude - 51.0) / 6.0);
     final double rg = r0 * (0.19 + 0.55 * (sunshine / s0));
 
-// final double rn = (1.0 - ALBEDO) * rg - BOLTZMANN_CONSTANT * Math.pow( 237.15 + temperature, 4.0 ) * (0.1 + 0.9
-// * (sunshine / s0)) * (0.34 - 0.044 * Math.pow( e, 0.5 ));
+    final double rn = (1.0 - ALBEDO) * rg - BOLTZMANN_CONSTANT * Math.pow( 237.15 + temperature, 4.0 ) * (0.1 + 0.9 * (sunshine / s0)) * (0.34 - 0.044 * Math.pow( e, 0.5 ));
 
-    final double etN = s * (0.6 * rg / l) + 0.655 * (3.75 / (temperature + 273.0)) * windVelocity * (es - e);
+    final double g = 0.6 * rg / rn * -1;
+
+    final double etN = s * ((rn - g) / l) + 0.655 * (3.75 / (temperature + 273.0)) * windVelocity * (es - e);
     final double etT = s + 0.655 * (1 + 0.34 * windVelocity);
 
     final double et0 = etN / etT;
