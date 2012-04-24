5c4325aceb44a933a4ee87557a6b7565c1368ce8
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-04-24 11:04:32 +0000

    Incomplete - task : grasverdunstung

--- trunk/KalypsoModelNA/src/org/kalypso/model/hydrology/operation/evaporation/LandbasedEvaporationCalculator.java
+++ trunk/KalypsoModelNA/src/org/kalypso/model/hydrology/operation/evaporation/LandbasedEvaporationCalculator.java
@@ -67,6 +67,40 @@ public class LandbasedEvaporationCalculator extends AbstractEvaporationCalculato
   @Override
   protected Double doCalculate( final double humidity, final double sunshine, final double temperature, final double windVelocity, final Calendar date )
   {
+
+    return doCalculateWendling( humidity, sunshine, temperature, windVelocity, date );
+
+  }
+
+  protected Double doCalculateWendling( final double humidity, final double sunshine, final double temperature, final double windVelocity, final Calendar date )
+  {
+    final double es = 6.11 * Math.pow( 10.0, 7.48 * temperature / (237.0 + temperature) );
+    final double e = es * (humidity / 100.0);
+    final double l = 249.8 - 0.242 * temperature;
+    final double s = es * (4284.0 / Math.pow( 243.12 + temperature, 2.0 ));
+
+    final double roh = 0.0172 * date.get( Calendar.DAY_OF_YEAR ) - 1.39;
+    final double rohSinus = Math.sin( roh );
+
+    final double r0 = 245.0 * (9.9 + 7.08 * rohSinus + 0.18 * (m_latitude - 51.0) * (rohSinus - 1.0));
+    final double s0 = 12.3 + rohSinus * (4.3 + (m_latitude - 51.0) / 6.0);
+    final double rg = r0 * (0.19 + 0.55 * (sunshine / s0));
+// final double rng = 0.6 * rg;
+
+    final double rn = (1.0 - ALBEDO) * rg - BOLTZMANN_CONSTANT * (Math.pow( 237.15 + temperature, 4.0 ) * (0.1 + 0.9 * (sunshine / s0)) * (0.34 - 0.044 * Math.pow( e, 0.5 )));
+
+    final double fT = es / s * (0.655 * 3.75 / (temperature + 273.0));
+    final double gTV = s / (s + 0.655 * (1.0 + 0.34 * windVelocity));
+
+    final double et0 = gTV * (rn / l + fT * 24.0 * windVelocity * (1.0 - humidity / 100.0));
+    if( et0 > 0.0 )
+      return et0;
+
+    return 0.0;
+  }
+
+  private Double doCalculateFAO( final double humidity, final double sunshine, final double temperature, final double windVelocity, final Calendar date )
+  {
     final double es = 6.11 * Math.pow( 10.0, 7.48 * temperature / (237.0 + temperature) );
     final double e = es * (humidity / 100.0);
     final double l = 249.8 - 0.242 * temperature;
@@ -79,11 +113,12 @@ public class LandbasedEvaporationCalculator extends AbstractEvaporationCalculato
     final double s0 = 12.3 + rohSinus * (4.3 + (m_latitude - 51.0) / 6.0);
     final double rg = r0 * (0.19 + 0.55 * (sunshine / s0));
 
-    final double rn = (1.0 - ALBEDO) * rg - BOLTZMANN_CONSTANT * Math.pow( 237.15 + temperature, 4.0 ) * (0.1 + 0.9 * (sunshine / s0)) * (0.34 - 0.044 * Math.pow( e, 0.5 ));
+// final double rn = (1.0 - ALBEDO) * rg - BOLTZMANN_CONSTANT * (Math.pow( 237.15 + temperature, 4.0 ) * (0.1 + 0.9 *
+// (sunshine / s0)) * (0.34 - 0.044 * Math.pow( e, 0.5 )));
 
-    final double g = 0.6 * rg / rn * -1;
+    final double rng = 0.6 * rg;
 
-    final double etN = s * ((rn - g) / l) + 0.655 * (3.75 / (temperature + 273.0)) * windVelocity * (es - e);
+    final double etN = s * (rng / l) + 0.655 * (3.75 / (temperature + 273.0)) * windVelocity * (es - e);
     final double etT = s + 0.655 * (1 + 0.34 * windVelocity);
 
     final double et0 = etN / etT;
