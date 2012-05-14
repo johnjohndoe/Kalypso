d506f4e46313f2d5679350ddb7fb92c334a95430
dirk.kuch <dirk.kuch@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-05-14 09:40:51 +0000

    Incomplete - task : Result Tree View

---
+++ trunk/KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/results/view/tree/HydrologyNodeParameterUiHandler.java
@@ -0,0 +1,109 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ *
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ *
+ *  and
+ *
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Contact:
+ *
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.ui.rrm.internal.results.view.tree;
+
+import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.kalypso.commons.databinding.IDataBinding;
+import org.kalypso.model.hydrology.binding.model.nodes.Node;
+import org.kalypso.ui.rrm.internal.results.view.base.KalypsoHydrologyResults.NODE_RESULT_TYPE;
+import org.kalypso.ui.rrm.internal.utils.featureTree.AbstractTreeNodeUiHandler;
+
+/**
+ * @author Dirk Kuch
+ */
+public class HydrologyNodeParameterUiHandler extends AbstractTreeNodeUiHandler
+{
+
+  private final NODE_RESULT_TYPE m_type;
+
+  private final Node m_node;
+
+  public HydrologyNodeParameterUiHandler( final Node node, final NODE_RESULT_TYPE type )
+  {
+    m_node = node;
+    m_type = type;
+  }
+
+  @Override
+  public String getTypeLabel( )
+  {
+    return m_type.getLabel();
+  }
+
+  @Override
+  public String getTreeLabel( )
+  {
+    return m_type.getLabel();
+  }
+
+  @Override
+  public ImageDescriptor getTreeImage( )
+  {
+    return m_type.getImage();
+  }
+
+  /**
+   * @see org.kalypso.ui.rrm.internal.utils.featureTree.AbstractTreeNodeUiHandler#createPropertiesControl(org.eclipse.swt.widgets.Composite,
+   *      org.kalypso.commons.databinding.IDataBinding, org.eclipse.jface.action.ToolBarManager)
+   */
+  @Override
+  protected Control createPropertiesControl( final Composite parent, final IDataBinding binding, final ToolBarManager sectionToolbar )
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /**
+   * @see org.kalypso.ui.rrm.internal.utils.featureTree.AbstractTreeNodeUiHandler#createHyperlinks(org.eclipse.ui.forms.widgets.FormToolkit,
+   *      org.eclipse.swt.widgets.Composite)
+   */
+  @Override
+  protected void createHyperlinks( final FormToolkit toolkit, final Composite actionPanel )
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+}
