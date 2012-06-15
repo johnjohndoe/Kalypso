9c2d9619f929ae2b6d24d67e869b0f6ada34aa1e
holger.albert <holger.albert@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-06-15 08:08:28 +0000

    branched for installation on 23.05.2012

---
+++ branches/hwv_11-02_2012-06-15_sn/KalypsoModelNA/src/org/kalypso/model/hydrology/internal/preprocessing/NaModelTweaker.java
@@ -0,0 +1,300 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.model.hydrology.internal.preprocessing;
+
+import org.kalypso.model.hydrology.binding.model.Branching;
+import org.kalypso.model.hydrology.binding.model.BranchingWithNode;
+import org.kalypso.model.hydrology.binding.model.Catchment;
+import org.kalypso.model.hydrology.binding.model.Channel;
+import org.kalypso.model.hydrology.binding.model.KontZufluss;
+import org.kalypso.model.hydrology.binding.model.NaModell;
+import org.kalypso.model.hydrology.binding.model.Node;
+import org.kalypso.model.hydrology.binding.model.VirtualChannel;
+import org.kalypso.ogc.sensor.util.ZmlLink;
+import org.kalypso.zml.obslink.TimeseriesLinkType;
+import org.kalypsodeegree.model.feature.IFeatureBindingCollection;
+
+/**
+ * Before any ascii files are written, the modell.gml (calcCase.gml) gets tweaked by this class.<br/>
+ * 
+ * @author Gernot Belger
+ */
+public class NaModelTweaker
+{
+  private final NaModell m_naModel;
+
+  private final Node m_rootNode;
+
+  public NaModelTweaker( final NaModell naModel, final Node rootNode )
+  {
+    m_naModel = naModel;
+    m_rootNode = rootNode;
+  }
+
+  /**
+   * update workspace and do some tricks in order to fix some things the fortran-kernel can not handle for now
+   * 
+   * @param workspace
+   * @throws Exception
+   */
+  public void tweakModel( ) throws Exception
+  {
+    updateGWNet();
+    updateNode2NodeNet();
+    updateZuflussNet();
+    updateResultAsZuflussNet();
+  }
+
+  /**
+   * Updates workspace, so that interflow and channelflow dependencies gets optimized <br>
+   * Groundwater flow can now run in opposite direction to channel flow.<br>
+   * before: <code>
+   * 
+   *     -C-o (existing channel c with catchment T and downstream node o)
+   *      ^
+   *      T
+   * 
+   * </code> after: <code>
+   * 
+   *     -C-o 
+   *        |
+   *        V<T  (new virtual channel with existing catchment T, downstream node o and no upstream node)
+   * 
+   * </code>
+   * 
+   * @param workspace
+   */
+  private void updateGWNet( )
+  {
+    final IFeatureBindingCollection<Catchment> catchments = m_naModel.getCatchments();
+    for( final Catchment catchment : catchments )
+    {
+      final Channel channel = catchment.getChannel();
+      if( channel != null )
+      {
+        final Node node = channel.getDownstreamNode();
+
+        /* Create new channel and relocate catchment to the new channel. */
+        final Channel newChannel = m_naModel.getChannels().addNew( VirtualChannel.FEATURE_VIRTUAL_CHANNEL );
+        catchment.setChannel( newChannel );
+
+        /* Connect the new channel into the net */
+        newChannel.setDownstreamNode( node );
+      }
+    }
+  }
+
+  /**
+   * before: <br>
+   * <code>
+   * 
+   * Node1 O <---  O Node2
+   * 
+   * </code> after: <br>
+   * <code>
+   * 
+   * Node1 O <--- newVChannel <-- newNode O <-- newVChannel
+   *                                      A
+   *                                      |
+   *                                      O-- Node2
+   * 
+   * </code>
+   * 
+   * @param workspace
+   * @throws Exception
+   */
+  private void updateNode2NodeNet( ) throws Exception
+  {
+    final IFeatureBindingCollection<Node> nodes = m_naModel.getNodes();
+    // Copy to array, as the list is manipulated on the fly.
+    final Node[] nodeArray = nodes.toArray( new Node[nodes.size()] );
+    for( final Node node : nodeArray )
+    {
+      final Branching branching = node.getBranching();
+      if( branching != null )
+      {
+        if( branching instanceof BranchingWithNode )
+        {
+          final BranchingWithNode branchingWithNode = (BranchingWithNode) branching;
+
+          final Node targetNode = branchingWithNode.getNode();
+          if( targetNode == null )
+          {
+            final String message = String.format( "Node not set for branching in Node '%s'", node.getName() );
+            throw new NAPreprocessorException( message );
+          }
+
+          final Node newNode = buildVChannelNet( targetNode );
+          branchingWithNode.setNode( newNode );
+        }
+      }
+    }
+  }
+
+  /**
+   * put one more VChannel to each Q-source, so that this discharge will appear in the result of the connected node <br>
+   * zml inflow <br>
+   * before: <br>
+   * <code>
+   * |Channel| <- o(1) <- input.zml <br>
+   * </code><br>
+   * now: <br>
+   * <code>
+   * |Channel| <- o(1) <- |VChannel (new)| <- o(new) <- |VChannel (new)|
+   *                                          A- input.zml <br>
+   * </code> constant inflow <br>
+   * before: <br>
+   * <code>
+   * |Channel| <- o(1) <- Q(constant) <br>
+   * </code><br>
+   * now: <br>
+   * <code>
+   * |Channel| <- o(1) <- |VChannel (new)| <- o(new) <- |VChannel (new)|
+   *                                          A- Q(constant)<br>
+   * </code>
+   * 
+   * @param workspace
+   * @throws Exception
+   */
+  private void updateZuflussNet( ) throws Exception
+  {
+    final IFeatureBindingCollection<Node> nodes = m_naModel.getNodes();
+    final Node[] nodeArray = nodes.toArray( new Node[nodes.size()] );
+    for( final Node node : nodeArray )
+    {
+      final TimeseriesLinkType zuflussLink = node.getZuflussLink();
+      if( zuflussLink != null )
+      {
+        final Node newNode = buildVChannelNet( node );
+
+        // move zufluss-property to new node
+        node.setZuflussLink( null );
+        newNode.setZuflussLink( zuflussLink );
+
+        final Boolean synteticZufluss = node.isSynteticZufluss();
+        newNode.setIsSynteticZufluss( synteticZufluss );
+      }
+
+      final Branching branching = node.getBranching();
+      if( branching instanceof KontZufluss )
+      {
+        // update zufluss
+        final Node newNode = buildVChannelNet( node );
+        // move constant-inflow to new node
+        node.setBranching( null );
+        newNode.setBranching( branching );
+      }
+    }
+  }
+
+  /**
+   * if results exists (from a former simulation) for a node, use this results as input, later the upstream nodes will
+   * be ignored for calculation
+   * 
+   * @param workspace
+   * @throws Exception
+   */
+  private void updateResultAsZuflussNet( ) throws Exception
+  {
+    final IFeatureBindingCollection<Node> nodes = m_naModel.getNodes();
+    final Node[] nodeArray = nodes.toArray( new Node[nodes.size()] );
+    for( final Node node : nodeArray )
+    {
+      final ZmlLink resultLink = node.getResultAsInflowLinkChecked( m_rootNode );
+      if( resultLink != null )
+      {
+        // disconnect everything upstream (channel -> node)
+        final Channel[] upstreamChannels = node.findUpstreamChannels();
+        for( final Channel channel : upstreamChannels )
+        {
+          final Node newEndNode = nodes.addNew( Node.FEATURE_NODE );
+          channel.setDownstreamNode( newEndNode );
+        }
+
+        // add as zufluss
+        final Node newNode = buildVChannelNet( node );
+        newNode.setZuflussLink( resultLink.getTimeseriesLink() );
+
+        final Boolean isSyntetic = node.isSynteticZufluss();
+        newNode.setIsSynteticZufluss( isSyntetic );
+      }
+    }
+  }
+
+  /**
+   * before: <code>
+   * 
+   *     o(existing)
+   * 
+   * </code> after: <code>
+   * 
+   *  |new Channel3|
+   *     |
+   *     V
+   *     o(new Node2)  (return value)
+   *     |
+   *     V
+   *  |new Channel1|
+   *     |
+   *     V
+   *     o(existing)
+   * 
+   * </code>
+   */
+  private Node buildVChannelNet( final Node existingNode ) throws Exception
+  {
+    final IFeatureBindingCollection<Node> nodes = m_naModel.getNodes();
+    final IFeatureBindingCollection<Channel> channels = m_naModel.getChannels();
+
+    // add to collections:
+    final Channel newChannel1 = channels.addNew( VirtualChannel.FEATURE_VIRTUAL_CHANNEL );
+    final Channel newChannel3 = channels.addNew( VirtualChannel.FEATURE_VIRTUAL_CHANNEL );
+    final Node newNode2 = nodes.addNew( Node.FEATURE_NODE );
+
+    /* Create network */
+    newChannel3.setDownstreamNode( newNode2 );
+    newNode2.setDownstreamChannel( newChannel1 );
+    newChannel1.setDownstreamNode( existingNode );
+
+    return newNode2;
+  }
+
+}
---
+++ branches/hwv_11-02_2012-06-15_sn/KalypsoModelNA/src/org/kalypso/model/hydrology/internal/test/resources/demoModel_WithSuds/asciiAfterCalc/start/error.gml
@@ -0,0 +1,195 @@
+<?xml version="1.0" encoding="UTF-8"?>
+ <LogMessage xmlns="http://www.tuhh.de/NAFortranLog" xmlns:gml="http://www.opengis.net/gml" 
+xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:zmlinline="inline.zml.kalypso.org" 
+xmlns:obslink="obslink.zml.kalypso.org" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+ <log>
+ <record>
+   <level>WARNING </level>
+   <class></class>
+   <method>isov</method>
+   <message>Unit Hydrograph wird korrigiert,da Summe UH-Ordinaten &lt; 1 (Grenzkriterium Summe&lt;0.95).</message>
+   <element>Teilgebiet       4503</element>
+   <param>Summe=         0.0000;Retentionskonstante=      6000.0000</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>WARNING </level>
+   <class></class>
+   <method>isov</method>
+   <message>Unit Hydrograph wird korrigiert,da Summe UH-Ordinaten &lt; 1 (Grenzkriterium Summe&lt;0.95).</message>
+   <element>Teilgebiet       4500</element>
+   <param>Summe=         0.0000;Retentionskonstante=      6000.0000</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>WARNING </level>
+   <class></class>
+   <method>isov</method>
+   <message>Unit Hydrograph wird korrigiert,da Summe UH-Ordinaten &lt; 1 (Grenzkriterium Summe&lt;0.95).</message>
+   <element>Teilgebiet       4501</element>
+   <param>Summe=         0.0000;Retentionskonstante=      6000.0000</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>WARNING </level>
+   <class></class>
+   <method>isov</method>
+   <message>Unit Hydrograph wird korrigiert,da Summe UH-Ordinaten &lt; 1 (Grenzkriterium Summe&lt;0.95).</message>
+   <element>Teilgebiet       4502</element>
+   <param>Summe=         0.0000;Retentionskonstante=      6000.0000</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>WARNING </level>
+   <class></class>
+   <method>isov</method>
+   <message>Unit Hydrograph wird korrigiert,da Summe UH-Ordinaten &lt; 1 (Grenzkriterium Summe&lt;0.95).</message>
+   <element>Teilgebiet       4506</element>
+   <param>Summe=         0.0000;Retentionskonstante=      6000.0000</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>WARNING </level>
+   <class></class>
+   <method>isov</method>
+   <message>Unit Hydrograph wird korrigiert,da Summe UH-Ordinaten &lt; 1 (Grenzkriterium Summe&lt;0.95).</message>
+   <element>Teilgebiet       4504</element>
+   <param>Summe=         0.0000;Retentionskonstante=      6000.0000</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>WARNING </level>
+   <class></class>
+   <method>gerinne</method>
+   <message>Der Abfluss ueberschreitet den angegebenen max. Abfluss der Kalinin-Miljukov Parameter!</message>
+   <element>Strang       4501</element>
+   <param>qmax=         0.5992</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>INFO    </level>
+   <class></class>
+   <method>gerinne</method>
+   <message>Unit Hydrograph wird normiert (Grenzkriterium Summe&lt;0.83).</message>
+   <element>Strang       4501</element>
+   <param>Speicheranzahl=         0.3318;Retentionskonstante=         0.2198;Normierungsfaktor=         1.2226;Summe UH-Ordinaten vor Normierung=         0.8179</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>INFO    </level>
+   <class></class>
+   <method>gerinne</method>
+   <message>Unit Hydrograph wird normiert (Grenzkriterium Summe&lt;0.83).</message>
+   <element>Strang       4501</element>
+   <param>Speicheranzahl=         0.3019;Retentionskonstante=         0.2130;Normierungsfaktor=         1.2778;Summe UH-Ordinaten vor Normierung=         0.7826</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>INFO    </level>
+   <class></class>
+   <method>gerinne</method>
+   <message>Unit Hydrograph wird normiert (Grenzkriterium Summe&lt;0.83).</message>
+   <element>Strang       4501</element>
+   <param>Speicheranzahl=         0.2510;Retentionskonstante=         0.4799;Normierungsfaktor=         1.3047;Summe UH-Ordinaten vor Normierung=         0.7665</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>INFO    </level>
+   <class></class>
+   <method>gerinne</method>
+   <message>Unit Hydrograph wird normiert (Grenzkriterium Summe&lt;0.83).</message>
+   <element>Strang       4501</element>
+   <param>Speicheranzahl=         0.2969;Retentionskonstante=         0.1614;Normierungsfaktor=         1.3204;Summe UH-Ordinaten vor Normierung=         0.7573</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>INFO    </level>
+   <class></class>
+   <method>gerinne</method>
+   <message>Unit Hydrograph wird normiert (Grenzkriterium Summe&lt;0.83).</message>
+   <element>Strang       4501</element>
+   <param>Speicheranzahl=         0.2455;Retentionskonstante=         0.0491;Normierungsfaktor=         1.7364;Summe UH-Ordinaten vor Normierung=         0.5759</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>WARNING </level>
+   <class></class>
+   <method>gerinne</method>
+   <message>Der Abfluss ueberschreitet den angegebenen max. Abfluss der Kalinin-Miljukov Parameter!</message>
+   <element>Strang       4506</element>
+   <param>qmax=         0.5992</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>INFO    </level>
+   <class></class>
+   <method>gerinne</method>
+   <message>Unit Hydrograph wird normiert (Grenzkriterium Summe&lt;0.83).</message>
+   <element>Strang       4506</element>
+   <param>Speicheranzahl=         0.3318;Retentionskonstante=         0.2198;Normierungsfaktor=         1.2226;Summe UH-Ordinaten vor Normierung=         0.8179</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>INFO    </level>
+   <class></class>
+   <method>gerinne</method>
+   <message>Unit Hydrograph wird normiert (Grenzkriterium Summe&lt;0.83).</message>
+   <element>Strang       4506</element>
+   <param>Speicheranzahl=         0.3019;Retentionskonstante=         0.2130;Normierungsfaktor=         1.2778;Summe UH-Ordinaten vor Normierung=         0.7826</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>INFO    </level>
+   <class></class>
+   <method>gerinne</method>
+   <message>Unit Hydrograph wird normiert (Grenzkriterium Summe&lt;0.83).</message>
+   <element>Strang       4506</element>
+   <param>Speicheranzahl=         0.2510;Retentionskonstante=         0.4799;Normierungsfaktor=         1.3047;Summe UH-Ordinaten vor Normierung=         0.7665</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>INFO    </level>
+   <class></class>
+   <method>gerinne</method>
+   <message>Unit Hydrograph wird normiert (Grenzkriterium Summe&lt;0.83).</message>
+   <element>Strang       4506</element>
+   <param>Speicheranzahl=         0.2969;Retentionskonstante=         0.1614;Normierungsfaktor=         1.3204;Summe UH-Ordinaten vor Normierung=         0.7573</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>INFO    </level>
+   <class></class>
+   <method>gerinne</method>
+   <message>Unit Hydrograph wird normiert (Grenzkriterium Summe&lt;0.83).</message>
+   <element>Strang       4506</element>
+   <param>Speicheranzahl=         0.2455;Retentionskonstante=         0.0491;Normierungsfaktor=         1.7364;Summe UH-Ordinaten vor Normierung=         0.5759</param>
+ </record>
+ </log>
+ <log>
+ <record>
+   <level>FINEST  </level>
+   <class></class>
+   <method>kalypso-na</method>
+   <message>Berechnung wurde ohne Fehler beendet!</message>
+   <element>       </element>
+   <param>=</param>
+ </record>
+ </log>
+ </LogMessage>
