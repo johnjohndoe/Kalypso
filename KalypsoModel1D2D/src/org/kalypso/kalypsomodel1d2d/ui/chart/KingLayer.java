50159f29898a55f26a8da4cc574f240b63be3a17
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-07-08 18:05:13 +0000

    Reducing data container stuff. For some cases it the abstration makes no sense.

--- KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/ui/chart/KingLayer.java
+++ KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/ui/chart/KingLayer.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.kalypsomodel1d2d.ui.chart;
 
@@ -44,6 +44,7 @@ import java.math.BigDecimal;
 
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Path;
+import org.kalypso.kalypsomodel1d2d.schema.binding.flowrel.IKingFlowRelation;
 
 import de.openali.odysseus.chart.factory.layer.AbstractChartLayer;
 import de.openali.odysseus.chart.framework.model.data.IDataRange;
@@ -53,19 +54,18 @@ import de.openali.odysseus.chart.framework.model.style.impl.StyleSet;
 
 /**
  * A layer which renders a King-Profile.
- * 
+ *
  * @author Gernot Belger
  */
 public class KingLayer extends AbstractChartLayer
 {
+  private final IKingFlowRelation m_kingRelation;
 
-  private final KingDataContainer m_data;
-
-  public KingLayer( final KingDataContainer data )
+  public KingLayer( final IKingFlowRelation kingRelation )
   {
     super( null,new StyleSet() );
 
-    m_data = data;
+    m_kingRelation = kingRelation;
   }
 
   @Override
@@ -81,7 +81,7 @@ public class KingLayer extends AbstractChartLayer
       /* Draw the bottom */
 
       /* Minimum requirement is the width */
-      final BigDecimal width = m_data.getKingRelation().getWidth();
+      final BigDecimal width = m_kingRelation.getWidth();
       if( width == null )
         return;
 
@@ -102,14 +102,11 @@ public class KingLayer extends AbstractChartLayer
 
   }
 
-  /**
-   * @see org.kalypso.swtchart.chart.layer.IChartLayer#getDomainRange()
-   */
   @Override
   public IDataRange< ? > getDomainRange( )
   {
-    final BigDecimal width = m_data.getKingRelation().getWidth();
-    final BigDecimal widthStorage = m_data.getKingRelation().getWidthStorage();
+    final BigDecimal width = m_kingRelation.getWidth();
+    final BigDecimal widthStorage = m_kingRelation.getWidthStorage();
 
     double widthProfile = 0.0;
     if( width != null )
@@ -126,25 +123,9 @@ public class KingLayer extends AbstractChartLayer
 
   }
 
-  
-  /**
-   * @see de.openali.odysseus.chart.framework.model.layer.IChartLayer#dispose()
-   */
-  @Override
-  public void dispose( )
-  {
-    // nothing to do
-
-  }
-
-  /**
-   * @see de.openali.odysseus.chart.framework.model.layer.IChartLayer#getTargetRange()
-   */
   @Override
   public IDataRange< ? > getTargetRange( final IDataRange< ? > range )
   {
-    // TODO Auto-generated method stub
     return null;
   }
-
 }
