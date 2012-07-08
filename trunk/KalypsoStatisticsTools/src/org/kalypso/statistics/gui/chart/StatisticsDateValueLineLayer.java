6a714b54646c101e05381b1fe7ddbb0cf2d8d845
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-07-08 18:03:45 +0000

    Some cleanup in layer stuff

--- trunk/KalypsoStatisticsTools/src/org/kalypso/statistics/gui/chart/StatisticsDateValueLineLayer.java
+++ trunk/KalypsoStatisticsTools/src/org/kalypso/statistics/gui/chart/StatisticsDateValueLineLayer.java
@@ -11,32 +11,38 @@ import org.kalypso.observation.result.TupleResult;
 import de.openali.odysseus.chart.framework.model.layer.ILayerProvider;
 import de.openali.odysseus.chart.framework.model.style.IStyleSet;
 
-public class StatisticsDateValueLineLayer extends TupleResultLineLayer {
+public class StatisticsDateValueLineLayer extends TupleResultLineLayer
+{
+  private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat( "dd.MM.yyyy HH:mm:ss" );
 
-	private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
-	private static final String TOOLTIP_FORMAT = "%s: %s %n%s: %s";
+  private static final String TOOLTIP_FORMAT = "%s: %s %n%s: %s";
 
-	public StatisticsDateValueLineLayer(ILayerProvider provider, TupleResultDomainValueData<?, ?> data, IStyleSet styleSet) {
-		super(provider, data, styleSet);
-	}
+  public StatisticsDateValueLineLayer( final ILayerProvider provider, final TupleResultDomainValueData< ? , ? > data, final IStyleSet styleSet )
+  {
+    super( provider, data, styleSet );
+  }
 
-	@Override
-	protected String getTooltip(int index) {
-		if (getValueData() == null)
-			return "";
-		final TupleResult tr = getValueData().getObservation().getResult();
-		final int targetComponentIndex = tr.indexOfComponent(getValueData().getTargetComponentName());
-		final int domainComponentIndex = tr.indexOfComponent(getValueData().getDomainComponentName());
-		final String targetComponentLabel = ComponentUtilities.getComponentLabel(tr.getComponent(targetComponentIndex));
-		final Object dateMillisObject = tr.get(index).getValue(domainComponentIndex);
-		final String domainComponentLabel = "Date/time";
-		final Object x;
-		if (dateMillisObject instanceof Long) {
-			x = SIMPLE_DATE_FORMAT.format(new Date((Long) dateMillisObject));
-		}else{
-			x = tr.get(index).getValue(domainComponentIndex);
-		}
-		final Object y = tr.get(index).getValue(targetComponentIndex);
-		return String.format(TOOLTIP_FORMAT, new Object[] { domainComponentLabel, x, targetComponentLabel, y });
-	}
+  @Override
+  protected String getTooltip( final int index )
+  {
+    if( getValueData() == null )
+      return "";
+    final TupleResult tr = getValueData().getObservation().getResult();
+    final int targetComponentIndex = tr.indexOfComponent( getValueData().getTargetComponentName() );
+    final int domainComponentIndex = tr.indexOfComponent( getValueData().getDomainComponentName() );
+    final String targetComponentLabel = ComponentUtilities.getComponentLabel( tr.getComponent( targetComponentIndex ) );
+    final Object dateMillisObject = tr.get( index ).getValue( domainComponentIndex );
+    final String domainComponentLabel = "Date/time";
+    final Object x;
+    if( dateMillisObject instanceof Long )
+    {
+      x = SIMPLE_DATE_FORMAT.format( new Date( (Long) dateMillisObject ) );
+    }
+    else
+    {
+      x = tr.get( index ).getValue( domainComponentIndex );
+    }
+    final Object y = tr.get( index ).getValue( targetComponentIndex );
+    return String.format( TOOLTIP_FORMAT, new Object[] { domainComponentLabel, x, targetComponentLabel, y } );
+  }
 }
