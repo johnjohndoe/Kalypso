6976cf9a5159928aaa6d585f818a11e15ab549fe
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-07-13 12:56:42 +0000

    Fixed: drag did drag more than was selected (caching problem)

--- trunk/KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/timeseries/view/dnd/MoveStationTransfer.java
+++ trunk/KalypsoUIRRM/src/org/kalypso/ui/rrm/internal/timeseries/view/dnd/MoveStationTransfer.java
@@ -2,49 +2,44 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ui.rrm.internal.timeseries.view.dnd;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -76,7 +71,6 @@ public class MoveStationTransfer extends ByteArrayTransfer
   public MoveStationTransfer( )
   {
     m_cache = CacheBuilder.newBuilder().expireAfterWrite( 2, TimeUnit.MINUTES ).build();
-
   }
 
   @Override
@@ -102,39 +96,29 @@ public class MoveStationTransfer extends ByteArrayTransfer
   @Override
   protected void javaToNative( final Object object, final TransferData transferData )
   {
-    if( object != null )
+    if( object == null )
+      return;
+
+    m_cache.cleanUp(); // clean old obsolete entries
+
+    final ITimeseries[] timeserieses = (ITimeseries[]) object;
+    final String[] identifiers = new String[timeserieses.length];
+
+    for( int i = 0; i < timeserieses.length; i++ )
     {
-      m_cache.cleanUp(); // clean old obsolete entries
+      final ITimeseries timeseries = timeserieses[i];
+      final String identifier = timeseries.getId();
 
-      final ITimeseries[] timeserieses = (ITimeseries[]) object;
-      for( final ITimeseries timeseries : timeserieses )
-      {
-        m_cache.put( timeseries.getId(), timeseries );
-      }
+      identifiers[i] = identifier;
 
-      final String[] identifiers = m_cache.asMap().keySet().toArray( new String[] {} );
-      final String transfer = Joiner.on( ';' ).join( identifiers ); //$NON-NLS-1$
-
-      final ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-      final DataOutputStream out = new DataOutputStream( byteOut );
-
-      byte[] bytes = null;
-      try
-      {
-        out.writeUTF( transfer );
-        out.close();
-        bytes = byteOut.toByteArray();
-      }
-      catch( final IOException e )
-      {
-        // when in doubt send nothing
-      }
-
-      if( bytes != null )
-      {
-        super.javaToNative( bytes, transferData );
-      }
+      m_cache.put( identifier, timeseries );
     }
+
+    final String transfer = Joiner.on( ';' ).join( identifiers ); //$NON-NLS-1$
+
+    final byte[] bytes = transfer.getBytes();
+
+    super.javaToNative( bytes, transferData );
   }
 
   /*
@@ -144,25 +128,18 @@ public class MoveStationTransfer extends ByteArrayTransfer
   protected Object nativeToJava( final TransferData transferData )
   {
     final byte[] bytes = (byte[]) super.nativeToJava( transferData );
-    final DataInputStream in = new DataInputStream( new ByteArrayInputStream( bytes ) );
+    final String transfer = new String( bytes );
 
-    try
+    final Set<ITimeseries> timeserieses = new LinkedHashSet<>();
+
+    final Iterable<String> identifiers = Splitter.on( ';' ).split( transfer ); //$NON-NLS-1$
+    for( final String identifier : identifiers )
     {
-      final Set<ITimeseries> timeserieses = new LinkedHashSet<>();
-
-      final Iterable<String> identifiers = Splitter.on( ';' ).split( in.readUTF() ); //$NON-NLS-1$
-      for( final String identifier : identifiers )
-      {
-        final ITimeseries timeseries = m_cache.getIfPresent( identifier );
-        if( Objects.isNotNull( timeseries ) )
-          timeserieses.add( timeseries );
-      }
-
-      return timeserieses.toArray( new ITimeseries[] {} );
+      final ITimeseries timeseries = m_cache.getIfPresent( identifier );
+      if( Objects.isNotNull( timeseries ) )
+        timeserieses.add( timeseries );
     }
-    catch( final IOException e )
-    {
-      return null;
-    }
+
+    return timeserieses.toArray( new ITimeseries[] {} );
   }
 }
