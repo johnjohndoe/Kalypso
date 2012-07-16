d9c9683f52057de9ba94684ec56cec3351ad0ea2
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-07-16 14:25:40 +0000

    Cleanup

--- KalypsoModelNA/src/org/kalypso/model/hydrology/internal/postprocessing/statistics/NAStatistics.java
+++ KalypsoModelNA/src/org/kalypso/model/hydrology/internal/postprocessing/statistics/NAStatistics.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- * 
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- * 
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.hydrology.internal.postprocessing.statistics;
 
@@ -284,37 +284,19 @@ public class NAStatistics implements INaStatistics
       case 3:
       case 5:
       {
-        final Double value = getDoubleValueFormatted( currentElement );
-        if( value == null )
-          return StringUtils.EMPTY;
+        final Double value = (Double) currentElement;
 
         // REMARK: using platform default (',' in german) because we want excel to open the file
+
+        // TODO: probably no fixable: if the user changes the decimal separator on his/her system to '.', but leaves the
+        // normal settings as german java is not abe to follow... so we will stil get a ',', but excel wil refuse to
+        // read the file.
+
         return String.format( "%.8f", value ); //$NON-NLS-1$
       }
     }
 
     throw new UnsupportedOperationException();
-  }
-
-  private Double getDoubleValueFormatted( final Object object )
-  {
-    if( object == null )
-      return null;
-
-    try
-    {
-      if( object instanceof Double )
-        return (Double) object;
-
-      if( object instanceof String )
-        return Double.parseDouble( (String) object );
-    }
-    catch( final Exception e )
-    {
-      Logger.getAnonymousLogger().log( Level.WARNING, e.getLocalizedMessage() );
-    }
-
-    return null;
   }
 
   public void add( final Feature resultFeature, final NAStatisticsData data )
