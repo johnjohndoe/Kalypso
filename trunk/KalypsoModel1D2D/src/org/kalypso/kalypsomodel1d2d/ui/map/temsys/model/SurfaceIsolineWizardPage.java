80a96e51c843bbed5ce62b18458d5e1c926d9e0d
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-09-05 20:32:05 +0000

    Fixed potential NPE in model-tin-exporter

--- trunk/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/ui/map/temsys/model/SurfaceIsolineWizardPage.java
+++ trunk/KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/ui/map/temsys/model/SurfaceIsolineWizardPage.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.kalypsomodel1d2d.ui.map.temsys.model;
 
@@ -58,37 +58,29 @@ public class SurfaceIsolineWizardPage extends WizardPage
 {
   private final SurfaceLineSymbolizer m_symbolizer;
 
-  private BigDecimal m_minValue = null;
-
-  private BigDecimal m_maxValue = null;
-
   private final CreateModelTinWizardPage m_exportPage;
 
   protected SurfaceIsolineWizardPage( final String pageName, final SurfaceLineSymbolizer symbolizer, final CreateModelTinWizardPage exportPage )
   {
-    super( pageName, Messages.getString("org.kalypso.kalypsomodel1d2d.ui.map.temsys.model.SurfaceIsolineWizardPage0"), null ); //$NON-NLS-1$
+    super( pageName, Messages.getString( "org.kalypso.kalypsomodel1d2d.ui.map.temsys.model.SurfaceIsolineWizardPage0" ), null ); //$NON-NLS-1$
     m_symbolizer = symbolizer;
     m_exportPage = exportPage;
-    
-    setDescription( Messages.getString("org.kalypso.kalypsomodel1d2d.ui.map.temsys.model.SurfaceIsolineWizardPage1") ); //$NON-NLS-1$
+
+    setDescription( Messages.getString( "org.kalypso.kalypsomodel1d2d.ui.map.temsys.model.SurfaceIsolineWizardPage1" ) ); //$NON-NLS-1$
   }
 
-  /**
-   * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
-   */
   @Override
   public void createControl( final Composite parent )
   {
-    m_minValue = m_exportPage.getMin();
-    m_maxValue = m_exportPage.getMax();
-    
+    final BigDecimal minValue = m_exportPage.getMin();
+    final BigDecimal maxValue = m_exportPage.getMax();
+
     final Composite panel = new Composite( parent, SWT.NONE );
     panel.setLayout( new FillLayout() );
 
     final LineColorMap colorMap = m_symbolizer.getColorMap();
-    new LineColorMapEditorComposite( panel, SWT.NONE, colorMap, m_minValue, m_maxValue );
+    new LineColorMapEditorComposite( panel, SWT.NONE, colorMap, minValue, maxValue );
 
     setControl( panel );
   }
-
 }
