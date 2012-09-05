c93df6c39aa186c802cd8adcecd6acbbada55f56
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-09-05 20:44:35 +0000

    Fixed order of wspm layers

--- KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/chart/utils/TuhhLayers.java
+++ KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/chart/utils/TuhhLayers.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.wspm.tuhh.ui.chart.utils;
 
@@ -97,7 +97,7 @@ public final class TuhhLayers
     final PointMarkerLayer dbLayer = new PointMarkerLayer( profil, IWspmTuhhConstants.MARKER_TYP_DURCHSTROEMTE, styleProvider, 5, true );
     final RiverChannelLayer tfLayer = new RiverChannelLayer( profil, styleProvider, 15, false );
     final PointMarkerLayer bvLayer = new PointMarkerLayer( profil, IWspmTuhhConstants.MARKER_TYP_BORDVOLL, styleProvider, 25, false );
-    final IProfilChartLayer[] subLayers = new IProfilChartLayer[] { dbLayer, tfLayer, bvLayer };
+    final IProfilChartLayer[] subLayers = new IProfilChartLayer[] { bvLayer, tfLayer, dbLayer };
 
     return new DeviderTheme( profil, subLayers, cmScreen );
   }
@@ -106,8 +106,8 @@ public final class TuhhLayers
   {
     final RoughnessLayer ks = new RoughnessLayer( profil, IWspmPointProperties.POINT_PROPERTY_RAUHEIT_KS, styleProvider );
     final RoughnessLayer kst = new RoughnessLayer( profil, IWspmPointProperties.POINT_PROPERTY_RAUHEIT_KST, styleProvider );
-// final RoughnessLayer clazz = new RoughnessLayer( profil, IWspmPointProperties.POINT_PROPERTY_ROUGHNESS_CLASS,
-// styleProvider ); //TODO see todo in RoughnessLayser.getValue()
+    // final RoughnessLayer clazz = new RoughnessLayer( profil, IWspmPointProperties.POINT_PROPERTY_ROUGHNESS_CLASS,
+    // styleProvider ); //TODO see todo in RoughnessLayser.getValue()
 
     final IProfilChartLayer[] subLayers = new IProfilChartLayer[] { kst, ks };
 
