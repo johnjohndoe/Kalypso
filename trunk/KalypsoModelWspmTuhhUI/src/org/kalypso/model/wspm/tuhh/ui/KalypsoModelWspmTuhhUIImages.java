80d353ca9ea1b2c116ad3af7ff2c354bc6d97d23
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-10-07 16:35:39 +0000

    Nice: label decorators for bulding profiles.

--- trunk/KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/KalypsoModelWspmTuhhUIImages.java
+++ trunk/KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/KalypsoModelWspmTuhhUIImages.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.wspm.tuhh.ui;
 
@@ -44,7 +44,7 @@ import org.kalypso.commons.eclipse.core.runtime.PluginImageProvider.ImageKey;
 
 /**
  * Utility class for handling images in this plugin.
- * 
+ *
  * @author Gernot Belger
  */
 public enum KalypsoModelWspmTuhhUIImages implements ImageKey
@@ -56,7 +56,14 @@ public enum KalypsoModelWspmTuhhUIImages implements ImageKey
   REMOVE_CSV_EXPORT_COLUMN("icons/obj16/removeCsvExportColumn.gif"), //$NON-NLS-1$
   REMOVE_CSV_CONFIGURATION("icons/obj16/removeCsvConfiguration.gif"), //$NON-NLS-1$
   ROUGHNESS_PANEL_ADD("icons/obj16/roughnessPanelAdd.gif"), //$NON-NLS-1$
-  ROUGHNESS_PANEL_REMOVE("icons/obj16/roughnessPanelRemove.gif"); //$NON-NLS-1$
+  ROUGHNESS_PANEL_REMOVE( "icons/obj16/roughnessPanelRemove.gif" ), //$NON-NLS-1$
+
+  WEIR( "icons/obj16/weir.png" ), //$NON-NLS-1$
+  BRIDGE( "icons/obj16/bridge.png" ), //$NON-NLS-1$
+  CULVERT_OVAL( "icons/obj16/culvertOval.png" ), //$NON-NLS-1$
+  CULVERT_CIRCLE( "icons/obj16/culvertCircle.png" ), //$NON-NLS-1$
+  CULVERT_TRAPEZOID( "icons/obj16/culvertTrapezoid.png" ), //$NON-NLS-1$
+  CULVERT_MOUTH( "icons/obj16/culvertMouth.png" ); //$NON-NLS-1$
 
   private final String m_imagePath;
 
@@ -65,9 +72,6 @@ public enum KalypsoModelWspmTuhhUIImages implements ImageKey
     m_imagePath = imagePath;
   }
 
-  /**
-   * @see org.kalypso.informdss.KalypsoInformDSSImages.ImageKey#getImagePath()
-   */
   @Override
   public String getImagePath( )
   {
---
+++ trunk/KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/internal/gml/WspmGmlDecorator.java
@@ -0,0 +1,120 @@
+/** This file is part of Kalypso
+ *
+ *  Copyright (c) 2012 by
+ *
+ *  Björnsen Beratende Ingenieure GmbH, Koblenz, Germany (Bjoernsen Consulting Engineers), http://www.bjoernsen.de
+ *  Technische Universität Hamburg-Harburg, Institut für Wasserbau, Hamburg, Germany
+ *  (Technical University Hamburg-Harburg, Institute of River and Coastal Engineering), http://www.tu-harburg.de/wb/
+ *
+ *  Kalypso is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU Lesser General Public License (LGPL) as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  Kalypso is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with Kalypso.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.kalypso.model.wspm.tuhh.ui.internal.gml;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.IDecoration;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.ILightweightLabelDecorator;
+import org.kalypso.commons.eclipse.core.runtime.PluginImageProvider;
+import org.kalypso.model.wspm.core.gml.IProfileFeature;
+import org.kalypso.model.wspm.core.profil.IProfile;
+import org.kalypso.model.wspm.tuhh.core.profile.profileobjects.building.BuildingBruecke;
+import org.kalypso.model.wspm.tuhh.core.profile.profileobjects.building.BuildingEi;
+import org.kalypso.model.wspm.tuhh.core.profile.profileobjects.building.BuildingKreis;
+import org.kalypso.model.wspm.tuhh.core.profile.profileobjects.building.BuildingMaul;
+import org.kalypso.model.wspm.tuhh.core.profile.profileobjects.building.BuildingTrapez;
+import org.kalypso.model.wspm.tuhh.core.profile.profileobjects.building.BuildingWehr;
+import org.kalypso.model.wspm.tuhh.core.profile.profileobjects.building.ICulvertBuilding;
+import org.kalypso.model.wspm.tuhh.core.profile.profileobjects.building.IProfileBuilding;
+import org.kalypso.model.wspm.tuhh.ui.KalypsoModelWspmTuhhUIImages;
+import org.kalypso.model.wspm.tuhh.ui.KalypsoModelWspmTuhhUIPlugin;
+
+/**
+ * @author Gernot Belger
+ */
+public class WspmGmlDecorator implements ILightweightLabelDecorator
+{
+  @Override
+  public void dispose( )
+  {
+  }
+
+  @Override
+  public void addListener( final ILabelProviderListener listener )
+  {
+  }
+
+  @Override
+  public void removeListener( final ILabelProviderListener listener )
+  {
+  }
+
+  @Override
+  public boolean isLabelProperty( final Object element, final String property )
+  {
+    return true;
+  }
+
+  @Override
+  public void decorate( final Object element, final IDecoration decoration )
+  {
+    if( element instanceof IProfileFeature )
+      decorateProfile( (IProfileFeature)element, decoration );
+  }
+
+  private void decorateProfile( final IProfileFeature profileFeature, final IDecoration decoration )
+  {
+    final IProfile profile = profileFeature.getProfile();
+
+    final IProfileBuilding[] buildings = profile.getProfileObjects( IProfileBuilding.class );
+    for( final IProfileBuilding building : buildings )
+    {
+      decorateBuilding( building, decoration );
+    }
+  }
+
+  private void decorateBuilding( final IProfileBuilding building, final IDecoration decoration )
+  {
+    final String typeLabel = building.getTypeLabel();
+
+    /* suffix */
+    if( building instanceof ICulvertBuilding )
+      decoration.addSuffix( String.format( " (Durchlass - %s)", typeLabel ) );
+    else
+      decoration.addSuffix( String.format( " (%s)", typeLabel ) );
+
+    /* icon */
+    final ImageDescriptor buildingImage = getBuildingImage( building );
+    if( buildingImage != null )
+      decoration.addOverlay( buildingImage, IDecoration.REPLACE );
+  }
+
+  private ImageDescriptor getBuildingImage( final IProfileBuilding building )
+  {
+    final PluginImageProvider imageProvider = KalypsoModelWspmTuhhUIPlugin.getImageProvider();
+
+    if( building instanceof BuildingBruecke )
+      return imageProvider.getImageDescriptor( KalypsoModelWspmTuhhUIImages.BRIDGE );
+
+    if( building instanceof BuildingWehr )
+      return imageProvider.getImageDescriptor( KalypsoModelWspmTuhhUIImages.WEIR );
+
+    if( building instanceof BuildingKreis )
+      return imageProvider.getImageDescriptor( KalypsoModelWspmTuhhUIImages.CULVERT_CIRCLE );
+    if( building instanceof BuildingEi )
+      return imageProvider.getImageDescriptor( KalypsoModelWspmTuhhUIImages.CULVERT_OVAL );
+    if( building instanceof BuildingMaul )
+      return imageProvider.getImageDescriptor( KalypsoModelWspmTuhhUIImages.CULVERT_MOUTH );
+    if( building instanceof BuildingTrapez )
+      return imageProvider.getImageDescriptor( KalypsoModelWspmTuhhUIImages.CULVERT_TRAPEZOID );
+
+    return null;
+  }
+}
