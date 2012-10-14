bcd2194dda960d203d3fdcc8d302d79f97c107ce
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-10-14 17:42:44 +0000

    Fixed messages of  Knauf export

--- trunk/KalypsoModelWspmTuhhCore/src/org/kalypso/model/wspm/tuhh/core/profile/export/knauf/beans/builders/KnaufProfileBeanBuilder.java
+++ trunk/KalypsoModelWspmTuhhCore/src/org/kalypso/model/wspm/tuhh/core/profile/export/knauf/beans/builders/KnaufProfileBeanBuilder.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.wspm.tuhh.core.profile.export.knauf.beans.builders;
 
@@ -98,7 +98,7 @@ public final class KnaufProfileBeanBuilder extends AbstractKnaufProfileBeanBuild
     }
     else if( Objects.isNotNull( building ) )
     {
-      final String message = String.format( Messages.getString( "KnaufProfileBeanBuilder_0" ), m_profile.getStation(), building.getClass().getName() ); //$NON-NLS-1$
+      final String message = String.format( Messages.getString( "KnaufProfileBeanBuilder_0" ), m_profile.getStation(), building.getTypeLabel() ); //$NON-NLS-1$
       final Status status = new Status( IStatus.WARNING, KalypsoModelWspmTuhhCorePlugin.getID(), message );
       stati.add( status );
 
