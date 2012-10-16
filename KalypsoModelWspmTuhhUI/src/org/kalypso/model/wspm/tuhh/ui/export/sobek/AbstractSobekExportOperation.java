10201a40851384d490e93c2ae689190211fd0645
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-10-16 14:57:49 +0000

    Better warning message for sobek export

--- KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/export/sobek/AbstractSobekExportOperation.java
+++ KalypsoModelWspmTuhhUI/src/org/kalypso/model/wspm/tuhh/ui/export/sobek/AbstractSobekExportOperation.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.wspm.tuhh.ui.export.sobek;
 
@@ -61,9 +61,12 @@ public abstract class AbstractSobekExportOperation implements ISobekProfileExpor
 
   private final SobekExportInfo m_info;
 
-  public AbstractSobekExportOperation( final SobekExportInfo info )
+  private final String m_operationLabel;
+
+  public AbstractSobekExportOperation( final SobekExportInfo info, final String operationLabel )
   {
     m_info = info;
+    m_operationLabel = operationLabel;
   }
 
   protected SobekExportInfo getInfo( )
@@ -97,11 +100,11 @@ public abstract class AbstractSobekExportOperation implements ISobekProfileExpor
 
       close();
 
-      return m_stati.asMultiStatusOrOK( Messages.getString( "AbstractSobekProfileExportOperation_3" ), null ); //$NON-NLS-1$
+      return m_stati.asMultiStatusOrOK( m_operationLabel );
     }
     catch( final Exception e )
     {
-      final String message = String.format( Messages.getString( "AbstractSobekProfileExportOperation_2" ) ); //$NON-NLS-1$
+      final String message = String.format( "'%s' failed", m_operationLabel );
       final IStatus status = new Status( IStatus.ERROR, KalypsoModelWspmCorePlugin.getID(), message, e );
       throw new CoreException( status );
     }
