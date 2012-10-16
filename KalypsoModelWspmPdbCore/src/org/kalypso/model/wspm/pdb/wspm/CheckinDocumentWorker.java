128b6e6e43f17127b6079e0b4546fa343f33feac
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-10-16 16:30:49 +0000

    Fixed NPE for images without referenced document

--- KalypsoModelWspmPdbCore/src/org/kalypso/model/wspm/pdb/wspm/CheckinDocumentWorker.java
+++ KalypsoModelWspmPdbCore/src/org/kalypso/model/wspm/pdb/wspm/CheckinDocumentWorker.java
@@ -6,11 +6,11 @@
  *  Technische Universität Hamburg-Harburg, Institut für Wasserbau, Hamburg, Germany
  *  (Technical University Hamburg-Harburg, Institute of River and Coastal Engineering), http://www.tu-harburg.de/wb/
  *
- *  Kalypso is free software: you can redistribute it and/or modify it under the terms  
- *  of the GNU Lesser General Public License (LGPL) as published by the Free Software 
+ *  Kalypso is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU Lesser General Public License (LGPL) as published by the Free Software
  *  Foundation, either version 3 of the License, or (at your option) any later version.
  *
- *  Kalypso is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ *  Kalypso is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
  *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
  *
  *  You should have received a copy of the GNU Lesser General Public
@@ -121,6 +121,9 @@ public class CheckinDocumentWorker
 
   private String asName( final URI uri )
   {
+    if( uri == null )
+      return "-"; //$NON-NLS-1$
+
     final String unencoded = URIUtil.toUnencodedString( uri );
     return FilenameUtils.getName( unencoded );
   }
