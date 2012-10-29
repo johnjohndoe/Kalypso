1bbc3d94714a9997a884f235fa262850708412eb
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-10-29 08:53:09 +0000

    Fixed: NPE in 1d2dDemoModel

--- KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/schema/functions/Element1DTypeGeometry.java
+++ KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/schema/functions/Element1DTypeGeometry.java
@@ -13,32 +13,25 @@ import org.kalypsodeegree_impl.model.feature.FeaturePropertyFunction;
  */
 public class Element1DTypeGeometry extends FeaturePropertyFunction
 {
-  /**
-   * @see org.kalypsodeegree_impl.model.feature.FeaturePropertyFunction#init(java.util.Map)
-   */
   @Override
   public void init( final Map<String, String> properties )
   {
     // nothing to do
   }
 
-  /**
-   * @see org.kalypsodeegree.model.feature.IFeaturePropertyHandler#getValue(org.kalypsodeegree.model.feature.Feature, org.kalypso.gmlschema.property.IPropertyType, java.lang.Object)
-   */
   @Override
   public Object getValue( final Feature feature, final IPropertyType pt, final Object currentValue )
   {
     final IFE1D2DEdge edge = ((IElement1D)feature).getEdge();
+    if( edge == null )
+      return null;
+
     return edge.getGeometry();
   }
 
-  /**
-   * @see org.kalypsodeegree.model.feature.IFeaturePropertyHandler#setValue(org.kalypsodeegree.model.feature.Feature, org.kalypso.gmlschema.property.IPropertyType, java.lang.Object)
-   */
   @Override
   public Object setValue( final Feature feature, final IPropertyType pt, final Object valueToSet )
   {
     return valueToSet;
   }
-
 }
