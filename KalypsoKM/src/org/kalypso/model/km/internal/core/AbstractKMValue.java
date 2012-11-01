286ba9fdf94a14eb1bc21e869326079c0b527711
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-11-01 09:50:54 +0000

    Fixed some warnings

--- KalypsoKM/src/org/kalypso/model/km/internal/core/AbstractKMValue.java
+++ KalypsoKM/src/org/kalypso/model/km/internal/core/AbstractKMValue.java
@@ -19,13 +19,15 @@ abstract class AbstractKMValue implements IKMValue
   @Override
   public final String toString( )
   {
-    final Formatter formatter = new Formatter();
-    formatter.format( "\n Q (Gesamt):  %8.3f", getLowerQ() ); //$NON-NLS-1$
-    formatter.format( "\t k (Fluss):   %8.4f", getK() ); //$NON-NLS-1$
-    formatter.format( "\t n (Fluss):   %8.2f", getN() ); //$NON-NLS-1$
-    formatter.format( "\n Q (Vorland): %8.3f", getLowerQforeland() ); //$NON-NLS-1$
-    formatter.format( "\t k (Vorland): %8.4f", getKForeland() ); //$NON-NLS-1$
-    formatter.format( "\t n(Vorland):  %8.2f", getNForeland() ); //$NON-NLS-1$
-    return formatter.toString();
+    try( final Formatter formatter = new Formatter() )
+    {
+      formatter.format( "\n Q (Gesamt):  %8.3f", getLowerQ() ); //$NON-NLS-1$
+      formatter.format( "\t k (Fluss):   %8.4f", getK() ); //$NON-NLS-1$
+      formatter.format( "\t n (Fluss):   %8.2f", getN() ); //$NON-NLS-1$
+      formatter.format( "\n Q (Vorland): %8.3f", getLowerQforeland() ); //$NON-NLS-1$
+      formatter.format( "\t k (Vorland): %8.4f", getKForeland() ); //$NON-NLS-1$
+      formatter.format( "\t n(Vorland):  %8.2f", getNForeland() ); //$NON-NLS-1$
+      return formatter.toString();
+    }
   }
 }
