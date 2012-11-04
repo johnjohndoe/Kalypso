3bc905500aff98be78eba5649f00f318762e2440
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-11-04 17:18:37 +0000

    Bug fixme

--- KalypsoModelRisk/src/org/kalypso/risk/plugin/DamagePotentialThemeInfo.java
+++ KalypsoModelRisk/src/org/kalypso/risk/plugin/DamagePotentialThemeInfo.java
@@ -15,6 +15,9 @@ public class DamagePotentialThemeInfo extends CoverageThemeInfo
     final int digits = KalypsoRiskPlugin.getThemeInfoPrecision();
     final String i18Format = Messages.getString( "SpecificDamagePotentialMap.gismapview.themeInfoLabel" ); //$NON-NLS-1$
     final String customPrecision = String.format( "%%,.%df", digits ); //$NON-NLS-1$
+
+    // FIXME: this prohibits that the annuality is displayed in the info tooltip!
+
     return props.getProperty( PROP_FORMAT, i18Format.replaceAll( "\\[CUSTOM_PRECISION\\]", customPrecision ) ); //$NON-NLS-1$
   }
 
