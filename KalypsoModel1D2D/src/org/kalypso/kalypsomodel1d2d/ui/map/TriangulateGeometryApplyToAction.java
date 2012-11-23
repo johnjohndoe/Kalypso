ad3b2f72a0594dc6925d65341eac596a6240f927
skurzbach <skurzbach@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-11-23 14:37:26 +0000

    Ditch generator (first usable version)

--- KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/ui/map/TriangulateGeometryApplyToAction.java
+++ KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/ui/map/TriangulateGeometryApplyToAction.java
@@ -40,6 +40,10 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.kalypsomodel1d2d.ui.map;
 
+import org.eclipse.core.databinding.beans.BeansObservables;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.core.databinding.observable.value.IValueChangeListener;
+import org.eclipse.core.databinding.observable.value.ValueChangeEvent;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.action.Action;
 import org.eclipse.swt.widgets.Event;
@@ -50,31 +54,48 @@ import org.kalypso.core.status.StatusDialog;
 import org.kalypso.kalypsomodel1d2d.KalypsoModel1D2DPlugin;
 import org.kalypso.kalypsomodel1d2d.KalypsoModel1D2DUIImages;
 import org.kalypso.kalypsomodel1d2d.ui.i18n.Messages;
+import org.kalypso.kalypsomodel1d2d.ui.map.dikeditchgen.TriangulationBuilder;
+import org.kalypso.ogc.gml.mapmodel.CommandableWorkspace;
 
 /**
  * @author Gernot Belger
  */
 public class TriangulateGeometryApplyToAction extends Action
 {
-  private final TriangulateGeometryOperation m_operation;
+  private final TriangulationBuilder m_triangulationBuilder;
 
-  public TriangulateGeometryApplyToAction( final TriangulateGeometryOperation operation )
+  private final CommandableWorkspace m_workspace;
+
+  public TriangulateGeometryApplyToAction( final TriangulationBuilder triangulationBuilder, final CommandableWorkspace workspace )
   {
-    m_operation = operation;
-
+    m_triangulationBuilder = triangulationBuilder;
+    m_workspace = workspace;
     setText( Messages.getString( "org.kalypso.kalypsomodel1d2d.ui.map.grid.GridWidgetFace.9" ) ); //$NON-NLS-1$
     setToolTipText( Messages.getString( "TriangulateGeometryApplyToAction.0" ) ); //$NON-NLS-1$
 
     final PluginImageProvider imageProvider = KalypsoModel1D2DPlugin.getImageProvider();
     setImageDescriptor( imageProvider.getImageDescriptor( KalypsoModel1D2DUIImages.IMGKEY.OK ) );
+
+    final IObservableValue modelTin = BeansObservables.observeValue( triangulationBuilder, TriangulationBuilder.PROPERTY_TIN );
+    modelTin.addValueChangeListener( new IValueChangeListener()
+    {
+
+      @Override
+      public void handleValueChange( final ValueChangeEvent event )
+      {
+        setEnabled( event.diff.getNewValue() != null );
+      }
+    } );
+    
+    setEnabled( triangulationBuilder.getTin() != null );
   }
 
   @Override
   public void runWithEvent( final Event event )
   {
     final Shell shell = event.widget.getDisplay().getActiveShell();
-
-    final IStatus result = ProgressUtilities.busyCursorWhile( m_operation );
+    final TriangulateGeometryOperation operation = new TriangulateGeometryOperation( m_triangulationBuilder, m_workspace );
+    final IStatus result = ProgressUtilities.busyCursorWhile( operation );
     if( !result.isOK() )
       StatusDialog.open( shell, result, getText() );
   }
--- KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/ui/map/dikeditchgen/GenerateDikeOrDitchWidget.java
+++ KalypsoModel1D2D/src/org/kalypso/kalypsomodel1d2d/ui/map/dikeditchgen/GenerateDikeOrDitchWidget.java
@@ -19,18 +19,22 @@
 package org.kalypso.kalypsomodel1d2d.ui.map.dikeditchgen;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.nio.charset.Charset;
 
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.forms.widgets.ExpandableComposite;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.eclipse.ui.forms.widgets.Section;
@@ -38,13 +42,17 @@ import org.kalypso.afgui.model.Util;
 import org.kalypso.commons.command.ICommandTarget;
 import org.kalypso.commons.databinding.forms.DatabindingForm;
 import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
+import org.kalypso.contribs.eclipse.jface.action.ActionButton;
 import org.kalypso.kalypsomodel1d2d.KalypsoModel1D2DPlugin;
+import org.kalypso.kalypsomodel1d2d.schema.binding.discr.IFE1D2DNode;
 import org.kalypso.kalypsomodel1d2d.schema.binding.discr.IFEDiscretisationModel1d2d;
 import org.kalypso.kalypsomodel1d2d.ui.map.TriangulateGeometryComposite;
+import org.kalypso.kalypsomodel1d2d.ui.map.util.PointSnapper;
 import org.kalypso.ogc.gml.IKalypsoLayerModell;
 import org.kalypso.ogc.gml.map.IMapPanel;
 import org.kalypso.ogc.gml.map.utilities.MapUtilities;
 import org.kalypso.ogc.gml.map.utilities.tooltip.ToolTipRenderer;
+import org.kalypso.ogc.gml.map.widgets.builders.PolygonGeometryBuilder;
 import org.kalypso.ogc.gml.mapmodel.CommandableWorkspace;
 import org.kalypso.ogc.gml.widgets.AbstractWidget;
 import org.kalypso.shape.ShapeType;
@@ -57,6 +65,7 @@ import org.kalypso.ui.editor.mapeditor.views.IWidgetWithOptions;
 import org.kalypsodeegree.KalypsoDeegreePlugin;
 import org.kalypsodeegree.graphics.transformation.GeoTransform;
 import org.kalypsodeegree.model.geometry.GM_Point;
+import org.kalypsodeegree.model.geometry.GM_Polygon;
 import org.kalypsodeegree.model.geometry.GM_TriangulatedSurface;
 
 /**
@@ -68,25 +77,23 @@ public class GenerateDikeOrDitchWidget extends AbstractWidget implements IWidget
 
   private CommandableWorkspace m_discModelWorkspace;
 
-//  private PointSnapper m_pointSnapper;
+  private PointSnapper m_pointSnapper;
 
   private final ToolTipRenderer m_toolTipRenderer = new ToolTipRenderer();
 
-//  private LineGeometryBuilder m_networkBuilder;
+  private PolygonGeometryBuilder m_networkBuilder;
 
   private Point m_currentPoint;
 
-  final TriangulationBuilder m_tinBuilder = new TriangulationBuilder();
+  private TriangulationBuilder m_tinBuilder;
 
   private double m_currentZ = Double.NaN;
 
-  private CreateStructuredNetworkStrategy m_strategy = DEFAULT_STRATEGY;
+  private final CreateStructuredNetworkStrategy m_strategy = DEFAULT_STRATEGY;
 
   public GenerateDikeOrDitchWidget( )
   {
     super( "org.kalypso.model.1d2d.workflow.DikeDitchGen" ); //$NON-NLS-1$
-    m_tinBuilder.setMinAngle( 22.5 );
-    m_tinBuilder.setNoSteinerOnBoundary( true );
 
     // parameters
 //  final double outerLeftWidth = 60;
@@ -108,7 +115,7 @@ public class GenerateDikeOrDitchWidget extends AbstractWidget implements IWidget
     final IMapPanel mapPanel = getMapPanel();
     if( mapPanel != null )
     {
-      final Section section = toolkit.createSection( body, Section.EXPANDED | Section.TITLE_BAR );
+      final Section section = toolkit.createSection( body, ExpandableComposite.EXPANDED | ExpandableComposite.TITLE_BAR );
       section.setText( "Network options" );
       section.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, false ) );
 
@@ -119,10 +126,14 @@ public class GenerateDikeOrDitchWidget extends AbstractWidget implements IWidget
       // network layer
       final IKalypsoLayerModell mapModell = mapPanel.getMapModell();
       m_strategy.createControl( sectionComposite, toolkit, mapModell );
+
+      final CreateStructuredNetworkAction runStrategyAction = new CreateStructuredNetworkAction( m_tinBuilder, m_strategy );
+      final Button buttonRunStrategy = ActionButton.createButton( toolkit, sectionComposite, runStrategyAction );
+      buttonRunStrategy.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, false ) );
     }
 
     // triangulation parameters
-    final TriangulateGeometryComposite triangulateGeometryComposite = new TriangulateGeometryComposite( toolkit, binding, this, m_tinBuilder, m_discModelWorkspace );
+    final TriangulateGeometryComposite triangulateGeometryComposite = new TriangulateGeometryComposite( toolkit, binding, m_tinBuilder, m_discModelWorkspace );
     triangulateGeometryComposite.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true ) );
 
     return body;
@@ -133,11 +144,12 @@ public class GenerateDikeOrDitchWidget extends AbstractWidget implements IWidget
   {
     super.activate( commandPoster, mapPanel );
     m_discModelWorkspace = Util.getCommandableWorkspace( IFEDiscretisationModel1d2d.class );
-//    final IKalypsoLayerModell mapModell = mapPanel.getMapModell();
-//    final String coordinateSystem = mapModell.getCoordinatesSystem();
-//    final IFEDiscretisationModel1d2d discModel = (IFEDiscretisationModel1d2d)m_discModelWorkspace.getRootFeature();
-//    m_pointSnapper = new PointSnapper( discModel, mapPanel );
-//    m_networkBuilder = new LineGeometryBuilder( 0, coordinateSystem );
+    m_tinBuilder = new TriangulationBuilder( mapPanel );
+    final IKalypsoLayerModell mapModell = mapPanel.getMapModell();
+    final String coordinateSystem = mapModell.getCoordinatesSystem();
+    final IFEDiscretisationModel1d2d discModel = (IFEDiscretisationModel1d2d)m_discModelWorkspace.getRootFeature();
+    m_pointSnapper = new PointSnapper( discModel, mapPanel );
+    m_networkBuilder = new PolygonGeometryBuilder( 0, coordinateSystem );
     reinit();
   }
 
@@ -146,7 +158,9 @@ public class GenerateDikeOrDitchWidget extends AbstractWidget implements IWidget
     final String mode = "Graben oder Deich zeichnen";
     m_toolTipRenderer.setBackgroundColor( new Color( 1f, 1f, 0.6f, 0.70f ) );
     m_toolTipRenderer.setTooltip( mode );
-//    m_networkBuilder.reset();
+    m_networkBuilder.reset();
+    m_tinBuilder.setMinAngle( 22.5 );
+    m_tinBuilder.setNoSteinerOnBoundary( true );
     m_tinBuilder.reset();
     repaintMap();
   }
@@ -157,49 +171,26 @@ public class GenerateDikeOrDitchWidget extends AbstractWidget implements IWidget
 
     GM_Point currentOrSnappedPoint = null;
     final GM_Point currentPoint = MapUtilities.transform( mapPanel, point );
-//    if( m_network != null && !m_network.isEmpty() )
-//    {
-//      try
-//      {
-//        final Coordinate jtsCoord = JTSAdapter.export( currentPoint.getPosition() );
-//        final double buffer = MapUtilities.calculateWorldDistance( mapPanel, currentPoint, 10 );
-//        final com.vividsolutions.jts.geom.Point jtsPoint = JTSAdapter.jtsFactory.createPoint( jtsCoord );
-//        if( m_network.getEnvelope().contains( jtsPoint ) && m_network.distance( jtsPoint ) < buffer )
-//        {
-//          final LinearLocation projected = m_networkIndexedLine.project( jtsCoord );
-//          projected.snapToVertex( m_network, buffer );
-//          final Coordinate extractPoint = m_networkIndexedLine.extractPoint( projected );
-//          if( extractPoint.distance( jtsCoord ) < buffer )
-//            currentOrSnappedPoint = GeometryFactory.createGM_Point( JTSAdapter.wrap( extractPoint ), currentPoint.getCoordinateSystem() );
-//        }
-//      }
-//      catch( final Exception e )
-//      {
-//        final IStatus status = StatusUtilities.statusFromThrowable( e );
-//        KalypsoModel1D2DPlugin.getDefault().getLog().log( status );
-//        reinit();
-//      }
-//    }
-//
-//    if( m_pointSnapper != null )
-//    {
-//      final IFE1D2DNode snapNode = m_pointSnapper.moved( currentPoint );
-//      if( snapNode != null )
-//        currentOrSnappedPoint = snapNode.getPoint();
-//    }
-//
-//    mapPanel.setCursor( Cursor.getDefaultCursor() );
-//    if( currentOrSnappedPoint == null )
-//    {
-    m_currentPoint = point;
-    currentOrSnappedPoint = currentPoint;
-//    }
-//    else
-//    {
-//      m_currentPoint = MapUtilities.retransform( mapPanel, currentOrSnappedPoint );
-//      mapPanel.setCursor( Cursor.getPredefinedCursor( Cursor.CROSSHAIR_CURSOR ) );
-//    }
-//
+
+    if( m_pointSnapper != null )
+    {
+      final IFE1D2DNode snapNode = m_pointSnapper.moved( currentPoint );
+      if( snapNode != null )
+        currentOrSnappedPoint = snapNode.getPoint();
+    }
+
+    mapPanel.setCursor( Cursor.getDefaultCursor() );
+    if( currentOrSnappedPoint == null )
+    {
+      m_currentPoint = point;
+      currentOrSnappedPoint = currentPoint;
+    }
+    else
+    {
+      m_currentPoint = MapUtilities.retransform( mapPanel, currentOrSnappedPoint );
+      mapPanel.setCursor( Cursor.getPredefinedCursor( Cursor.CROSSHAIR_CURSOR ) );
+    }
+
     return currentOrSnappedPoint;
   }
 
@@ -218,69 +209,53 @@ public class GenerateDikeOrDitchWidget extends AbstractWidget implements IWidget
   @Override
   public void mousePressed( final MouseEvent event )
   {
-//    if( event.getButton() != MouseEvent.BUTTON1 || event.getClickCount() > 1 )
-//      return;
-//
-//    try
-//    {
-//      final GM_Point snapPoint = snapTo( event.getPoint() );
-//      m_networkBuilder.addPoint( snapPoint );
-//    }
-//    catch( final Exception e )
-//    {
-//      final IStatus status = StatusUtilities.statusFromThrowable( e );
-//      KalypsoModel1D2DPlugin.getDefault().getLog().log( status );
-//      reinit();
-//    }
+    if( event.getButton() != MouseEvent.BUTTON1 || event.getClickCount() > 1 )
+      return;
+
+    try
+    {
+      final GM_Point snapPoint = snapTo( event.getPoint() );
+      m_networkBuilder.addPoint( snapPoint );
+    }
+    catch( final Exception e )
+    {
+      final IStatus status = StatusUtilities.statusFromThrowable( e );
+      KalypsoModel1D2DPlugin.getDefault().getLog().log( status );
+      reinit();
+    }
   }
 
   @Override
   public void mouseClicked( final MouseEvent event )
   {
-//    if( event.getButton() != MouseEvent.BUTTON1 || event.getClickCount() < 2 )
-//      return;
-//
-//    try
-//    {
-//      final GM_Curve curve = (GM_Curve)m_networkBuilder.finish();
-//      if( curve == null )
-//        return;
-//    }
-//    catch( final Exception e )
-//    {
-//      final IStatus status = StatusUtilities.statusFromThrowable( e );
-//      KalypsoModel1D2DPlugin.getDefault().getLog().log( status );
-//      reinit();
-//    }
-//    finally
-//    {
-//      m_networkBuilder.reset();
-//      repaintMap();
-//    }
-  }
+    if( event.getButton() != MouseEvent.BUTTON1 || event.getClickCount() < 2 )
+      return;
 
-//  private void addCurveToNetwork( final GM_Curve curve ) throws GM_Exception
-//  {
-//    // merge lines
-//    final LineMerger merger = new LineMerger();
-//    merger.add( m_network );
-//    merger.add( JTSAdapter.export( curve ) );
-//    final Collection<LineString> mergedLineStrings = merger.getMergedLineStrings();
-//    m_network = JTSAdapter.jtsFactory.buildGeometry( mergedLineStrings );
-//    m_networkIndexedLine = new LocationIndexedLine( m_network );
-//  }
+    try
+    {
+      final GM_Polygon boundary = (GM_Polygon)m_networkBuilder.finish();
+      if( boundary == null )
+        return;
+      m_tinBuilder.setBoundary( boundary, false );
+    }
+    catch( final Exception e )
+    {
+      final IStatus status = StatusUtilities.statusFromThrowable( e );
+      KalypsoModel1D2DPlugin.getDefault().getLog().log( status );
+      reinit();
+    }
+    finally
+    {
+      m_networkBuilder.reset();
+      repaintMap();
+    }
+  }
 
   @Override
   public void keyPressed( final KeyEvent e )
   {
     if( e.getKeyCode() == KeyEvent.VK_ESCAPE )
       reinit();
-    else if( e.getKeyCode() == KeyEvent.VK_ENTER )
-    {
-      // create mesh
-      m_tinBuilder.reset();
-      m_strategy.createMesh( m_tinBuilder );
-    }
     else if( e.getKeyCode() == KeyEvent.VK_E )
     {
       try
@@ -301,24 +276,29 @@ public class GenerateDikeOrDitchWidget extends AbstractWidget implements IWidget
         reinit();
       }
     }
-//    else if( e.getKeyCode() == KeyEvent.VK_BACK_SPACE )
-//      m_networkBuilder.removeLastPoint();
+    else if( e.getKeyCode() == KeyEvent.VK_BACK_SPACE )
+      m_networkBuilder.removeLastPoint();
     repaintMap();
+  }
+
+  @Override
+  public void paint( final Graphics g, final GeoTransform projection, final IProgressMonitor monitor )
+  {
+    if( m_tinBuilder != null )
+      m_tinBuilder.paint( g, projection, monitor );
   }
 
   @Override
   public void paint( final Graphics g )
   {
     final IMapPanel mapPanel = getMapPanel();
+
+    if( m_pointSnapper != null )
+      m_pointSnapper.paint( g );
+
     final GeoTransform projection = mapPanel.getProjection();
-
-    m_tinBuilder.paint( g, projection );
-
-//    if( m_pointSnapper != null )
-//      m_pointSnapper.paint( g );
-
-//    if( m_networkBuilder != null )
-//      m_networkBuilder.paint( g, projection, m_currentPoint );
+    if( m_networkBuilder != null )
+      m_networkBuilder.paint( g, projection, m_currentPoint );
 
     if( m_currentPoint != null )
     {
@@ -341,14 +321,13 @@ public class GenerateDikeOrDitchWidget extends AbstractWidget implements IWidget
   @Override
   public void disposeControl( )
   {
-    // TODO Auto-generated method stub
-
+    // make sure that the tin is disposed
+    m_tinBuilder.reset();
   }
 
   @Override
   public String getPartName( )
   {
-    // TODO Auto-generated method stub
-    return null;
+    return getName();
   }
 }
