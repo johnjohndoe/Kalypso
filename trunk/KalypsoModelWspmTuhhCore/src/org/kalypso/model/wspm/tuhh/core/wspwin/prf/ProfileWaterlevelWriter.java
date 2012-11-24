4f7b71027ec2c29b1e727a8f39e57cd11b3cfe68
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-11-24 11:20:34 +0000

    PrfWriter now writes new 2d-waterlevels objects as prf waterlevels, to be openede with plotter.exe. Dropped support for old 2d-waterlevel component.

--- trunk/KalypsoModelWspmTuhhCore/src/org/kalypso/model/wspm/tuhh/core/wspwin/prf/ProfileWaterlevelWriter.java
+++ trunk/KalypsoModelWspmTuhhCore/src/org/kalypso/model/wspm/tuhh/core/wspwin/prf/ProfileWaterlevelWriter.java
@@ -43,22 +43,20 @@ package org.kalypso.model.wspm.tuhh.core.wspwin.prf;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import org.kalypso.commons.math.LinearEquation;
-import org.kalypso.commons.math.LinearEquation.SameXValuesException;
-import org.kalypso.model.wspm.core.IWspmConstants;
-import org.kalypso.model.wspm.core.IWspmPhenomenonConstants;
+import org.apache.commons.lang3.StringUtils;
+import org.kalypso.commons.java.lang.Doubles;
 import org.kalypso.model.wspm.core.profil.IProfile;
+import org.kalypso.model.wspm.core.profil.IProfileObject;
+import org.kalypso.model.wspm.core.profil.IProfileObjectRecord;
+import org.kalypso.model.wspm.tuhh.core.IWspmTuhhConstants;
 import org.kalypso.model.wspm.tuhh.core.i18n.Messages;
-import org.kalypso.observation.phenomenon.IPhenomenon;
-import org.kalypso.observation.result.IComponent;
-import org.kalypso.observation.result.IRecord;
 import org.kalypso.wspwin.core.prf.datablock.IDataBlock;
 import org.kalypso.wspwin.core.prf.datablock.IDataBlockNames;
 
 /**
  * Special case for profiles containing (2d-)waterlevels: converts a waterlevel component into a plotter readable
  * datablock.
- *
+ * 
  * @author Gernot Belger
  */
 public class ProfileWaterlevelWriter
@@ -72,51 +70,48 @@ public class ProfileWaterlevelWriter
 
   public IDataBlock[] createDataBlocks( )
   {
-    final IComponent[] pointProperties = m_profile.getPointProperties();
-
     final Collection<IDataBlock> dbs = new ArrayList<>();
 
-    for( final IComponent property : pointProperties )
+    final IProfileObject[] profileObjects = m_profile.getProfileObjects();
+    for( final IProfileObject profileObject : profileObjects )
     {
-      final IDataBlock db = createDataBlock( property );
+      final IDataBlock db = createDataBlock( profileObject );
+
       if( db != null )
-      {
         dbs.add( db );
-      }
     }
 
     return dbs.toArray( new IDataBlock[dbs.size()] );
   }
 
-  private IDataBlock createDataBlock( final IComponent property )
+  private IDataBlock createDataBlock( final IProfileObject object )
   {
-    final IPhenomenon phenomenon = property.getPhenomenon();
-    final String phenomenonId = phenomenon.getID();
-    if( !IWspmPhenomenonConstants.PHENOMENON_WATERLEVEL_2D.equals( phenomenonId ) )
+    final String type = object.getType();
+    if( !IWspmTuhhConstants.OBJECT_TYPE_WATERLEVEL_SEGMENT.equals( type ) )
       return null;
 
-    final String componentName = property.getName();
+    final String eventName = object.getValue( IWspmTuhhConstants.PROFIL_PROPERTY_EVENT_NAME, null );
+    // final String eventType = object.getValue( IWspmTuhhConstants.PROFIL_PROPERTY_EVENT_TYPE, null );
+    if( StringUtils.isEmpty( eventName ) )
+      return null;
+
     final String strandName = Messages.getString( "ProfileWaterlevelWriter_0" ); //$NON-NLS-1$
-    final String secondLine = String.format( "%100s%s@%s", " ", componentName, strandName ); //$NON-NLS-1$ //$NON-NLS-2$
+    final String secondLine = String.format( "%100s%s@%s", " ", eventName, strandName ); //$NON-NLS-1$ //$NON-NLS-2$
 
     final CoordDataBlockCreator creator = new CoordDataBlockCreator( IDataBlockNames.WSP_HOEHE, secondLine );
 
-    return writeCoords( m_profile, property, creator );
+    return writeCoords( object, creator );
   }
 
-  private IDataBlock writeCoords( final IProfile profil, final IComponent waterlevelProperty, final CoordDataBlockCreator creator )
+  private IDataBlock writeCoords( final IProfileObject object, final CoordDataBlockCreator creator )
   {
-    final IRecord[] points = profil.getPoints();
-    if( points.length < 2 )
+    final IProfileObjectRecord[] records = object.getRecords().getAll();
+    if( records.length < 2 )
       return null;
-
-    final int iBreite = profil.indexOfProperty( IWspmConstants.POINT_PROPERTY_BREITE );
-    final int iHoehe = profil.indexOfProperty( IWspmConstants.POINT_PROPERTY_HOEHE );
-    final int iWaterlevel = profil.indexOfProperty( waterlevelProperty );
 
     try
     {
-      writeWaterlevel( creator, points, iBreite, iHoehe, iWaterlevel );
+      writeWaterlevel( creator, records );
       return creator.createDataBlock();
     }
     catch( final Exception e )
@@ -126,210 +121,221 @@ public class ProfileWaterlevelWriter
     }
   }
 
-  private void writeWaterlevel( final CoordDataBlockCreator creator, final IRecord[] points, final int iBreite, final int iHoehe, final int iWaterlevel )
+  private void writeWaterlevel( final CoordDataBlockCreator creator, final IProfileObjectRecord[] records )
   {
-    final Double[] extrapolatedWaterlevel = extrapolateWaterlevel( points, iWaterlevel, iBreite, iHoehe );
-    for( int i = 1; i < points.length; i++ )
+    // final Double[] extrapolatedWaterlevel = extrapolateWaterlevel( points, iWaterlevel, iBreite, iHoehe );
+
+    /* write 2d waterlevel as pairs of points, so plotter will understand it */
+    for( int i = 1; i < records.length; i++ )
     {
-      final IRecord pointFirst = points[i - 1];
-      final IRecord pointSecond = points[i];
+      final IProfileObjectRecord pointFirst = records[i - 1];
+      final IProfileObjectRecord pointSecond = records[i];
 
-      try
+//      try
+//      {
+      final Double breiteFirst = pointFirst.getBreite();
+      final Double breiteSecond = pointSecond.getBreite();
+
+      // final Object hoeheFirst = pointFirst.getValue( iHoehe );
+      // final Object hoeheSecond = pointSecond.getValue( iHoehe );
+
+      // final Double waterlevelFirst = extrapolatedWaterlevel[i - 1];
+      // final Double waterlevelSecond = extrapolatedWaterlevel[i];
+
+      final Double waterlevelFirst = pointFirst.getHoehe();
+      final Double waterlevelSecond = pointSecond.getHoehe();
+
+      if( !Doubles.isNullOrInfinite( breiteFirst, breiteSecond, waterlevelFirst, waterlevelSecond ) )
       {
-        final Object breiteFirst = pointFirst.getValue( iBreite );
-        final Object breiteSecond = pointSecond.getValue( iBreite );
+        // writeWaterlevelSegment( creator, breiteFirst, breiteSecond, hoeheFirst, hoeheSecond, waterlevelFirst, waterlevelSecond );
 
-        final Object hoeheFirst = pointFirst.getValue( iHoehe );
-        final Object hoeheSecond = pointSecond.getValue( iHoehe );
-
-        final Double waterlevelFirst = extrapolatedWaterlevel[i - 1];
-        final Double waterlevelSecond = extrapolatedWaterlevel[i];
-
-        writeWaterlevelSegment( creator, breiteFirst, breiteSecond, hoeheFirst, hoeheSecond, waterlevelFirst, waterlevelSecond );
+        creator.add( breiteFirst, waterlevelFirst );
+        creator.add( breiteSecond, waterlevelSecond );
       }
-      catch( final SameXValuesException e )
-      {
-        e.printStackTrace();
-      }
+//      }
+//      catch( final SameXValuesException e )
+//      {
+//        e.printStackTrace();
+//      }
     }
   }
 
-  private Double[] extrapolateWaterlevel( final IRecord[] points, final int iWaterlevel, final int iBreite, final int iHoehe )
-  {
-    final Double[] result = new Double[points.length];
+//  private Double[] extrapolateWaterlevel( final IRecord[] points, final int iWaterlevel, final int iBreite, final int iHoehe )
+//  {
+//    final Double[] result = new Double[points.length];
+//
+//    for( int i = 0; i < points.length; i++ )
+//    {
+//      final Double waterlevel = getExtrapolatedWaterlevel( points, i, iWaterlevel, iBreite, iHoehe );
+//      result[i] = waterlevel;
+//    }
+//
+//    return result;
+//  }
 
-    for( int i = 0; i < points.length; i++ )
-    {
-      final Double waterlevel = getExtrapolatedWaterlevel( points, i, iWaterlevel, iBreite, iHoehe );
-      result[i] = waterlevel;
-    }
+//  private Double getExtrapolatedWaterlevel( final IRecord[] points, final int i, final int iWaterlevel, final int iBreite, final int iHoehe )
+//  {
+//    final IRecord point = points[i];
+//
+//    final Object value = point.getValue( iWaterlevel );
+//
+//    if( value instanceof Number )
+//      return ((Number)value).doubleValue();
+//
+//    final IRecord prevWaterlevel = findPrevWaterlevel( points, i, iWaterlevel, iHoehe );
+//    final IRecord nextWaterlevel = findNextWaterlevel( points, i, iWaterlevel, iHoehe );
+//
+//    if( nextWaterlevel != null && prevWaterlevel != null )
+//    {
+//      final double prevW = ((Number)prevWaterlevel.getValue( iWaterlevel )).doubleValue();
+//      final double nextW = ((Number)nextWaterlevel.getValue( iWaterlevel )).doubleValue();
+//
+//      final Object breite = point.getValue( iBreite );
+//      final Object prevBreite = prevWaterlevel.getValue( iBreite );
+//      final Object nextBreite = nextWaterlevel.getValue( iBreite );
+//
+//      if( breite instanceof Number && prevBreite instanceof Number && nextBreite instanceof Number )
+//      {
+//        final double prevB = ((Number)prevBreite).doubleValue();
+//        final double nextB = ((Number)nextBreite).doubleValue();
+//
+//        try
+//        {
+//          final LinearEquation linearEquation = new LinearEquation( prevB, prevW, nextB, nextW );
+//          return linearEquation.computeY( ((Number)breite).doubleValue() );
+//        }
+//        catch( final SameXValuesException e )
+//        {
+//          e.printStackTrace();
+//        }
+//      }
+//    }
+//
+//    if( nextWaterlevel != null )
+//      return ((Number)nextWaterlevel.getValue( iWaterlevel )).doubleValue();
+//
+//    if( prevWaterlevel != null )
+//      return ((Number)prevWaterlevel.getValue( iWaterlevel )).doubleValue();
+//
+//    return null;
+//  }
 
-    return result;
-  }
+//  private IRecord findPrevWaterlevel( final IRecord[] points, final int i, final int iWaterlevel, final int iHoehe )
+//  {
+//    double maxHeight = -Double.MAX_VALUE;
+//
+//    for( int j = i - 1; j >= 0; j-- )
+//    {
+//      final IRecord point = points[j];
+//
+//      final Object valueWaterlevel = point.getValue( iWaterlevel );
+//      final Object valueHoehe = point.getValue( iHoehe );
+//
+//      if( valueHoehe instanceof Number )
+//      {
+//        final double h = ((Number)valueHoehe).doubleValue();
+//        maxHeight = Math.max( maxHeight, h );
+//      }
+//
+//      if( valueWaterlevel instanceof Number )
+//      {
+//        final double w = ((Number)valueWaterlevel).doubleValue();
+//        if( w > maxHeight )
+//          return point;
+//
+//        /* Stop on first encountered waterlevel */
+//        return null;
+//      }
+//    }
+//
+//    return null;
+//  }
+//
+//  private IRecord findNextWaterlevel( final IRecord[] points, final int i, final int iWaterlevel, final int iHoehe )
+//  {
+//    double maxHeight = -Double.MAX_VALUE;
+//
+//    for( int j = i + 1; j < points.length; j++ )
+//    {
+//      final IRecord point = points[j];
+//
+//      final Object valueWaterlevel = point.getValue( iWaterlevel );
+//      final Object valueHoehe = point.getValue( iHoehe );
+//
+//      if( valueHoehe instanceof Number )
+//      {
+//        final double h = ((Number)valueHoehe).doubleValue();
+//        maxHeight = Math.max( maxHeight, h );
+//      }
+//
+//      if( valueWaterlevel instanceof Number )
+//      {
+//        final double w = ((Number)valueWaterlevel).doubleValue();
+//        if( w > maxHeight )
+//          return point;
+//
+//        /* Stop on first encountered waterlevel */
+//        return null;
+//      }
+//    }
+//
+//    return null;
+//  }
 
-  private Double getExtrapolatedWaterlevel( final IRecord[] points, final int i, final int iWaterlevel, final int iBreite, final int iHoehe )
-  {
-    final IRecord point = points[i];
+//  private void writeWaterlevelSegment( final CoordDataBlockCreator creator, final Object breiteFirst, final Object breiteSecond, final Object hoeheFirst, final Object hoeheSecond, final Double waterlevelFirst, final Double waterlevelSecond ) throws SameXValuesException
+//  {
+//    if( breiteFirst instanceof Number && breiteSecond instanceof Number && hoeheFirst instanceof Number && hoeheSecond instanceof Number )
+//    {
+//      final double b1 = ((Number)breiteFirst).doubleValue();
+//      final double b2 = ((Number)breiteSecond).doubleValue();
+//      final double h1 = ((Number)hoeheFirst).doubleValue();
+//      final double h2 = ((Number)hoeheSecond).doubleValue();
+//
+//      if( waterlevelFirst != null && waterlevelSecond != null )
+//      {
+//        final double w1 = ((Number)waterlevelFirst).doubleValue();
+//        final double w2 = ((Number)waterlevelSecond).doubleValue();
+//        addWaterlevelSegment( creator, b1, w1, b2, w2, h1, h2 );
+//      }
+//    }
+//  }
 
-    final Object value = point.getValue( iWaterlevel );
-
-    if( value instanceof Number )
-      return ((Number) value).doubleValue();
-
-    final IRecord prevWaterlevel = findPrevWaterlevel( points, i, iWaterlevel, iHoehe );
-    final IRecord nextWaterlevel = findNextWaterlevel( points, i, iWaterlevel, iHoehe );
-
-    if( nextWaterlevel != null && prevWaterlevel != null )
-    {
-      final double prevW = ((Number) prevWaterlevel.getValue( iWaterlevel )).doubleValue();
-      final double nextW = ((Number) nextWaterlevel.getValue( iWaterlevel )).doubleValue();
-
-      final Object breite = point.getValue( iBreite );
-      final Object prevBreite = prevWaterlevel.getValue( iBreite );
-      final Object nextBreite = nextWaterlevel.getValue( iBreite );
-
-      if( breite instanceof Number && prevBreite instanceof Number && nextBreite instanceof Number )
-      {
-        final double prevB = ((Number) prevBreite).doubleValue();
-        final double nextB = ((Number) nextBreite).doubleValue();
-
-        try
-        {
-          final LinearEquation linearEquation = new LinearEquation( prevB, prevW, nextB, nextW );
-          return linearEquation.computeY( ((Number) breite).doubleValue() );
-        }
-        catch( final SameXValuesException e )
-        {
-          e.printStackTrace();
-        }
-      }
-    }
-
-    if( nextWaterlevel != null )
-      return ((Number) nextWaterlevel.getValue( iWaterlevel )).doubleValue();
-
-    if( prevWaterlevel != null )
-      return ((Number) prevWaterlevel.getValue( iWaterlevel )).doubleValue();
-
-    return null;
-  }
-
-  private IRecord findPrevWaterlevel( final IRecord[] points, final int i, final int iWaterlevel, final int iHoehe )
-  {
-    double maxHeight = -Double.MAX_VALUE;
-
-    for( int j = i - 1; j >= 0; j-- )
-    {
-      final IRecord point = points[j];
-
-      final Object valueWaterlevel = point.getValue( iWaterlevel );
-      final Object valueHoehe = point.getValue( iHoehe );
-
-      if( valueHoehe instanceof Number )
-      {
-        final double h = ((Number) valueHoehe).doubleValue();
-        maxHeight = Math.max( maxHeight, h );
-      }
-
-      if( valueWaterlevel instanceof Number )
-      {
-        final double w = ((Number) valueWaterlevel).doubleValue();
-        if( w > maxHeight )
-          return point;
-
-        /* Stop on first encountered waterlevel */
-        return null;
-      }
-    }
-
-    return null;
-  }
-
-  private IRecord findNextWaterlevel( final IRecord[] points, final int i, final int iWaterlevel, final int iHoehe )
-  {
-    double maxHeight = -Double.MAX_VALUE;
-
-    for( int j = i + 1; j < points.length; j++ )
-    {
-      final IRecord point = points[j];
-
-      final Object valueWaterlevel = point.getValue( iWaterlevel );
-      final Object valueHoehe = point.getValue( iHoehe );
-
-      if( valueHoehe instanceof Number )
-      {
-        final double h = ((Number) valueHoehe).doubleValue();
-        maxHeight = Math.max( maxHeight, h );
-      }
-
-      if( valueWaterlevel instanceof Number )
-      {
-        final double w = ((Number) valueWaterlevel).doubleValue();
-        if( w > maxHeight )
-          return point;
-
-        /* Stop on first encountered waterlevel */
-        return null;
-      }
-    }
-
-    return null;
-  }
-
-  private void writeWaterlevelSegment( final CoordDataBlockCreator creator, final Object breiteFirst, final Object breiteSecond, final Object hoeheFirst, final Object hoeheSecond, final Double waterlevelFirst, final Double waterlevelSecond ) throws SameXValuesException
-  {
-    if( breiteFirst instanceof Number && breiteSecond instanceof Number && hoeheFirst instanceof Number && hoeheSecond instanceof Number )
-    {
-      final double b1 = ((Number) breiteFirst).doubleValue();
-      final double b2 = ((Number) breiteSecond).doubleValue();
-      final double h1 = ((Number) hoeheFirst).doubleValue();
-      final double h2 = ((Number) hoeheSecond).doubleValue();
-
-      if( waterlevelFirst != null && waterlevelSecond != null )
-      {
-        final double w1 = ((Number) waterlevelFirst).doubleValue();
-        final double w2 = ((Number) waterlevelSecond).doubleValue();
-        addWaterlevelSegment( creator, b1, w1, b2, w2, h1, h2 );
-      }
-    }
-  }
-
-  // Plotter needs always pairs of two values as a waterlevel segment
-  private void addWaterlevelSegment( final CoordDataBlockCreator creator, final double b1, final double w1, final double b2, final double w2, final double h1, final double h2 ) throws SameXValuesException
-  {
-    if( w1 < h1 && w2 > h2 )
-    {
-      /* Find b, where w == h and use it as left hand side */
-      final LinearEquation le0 = new LinearEquation( b1, w1 - h1, b2, w2 - h2 );
-      final LinearEquation leW = new LinearEquation( b1, w1, b2, w2 );
-
-      final double b0 = le0.computeX( 0.0 );
-      final double w0 = leW.computeY( b0 );
-
-      creator.add( b0, w0 );
-      creator.add( b2, w2 );
-    }
-    else if( w1 > h1 && w2 < h2 )
-    {
-      /* Find b, where w == h and use it as right hand side */
-      final LinearEquation le0 = new LinearEquation( b1, w1 - h1, b2, w2 - h2 );
-      final LinearEquation leW = new LinearEquation( b1, w1, b2, w2 );
-
-      final double b0 = le0.computeX( 0.0 );
-      final double w0 = leW.computeY( b0 );
-
-      creator.add( b1, w1 );
-      creator.add( b0, w0 );
-    }
-    else if( w1 < h1 && w2 < h2 )
-    {
-      /* Waterlevel completely under soil: ignore */
-    }
-    else
-    {
-      /* Waterlevel completely above soil: just add */
-      creator.add( b1, w1 );
-      creator.add( b2, w2 );
-    }
-  }
+//  // Plotter needs always pairs of two values as a waterlevel segment
+//  private void addWaterlevelSegment( final CoordDataBlockCreator creator, final double b1, final double w1, final double b2, final double w2, final double h1, final double h2 ) throws SameXValuesException
+//  {
+//    if( w1 < h1 && w2 > h2 )
+//    {
+//      /* Find b, where w == h and use it as left hand side */
+//      final LinearEquation le0 = new LinearEquation( b1, w1 - h1, b2, w2 - h2 );
+//      final LinearEquation leW = new LinearEquation( b1, w1, b2, w2 );
+//
+//      final double b0 = le0.computeX( 0.0 );
+//      final double w0 = leW.computeY( b0 );
+//
+//      creator.add( b0, w0 );
+//      creator.add( b2, w2 );
+//    }
+//    else if( w1 > h1 && w2 < h2 )
+//    {
+//      /* Find b, where w == h and use it as right hand side */
+//      final LinearEquation le0 = new LinearEquation( b1, w1 - h1, b2, w2 - h2 );
+//      final LinearEquation leW = new LinearEquation( b1, w1, b2, w2 );
+//
+//      final double b0 = le0.computeX( 0.0 );
+//      final double w0 = leW.computeY( b0 );
+//
+//      creator.add( b1, w1 );
+//      creator.add( b0, w0 );
+//    }
+//    else if( w1 < h1 && w2 < h2 )
+//    {
+//      /* Waterlevel completely under soil: ignore */
+//    }
+//    else
+//    {
+//      /* Waterlevel completely above soil: just add */
+//      creator.add( b1, w1 );
+//      creator.add( b2, w2 );
+//    }
+//  }
 }
