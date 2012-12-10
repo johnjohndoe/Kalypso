90dabfa0a738e9598422ba6f2456401c741c70e9
holger.albert <holger.albert@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2012-12-10 16:33:29 +0000

    Initiated branch for Sachsen after installation

---
+++ KalypsoManualGrafik/IKalypsoHelpContextIds.java
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+
+
+				
+				
+/**
+ * GENERATED FILE, DO NOT MODIFY.
+ * @see KalypsoManual common-eclipse.xsl and KalypsoBuild/publishing/docbook-xsl-1.69.1/eclipse/eclipse-ex.xsl
+ *
+ */				
+public interface IKalypsoHelpContextIds
+{
+  public static final String PREFIX = "org.kalypso.manual.";
+					
+				
+			
+  public static final String Grafik_Programm =PREFIX + "Grafik_Programm";
+		
+			
+  public static final String grafik_einfuehrung =PREFIX + "grafik_einfuehrung";
+		
+			
+  public static final String grafik_menu_datei =PREFIX + "grafik_menu_datei";
+		
+			
+  public static final String grafik_menu_bearbeiten =PREFIX + "grafik_menu_bearbeiten";
+		
+			
+  public static final String menu_bearbeiten_korrekturmodus =PREFIX + "menu_bearbeiten_korrekturmodus";
+		
+			
+  public static final String grafik_menu_anzeige =PREFIX + "grafik_menu_anzeige";
+		
+			
+  public static final String grafik_menu_aktuelle-kurven =PREFIX + "grafik_menu_aktuelle-kurven";
+		
+			
+  public static final String grafik_menu_auswertung =PREFIX + "grafik_menu_auswertung";
+		
+			
+  public static final String grafik_menu_extras =PREFIX + "grafik_menu_extras";
+		
+			
+  public static final String grafik_messwertausfall =PREFIX + "grafik_messwertausfall";
+		
+			
+  public static final String grafik_abflussbeiwert =PREFIX + "grafik_abflussbeiwert";
+		
+			
+  public static final String grafik_vorlagenliste =PREFIX + "grafik_vorlagenliste";
+		
+}
+				
+			
