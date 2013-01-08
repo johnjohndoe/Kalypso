3352693599b1159f545c27e19de8669b8b666fff
holger.albert <holger.albert@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2013-01-08 16:51:46 +0000

    Started coded tripple import

---
+++ KalypsoModelWspmTuhhCore/src/org/kalypso/model/wspm/tuhh/core/ctripple/CodedTripple.java
@@ -0,0 +1,29 @@
+/** This file is part of Kalypso
+ *
+ *  Copyright (c) 2012 by
+ *
+ *  Björnsen Beratende Ingenieure GmbH, Koblenz, Germany (Bjoernsen Consulting Engineers), http://www.bjoernsen.de
+ *  Technische Universität Hamburg-Harburg, Institut für Wasserbau, Hamburg, Germany
+ *  (Technical University Hamburg-Harburg, Institute of River and Coastal Engineering), http://www.tu-harburg.de/wb/
+ *
+ *  Kalypso is free software: you can redistribute it and/or modify it under the terms  
+ *  of the GNU Lesser General Public License (LGPL) as published by the Free Software 
+ *  Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  Kalypso is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with Kalypso.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.kalypso.model.wspm.tuhh.core.ctripple;
+
+/**
+ * @author Holger Albert
+ */
+public class CodedTripple
+{
+  public CodedTripple( )
+  {
+  }
+}
