29fc957844d0117f80c52ead2bad61c477511076
gernot.belger <gernot.belger@b2c9d618-050e-4775-8f90-c816fdcbfbcd>
2013-01-10 10:12:39 +0000

    Fixed: storage overflow result timeseries was not read back to kalypso after simulation.

--- KalypsoModelNA/src/org/kalypso/model/hydrology/timeseries/TSResultDescriptor.java
+++ KalypsoModelNA/src/org/kalypso/model/hydrology/timeseries/TSResultDescriptor.java
@@ -53,25 +53,25 @@ import org.kalypso.ogc.sensor.metadata.ITimeseriesConstants;
  */
 public enum TSResultDescriptor
 {
-  qgs(Node.FEATURE_NODE, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Gesamtabfluss", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" )), //$NON-NLS-1$ //$NON-NLS-2$
+  qgs( Node.FEATURE_NODE, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Gesamtabfluss", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" ) ), //$NON-NLS-1$ //$NON-NLS-2$
 
-  pre(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RAINFALL, 1.0d, "Niederschlag", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.6" )), //$NON-NLS-1$ //$NON-NLS-2$
-  tmp(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_TEMPERATURE, 1.0d, "Temperatur", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.5" )), //$NON-NLS-1$ //$NON-NLS-2$
-  qif(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Interflow", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" )), //$NON-NLS-1$ //$NON-NLS-2$
-  qgw(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Grundwasserabfluss", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" )), //$NON-NLS-1$ //$NON-NLS-2$
-  qgg(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Gesamtabfluss", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" )), //$NON-NLS-1$ //$NON-NLS-2$
-  gws(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_WATERLEVEL, 100.0d, "Grundwasserstand", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.10" )), //$NON-NLS-1$ //$NON-NLS-2$
-  qbs(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Basisabfluss", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" )), //$NON-NLS-1$ //$NON-NLS-2$
-  qna(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Oberflaechenabfluss(natuerlich)", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" )), //$NON-NLS-1$ //$NON-NLS-2$
-  qvs(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Oberflaechenabfluss(versiegelt)", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" )), //$NON-NLS-1$ //$NON-NLS-2$
-  sch(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_WATERLEVEL, 0.1d, "Schneehoehe", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.7" )), //$NON-NLS-1$ //$NON-NLS-2$
+  pre( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RAINFALL, 1.0d, "Niederschlag", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.6" ) ), //$NON-NLS-1$ //$NON-NLS-2$
+  tmp( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_TEMPERATURE, 1.0d, "Temperatur", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.5" ) ), //$NON-NLS-1$ //$NON-NLS-2$
+  qif( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Interflow", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" ) ), //$NON-NLS-1$ //$NON-NLS-2$
+  qgw( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Grundwasserabfluss", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" ) ), //$NON-NLS-1$ //$NON-NLS-2$
+  qgg( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Gesamtabfluss", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" ) ), //$NON-NLS-1$ //$NON-NLS-2$
+  gws( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_WATERLEVEL, 100.0d, "Grundwasserstand", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.10" ) ), //$NON-NLS-1$ //$NON-NLS-2$
+  qbs( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Basisabfluss", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" ) ), //$NON-NLS-1$ //$NON-NLS-2$
+  qna( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Oberflaechenabfluss(natuerlich)", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" ) ), //$NON-NLS-1$ //$NON-NLS-2$
+  qvs( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Oberflaechenabfluss(versiegelt)", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" ) ), //$NON-NLS-1$ //$NON-NLS-2$
+  sch( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_WATERLEVEL, 0.1d, "Schneehoehe", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.7" ) ), //$NON-NLS-1$ //$NON-NLS-2$
 
   // REMARK: beide nicht in Oberfläche, lt. Niloufar fraglich ob Ausgabe sinnvoll
-  qt1(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Kluftgrundw1abfluss", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" )), //$NON-NLS-1$ //$NON-NLS-2$
-  qtg(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "KluftGWAbfluss", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" )), //$NON-NLS-1$ //$NON-NLS-2$
+  qt1( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Kluftgrundw1abfluss", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" ) ), //$NON-NLS-1$ //$NON-NLS-2$
+  qtg( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "KluftGWAbfluss", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.11" ) ), //$NON-NLS-1$ //$NON-NLS-2$
 
   // TODO: evapotranspiration != evaporation -> use other datatype
-  vet(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_EVAPORATION, 0.1d, "Evapotranspiration", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.12" )), //$NON-NLS-1$ //$NON-NLS-2$
+  vet( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_EVAPORATION, 0.1d, "Evapotranspiration", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.12" ) ), //$NON-NLS-1$ //$NON-NLS-2$
 
   // TODO: nicht in Oberfläche: Pasche wollte es nicht?!
   // bof(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_SOIL_MOISTURE, 0.1d, "Bodenfeuchte" , Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.8" ) ), //$NON-NLS-1$ //$NON-NLS-2$
@@ -81,17 +81,17 @@ public enum TSResultDescriptor
 
   // hyd(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_XXX, 0.1d, "Hydrotope" , Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.16" ) ), //$NON-NLS-1$ //$NON-NLS-2$
 
-  spv(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_EVAPORATION_WATER_BASED, 0.1d, "Talsperrenverdunstung", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.21" )), //$NON-NLS-1$ //$NON-NLS-2$
+  spv( Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_EVAPORATION_WATER_BASED, 0.1d, "Talsperrenverdunstung", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.21" ) ), //$NON-NLS-1$ //$NON-NLS-2$
   // REMARK: werden schon rausgeschrieben, macht aber lt. Niloufar keinen Sinn
   // spn(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_XXX, 0.1d, "Niederschlag" , Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.22" ) ), //$NON-NLS-1$ //$NON-NLS-2$
   // spb(Catchment.FEATURE_CATCHMENT, ITimeseriesConstants.TYPE_XXX, 0.1d, "Zehrung" , Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.24" ) ), //$NON-NLS-1$ //$NON-NLS-2$
 
   // Speicher
   // REMARK: nicht in Oberfläche, wird lt. Niloufar nicht gebraucht
-  sph(StorageChannel.FEATURE_STORAGE_CHANNEL, ITimeseriesConstants.TYPE_NORMNULL, 1.0d, "Wasserspiegelhoehe", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.20" )), //$NON-NLS-1$ //$NON-NLS-2$
+  sph( StorageChannel.FEATURE_STORAGE_CHANNEL, ITimeseriesConstants.TYPE_NORMNULL, 1.0d, "Wasserspiegelhoehe", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.20" ) ), //$NON-NLS-1$ //$NON-NLS-2$
 
-  sup(StorageChannel.FEATURE_STORAGE_CHANNEL, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Speicherueberlauf", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.25" )), //$NON-NLS-1$ //$NON-NLS-2$
-  spi(StorageChannel.FEATURE_STORAGE_CHANNEL, ITimeseriesConstants.TYPE_VOLUME, 1000000.0d, "Fuellvolumen", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.19" )); //$NON-NLS-1$ //$NON-NLS-2$
+  sub( StorageChannel.FEATURE_STORAGE_CHANNEL, ITimeseriesConstants.TYPE_RUNOFF, 1.0d, "Speicherueberlauf", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.25" ) ), //$NON-NLS-1$ //$NON-NLS-2$
+  spi( StorageChannel.FEATURE_STORAGE_CHANNEL, ITimeseriesConstants.TYPE_VOLUME, 1000000.0d, "Fuellvolumen", Messages.getString( "org.kalypso.convert.namodel.NaModelInnerCalcJob.19" ) ); //$NON-NLS-1$ //$NON-NLS-2$
 
   /*
    * In altem switch über Ausgabedateinamen, sind aber vermutlich keine Zeitreihen: // n Abflussbilanz .bil case "bil":
